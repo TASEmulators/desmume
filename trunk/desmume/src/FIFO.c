585e02a79f7331b8ea2055a68ddf9c51bf8c715c
yabause <yabause@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2006-05-14 12:08:05 +0000

    FIFO is now in C.

---
+++ trunk/desmume/src/FIFO.c
@@ -0,0 +1,61 @@
+/*  Copyright (C) 2006 yopyop
+    yopyop156@ifrance.com
+    yopyop156.ifrance.com
+
+    This file is part of DeSmuME
+
+    DeSmuME is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    DeSmuME is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DeSmuME; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "FIFO.h"
+
+void FIFOInit(FIFO * fifo)
+{
+	unsigned long i;
+
+	fifo->begin = 0;
+        fifo->end = 0;
+        for(i = 0; i<0x2000; ++i)
+        	fifo->data[i] = 0;
+        fifo->full = FALSE;
+        fifo->empty = TRUE;
+        fifo->error = FALSE;
+}
+
+void FIFOAdd(FIFO * fifo, unsigned long v)
+{
+	if(fifo->full) 
+        {
+        	fifo->error = TRUE;
+                return;
+        }
+        fifo->data[fifo->end] = v;
+        fifo->end = (fifo->end + 1)& 0x1FFF;
+        fifo->full = (fifo->end == fifo->begin);
+        fifo->empty = FALSE;
+}
+       
+unsigned long FIFOValue(FIFO * fifo)
+{
+	if(fifo->empty)
+        {
+        	fifo->error = TRUE;
+                return 0;
+        }
+        unsigned long v = fifo->data[fifo->begin];
+        fifo->begin = (fifo->begin + 1)& 0x1FFF;
+        fifo->empty = (fifo->begin == fifo->end);
+        return v;
+}
