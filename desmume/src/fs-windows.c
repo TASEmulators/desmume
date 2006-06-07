3de4593edf393087b81386a41cbab8578d1574e3
yabause <yabause@users.sf.net>
2006-06-07 22:00:48 +0000

    Moved fs.c to fs-windows.c and added some dummy fs-linux.c

---
+++ desmume/src/fs-windows.c
@@ -0,0 +1,50 @@
+#include "fs.h"
+
+#include <windows.h>
+
+void * FsReadFirst(const char * path, FsEntry * entry) {
+	WIN32_FIND_DATA FindFileData;
+	HANDLE hFind;
+	HANDLE * ret;
+
+	hFind = FindFirstFile(path, &FindFileData);
+	if (hFind == INVALID_HANDLE_VALUE)
+		return NULL;
+
+	strcpy(entry->cFileName, FindFileData.cFileName);
+	strcpy(entry->cAlternateFileName, FindFileData.cAlternateFileName);
+	entry->flags = 0;
+	if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+		entry->flags = FS_IS_DIR;
+	}
+
+	ret = malloc(sizeof(HANDLE));
+	*ret = hFind;
+	return ret;
+}
+
+int FsReadNext(void * search, FsEntry * entry) {
+	WIN32_FIND_DATA FindFileData;
+	HANDLE * h = (HANDLE *) search;
+	int ret;
+
+	ret = FindNextFile(*h, &FindFileData);
+
+	strcpy(entry->cFileName, FindFileData.cFileName);
+	strcpy(entry->cAlternateFileName, FindFileData.cAlternateFileName);
+	entry->flags = 0;
+	if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+		entry->flags = FS_IS_DIR;
+	}
+
+	return ret;
+}
+
+void FsClose(void * search) {
+	FindClose(*((HANDLE *) search));
+}
+
+int FsError(void) {
+	if (GetLastError() == ERROR_NO_MORE_FILES)
+		return FS_ERR_NO_MORE_FILES;
+}
