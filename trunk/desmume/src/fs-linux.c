8b18e499293916af6dc836f2d2b25bc4c8e7d4eb
yabause <yabause@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2006-06-07 22:52:24 +0000

    Added some code to fs-linux.c, it's now working enough to display files but doesn't parse subdirs. The code needs some cleaning...

--- trunk/desmume/src/fs-linux.c
+++ trunk/desmume/src/fs-linux.c
@@ -1,14 +1,48 @@
 #include "fs.h"
 
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+
 void * FsReadFirst(const char * path, FsEntry * entry) {
-	return 0;
+	DIR * dir;
+	struct dirent * e;
+
+	printf("reading %s\n", path);
+	dir = opendir(path);
+	if (!dir)
+		return NULL;
+
+	e = readdir(dir);
+	if (!e)
+		return NULL;
+
+	strcpy(entry->cFileName, e->d_name);
+	strncpy(entry->cAlternateFileName, e->d_name, 12);
+	entry->flags = 0;
+
+	return dir;
 }
 
 int FsReadNext(void * search, FsEntry * entry) {
-	return 0;
+	struct dirent * e;
+
+	e = readdir(search);
+	if (!e)
+		return 0;
+
+	strcpy(entry->cFileName, e->d_name);
+	strncpy(entry->cAlternateFileName, e->d_name, 12);
+	entry->flags = 0;
+
+	return 1;
 }
 
 void FsClose(void * search) {
+	DIR * dir = search;
+
+	closedir(dir);
 }
 
 int FsError(void) {
