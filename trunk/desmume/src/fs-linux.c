17d1283147c453dd81a47d847cd1ccfe62692097
yabause <yabause@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2006-06-07 22:00:48 +0000

    Moved fs.c to fs-windows.c and added some dummy fs-linux.c

---
+++ trunk/desmume/src/fs-linux.c
@@ -0,0 +1,16 @@
+#include "fs.h"
+
+void * FsReadFirst(const char * path, FsEntry * entry) {
+	return 0;
+}
+
+int FsReadNext(void * search, FsEntry * entry) {
+	return 0;
+}
+
+void FsClose(void * search) {
+}
+
+int FsError(void) {
+	return 0;
+}
