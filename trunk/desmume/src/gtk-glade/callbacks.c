55f4246df45c005f5eec26133120cd4e6d85502a
damdoum <damdoum@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2007-01-03 23:54:08 +0000

    damdoum 2007-01: gtk-glade ui files

---
+++ trunk/desmume/src/gtk-glade/callbacks.c
@@ -0,0 +1,235 @@
+#include "callbacks.h"
+#include "DeSmuME.xpm"
+
+/* globals */
+uint Frameskip = 0;
+static int ScreenCoeff_Size=1;
+
+/* inline & protos */
+
+void inline MAINWINDOW_RESIZE() {
+	gtk_window_resize ((GtkWindow*)pWindow,1,1);
+}
+
+void inline SET_SENSITIVE(gchar *w, gboolean b) {
+	gtk_widget_set_sensitive(
+		glade_xml_get_widget(xml, w), TRUE);
+}
+
+
+/* MENU FILE ***** ***** ***** ***** */
+void inline ADD_FILTER(GtkWidget * filech, const char * pattern, const char * name) {
+	GtkFileFilter *pFilter;
+	pFilter = gtk_file_filter_new();
+	gtk_file_filter_add_pattern(pFilter, pattern);
+	gtk_file_filter_set_name(pFilter, name);
+	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(filech), pFilter);
+}
+
+void file_open() {
+	desmume_pause();
+	
+	GtkWidget *pFileSelection;
+	GtkWidget *pParent;
+	gchar *sChemin;
+
+	pParent = GTK_WIDGET(pWindow);
+	
+	/* Creating the selection window */
+	pFileSelection = gtk_file_chooser_dialog_new("Open...",
+			GTK_WINDOW(pParent),
+			GTK_FILE_CHOOSER_ACTION_OPEN,
+			GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+			GTK_STOCK_OPEN, GTK_RESPONSE_OK,
+			NULL);
+	/* On limite les actions a cette fenetre */
+	gtk_window_set_modal(GTK_WINDOW(pFileSelection), TRUE);
+
+	ADD_FILTER(pFileSelection, "*.nds", "Nds binary (.nds)");
+	ADD_FILTER(pFileSelection, "*.ds.gba", "Nds binary with loader (.ds.gba)");
+	ADD_FILTER(pFileSelection, "*", "All files");
+	//ADD_FILTER(pFileSelection, "*.zip", "Nds zipped binary");
+	
+	/* Affichage fenetre*/
+	switch(gtk_dialog_run(GTK_DIALOG(pFileSelection)))
+	{
+		case GTK_RESPONSE_OK:
+			/* Recuperation du chemin */
+			sChemin = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(pFileSelection));
+			if(desmume_open((const char*)sChemin) < 0)
+			{
+				GtkWidget *pDialog = gtk_message_dialog_new(GTK_WINDOW(pFileSelection),
+						GTK_DIALOG_MODAL,
+						GTK_MESSAGE_ERROR,
+						GTK_BUTTONS_OK,
+						"Unable to load :\n%s", sChemin);
+				gtk_dialog_run(GTK_DIALOG(pDialog));
+				gtk_widget_destroy(pDialog);
+			} else {
+				SET_SENSITIVE("menu_exec", TRUE);
+				SET_SENSITIVE("menu_pause", TRUE);
+				SET_SENSITIVE("menu_reset", TRUE);
+				SET_SENSITIVE("wgt_Exec", TRUE);
+				SET_SENSITIVE("wgt_Reset", TRUE);
+				desmume_resume();
+			}
+
+			g_free(sChemin);
+			break;
+		default:
+			break;
+	}
+	gtk_widget_destroy(pFileSelection);
+}
+
+void  on_menu_ouvrir_activate  (GtkMenuItem *menuitem, gpointer user_data) { file_open();}
+void  on_menu_pscreen_activate (GtkMenuItem *menuitem, gpointer user_data) {  WriteBMP("./test.bmp",GPU_screen); }
+void  on_menu_quit_activate    (GtkMenuItem *menuitem, gpointer user_data) { gtk_main_quit(); }
+
+
+
+/* MENU EMULATION ***** ***** ***** ***** */
+void  on_menu_exec_activate   (GtkMenuItem *menuitem, gpointer user_data) { desmume_resume(); }
+void  on_menu_pause_activate  (GtkMenuItem *menuitem, gpointer user_data) { desmume_pause(); }
+void  on_menu_reset_activate  (GtkMenuItem *menuitem, gpointer user_data) { desmume_reset(); }
+void  on_menu_layers_activate (GtkMenuItem *menuitem, gpointer user_data) {
+	/* we want to hide or show the checkbox for the layers */
+	GtkWidget * w1 = glade_xml_get_widget(xml, "wvb_1_Main");
+	GtkWidget * w2 = glade_xml_get_widget(xml, "wvb_2_Sub");
+	if (gtk_check_menu_item_get_active((GtkCheckMenuItem*)menuitem)==TRUE) {
+		gtk_widget_show(w1);
+		gtk_widget_show(w2);
+	} else {
+		gtk_widget_hide(w1);
+		gtk_widget_hide(w2);
+	}
+	/* pack the window */
+	MAINWINDOW_RESIZE();
+}
+
+
+/* SUBMENU FRAMESKIP ***** ***** ***** ***** */
+void  on_fs0_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 0; }
+void  on_fs1_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 1; }
+void  on_fs2_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 2; }
+void  on_fs3_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 3; }
+void  on_fs4_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 4; }
+void  on_fs5_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 5; }
+void  on_fs6_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 6; }
+void  on_fs7_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 7; }
+void  on_fs8_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 8; }
+void  on_fs9_activate  (GtkMenuItem *menuitem,gpointer user_data) { Frameskip = 9; }
+
+
+/* SUBMENU SIZE ***** ***** ***** ***** */
+void resize (int Size) {
+	/* we want to scale drawing areas by a factor (1x,2x or 3x) */
+	gtk_drawing_area_size(GTK_DRAWING_AREA(pDrawingArea), 256 * Size, 192 * Size);
+	gtk_widget_set_usize (pDrawingArea, 256 * Size, 192 * Size);	
+	gtk_drawing_area_size(GTK_DRAWING_AREA(pDrawingArea2), 256 * Size, 192 * Size);
+	gtk_widget_set_usize (pDrawingArea2, 256 * Size, 192 * Size);	
+	ScreenCoeff_Size = Size;
+	/* pack the window */
+	MAINWINDOW_RESIZE();
+}
+
+void  on_size1x_activate (GtkMenuItem *menuitem, gpointer user_data) { resize(1); }
+void  on_size2x_activate (GtkMenuItem *menuitem, gpointer user_data) { resize(2); }
+void  on_size3x_activate (GtkMenuItem *menuitem, gpointer user_data) { resize(3); }
+
+
+/* MENU CONFIG ***** ***** ***** ***** */
+gint Keypad_Temp[DESMUME_NB_KEYS];
+
+void  on_menu_controls_activate     (GtkMenuItem *menuitem, gpointer user_data) {
+	edit_controls();
+}
+
+void  on_menu_rightscreen_activate  (GtkMenuItem *menuitem, gpointer user_data) {
+	GtkBox * sbox = (GtkBox*)glade_xml_get_widget(xml, "whb_Sub");
+	GtkWidget * mbox = glade_xml_get_widget(xml, "whb_Main");
+	GtkWidget * vbox = glade_xml_get_widget(xml, "wvb_Layout");
+	GtkWidget * w    = glade_xml_get_widget(xml, "wvb_2_Sub");
+
+	/* we want to change the layout, lower screen goes left */
+	if (gtk_check_menu_item_get_active((GtkCheckMenuItem*)menuitem)==TRUE) {
+		gtk_box_reorder_child(sbox,w,-1);
+		gtk_widget_reparent((GtkWidget*)sbox,mbox);
+	} else {
+	/* we want to change the layout, lower screen goes down */
+		gtk_box_reorder_child(sbox,w,0);
+		gtk_widget_reparent((GtkWidget*)sbox,vbox);
+	}
+	/* pack the window */
+	MAINWINDOW_RESIZE();
+}
+
+
+/* MENU TOOLS ***** ***** ***** ***** */
+void  on_menu_IO_regs_activate      (GtkMenuItem *menuitem, gpointer user_data) {
+
+}
+
+
+/* MENU ? ***** ***** ***** ***** */
+void  on_menu_apropos_activate      (GtkMenuItem *menuitem, gpointer user_data) {
+	GtkAboutDialog * wAbout = (GtkAboutDialog*)glade_xml_get_widget(xml, "wAboutDlg");
+	gtk_about_dialog_set_version(wAbout, VERSION);
+	gtk_widget_show((GtkWidget*)wAbout);
+}
+
+
+
+
+
+
+/* TOOLBAR ***** ***** ***** ***** */
+void  on_wgt_Open_clicked  (GtkToolButton *toolbutton, gpointer user_data) { file_open(); }
+void  on_wgt_Reset_clicked (GtkToolButton *toolbutton, gpointer user_data) { desmume_reset(); }
+void  on_wgt_Quit_clicked  (GtkToolButton *toolbutton, gpointer user_data) { gtk_main_quit(); }
+void  on_wgt_Exec_toggled  (GtkToggleToolButton *toggletoolbutton, gpointer user_data) {
+	if (gtk_toggle_tool_button_get_active(toggletoolbutton)==TRUE)
+		desmume_resume();
+	else 
+		desmume_pause();
+}
+
+
+
+/* LAYERS ***** ***** ***** ***** */
+void change_bgx_layer(int layer, gboolean state, Screen scr) {
+	//if(!desmume_running()) return;
+	if(state==TRUE) { 
+		if (!scr.gpu->dispBG[layer]) GPU_addBack(scr.gpu, layer);
+	} else {
+		if (scr.gpu->dispBG[layer])  GPU_remove(scr.gpu, layer); 
+	}
+	//fprintf(stderr,"Changed Layer %s to %d\n",layer,state);
+}
+void change_obj_layer(gboolean state, Screen scr) {
+	// TODO
+}
+
+/* LAYERS MAIN SCREEN ***** ***** ***** ***** */
+void  on_wc_1_BG0_toggled  (GtkToggleButton *togglebutton, gpointer user_data) { 
+	change_bgx_layer(0, gtk_toggle_button_get_active(togglebutton), MainScreen); }
+void  on_wc_1_BG1_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_bgx_layer(1, gtk_toggle_button_get_active(togglebutton), MainScreen); }
+void  on_wc_1_BG2_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_bgx_layer(2, gtk_toggle_button_get_active(togglebutton), MainScreen); }
+void  on_wc_1_BG3_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_bgx_layer(3, gtk_toggle_button_get_active(togglebutton), MainScreen); }
+void  on_wc_1_OBJ_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_obj_layer(gtk_toggle_button_get_active(togglebutton), MainScreen); }
+
+/* LAYERS SECOND SCREEN ***** ***** ***** ***** */
+void  on_wc_2b_BG0_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_bgx_layer(0, gtk_toggle_button_get_active(togglebutton), SubScreen); }
+void  on_wc_2b_BG1_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_bgx_layer(1, gtk_toggle_button_get_active(togglebutton), SubScreen); }
+void  on_wc_2b_BG2_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_bgx_layer(2, gtk_toggle_button_get_active(togglebutton), SubScreen); }
+void  on_wc_2b_BG3_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_bgx_layer(3, gtk_toggle_button_get_active(togglebutton), SubScreen); }
+void  on_wc_2b_OBJ_toggled  (GtkToggleButton *togglebutton, gpointer user_data) {
+	change_obj_layer(gtk_toggle_button_get_active(togglebutton), SubScreen); }
---
+++ trunk/desmume/src/gtk-glade/globals.h
@@ -0,0 +1,44 @@
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+#include <unistd.h>
+
+#include <SDL/SDL.h>
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+#include <glade/glade.h>
+
+#include "../MMU.h"
+#include "../armcpu.h"
+#include "../NDSSystem.h"
+#include "../cflash.h"
+#include "../sndsdl.h"
+#include "../types.h"
+#include "desmume.h"
+
+uint Frameskip;
+gint Keypad_Config[DESMUME_NB_KEYS];
+gint Keypad_Temp[DESMUME_NB_KEYS];
+
+/* main.c */
+GtkWidget * pWindow;
+GtkWidget * pDrawingArea;
+GtkWidget * pDrawingArea2;
+GladeXML  * xml;
+
+/* callbacks_IO.c */
+void edit_controls();
+
+/* printscreen.c */
+int WriteBMP(const char *filename,u16 *bmp);
+
+/* keyvalnames.c   -see <gdk/gdkkeysyms.h>- */
+char * KEYVAL_NAMES[0x10000];
+char * KEYNAME(int k);
+void init_keyvals();
+
+#endif /* __GLOBALS_H__ */
