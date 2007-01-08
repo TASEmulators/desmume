af1bde05d9710669861b7518fc33cf8392119fc6
evilynux <evilynux@users.sf.net>
2007-01-08 20:56:27 +0000

    Centralized includes.

---
+++ desmume/src/gtk/globals.h
@@ -0,0 +1,47 @@
+/* globals.h - this file is part of $PROJECT_NAME_HERE$
+ *
+ * Copyright (C) 2007 Pascal Giard
+ *
+ * Author: Pascal Giard <evilynux@gmail.com>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include <SDL/SDL.h>
+#include <gtk/gtk.h>
+
+#include "../MMU.h"
+//#include "../registers.h"
+#include "../armcpu.h"
+#include "../NDSSystem.h"
+#include "../cflash.h"
+#include "../sndsdl.h"
+#include "../types.h"
+#include "desmume.h"
+
+u16 Joypad_Config[DESMUME_NB_KEYS];
+
+#endif /* GLOBALS_H */
