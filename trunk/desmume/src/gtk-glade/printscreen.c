c54b8b793ea81912604e5817730c7e1465f68f49
damdoum <damdoum@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2007-01-08 13:14:10 +0000

    added GPL notice

--- trunk/desmume/src/gtk-glade/printscreen.c
+++ trunk/desmume/src/gtk-glade/printscreen.c
@@ -1,6 +1,23 @@
-#include <gtk/gtk.h>
+/* printscreen.c - this file is part of DeSmuME
+ *
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
-#include "../armcpu.h"
+#include "global.h"
 
 /////////////////////////////// PRINTSCREEN /////////////////////////////////
 // TODO: improve (let choose filename, and use png)
