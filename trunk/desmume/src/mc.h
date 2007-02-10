3ebea21b1dbefd32fde7a8c2ca6fdbf1753eca86
cyberwarriorx <cyberwarriorx@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2007-02-10 02:09:48 +0000

    -Noticed a couple of files with missing copyrights. I hope it's correct now. -Added support for importing *.duc files. You just have to call NDS_ImportSave with a valid filename specified and it should load any *.duc file

--- trunk/desmume/src/mc.h
+++ trunk/desmume/src/mc.h
@@ -1,3 +1,23 @@
+/*  Copyright (C) 2006 thoduv
+    Copyright (C) 2006 Theo Berkau
+
+    This file is part of DeSmuME
+
+    DeSmuME is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    DeSmuME is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DeSmuME; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #ifndef __FW_H__
 #define __FW_H__
 
@@ -60,6 +80,7 @@ void mc_init(memory_chip_t *mc, int type);    /* reset and init values for memor
 u8 *mc_alloc(memory_chip_t *mc, u32 size);  /* alloc mc memory */
 void mc_realloc(memory_chip_t *mc, int type, u32 size);      /* realloc mc memory */
 void mc_load_file(memory_chip_t *mc, const char* filename); /* load save file and setup fp */
+int mc_load_duc(memory_chip_t *mc, const char* filename); /* load Action Replay DS save file */
 void mc_free(memory_chip_t *mc);    /* delete mc memory */
 void mc_reset_com(memory_chip_t *mc);       /* reset communication with mc */
 u8 fw_transfer(memory_chip_t *mc, u8 data); /* transfer to, then receive data from firmware */
