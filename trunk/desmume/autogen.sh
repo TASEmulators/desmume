05c064bb4ecc8b30d60250a346501593b600f7f5
evilynux <evilynux@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2007-07-16 05:38:22 +0000

    There's an annoying bug in /usr/share/intltool/Makefile.in.in requiring me to modify it after a call to intltoolize (localedir). Since i can't figure what's wrong, i'm commenting the call to intltoolize and adding the corresponding files to CVS (at least for now).
    Solution proposals are much welcome.

--- trunk/desmume/autogen.sh
+++ trunk/desmume/autogen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-echo "Running intltoolize"
-intltoolize --copy --force --automake
+# There's an annoying bug in /usr/share/intltool/Makefile.in.in requiring
+# me to modify it after a call to intltoolize. Since i can't seem to figure
+# what's wrong, i'm commenting the call to intltoolize and adding the
+# corresponding files to CVS.
+
+#echo "Running intltoolize"
+#intltoolize --copy --force --automake
 
 autoreconf --install --force --verbose
