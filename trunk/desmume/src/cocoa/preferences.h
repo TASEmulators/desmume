3cb73e23454d88a52b264915a200db677d0f691e
gecko_reverse <gecko_reverse@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2007-08-19 10:53:37 +0000

    cocoa port updates

---
+++ trunk/desmume/src/cocoa/preferences.h
@@ -0,0 +1,34 @@
+/*  Copyright (C) 2007 Jeff Bland
+
+	This file is part of DeSmuME
+
+	DeSmuME is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	DeSmuME is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with DeSmuME; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef PREFERENCES_H_INCLUDED
+#define PREFERENCES_H_INCLUDED
+
+//These string have to be used in various places,
+//so having defines makes sure they are consistent
+
+#define PREF_EXECUTE_UPON_LOAD @"Execute Upon Load"
+
+#define PREF_FIRMWARE_LANGUAGE @"Firmware Language"
+#define PREF_FIRMWARE_PLAYER_NAME @"Firmware Player Name"
+
+#define PREF_3D_PLUGIN @"3D Graphics Plugin"
+#define PREF_SOUND_PLUGIN @"Sound Plugin"
+
+#endif
