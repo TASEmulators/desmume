b9f69f28b85584b6765f3ffe672e1b7ef086fa8b
mtabachenko <mtabachenko@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2008-05-02 06:07:29 +0000

    Windows port: - added debug console for BETA versions;

--- trunk/desmume/src/debug.c
+++ trunk/desmume/src/debug.c
@@ -23,6 +23,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+/////// Console vars
+#define BUFFER_SIZE 100
+HANDLE hConsole;
+///////
+
 //////////////////////////////////////////////////////////////////////////////
 
 Debug * DebugInit(const char * n, DebugOutType t, char * s) {
@@ -158,3 +163,53 @@ void LogStop(void) {
 
 //////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////// Console
+#ifdef WIN32
+void OpenConsole() 
+{
+	COORD csize;
+	CONSOLE_SCREEN_BUFFER_INFO csbiInfo; 
+	SMALL_RECT srect;
+	char buf[256];
+
+	if (hConsole) return;
+	AllocConsole();
+	memset(buf,0,256);
+	sprintf(buf,"DeSmuME v%s OUTPUT", VERSION);
+	SetConsoleTitle(TEXT(buf));
+	csize.X = 60;
+	csize.Y = 800;
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), csize);
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbiInfo);
+	srect = csbiInfo.srWindow;
+	srect.Right = srect.Left + 99;
+	srect.Bottom = srect.Top + 64;
+	SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &srect);
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+}
+
+void CloseConsole() {
+	if (hConsole == NULL) return;
+	FreeConsole(); 
+	hConsole = NULL;
+}
+
+void printlog(char *fmt, ...) {
+	va_list list;
+	char msg[512],msg2[522];
+	wchar_t msg3[522];
+	char *ptr;
+	DWORD tmp;
+	int len, s;
+	int i, j;
+
+	LPWSTR ret;
+
+	va_start(list,fmt);
+	_vsnprintf(msg,511,fmt,list);
+	msg[511] = '\0';
+	va_end(list);
+	ptr=msg; len=strlen(msg);
+	WriteConsole(hConsole,ptr, (DWORD)len, &tmp, 0);
+}
+#endif
