41fae5a7f09979ddcb52889e2465bbbd9c643675
adelikat <adelikat@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2008-09-11 03:42:48 +0000

    Added "N" accel key is preparation for addinga  frame advance hotkey

--- trunk/desmume/src/windows/resource.h
+++ trunk/desmume/src/windows/resource.h
@@ -284,14 +284,15 @@
 #define IDC_LIGHT_VIEWER_LIGHT3VECTOR_EDIT 1334
 #define IDM_ABOUT                       40003
 #define ACCEL_P                         40004
-#define ACCEL_SPACEBAR                  40004
+#define ACCEL_SPACEBAR                  40005
+#define ACCEL_N                         40007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_COMMAND_VALUE         40006
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
