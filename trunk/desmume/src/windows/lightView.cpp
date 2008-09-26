df4cb06fe186ebe8566253aad33d5436244c2cbf
zeromus <zeromus@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2008-09-26 06:36:18 +0000

    windows: just a little cleanup

--- trunk/desmume/src/windows/lightView.cpp
+++ trunk/desmume/src/windows/lightView.cpp
@@ -20,10 +20,17 @@
 #include "lightView.h"
 #include "commctrl.h"
 #include "colorctrl.h"
-#include "colorconv.h"
 #include "gfx3d.h"
 #include "resource.h"
 #include "debug.h"
+
+// Convert B5G5R5 color format into R8G8B8 color format
+unsigned int ColorConv_B5R5R5ToR8G8B8(const unsigned int color)
+{
+	return	(((color&31)<<16)<<3)		|	// red
+			((((color>>5)&31)<<8)<<3)	|	// green
+			(((color>>10)&31)<<3);			// blue
+}
 
 typedef struct
 {
