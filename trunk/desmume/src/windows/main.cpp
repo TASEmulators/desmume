a7e648a2893315e29b7336f316661f2eec31ba7a
luigi__ <luigi__@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2008-12-27 23:42:50 +0000

    Choosing Danish as language would save French instead of Danish. Fixed.

--- trunk/desmume/src/windows/main.cpp
+++ trunk/desmume/src/windows/main.cpp
@@ -2410,7 +2410,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                      CheckLanguage(LOWORD(wParam));
                   return 0;
                   case IDC_LANGDANISH:
-                     SaveLanguage(1);
+                     SaveLanguage(2);
                      ChangeLanguage(2);
                      CheckLanguage(LOWORD(wParam));
                   return 0;
