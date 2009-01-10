9690913aa07fccb919c5cf6fc5609b1acbc7563e
luigi__ <luigi__@users.sf.net>
2009-01-10 21:02:13 +0000

    Resize handling entirely remade, it should work flawlessly now, even under Vista. Except there's still a display bug when using a rotation of 90 or 270 degrees :( but hopefully I should fix this quickly enough

--- desmume/src/windows/CWindow.cpp
+++ desmume/src/windows/CWindow.cpp
@@ -30,6 +30,9 @@ WINCLASS::WINCLASS(LPSTR rclass, HINSTANCE hInst)
 	hwnd = NULL;
 	hmenu = NULL;
 	hInstance = hInst;
+	
+	minWidth = 0;
+	minHeight = 0;
 }
 
 WINCLASS::~WINCLASS()
@@ -80,6 +83,123 @@ void WINCLASS::Show(int mode)
 void WINCLASS::Hide()
 {
 	ShowWindow(hwnd, SW_HIDE);
+}
+
+void WINCLASS::setMinSize(int width, int height)
+{
+	minWidth = width;
+	minHeight = height;
+}
+
+void WINCLASS::sizingMsg(WPARAM wParam, LPARAM lParam, BOOL keepRatio)
+{
+	RECT *rect = (RECT*)lParam;
+
+	int _minWidth, _minHeight;
+
+	int xborder, yborder;
+	int ymenu, ymenunew;
+	int ycaption;
+
+	MENUBARINFO mbi;
+
+	/* Get the size of the border */
+	xborder = GetSystemMetrics(SM_CXSIZEFRAME);
+	yborder = GetSystemMetrics(SM_CYSIZEFRAME);
+	
+	/* Get the size of the menu bar */
+	ZeroMemory(&mbi, sizeof(mbi));
+	mbi.cbSize = sizeof(mbi);
+	GetMenuBarInfo(hwnd, OBJID_MENU, 0, &mbi);
+	ymenu = (mbi.rcBar.bottom - mbi.rcBar.top + 1);
+
+	/* Get the size of the caption bar */
+	ycaption = GetSystemMetrics(SM_CYCAPTION);
+
+	/* Calculate the minimum size in pixels */
+	_minWidth = (xborder + minWidth + xborder);
+	_minHeight = (ycaption + yborder + ymenu + minHeight + yborder);
+
+	/* Clamp the size to the minimum size (256x384) */
+	rect->right = (rect->left + std::max(_minWidth, (int)(rect->right - rect->left)));
+	rect->bottom = (rect->top + std::max(_minHeight, (int)(rect->bottom - rect->top)));
+
+	/* Apply the ratio stuff */
+	if(keepRatio)
+	{
+		switch(wParam)
+		{
+		case WMSZ_LEFT:
+		case WMSZ_RIGHT:
+		case WMSZ_TOPLEFT:
+		case WMSZ_TOPRIGHT:
+		case WMSZ_BOTTOMLEFT:
+		case WMSZ_BOTTOMRIGHT:
+			{
+				float ratio = ((rect->right - rect->left - xborder - xborder) / (float)minWidth);
+				rect->bottom = (rect->top + ycaption + yborder + ymenu + (minHeight * ratio) + yborder);
+			}
+			break;
+				
+		case WMSZ_TOP:
+		case WMSZ_BOTTOM:
+			{
+				float ratio = ((rect->bottom - rect->top - ycaption - yborder - ymenu - yborder) / (float)minHeight);
+				rect->right = (rect->left + xborder + (minWidth * ratio) + xborder);
+			}
+			break;
+		}
+	}
+
+	/* Check if the height of the menu has changed during the resize */
+	ZeroMemory(&mbi, sizeof(mbi));
+	mbi.cbSize = sizeof(mbi);
+	GetMenuBarInfo(hwnd, OBJID_MENU, 0, &mbi);
+	ymenunew = (mbi.rcBar.bottom - mbi.rcBar.top + 1);
+
+	if(ymenunew != ymenu)
+		rect->bottom += (ymenunew - ymenu);
+}
+
+void WINCLASS::setClientSize(int width, int height)
+{
+	int xborder, yborder;
+	int ymenu, ymenunew;
+	int ycaption;
+
+	MENUBARINFO mbi;
+
+	RECT wndRect;
+	int finalx, finaly;
+
+	/* Get the size of the border */
+	xborder = GetSystemMetrics(SM_CXSIZEFRAME);
+	yborder = GetSystemMetrics(SM_CYSIZEFRAME);
+	
+	/* Get the size of the menu bar */
+	ZeroMemory(&mbi, sizeof(mbi));
+	mbi.cbSize = sizeof(mbi);
+	GetMenuBarInfo(hwnd, OBJID_MENU, 0, &mbi);
+	ymenu = (mbi.rcBar.bottom - mbi.rcBar.top + 1);
+
+	/* Get the size of the caption bar */
+	ycaption = GetSystemMetrics(SM_CYCAPTION);
+
+	/* Finally, resize the window */
+	GetWindowRect(hwnd, &wndRect);
+	finalx = (xborder + width + xborder);
+	finaly = (ycaption + yborder + ymenu + height + yborder);
+	MoveWindow(hwnd, wndRect.left, wndRect.top, finalx, finaly, TRUE);
+
+	/* Oops, we also need to check if the height */
+	/* of the menu bar has changed after the resize */
+	ZeroMemory(&mbi, sizeof(mbi));
+	mbi.cbSize = sizeof(mbi);
+	GetMenuBarInfo(hwnd, OBJID_MENU, 0, &mbi);
+	ymenunew = (mbi.rcBar.bottom - mbi.rcBar.top + 1);
+
+	if(ymenunew != ymenu)
+		MoveWindow(hwnd, wndRect.left, wndRect.top, finalx, (finaly + (ymenunew - ymenu)), TRUE);
 }
 
 //========================================================= Thread class
