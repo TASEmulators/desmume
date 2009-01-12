6340db81508d77953eb4a3d8ae685d7cec93ef4e
riccardom <riccardom@users.sf.net>
2009-01-12 20:48:31 +0000

    Move stub mic implementation for not win32 platforms to a separate file.

---
+++ desmume/src/mic.cpp
@@ -0,0 +1,23 @@
+#ifndef WIN32
+
+#include "types.h"
+#include "mic.h"
+
+BOOL Mic_Init()
+{
+	return TRUE;
+}
+
+void Mic_Reset()
+{
+}
+
+void Mic_DeInit()
+{
+}
+
+u8 Mic_ReadSample() {
+	return 0;
+}
+
+#endif
