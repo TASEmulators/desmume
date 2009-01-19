eeb78c408698925c24065eb074131ba9895f4eed
yabause <yabause@users.sf.net>
2009-01-19 20:25:40 +0000

    Added/fixed some copyright headers. Feel free to double check and further fix what I've done.

--- desmume/src/gtk/tools/ioregsView.cpp
+++ desmume/src/gtk/tools/ioregsView.cpp
@@ -1,5 +1,6 @@
 /* ioregsView.cpp - this file is part of DeSmuME
  *
+ * Copyright (C) 2006 Thoduv
  * Copyright (C) 2006,2007 DeSmuME Team
  *
  * This file is free software; you can redistribute it and/or modify
