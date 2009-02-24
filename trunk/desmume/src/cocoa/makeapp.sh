134fcb9881dc8f8bbfbdf084fb5d9884180e1387
gecko_reverse <gecko_reverse@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2009-02-24 08:25:50 +0000

    cocoa port codeblocksproj/rakefile put their stuff in the build folder now

--- trunk/desmume/src/cocoa/makeapp.sh
+++ trunk/desmume/src/cocoa/makeapp.sh
@@ -2,22 +2,22 @@
 #This script builds the application bundle so that DeSmuME can run when compiled from Code::Blocks
 
 #
-lipo DeSmuME.app/Contents/MacOS/DeSmuME_x86 DeSmuME.app/Contents/MacOS/DeSmuME_ppc -create -output DeSmuME.app/Contents/MacOS/DeSmuME
-rm -f DeSmuME.app/Contents/MacOS/DeSmuME_x86
-rm -f DeSmuME.app/Contents/MacOS/DeSmuME_ppc
+lipo build/codeblocks/DeSmuME.app/Contents/MacOS/DeSmuME_x86 DeSmuME.app/Contents/MacOS/DeSmuME_ppc -create -output DeSmuME.app/Contents/MacOS/DeSmuME
+rm -f build/codeblocks/DeSmuME.app/Contents/MacOS/DeSmuME_x86
+rm -f build/codeblocks/DeSmuME.app/Contents/MacOS/DeSmuME_ppc
 
 #
-mkdir -p DeSmuME.app/Contents/Resources
-cp Info.plist DeSmuME.app/Contents/Info.plist
-cp PkgInfo DeSmuME.app/Contents/PkgInfo
-cp InfoPlist.strings DeSmuME.app/Contents/Resources/InfoPlist.strings
-cp DeSmuME.icns DeSmuME.app/Contents/Resources/DeSmuME.icns
-cp ../../COPYING DeSmuME.app/Contents/Resources/COPYING
-cp ../../README DeSmuME.app/Contents/Resources/README
-cp ../../README.MAC DeSmuME.app/Contents/Resources/README.MAC
-cp ../../AUTHORS DeSmuME.app/Contents/Resources/AUTHORS
-cp ../../README.TRANSLATION DeSmuME.app/Contents/Resources/README.TRANSLATION
-cp ../../ChangeLog DeSmuME.app/Contents/Resources/ChangeLog
+mkdir -p build/codeblocks/DeSmuME.app/Contents/Resources
+cp Info.plist build/codeblocks/DeSmuME.app/Contents/Info.plist
+cp PkgInfo build/codeblocks/DeSmuME.app/Contents/PkgInfo
+cp InfoPlist.strings build/codeblocks/DeSmuME.app/Contents/Resources/InfoPlist.strings
+cp DeSmuME.icns build/codeblocks/DeSmuME.app/Contents/Resources/DeSmuME.icns
+cp ../../COPYING build/codeblocks/DeSmuME.app/Contents/Resources/COPYING
+cp ../../README build/codeblocks/DeSmuME.app/Contents/Resources/README
+cp ../../README.MAC build/codeblocks/DeSmuME.app/Contents/Resources/README.MAC
+cp ../../AUTHORS build/codeblocks/DeSmuME.app/Contents/Resources/AUTHORS
+cp ../../README.TRANSLATION build/codeblocks/DeSmuME.app/Contents/Resources/README.TRANSLATION
+cp ../../ChangeLog build/codeblocks/DeSmuME.app/Contents/Resources/ChangeLog
 
 #Localizations
-cp -R translations/*.lproj DeSmuME.app/Contents/Resources/
+cp -R translations/*.lproj build/codeblocks/DeSmuME.app/Contents/Resources/
