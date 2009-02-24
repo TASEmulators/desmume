e12cc84fe17cbcc31fd8de9501a553115d5f880c
gecko_reverse <gecko_reverse@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2009-02-24 03:22:12 +0000

    added Italian translation to cocoa port thanks to Paolo Bernini. move cocoa translation files to their own folder to simplify things

--- trunk/desmume/src/cocoa/makeapp.sh
+++ trunk/desmume/src/cocoa/makeapp.sh
@@ -19,17 +19,5 @@ cp ../../AUTHORS DeSmuME.app/Contents/Resources/AUTHORS
 cp ../../README.TRANSLATION DeSmuME.app/Contents/Resources/README.TRANSLATION
 cp ../../ChangeLog DeSmuME.app/Contents/Resources/ChangeLog
 
-#English
-mkdir -p DeSmuME.app/Contents/Resources/English.lproj
-cp -R English.nib DeSmuME.app/Contents/Resources/English.lproj/MainMenu.nib
-cp English.strings DeSmuME.app/Contents/Resources/English.lproj/Localizable.strings
-
-#Japanese
-mkdir -p DeSmuME.app/Contents/Resources/Japanese.lproj
-cp -R Japanese.nib DeSmuME.app/Contents/Resources/Japanese.lproj/MainMenu.nib
-cp Japanese.strings DeSmuME.app/Contents/Resources/Japanese.lproj/Localizable.strings
-
-#French
-mkdir -p DeSmuME.app/Contents/Resources/French.lproj
-cp -R French.nib DeSmuME.app/Contents/Resources/French.lproj/MainMenu.nib
-cp French.strings DeSmuME.app/Contents/Resources/French.lproj/Localizable.strings
+#Localizations
+cp -R translations/*.lproj DeSmuME.app/Contents/Resources/
