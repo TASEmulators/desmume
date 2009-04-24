c3805ef34b742864f63a8a82c4c4f4bd5061f729
yabause <yabause@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2009-04-24 19:27:41 +0000

    I tagged too soon... again... sorry ^^;

--- tags/release_0_9_2-1/src/PACKED_END.h
+++
@@ -1,3 +0,0 @@
-#if defined(_MSC_VER) || defined(__INTEL_COMPILER)
-#pragma pack(pop)
-#endif
--- tags/release_0_9_2-1/src/mic.h
+++
@@ -1,9 +0,0 @@
-#ifndef MIC_H
-#define MIC_H
-
-BOOL Mic_Init();
-void Mic_Reset();
-void Mic_DeInit();
-u8 Mic_ReadSample();
-
-#endif
