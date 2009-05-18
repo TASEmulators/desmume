e461cc9ce50179b2eb3816bc34eed5d47fe4e6c9
zeromus <zeromus@users.sf.net>
2009-05-18 17:15:22 +0000

    add another autodetection

--- desmume/src/mc.cpp
+++ desmume/src/mc.cpp
@@ -597,7 +597,8 @@ void BackupDevice::reset_command()
 		addr_size = autodetect_size - 1;
 		if(autodetect_size==6) addr_size = 2; //castlevania dawn of sorrow 64kbit eeprom (EEPROM2 in the old system)
 		if(autodetect_size==258) addr_size = 2; //warioware touched
-		if(autodetect_size==9) addr_size = 1; //star wars III
+		if(autodetect_size==9) addr_size = 1; //star wars III (BM_CMD_READLOW)
+		if(autodetect_size==113) addr_size = 1; //space invaders revolution (BM_CMD_READLOW)
 		if(addr_size>4)
 		{
 			LOG("Unexpected backup memory address size: %d\n",addr_size);
