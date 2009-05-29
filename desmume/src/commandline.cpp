26302fda391ecf20408b7a8023bb816ee4f935bf
zeromus <zeromus@users.sf.net>
2009-05-29 08:39:36 +0000

    win32: odds and ends to get things compiling in all cases

--- desmume/src/commandline.cpp
+++ desmume/src/commandline.cpp
@@ -19,6 +19,8 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
+//windows note: make sure this file gets compiled with _cdecl
+
 #include <glib.h>
 
 #include <stdio.h>
