28164e5bb28537efee28a02f9eef16607022ac8b
zeromus <zeromus@users.sf.net>
2009-06-09 03:50:58 +0000

    fix screenshot bugs with rotation and accidentally including hud. rotation is fixed by writing screenshot unrotated since rotation is handled by platform code now and screenshotting is in the core.

--- desmume/src/NDSSystem.cpp
+++ desmume/src/NDSSystem.cpp
@@ -1124,7 +1124,7 @@ int NDS_WritePNG(const char *fname)
 	int x, y;
 	int width=256;
 	int height=192*2;
-	u16 * bmp = (u16 *)GPU_screen;
+	u16 * bmp = (u16 *)GPU_tempScreen;
 	FILE *pp=NULL;
 	uint8 *compmem = NULL;
 	uLongf compmemsize = (uLongf)( (height * (width + 1) * 3 * 1.001 + 1) + 12 );
@@ -1250,7 +1250,7 @@ int NDS_WriteBMP(const char *filename)
 	bmpimgheader_struct imageheader;
 	FILE *file;
 	int i,j;
-	u16 * bmp = (u16 *)GPU_screen;
+	u16 * bmp = (u16 *)GPU_tempScreen;
 	size_t elems_written = 0;
 
 	memset(&fileheader, 0, sizeof(fileheader));
