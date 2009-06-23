508ad5982973e10e8fcf735538f2f4b4e7274cc3
zeromus <zeromus@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2009-06-23 03:56:38 +0000

    win32: add background pause

--- trunk/desmume/src/windows/resource.h
+++ trunk/desmume/src/windows/resource.h
@@ -100,6 +100,7 @@
 #define IDC_8_BIT                       302
 #define IDC_16_BIT                      303
 #define IDC_32_BIT                      304
+#define IDC_BACKGROUNDPAUSE             305
 #define IDC_DES_BOX                     402
 #define IDC_R0                          403
 #define IDC_R1                          404
@@ -613,6 +614,7 @@
 #define IDM_ABOUT                       40003
 #define IDM_EMULATIONSETTINGS           40004
 #define IDC_FRAMELIMIT                  40005
+#define ID_CONFIG_PAUSEWHENFOCUSLOST    40005
 #define IDC_WINDOW1_5X                  40006
 #define IDM_WIFISETTINGS                40007
 #define IDM_ALWAYS_ON_TOP               40008
@@ -727,7 +729,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
-#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_COMMAND_VALUE         40008
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
