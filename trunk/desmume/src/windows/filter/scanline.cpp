951b2b7effad22a9993d6036a199fd9df121cc91
zeromus <zeromus@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2009-07-15 17:44:06 +0000

    win32: make scanlines look more like lcd scanlines.

--- trunk/desmume/src/windows/filter/scanline.cpp
+++ trunk/desmume/src/windows/filter/scanline.cpp
@@ -5,11 +5,20 @@
 
 typedef u64 uint64;
 
+extern CACHE_ALIGN u16 fadeOutColors[17][0x8000];
+
 // stretches a single line
 inline void DoubleLine16( uint16 *lpDst, uint16 *lpSrc, unsigned int Width){
 	while(Width--){
 		*lpDst++ = *lpSrc;
-		*lpDst++ = *lpSrc++;
+		*lpDst++ = fadeOutColors[3][(*lpSrc++)];
+	}
+}
+
+inline void DoubleLine16_2( uint16 *lpDst, uint16 *lpSrc, unsigned int Width){
+	while(Width--){
+		*lpDst++ = fadeOutColors[3][(*lpSrc)];
+		*lpDst++ = fadeOutColors[6][(*lpSrc++)];
 	}
 }
 
@@ -28,7 +37,8 @@ void RenderScanline( SSurface Src, SSurface Dst)
 	if(Src.Width != 512)
 		for (H = 0; H < srcHeight; H++, lpSrc += srcPitch)
 			DoubleLine16 (lpDst, lpSrc, Src.Width), lpDst += dstPitch,
-			memset (lpDst, 0, 512*2), lpDst += dstPitch;
+			DoubleLine16_2 (lpDst, lpSrc, Src.Width), lpDst += dstPitch;
+			//memset (lpDst, 0, 512*2), lpDst += dstPitch;
 	else
 		for (H = 0; H < srcHeight; H++, lpSrc += srcPitch)
 			memcpy (lpDst, lpSrc, Src.Width << 1), lpDst += dstPitch,
