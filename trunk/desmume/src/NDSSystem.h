3e69f6402b46cea86c77723890cc4fc99173214d
gocha <gocha@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2009-09-08 22:53:43 +0000

    Moved fastFetchExecute to CommonSettings, and set the default value to false.

--- trunk/desmume/src/NDSSystem.h
+++ trunk/desmume/src/NDSSystem.h
@@ -431,6 +431,7 @@ extern struct TCommonSettings {
 		, spuAdpcmCache(false)
 		, gfx3d_flushMode(0)
 		, manualBackupType(0)
+		, armFastFetchExecute(false)
 	{
 		strcpy(ARM9BIOS, "biosnds9.bin");
 		strcpy(ARM7BIOS, "biosnds7.bin");
@@ -470,6 +471,8 @@ extern struct TCommonSettings {
 
 	//this is the user's choice of manual backup type, for cases when the autodetection can't be trusted
 	int manualBackupType;
+
+	bool armFastFetchExecute;
 
 	bool spu_muteChannels[16];
 
