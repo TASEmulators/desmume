63e66733bd22133958e4574fbac85a4e470377d7
riccardom <riccardom@users.sf.net>
2010-03-05 13:31:56 +0000

    Whitespace fixup.

--- desmume/src/gtk/main.cpp
+++ desmume/src/gtk/main.cpp
@@ -1988,11 +1988,10 @@ common_gtk_main( struct configured_features *my_config)
     if (my_config->is_cflash_configured)
         addon_type = NDS_ADDON_CFLASH;
 
-	if(my_config->gbaslot_rom != "")
-	{
-		addon_type = NDS_ADDON_GBAGAME;
-		strcpy(GBAgameName, my_config->gbaslot_rom.c_str());
-	}
+    if(my_config->gbaslot_rom != "") {
+        addon_type = NDS_ADDON_GBAGAME;
+        strncpy(GBAgameName, my_config->gbaslot_rom.c_str(), MAX_PATH);
+    }
 
     switch (addon_type) {
     case NDS_ADDON_CFLASH:
