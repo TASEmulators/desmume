c8210b838d6b65a269bde3a0dab7ee899da8ddcb
zeromus <zeromus@users.sf.net>
2010-03-25 03:10:11 +0000

    fix dualscreen 3d frameskip screen cloning/flickering regression from r3422

--- desmume/src/NDSSystem.cpp
+++ desmume/src/NDSSystem.cpp
@@ -1478,6 +1478,15 @@ static void execHardware_hstart()
 
 	nds.VCount++;
 
+	//end of 3d vblank
+	//this should be 214, but we are going to be generous for games with tight timing
+	//they shouldnt be changing any textures at 262 but they might accidentally still be at 214
+	//so..
+	if(CommonSettings.rigorous_timing && nds.VCount==214 || !CommonSettings.rigorous_timing && nds.VCount==262)
+	{
+		gfx3d_VBlankEndSignal(frameSkipper.ShouldSkip3D());
+	}
+
 	if(nds.VCount==263)
 	{
 		nds.VCount=0;
@@ -1514,16 +1523,6 @@ static void execHardware_hstart()
 		//but that isnt even possible until we have some sort of sub-scanline timing.
 		//it may not be necessary.
 		triggerDma(EDMAMode_MemDisplay);
-	}
-
-
-	//end of 3d vblank
-	//this should be 214, but we are going to be generous for games with tight timing
-	//they shouldnt be changing any textures at 262 but they might accidentally still be at 214
-	//so..
-	if(CommonSettings.rigorous_timing && nds.VCount==214 || !CommonSettings.rigorous_timing && nds.VCount==262)
-	{
-		gfx3d_VBlankEndSignal(frameSkipper.ShouldSkip3D());
 	}
 }
 
