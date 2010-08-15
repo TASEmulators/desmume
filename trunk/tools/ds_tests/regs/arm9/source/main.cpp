6149c08486532c1fd7dd633419f02374a1c0442b
zeromus <zeromus@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2010-08-15 21:15:59 +0000

    some spu regs needed to be fewer bits and write-only. also added tests

--- trunk/tools/ds_tests/regs/arm9/source/main.cpp
+++ trunk/tools/ds_tests/regs/arm9/source/main.cpp
@@ -19,8 +19,8 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 #include <nds.h>
-
 #include <stdio.h>
+#include "../../regstest.h"
 
 #define myassert(e,msg)       ((e) ? (void)0 : _myassert(__FILE__, __LINE__, #e, msg))
 
@@ -33,14 +33,37 @@ void _myassert(const char *fileName, int lineNumber, const char* conditionString
 int main(void) {
 	consoleDemoInit();
 
+	arm7comm->code = 0xDEADBEEF;
+
+	//fog table entries should not be readable. they should return 0
 	for(int i=0;i<32;i++)
 	{
 		GFX_FOG_TABLE[i] = 0xFF;
 		iprintf("%02X\n",GFX_FOG_TABLE[i]);
-		myassert(GFX_FOG_TABLE[i] == 0x00,"test whether fog table entries are writeable");
+		myassert(GFX_FOG_TABLE[i] == 0x00,"test whether fog table entries are non-readable");
+	}
+
+	iprintf("waiting for arm7 test to finish!\n");
+
+	//wait for arm7 test to finish
+	for(;;) {
+		if(arm7comm->code != 0xDEADBEEF) break;
+		swiWaitForVBlank();
+	}
+
+	iprintf("arm7 finish code: %d\n",arm7comm->code);
+
+	if(arm7comm->code == 1)
+	{
+		iprintf("arm7 test failed!\n");
+		iprintf("%s\n",arm7comm->message);
+		iprintf("offending val: 0x%08X\n",arm7comm->offender);
+	}
+	else
+	{
+		iprintf("all tests OK");
 	}
 	
-	iprintf("all tests OK");
 	for(;;) swiWaitForVBlank();
 	return 0;
 }
---
+++ trunk/tools/ds_tests/regs/regstest.h
@@ -0,0 +1,8 @@
+typedef struct _arm7comm_t
+{
+	int code;
+	u32 offender;
+	char message[1024];
+} arm7comm_t;
+
+#define arm7comm ( (arm7comm_t*)0x02200000 )
