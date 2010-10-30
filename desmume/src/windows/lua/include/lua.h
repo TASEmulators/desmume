b69173134129f0dcbb9d03e8f7b6069f13fa5851
zeromus <zeromus@users.sf.net>
2010-10-30 21:15:50 +0000

    visual studio: clean up lua-related building mess

--- desmume/src/windows/lua/include/lua.h
+++ desmume/src/windows/lua/include/lua.h
@@ -108,7 +108,7 @@ typedef LUA_INTEGER lua_Integer;
 ** state manipulation
 */
 LUA_API lua_State *(lua_newstate) (lua_Alloc f, void *ud);
-LUA_API void       lua_close (lua_State *L);
+LUA_API void       (lua_close) (lua_State *L);
 LUA_API lua_State *(lua_newthread) (lua_State *L);
 
 LUA_API lua_CFunction (lua_atpanic) (lua_State *L, lua_CFunction panicf);
