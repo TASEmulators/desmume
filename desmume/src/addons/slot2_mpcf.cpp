dad016d6e5f871c37d32109425cc39c71f22d6ba
zeromus <zeromus@users.sf.net>
2011-07-26 22:04:27 +0000

    win32: change compactflash rompath to use the actual rom's path instead of the rom path setting configuration (that isnt what anybody wanted)

--- desmume/src/addons/slot2_mpcf.cpp
+++ desmume/src/addons/slot2_mpcf.cpp
@@ -1,22 +1,23 @@
-/*  Copyright (C) 2006 yopyop
-    Copyright (C) 2006 Mic
-    Copyright (C) 2009-2011 DeSmuME team
+/*
+	Copyright (C) 2006 yopyop
+	Copyright (C) 2006 Mic
+	Copyright (C) 2009-2011 DeSmuME team
 
-    This file is part of DeSmuME
+	This file is part of DeSmuME
 
-    DeSmuME is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	DeSmuME is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    DeSmuME is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	DeSmuME is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with DeSmuME; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+	You should have received a copy of the GNU General Public License
+	along with DeSmuME; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
 #include "../addons.h"
@@ -82,7 +83,7 @@ static BOOL cflash_init()
 
 	if (CFlash_Mode == ADDON_CFLASH_MODE_RomPath)
 	{
-		sFlashPath = path.pathToRoms;
+		sFlashPath = path.RomDirectory;
 		INFO("Using CFlash directory of rom: %s\n", sFlashPath.c_str());
 	}
 	else if(CFlash_Mode == ADDON_CFLASH_MODE_Path)
--- desmume/src/path.cpp
+++ desmume/src/path.cpp
@@ -1,4 +1,5 @@
-/*  Copyright 2009-2010 DeSmuME team
+/*
+	Copyright 2009-2011 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -19,6 +20,104 @@
 #include "path.h"
 #include <stdio.h>
 
+
+//-----------------------------------
+//This is taken from mono Path.cs
+static const char InvalidPathChars[] = {
+	'\x22', '\x3C', '\x3E', '\x7C', '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07',
+	'\x08', '\x09', '\x0A', '\x0B', '\x0C', '\x0D', '\x0E', '\x0F', '\x10', '\x11', '\x12', 
+	'\x13', '\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1A', '\x1B', '\x1C', '\x1D', 
+	'\x1E', '\x1F'
+};
+
+//but it is sort of windows-specific. Does it work in linux? Maybe we'll have to make it smarter
+static const char VolumeSeparatorChar = ':';
+static const char AltDirectorySeparatorChar = '/';
+static bool dirEqualsVolume = (DIRECTORY_DELIMITER_CHAR == VolumeSeparatorChar);
+
+
+bool Path::IsPathRooted (const std::string &path)
+{
+	if (path.empty()) {
+		return false;
+	}
+	
+	if (path.find_first_of(InvalidPathChars) != std::string::npos) {
+		return false;
+	}
+	
+	char c = path[0];
+	return (c == DIRECTORY_DELIMITER_CHAR 	||
+			c == AltDirectorySeparatorChar 	||
+			(!dirEqualsVolume && path.size() > 1 && path[1] == VolumeSeparatorChar));
+}
+
+std::string Path::GetFileDirectoryPath(std::string filePath)
+{
+	if (filePath.empty()) {
+		return "";
+	}
+	
+	size_t i = filePath.find_last_of(DIRECTORY_DELIMITER_CHAR);
+	if (i == std::string::npos) {
+		return filePath;
+	}
+	
+	return filePath.substr(0, i);
+}
+
+std::string Path::GetFileNameFromPath(std::string filePath)
+{
+	if (filePath.empty()) {
+		return "";
+	}
+	
+	size_t i = filePath.find_last_of(DIRECTORY_DELIMITER_CHAR);
+	if (i == std::string::npos) {
+		return filePath;
+	}
+	
+	return filePath.substr(i + 1);
+}
+
+std::string Path::GetFileNameWithoutExt(std::string fileName)
+{
+	if (fileName.empty()) {
+		return "";
+	}
+	
+	size_t i = fileName.find_last_of(FILE_EXT_DELIMITER_CHAR);
+	if (i == std::string::npos) {
+		return fileName;
+	}
+	
+	return fileName.substr(0, i);
+}
+
+std::string Path::GetFileNameFromPathWithoutExt(std::string filePath)
+{
+	if (filePath.empty()) {
+		return "";
+	}
+	
+	std::string fileName = GetFileNameFromPath(filePath);
+	
+	return GetFileNameWithoutExt(fileName);
+}
+
+std::string Path::GetFileExt(std::string fileName)
+{
+	if (fileName.empty()) {
+		return "";
+	}
+	
+	size_t i = fileName.find_last_of(FILE_EXT_DELIMITER_CHAR);
+	if (i == std::string::npos) {
+		return fileName;
+	}
+	
+	return fileName.substr(i + 1);
+}
 
 //-----------------------------------
 #ifdef _WINDOWS
