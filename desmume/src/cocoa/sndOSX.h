c02ffe1548adf00b4173ba54fdf23b6a2ffd13bc
rogerman <rogerman@users.sf.net>
2012-01-04 21:13:13 +0000

    Cocoa Port: - Update necessary files that I forgot in the last commit.

--- desmume/src/cocoa/sndOSX.h
+++ desmume/src/cocoa/sndOSX.h
@@ -1,48 +1,45 @@
-/*  Copyright 2007 Jeff Bland
+/*
+	Copyright (C) 2007 Jeff Bland
+	Copyright (C) 2007-2011 DeSmuME team
 
-    This file is part of DeSmuME.
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
 
-    DeSmuME  is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    DeSmuME is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DeSmuME; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "../SPU.h"
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #define SNDCORE_OSX 58325 //hopefully this is unique number
 
-//This is the sound inerface so the emulator core can send us sound info and whatnot
+
+// This is the sound interface so the emulator core can send us sound info and whatnot
 extern SoundInterface_struct SNDOSX;
 
-//Beyond this point are sound interface extensions specific to the mac port
-
-
+// Sound interface extensions for CoreAudio
+void SNDOSXStartup();
+void SNDOSXShutdown();
+int SNDOSXInit(int buffer_size);
+void SNDOSXDeInit();
 int SNDOSXReset();
+void SNDOSXUpdateAudio(s16 *buffer, u32 num_samples);
+u32 SNDOSXGetAudioSpace();
 void SNDOSXMuteAudio();
 void SNDOSXUnMuteAudio();
 void SNDOSXSetVolume(int volume);
+void SNDOSXClearBuffer();
 
-//Recording
+// Recording
+// Not supported as of 2011/12/28 - rogerman
 bool SNDOSXOpenFile(void *fname);  //opens a file for recording (if filename is the currently opened one, it will restart the file), fname is an NSString
 void SNDOSXStartRecording();      //begins recording to the currently open file if there is an open file
 void SNDOSXStopRecording();       //pauses recording (you can continue recording later)
 void SNDOSXCloseFile();           //closes the file, making sure it's saved
-
-#ifdef __cplusplus
-}
-#endif
