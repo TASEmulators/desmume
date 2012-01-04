719e210de6ea65382f0d96ab80688dff60942aca
rogerman <rogerman@users.sf.net>
2012-01-04 21:10:19 +0000

    Cocoa Port: - Major rewrite of the OS X Core Audio backend code! This brings performance improvements, bug fixes, and general code cleanup. - Remove all Objective-C code from sndOSX.mm, and change into a pure C++ file. - Fixed audio noise issues that intermittently occurred while the emulation was running. This most often occurred after a ROM has been loaded. - Fix possible crash on application exit.

---
+++ desmume/src/cocoa/cocoa_globals.h
@@ -0,0 +1,398 @@
+/*
+	Copyright (C) 2011 DeSmuME team
+
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define STRING_DESMUME_WEBSITE						"http://www.desmume.org"
+#define STRING_DESMUME_FORUM_SITE					"http://forums.desmume.org/index.php"
+#define STRING_DESMUME_BUG_SITE						"http://sourceforge.net/tracker/?group_id=164579&atid=832291"
+
+#define NSSTRING_TITLE_OPEN_ROM_PANEL				NSLocalizedString(@"Open ROM", nil)
+#define NSSTRING_TITLE_OPEN_STATE_FILE_PANEL		NSLocalizedString(@"Open State File", nil)
+#define NSSTRING_TITLE_SAVE_STATE_FILE_PANEL		NSLocalizedString(@"Save State File", nil)
+#define NSSTRING_TITLE_IMPORT_BACKUP_MEMORY_PANEL	NSLocalizedString(@"Import Backup Memory", nil)
+#define NSSTRING_TITLE_EXPORT_BACKUP_MEMORY_PANEL	NSLocalizedString(@"Export Backup Memory", nil)
+#define NSSTRING_TITLE_SELECT_ROM_PANEL				NSLocalizedString(@"Select ROM", nil)
+#define NSSTRING_TITLE_SELECT_ADVANSCENE_DB_PANEL	NSLocalizedString(@"Select ADVANsCEne Database", nil)
+#define NSSTRING_TITLE_SELECT_R4_CHEAT_DB_PANEL		NSLocalizedString(@"Select R4 Cheat Database", nil)
+#define NSSTRING_TITLE_SELECT_ARM7_IMAGE_PANEL		NSLocalizedString(@"Select ARM7 BIOS Image", nil)
+#define NSSTRING_TITLE_SELECT_ARM9_IMAGE_PANEL		NSLocalizedString(@"Select ARM9 BIOS Image", nil)
+#define NSSTRING_TITLE_SELECT_FIRMWARE_IMAGE_PANEL	NSLocalizedString(@"Select Firmware Image", nil)
+
+#define NSSTRING_TITLE_EXECUTE_CONTROL				NSLocalizedString(@"Execute", nil)
+#define NSSTRING_TITLE_PAUSE_CONTROL				NSLocalizedString(@"Pause", nil)
+#define NSSTRING_TITLE_DISABLE_SPEED_LIMIT			NSLocalizedString(@"Disable Speed Limit", nil)
+#define NSSTRING_TITLE_ENABLE_SPEED_LIMIT			NSLocalizedString(@"Enable Speed Limit", nil)
+#define NSSTRING_TITLE_DISABLE_AUTO_FRAME_SKIP		NSLocalizedString(@"Disable Auto Frame Skip", nil)
+#define NSSTRING_TITLE_ENABLE_AUTO_FRAME_SKIP		NSLocalizedString(@"Enable Auto Frame Skip", nil)
+#define NSSTRING_TITLE_DISABLE_CHEATS				NSLocalizedString(@"Disable Cheats", nil)
+#define NSSTRING_TITLE_ENABLE_CHEATS				NSLocalizedString(@"Enable Cheats", nil)
+#define NSSTRING_TITLE_DISABLE_HUD					NSLocalizedString(@"Disable HUD", nil)
+#define NSSTRING_TITLE_ENABLE_HUD					NSLocalizedString(@"Enable HUD", nil)
+#define NSSTRING_TITLE_SPEED_1X						NSLocalizedString(@"Speed 1x", nil)
+#define NSSTRING_TITLE_SPEED_2X						NSLocalizedString(@"Speed 2x", nil)
+#define NSSTRING_TITLE_SLOT_NUMBER					NSLocalizedString(@"Slot %d", nil)
+
+#define NSSTRING_ALERT_CRITICAL_FILE_MISSING_PRI	NSLocalizedString(@"A critical file is missing. DeSmuME will now quit.", nil)
+#define NSSTRING_ALERT_CRITICAL_FILE_MISSING_SEC	NSLocalizedString(@"The file \"DefaultUserPrefs.plist\" is missing. Please reinstall DeSmuME.", nil)
+
+#define NSSTRING_STATUS_READY						NSLocalizedString(@"Ready.", nil)
+#define NSSTRING_STATUS_SAVESTATE_LOADING_FAILED	NSLocalizedString(@"Save state file loading failed!", nil)
+#define NSSTRING_STATUS_SAVESTATE_LOADED			NSLocalizedString(@"Save state file loaded.", nil)
+#define NSSTRING_STATUS_SAVESTATE_SAVING_FAILED		NSLocalizedString(@"Save state file saving failed!", nil)
+#define NSSTRING_STATUS_SAVESTATE_SAVED				NSLocalizedString(@"Save state file saved.", nil)
+#define NSSTRING_STATUS_SAVESTATE_REVERTING_FAILED	NSLocalizedString(@"Save state file reverting failed!", nil)
+#define NSSTRING_STATUS_SAVESTATE_REVERTED			NSLocalizedString(@"Save state file reverted.", nil)
+#define NSSTRING_STATUS_BATTERY_IMPORT_FAILED		NSLocalizedString(@"Battery file import failed!", nil)
+#define NSSTRING_STATUS_BATTERY_IMPORTED			NSLocalizedString(@"Battery save file imported.", nil)
+#define NSSTRING_STATUS_BATTERY_EXPORT_FAILED		NSLocalizedString(@"Battery file export failed!", nil)
+#define NSSTRING_STATUS_BATTERY_EXPORTED			NSLocalizedString(@"Battery save file exported.", nil)
+#define NSSTRING_STATUS_ROM_LOADING					NSLocalizedString(@"Loading ROM...", nil)
+#define NSSTRING_STATUS_ROM_LOADING_FAILED			NSLocalizedString(@"ROM loading failed!", nil)
+#define NSSTRING_STATUS_ROM_LOADED					NSLocalizedString(@"ROM loaded.", nil)
+#define NSSTRING_STATUS_ROM_UNLOADING				NSLocalizedString(@"Unloading ROM...", nil)
+#define NSSTRING_STATUS_ROM_UNLOADED				NSLocalizedString(@"ROM unloaded.", nil)
+#define NSSTRING_STATUS_EMULATOR_RESETTING			NSLocalizedString(@"Emulator resetting...", nil)
+#define NSSTRING_STATUS_EMULATOR_RESET				NSLocalizedString(@"Emulator reset.", nil)
+#define NSSTRING_STATUS_CANNOT_GENERATE_SAVE_PATH	NSLocalizedString(@"Cannot generate the save file path!", nil)
+#define NSSTRING_STATUS_CANNOT_CREATE_SAVE_DIRECTORY	NSLocalizedString(@"Cannot create the save directory!", nil)
+
+#define NSSTRING_STATUS_SPEED_LIMIT_DISABLED		NSLocalizedString(@"Speed limit disabled.", nil)
+#define NSSTRING_STATUS_SPEED_LIMIT_ENABLED			NSLocalizedString(@"Speed limit enabled.", nil)
+#define NSSTRING_STATUS_AUTO_FRAME_SKIP_DISABLED	NSLocalizedString(@"Auto frame skip disabled.", nil)
+#define NSSTRING_STATUS_AUTO_FRAME_SKIP_ENABLED		NSLocalizedString(@"Auto frame skip enabled.", nil)
+#define NSSTRING_STATUS_CHEATS_DISABLED				NSLocalizedString(@"Cheats disabled.", nil)
+#define NSSTRING_STATUS_CHEATS_ENABLED				NSLocalizedString(@"Cheats enabled.", nil)
+#define NSSTRING_STATUS_HUD_DISABLED				NSLocalizedString(@"HUD disabled.", nil)
+#define NSSTRING_STATUS_HUD_ENABLED					NSLocalizedString(@"HUD enabled.", nil)
+#define NSSTRING_STATUS_VOLUME						NSLocalizedString(@"Volume: %1.1f%%", nil)
+#define NSSTRING_STATUS_NO_ROM_LOADED				NSLocalizedString(@"No ROM loaded.", nil)
+#define NSSTRING_STATUS_SIZE_BYTES					NSLocalizedString(@"%i bytes", nil)
+
+#define NSSTRING_DISPLAYMODE_MAIN					NSLocalizedString(@"Main", nil)
+#define NSSTRING_DISPLAYMODE_TOUCH					NSLocalizedString(@"Touch", nil)
+#define NSSTRING_DISPLAYMODE_COMBO					NSLocalizedString(@"Combo", nil)
+
+#define PATH_CONFIG_DIRECTORY_0_9_6					"~/.config/desmume"
+
+#define FILE_EXT_FIRMWARE_CONFIG					"dfc"
+#define FILE_EXT_SAVE_STATE							"dst"
+#define FILE_EXT_ROM_SAVE							"dsv"
+#define FILE_EXT_CHEAT								"dct"
+#define FILE_EXT_ROM_SAVE_NOGBA						"sav"
+#define FILE_EXT_ACTION_REPLAY_SAVE					"duc"
+#define FILE_EXT_ROM_DS								"nds"
+#define FILE_EXT_ROM_GBA							"ds.gba"
+#define FILE_EXT_HW_IMAGE_FILE						"bin"
+#define FILE_EXT_ADVANSCENE_DB						"xml"
+#define FILE_EXT_R4_CHEAT_DB						"dat"
+
+#define MAX_SAVESTATE_SLOTS							10
+
+#define MAX_VOLUME									100.0f
+#define MAX_BRIGHTNESS								100.0f
+
+#define CHEAT_DESCRIPTION_LENGTH					1024
+
+#define GPU_DISPLAY_WIDTH							256
+#define GPU_DISPLAY_HEIGHT							192
+#define GPU_DISPLAY_COLOR_DEPTH						sizeof(UInt16)
+#define GPU_SCREEN_SIZE_BYTES						(GPU_DISPLAY_WIDTH * GPU_DISPLAY_HEIGHT * GPU_DISPLAY_COLOR_DEPTH) // The numbers are: 256px width, 192px height, 16bit color depth
+
+#define WINDOW_STATUS_BAR_HEIGHT					24		// Height of an emulation window status bar in pixels.
+
+#define SPEED_SCALAR_HALF							0.5		// Speed scalar for half execution speed.
+#define SPEED_SCALAR_NORMAL							1.0		// Speed scalar for normal execution speed.
+#define SPEED_SCALAR_DOUBLE							2.0		// Speed scalar for double execution speed.
+#define SPEED_SCALAR_MIN							0.005	// Lower limit for the speed multiplier.
+
+#define DS_FRAMES_PER_SECOND						59.73	// Number of DS frames per second.
+#define DS_SECONDS_PER_FRAME						(1.0 / DS_FRAMES_PER_SECOND) // The length of time in seconds that, ideally, a frame should be processed within.
+
+#define FRAME_SKIP_AGGRESSIVENESS					2.0
+#define FRAME_SKIP_BIAS								0.5
+#define MAX_FRAME_SKIP								(DS_FRAMES_PER_SECOND * 2.0)
+
+#define SPU_SAMPLE_RATE								44100	// Samples per second
+#define SPU_SAMPLE_RESOLUTION						16		// Bits per sample; must be a multiple of 8
+#define SPU_STEREO_SAMPLE_SIZE						((SPU_SAMPLE_RESOLUTION / 8) * 2) // Bytes per sample, multiply by 2 for stereo
+#define SPU_BUFFER_BYTES							((SPU_SAMPLE_RATE / DS_FRAMES_PER_SECOND) * SPU_STEREO_SAMPLE_SIZE)
+
+#define CLOCKWISE_DEGREES(x)						(360.0 - x) // Converts an angle in degrees from normal-direction to clockwise-direction.
+
+#define VOLUME_THRESHOLD_LOW						35.0f
+#define VOLUME_THRESHOLD_HIGH						75.0f
+
+#define ROM_ICON_WIDTH								32
+#define ROM_ICON_HEIGHT								32
+
+#define ROMINFO_GAME_TITLE_LENGTH					12
+#define ROMINFO_GAME_CODE_LENGTH					4
+#define ROMINFO_GAME_BANNER_LENGTH					128
+
+#define MIC_NULL_SAMPLE_VALUE						0
+#define MIC_SAMPLE_RATE								16000
+#define MIC_MAX_BUFFER_SAMPLES						320
+#define MIC_BUFFER_SIZE								(sizeof(UInt8) * MIC_MAX_BUFFER_SAMPLES)
+
+#define COCOA_DIALOG_CANCEL							0
+#define COCOA_DIALOG_DEFAULT						1
+#define COCOA_DIALOG_OK								1
+#define COCOA_DIALOG_OPTION							2
+
+enum
+{
+	ROMAUTOLOADOPTION_LOAD_LAST						= 0,
+	ROMAUTOLOADOPTION_LOAD_SELECTED					= 1
+};
+
+enum
+{
+	REASONFORCLOSE_NORMAL = 0,
+	REASONFORCLOSE_OPEN,
+	REASONFORCLOSE_TERMINATE
+};
+
+enum
+{
+	EMULATION_ENSATA_BIT							= 0,
+	EMULATION_ADVANCED_BUS_LEVEL_TIMING_BIT			= 1,
+	EMULATION_USE_EXTERNAL_BIOS_BIT					= 2,
+	EMULATION_BIOS_SWI_BIT							= 3,
+	EMULATION_PATCH_DELAY_LOOP_BIT					= 4,
+	EMULATION_USE_EXTERNAL_FIRMWARE_BIT				= 5,
+	EMULATION_BOOT_FROM_FIRMWARE_BIT				= 6,
+	EMULATION_SLEEP_BIT								= 7,
+	EMULATION_CARD_EJECT_BIT						= 8,
+	EMULATION_DEBUG_CONSOLE_BIT						= 9
+};
+
+enum
+{
+	EMULATION_ENSATA_MASK							= 1 << EMULATION_ENSATA_BIT,
+	EMULATION_ADVANCED_BUS_LEVEL_TIMING_MASK		= 1 << EMULATION_ADVANCED_BUS_LEVEL_TIMING_BIT,
+	EMULATION_USE_EXTERNAL_BIOS_MASK				= 1 << EMULATION_USE_EXTERNAL_BIOS_BIT,
+	EMULATION_BIOS_SWI_MASK							= 1 << EMULATION_BIOS_SWI_BIT,
+	EMULATION_PATCH_DELAY_LOOP_MASK					= 1 << EMULATION_PATCH_DELAY_LOOP_BIT,
+	EMULATION_USE_EXTERNAL_FIRMWARE_MASK			= 1 << EMULATION_USE_EXTERNAL_FIRMWARE_BIT,
+	EMULATION_BOOT_FROM_FIRMWARE_MASK				= 1 << EMULATION_BOOT_FROM_FIRMWARE_BIT,
+	EMULATION_SLEEP_MASK							= 1 << EMULATION_SLEEP_BIT,
+	EMULATION_CARD_EJECT_MASK						= 1 << EMULATION_CARD_EJECT_BIT,
+	EMULATION_DEBUG_CONSOLE_MASK					= 1 << EMULATION_DEBUG_CONSOLE_BIT
+};
+
+enum
+{
+	GPUSTATE_MAIN_GPU_BIT							= 0,
+	GPUSTATE_MAIN_BG0_BIT							= 1,
+	GPUSTATE_MAIN_BG1_BIT							= 2,
+	GPUSTATE_MAIN_BG2_BIT							= 3,
+	GPUSTATE_MAIN_BG3_BIT							= 4,
+	GPUSTATE_MAIN_OBJ_BIT							= 5,
+	GPUSTATE_SUB_GPU_BIT							= 6,
+	GPUSTATE_SUB_BG0_BIT							= 7,
+	GPUSTATE_SUB_BG1_BIT							= 8,
+	GPUSTATE_SUB_BG2_BIT							= 9,
+	GPUSTATE_SUB_BG3_BIT							= 10,
+	GPUSTATE_SUB_OBJ_BIT							= 11
+};
+
+enum
+{
+	GPUSTATE_MAIN_GPU_MASK							= 1 << GPUSTATE_MAIN_GPU_BIT,
+	GPUSTATE_MAIN_BG0_MASK							= 1 << GPUSTATE_MAIN_BG0_BIT,
+	GPUSTATE_MAIN_BG1_MASK							= 1 << GPUSTATE_MAIN_BG1_BIT,
+	GPUSTATE_MAIN_BG2_MASK							= 1 << GPUSTATE_MAIN_BG2_BIT,
+	GPUSTATE_MAIN_BG3_MASK							= 1 << GPUSTATE_MAIN_BG3_BIT,
+	GPUSTATE_MAIN_OBJ_MASK							= 1 << GPUSTATE_MAIN_OBJ_BIT,
+	GPUSTATE_SUB_GPU_MASK							= 1 << GPUSTATE_SUB_GPU_BIT,
+	GPUSTATE_SUB_BG0_MASK							= 1 << GPUSTATE_SUB_BG0_BIT,
+	GPUSTATE_SUB_BG1_MASK							= 1 << GPUSTATE_SUB_BG1_BIT,
+	GPUSTATE_SUB_BG2_MASK							= 1 << GPUSTATE_SUB_BG2_BIT,
+	GPUSTATE_SUB_BG3_MASK							= 1 << GPUSTATE_SUB_BG3_BIT,
+	GPUSTATE_SUB_OBJ_MASK							= 1 << GPUSTATE_SUB_OBJ_BIT
+};
+
+enum
+{
+	SPU_SYNC_MODE_DUAL_SYNC_ASYNC					= 0,
+	SPU_SYNC_MODE_SYNCHRONOUS						= 1
+};
+
+enum
+{
+	SPU_SYNC_METHOD_N								= 0,
+	SPU_SYNC_METHOD_Z								= 1,
+	SPU_SYNC_METHOD_P								= 2
+};
+
+enum
+{
+	CHEAT_TYPE_INTERNAL								= 0,
+	CHEAT_TYPE_ACTION_REPLAY						= 1,
+	CHEAT_TYPE_CODE_BREAKER							= 2
+};
+
+enum
+{
+	CHEATSEARCH_SEARCHSTYLE_EXACT_VALUE				= 0,
+	CHEATSEARCH_SEARCHSTYLE_COMPARATIVE				= 1
+};
+
+enum
+{
+	CHEATSEARCH_COMPARETYPE_GREATER_THAN			= 0,
+	CHEATSEARCH_COMPARETYPE_LESSER_THAN				= 1,
+	CHEATSEARCH_COMPARETYPE_EQUALS_TO				= 2,
+	CHEATSEARCH_COMPARETYPE_NOT_EQUALS_TO			= 3
+};
+
+enum
+{
+	CHEATSEARCH_UNSIGNED							= 0,
+	CHEATSEARCH_SIGNED								= 1
+};
+
+enum
+{
+	CHEATEXPORT_ERROR_FILE_NOT_FOUND				= 1,
+	CHEATEXPORT_ERROR_WRONG_FILE_FORMAT				= 2,
+	CHEATEXPORT_ERROR_SERIAL_NOT_FOUND				= 3,
+	CHEATEXPORT_ERROR_EXPORT_FAILED					= 4
+};
+
+/*
+ PORT MESSAGES
+ */
+enum
+{
+	MESSAGE_CHECK_FOR_RESPONSE = 100,		// Message to check if a port is responding. Usually sent to make sure that a thread is alive.
+	MESSAGE_CHECK_RESPONSE_ECHO,			// Response message when another port sends MESSAGE_CHECK_FOR_RESPONSE. Sent to confirm that a thread is indeed alive.
+	MESSAGE_EXIT_THREAD,					// Sent whenever there is a need to stop a thread.
+	
+	MESSAGE_EMU_FRAME_PROCESSED = 1000,		// Sent whenever an emulation frame is fully processed. This is mostly used to signal outputs to update themselves based on the new emulation frame.
+	MESSAGE_OUTPUT_FINISHED_FRAME,			// Sent from an output device whenever it finishes processing the frame data.
+	MESSAGE_SET_EMULATION_FLAGS,
+	
+	// Video Messages
+	MESSAGE_RESIZE_VIEW,
+	MESSAGE_REDRAW_VIEW,
+	MESSAGE_SET_GPU_STATE_FLAGS,
+	MESSAGE_CHANGE_DISPLAY_TYPE,
+	MESSAGE_CHANGE_BILINEAR_OUTPUT,
+	MESSAGE_CHANGE_VIDEO_FILTER,
+	MESSAGE_SET_RENDER3D_METHOD,
+	MESSAGE_SET_RENDER3D_HIGH_PRECISION_COLOR_INTERPOLATION,
+	MESSAGE_SET_RENDER3D_EDGE_MARKING,
+	MESSAGE_SET_RENDER3D_FOG,
+	MESSAGE_SET_RENDER3D_TEXTURES,
+	MESSAGE_SET_RENDER3D_DEPTH_COMPARISON_THRESHOLD,
+	MESSAGE_SET_RENDER3D_THREADS,
+	MESSAGE_SET_RENDER3D_LINE_HACK,
+	MESSAGE_SET_VIEW_TO_BLACK,
+	MESSAGE_SET_VIEW_TO_WHITE,
+	
+	MESSAGE_SET_AUDIO_PROCESS_METHOD,
+	MESSAGE_SET_SPU_ADVANCED_LOGIC,
+	MESSAGE_SET_SPU_SYNC_MODE,
+	MESSAGE_SET_SPU_SYNC_METHOD,
+	MESSAGE_SET_SPU_INTERPOLATION_MODE,
+	MESSAGE_SET_VOLUME,
+	
+	MESSAGE_COPY_TO_PASTEBOARD
+};
+
+/*
+ DS DISPLAY TYPES
+ */
+enum
+{
+	DS_DISPLAY_TYPE_MAIN = 0,
+	DS_DISPLAY_TYPE_TOUCH,
+	DS_DISPLAY_TYPE_COMBO
+};
+
+/*
+ DS GPU TYPES
+ */
+enum
+{
+	DS_GPU_TYPE_MAIN = 0,
+	DS_GPU_TYPE_SUB,
+	DS_GPU_TYPE_COMBO
+};
+
+/*
+ COCOA DS CORE STATES
+ */
+enum
+{
+	CORESTATE_EXECUTE = 0,
+	CORESTATE_PAUSE
+};
+
+/*
+ DESMUME 3D RENDERER TYPES
+ */
+enum
+{
+	CORE3DLIST_NULL = 0,
+	CORE3DLIST_SWRASTERIZE,
+	CORE3DLIST_OPENGL
+};
+
+/*
+ MICROPHONE MODE
+ */
+enum
+{
+	MICMODE_NONE = 0,
+	MICMODE_INTERNAL_NOISE,
+	MICMODE_SOUND_FILE,
+	MICMODE_WHITE_NOISE,
+	MICMODE_PHYSICAL
+};
+
+/*
+ CONTROL TAGS FOR PREFERENCE WINDOW INPUT CONFIG BUTTONS
+ */
+enum
+{
+	PREF_INPUT_BUTTON_UP = 1000,
+	PREF_INPUT_BUTTON_DOWN,
+	PREF_INPUT_BUTTON_LEFT,
+	PREF_INPUT_BUTTON_RIGHT,
+	PREF_INPUT_BUTTON_A,
+	PREF_INPUT_BUTTON_B,
+	PREF_INPUT_BUTTON_X,
+	PREF_INPUT_BUTTON_Y,
+	PREF_INPUT_BUTTON_L,
+	PREF_INPUT_BUTTON_R,
+	PREF_INPUT_BUTTON_START,
+	PREF_INPUT_BUTTON_SELECT,
+	
+	PREF_INPUT_BUTTON_SIM_MIC = 2000,
+	
+	PREF_INPUT_BUTTON_LID = 3000,
+	PREF_INPUT_BUTTON_DEBUG,
+	PREF_INPUT_BUTTON_SPEED_HALF,
+	PREF_INPUT_BUTTON_SPEED_DOUBLE,
+	PREF_INPUT_BUTTON_TOGGLE_HUD,
+	PREF_INPUT_BUTTON_EXECUTE,
+	PREF_INPUT_BUTTON_PAUSE,
+	PREF_INPUT_BUTTON_RESET,
+	
+	PREF_INPUT_BUTTON_TOUCH = 4000
+};
