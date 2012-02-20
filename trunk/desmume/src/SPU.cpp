0270bca87cb9ec3f789429dcdfc7fb735b52539c
zeromus <zeromus@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2012-02-20 19:35:44 +0000

    just a tiny bit of cleanup

--- trunk/desmume/src/SPU.cpp
+++ trunk/desmume/src/SPU.cpp
@@ -1,26 +1,24 @@
-/*  SPU.cpp
-    Copyright (C) 2006 yopyop
-    Copyright (C) 2006 Theo Berkau
-    Copyright (C) 2008-2012 DeSmuME team
+/*
+	Copyright (C) 2006 yopyop
+	Copyright (C) 2006 Theo Berkau
+	Copyright (C) 2008-2012 DeSmuME team
 
-    Ideas borrowed from Stephane Dallongeville's SCSP core
+	Ideas borrowed from Stephane Dallongeville's SCSP core
 
-    This file is part of DeSmuME
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
 
-    DeSmuME is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    DeSmuME is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DeSmuME; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 #define _USE_MATH_DEFINES
 #include <math.h>
