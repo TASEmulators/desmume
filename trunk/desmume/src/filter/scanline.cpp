1562eb3aba8a6ad8e806d559d1e3b70698d978c9
zeromus <zeromus@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2012-07-12 05:49:13 +0000

    tweak scanline filter to be more versatile

--- trunk/desmume/src/filter/scanline.cpp
+++ trunk/desmume/src/filter/scanline.cpp
@@ -24,56 +24,28 @@
 
 typedef u64 uint64;
 
-extern CACHE_ALIGN u16 fadeOutColors[17][0x8000];
+extern int scanline_filter_a, scanline_filter_b, scanline_filter_c, scanline_filter_d;
+static int fac_a, fac_b, fac_c, fac_d;
 
-extern int scanline_filter_a, scanline_filter_b;
-static int fac_a, fac_b;
-
-FORCEINLINE void ScanLine16( uint16 *lpDst, uint16 *lpSrc, unsigned int Width){
-	while(Width--){
-		*lpDst++ = *lpSrc;
-		*lpDst++ = fadeOutColors[scanline_filter_a][(*lpSrc++)];
-	}
-}
-
-FORCEINLINE void ScanLine16_2( uint16 *lpDst, uint16 *lpSrc, unsigned int Width){
-	while(Width--){
-		*lpDst++ = fadeOutColors[scanline_filter_a][(*lpSrc)];
-		*lpDst++ = fadeOutColors[scanline_filter_b][(*lpSrc++)];
-	}
-}
-
-FORCEINLINE void ScanLine32( uint32 *lpDst, uint32 *lpSrc, unsigned int Width){
-	while(Width--){
-		*lpDst++ = *lpSrc;
+FORCEINLINE void ScanLine32( uint32 *lpDst, uint32 *lpSrc, unsigned int Width, int fac_left, int fac_right)
+{
+	while(Width--)
+	{
 		u8* u8dst = (u8*)lpDst;
 		u8* u8src = (u8*)lpSrc;
-		*u8dst++ = *u8src++ * fac_a / 16;
-		*u8dst++ = *u8src++ * fac_a / 16;
-		*u8dst++ = *u8src++ * fac_a / 16;
-		lpDst++; 
-		lpSrc++;
-	}
-}
-
-FORCEINLINE void ScanLine32_2( uint32 *lpDst, uint32 *lpSrc, unsigned int Width){
-	while(Width--){
-		u8* u8dst = (u8*)lpDst;
-		u8* u8src = (u8*)lpSrc;
-		*u8dst++ = *u8src++ * fac_a / 16;
-		*u8dst++ = *u8src++ * fac_a / 16;
-		*u8dst++ = *u8src++ * fac_a / 16;
+		*u8dst++ = *u8src++ * fac_left / 16;
+		*u8dst++ = *u8src++ * fac_left / 16;
+		*u8dst++ = *u8src++ * fac_left / 16;
 		u8dst++;
 		u8src = (u8*)lpSrc;
-		*u8dst++ = *u8src++ * fac_b / 16;
-		*u8dst++ = *u8src++ * fac_b / 16;
-		*u8dst++ = *u8src++ * fac_b / 16;
+		*u8dst++ = *u8src++ * fac_right / 16;
+		*u8dst++ = *u8src++ * fac_right / 16;
+		*u8dst++ = *u8src++ * fac_right / 16;
 		u8dst++; u8src++;
 		lpDst+=2; 
 		lpSrc++;
 	}
 }
-
 
 FORCEINLINE void DoubleLine32( uint32 *lpDst, uint32 *lpSrc, unsigned int Width){
 	while(Width--){
@@ -86,6 +58,8 @@ void RenderScanline( SSurface Src, SSurface Dst)
 {
 	fac_a = (16-scanline_filter_a);
 	fac_b = (16-scanline_filter_b);
+	fac_c = (16-scanline_filter_c);
+	fac_d = (16-scanline_filter_d);
 	unsigned int H;
 
 	const uint32 srcHeight = Src.Height;
@@ -96,9 +70,12 @@ void RenderScanline( SSurface Src, SSurface Dst)
 	const unsigned int dstPitch = Dst.Pitch >> 1;
 	u32 *lpDst = (u32*)Dst.Surface;
 	for (H = 0; H < srcHeight; H++, lpSrc += srcPitch)
-		ScanLine32 (lpDst, lpSrc, Src.Width), lpDst += dstPitch,
-		ScanLine32_2 (lpDst, lpSrc, Src.Width), lpDst += dstPitch;
-		//memset (lpDst, 0, 512*2), lpDst += dstPitch;
+	{
+		ScanLine32(lpDst, lpSrc, Src.Width, fac_a, fac_b);
+		lpDst += dstPitch;
+		ScanLine32(lpDst, lpSrc, Src.Width, fac_c, fac_d);
+		lpDst += dstPitch;
+	}
 }
 
 void RenderNearest2X (SSurface Src, SSurface Dst)
