TASEmulators/desmume

commit 1f909eae7634cf9a30b1c9e65f94f2deeda1c839

winport: - disable render screen when emulation is paused; - add loading screen;
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@4643 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/windows/main.cpp
@@ -1898,8 +1898,9 @@ static void DoDisplay(bool firstTime)
 	}
 }
 
-void displayProc()
+void displayProc(bool force = false)
 {
+	if (!force && !execute) return;
 	g_mutex_lock(display_mutex);
 
 	//find a buffer to display
@@ -2377,6 +2378,10 @@ static BOOL LoadROM(const char * filename, const char * physicalName, const char
 	Pause();
 	//if (strcmp(filename,"")!=0) INFO("Attempting to load ROM: %s\n",filename);
 
+	video.clear();
+	osd->addFixed(100, 100, "Loading ROM...");
+	osd->addFixed(100, 120, "Please, wait...");
+	displayProc(true);
 	if (NDS_LoadROM(filename, physicalName, logicalName) > 0)
 	{
 		INFO("Loading %s was successful\n",logicalName);
@@ -3936,7 +3941,7 @@ void CloseRom()
 	// (TODO: maybe NDS_Reset should do this?)
 	memset(GPU_screen, 0xFF, sizeof(GPU_screen));
 
-	InvalidateRect(MainWindow->getHWnd(), NULL, FALSE); // make sure the window refreshes with the cleared screen
+	InvalidateRect(MainWindow->getHWnd(), NULL, TRUE); // make sure the window refreshes with the cleared screen
 
 	MainWindowToolbar->EnableButton(IDM_PAUSE, false);
 	MainWindowToolbar->EnableButton(IDM_CLOSEROM, false);
@@ -4867,6 +4872,7 @@ DOKEYDOWN:
 			}
 			break;
 		}
+		displayProc(true);
 		return 0;
 	case WM_PAINT:
 		{

desmume/src/windows/video.h
@@ -60,6 +60,15 @@ public:
 		NUM_FILTERS,
 	};
 
+	void clear()
+	{
+		if (srcBuffer)
+		{
+			memset(srcBuffer, 0xFF, size() * 2);
+		}
+		memset(&buffer[0], 0, sizeof(buffer));
+		memset(&filteredbuffer[0], 0, sizeof(filteredbuffer));
+	}
 
 	void reset() {
 		width = 256;

commit 01756f15f444dd48d4e220b1cbc5be0be33870a3

winport: - fix app. crash on some systems introduced in r4643;

desmume/src/windows/main.cpp
@@ -292,6 +292,7 @@ bool bSocketsAvailable = false;
 #endif
 
 VideoInfo video;
+bool bRefreshDisplay = false;
 
 #ifdef HAVE_WX
 #include "wx/wxprec.h"
@@ -1898,9 +1899,11 @@ static void DoDisplay(bool firstTime)
 	}
 }
 
-void displayProc(bool force = false)
+void displayProc()
 {
-	if (!force && !execute) return;
+	if (!bRefreshDisplay && !execute) return;
+	bRefreshDisplay = false;
+
 	g_mutex_lock(display_mutex);
 
 	//find a buffer to display
@@ -2384,7 +2387,8 @@ static BOOL LoadROM(const char * filename, const char * physicalName, const char
 	osd->addFixed(90, 100, "Please, wait...");
 	osd->addFixed(90, 192 + 80,  "Loading ROM.");
 	osd->addFixed(90, 192 + 100, "Please, wait...");
-	displayProc(true);
+	bRefreshDisplay = true;
+	Display();
 	if (NDS_LoadROM(filename, physicalName, logicalName) > 0)
 	{
 		INFO("Loading %s was successful\n",logicalName);
@@ -3950,6 +3954,7 @@ void CloseRom()
 	MainWindowToolbar->EnableButton(IDM_CLOSEROM, false);
 	MainWindowToolbar->EnableButton(IDM_RESET, false);
 	MainWindowToolbar->ChangeButtonBitmap(IDM_PAUSE, IDB_PLAY);
+	bRefreshDisplay = false;
 }
 
 int GetInitialModifiers(int key) // async version for input thread
@@ -4875,7 +4880,7 @@ DOKEYDOWN:
 			}
 			break;
 		}
-		displayProc(true);
+		bRefreshDisplay = true;
 		return 0;
 	case WM_PAINT:
 		{