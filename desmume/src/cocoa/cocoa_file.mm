TASEmulators/desmume

commit 20913ba9acf92b79b709ef5eb65479735c39b2bf

- moved import/export backup code from NDSystem into MC; - moved write BMP/PNG code from NDSSystem into common; - removed old GC log;
git-svn-id: https://svn.code.sf.net/p/desmume/code/trunk@4843 5577840e-afb3-4e59-9ef3-2b7f898b1952

desmume/src/cocoa/cocoa_file.mm
@@ -22,6 +22,7 @@
 #import "cocoa_util.h"
 
 #include "../NDSSystem.h"
+#include "../MMU.h"
 #include "../path.h"
 #include "../saves.h"
 #undef BOOL
@@ -194,7 +195,7 @@ static NSMutableDictionary *_gURLDictionary = nil;
 	BOOL result = NO;
 	const char *romSavePath = [[romSaveURL path] cStringUsingEncoding:NSUTF8StringEncoding];
 	
-	NSInteger resultCode = NDS_ImportSave(romSavePath);
+	NSInteger resultCode = MMU_new.backupDevice.importData(romSavePath);
 	if (resultCode == 0)
 	{
 		return result;
@@ -224,7 +225,7 @@ static NSMutableDictionary *_gURLDictionary = nil;
 		case ROMSAVEFORMAT_NOGBA:
 		{
 			const char *destinationPath = [[[destinationURL path] stringByAppendingPathExtension:@FILE_EXT_ROM_SAVE_NOGBA] cStringUsingEncoding:NSUTF8StringEncoding];
-			bool resultCode = NDS_ExportSave(destinationPath);
+			bool resultCode = MMU_new.backupDevice.exportData(destinationPath);
 			if (resultCode)
 			{
 				result = YES;
@@ -235,7 +236,7 @@ static NSMutableDictionary *_gURLDictionary = nil;
 		case ROMSAVEFORMAT_RAW:
 		{
 			const char *destinationPath = [[[destinationURL path] stringByAppendingPathExtension:@FILE_EXT_ROM_SAVE_RAW] cStringUsingEncoding:NSUTF8StringEncoding];
-			bool resultCode = NDS_ExportSave(destinationPath);
+			bool resultCode = MMU_new.backupDevice.exportData(destinationPath);
 			if (resultCode)
 			{
 				result = YES;

desmume/src/MMU.cpp
@@ -1263,29 +1263,12 @@ void MMU_GC_endTransfer(u32 PROCNUM)
 	// if needed, throw irq for the end of transfer
 	if(MMU.AUX_SPI_CNT & 0x4000)
 		NDS_makeIrq(PROCNUM, IRQ_BIT_GC_TRANSFER_COMPLETE);
-
-#ifdef _LOG_NEW_BOOT
-	{
-		extern FILE *fp_dis7;
-
-		if (fp_dis7)
-			fprintf(fp_dis7, "ARM%c: ctrl %08X - stop transfer !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", PROCNUM?'7':'9', cnt);
-		printf("ARM%c: ctrl %08X - stop transfer !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", PROCNUM?'7':'9', cnt);
-	}
-#endif
 }
 
 
 template<int PROCNUM>
 void FASTCALL MMU_writeToGCControl(u32 val)
 {
-#ifdef _LOG_NEW_BOOT
-	extern bool dolog;
-	extern FILE *fp_dis9;
-	extern FILE *fp_dis7;
-
-	u32 oldCnt = T1ReadLong(MMU.MMU_MEM[PROCNUM][0x40], 0x1A4);
-#endif
 
 	int dbsize = (val>>24)&7;
 	static int gcctr=0;
@@ -4785,9 +4768,6 @@ void FASTCALL _MMU_ARM7_write08(u32 adr, u8 val)
 			case REG_POSTFLG:
 				//printf("ARM7: write POSTFLG %02X PC:0x%08X\n", val, NDS_ARM7.instruct_adr);
 
-#ifdef _LOG_NEW_BOOT
-				printf("ARM7: write POSTFLG %02X PC:0x%08X\n", val, NDS_ARM7.instruct_adr);
-#endif
 				//The NDS7 register can be written to only from code executed in BIOS.
 				if (NDS_ARM7.instruct_adr > 0x3FFF) return;
 #ifdef HAVE_JIT

commit 4700d63db86249279f4740e0b0d4bd9fa39be6ef

Cocoa Port: - Add slot2_passme.cpp to all projects. - Fix compiling. (Regression from r4843.)

desmume/src/cocoa/DeSmuME (Legacy).xcodeproj/project.pbxproj
@@ -324,6 +324,13 @@
 		AB0E770414C5635800EF460C /* InfoPlist.strings in Resources */ = {isa = PBXBuildFile; fileRef = ABEB321914C56182007F0D4C /* InfoPlist.strings */; };
 		AB0E770514C5635900EF460C /* InfoPlist.strings in Resources */ = {isa = PBXBuildFile; fileRef = ABEB321914C56182007F0D4C /* InfoPlist.strings */; };
 		AB0E770614C5635900EF460C /* InfoPlist.strings in Resources */ = {isa = PBXBuildFile; fileRef = ABEB321914C56182007F0D4C /* InfoPlist.strings */; };
+		AB126D25182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */; };
+		AB126D26182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */; };
+		AB126D27182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */; };
+		AB126D28182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */; };
+		AB126D29182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */; };
+		AB126D2A182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */; };
+		AB126D2B182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */; };
 		AB18136D15D1FB73007A6CC3 /* arm_jit.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB18136C15D1FB73007A6CC3 /* arm_jit.cpp */; };
 		AB18136E15D1FB73007A6CC3 /* arm_jit.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB18136C15D1FB73007A6CC3 /* arm_jit.cpp */; };
 		AB18136F15D1FB73007A6CC3 /* arm_jit.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB18136C15D1FB73007A6CC3 /* arm_jit.cpp */; };
@@ -1365,6 +1372,7 @@
 		AB0A0EA914AAF77000E83E91 /* ChangeLog */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text; name = ChangeLog; path = ../../ChangeLog; sourceTree = SOURCE_ROOT; };
 		AB0A0EAA14AAF77000E83E91 /* COPYING */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text; name = COPYING; path = ../../COPYING; sourceTree = SOURCE_ROOT; };
 		AB0A0EAB14AAF77000E83E91 /* README */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text; name = README; path = ../../README; sourceTree = SOURCE_ROOT; };
+		AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = slot2_passme.cpp; sourceTree = "<group>"; };
 		AB18136C15D1FB73007A6CC3 /* arm_jit.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = arm_jit.cpp; path = ../arm_jit.cpp; sourceTree = SOURCE_ROOT; };
 		AB18137115D1FB8F007A6CC3 /* arm_jit.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = arm_jit.h; path = ../arm_jit.h; sourceTree = SOURCE_ROOT; };
 		AB18137215D1FBA6007A6CC3 /* instruction_attributes.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = instruction_attributes.h; path = ../instruction_attributes.h; sourceTree = SOURCE_ROOT; };
@@ -1879,6 +1887,7 @@
 				AB06CDA2135B8AED00E977B3 /* slot2_mpcf.cpp */,
 				AB06CDA3135B8AED00E977B3 /* slot2_none.cpp */,
 				AB06CDA4135B8AED00E977B3 /* slot2_paddle.cpp */,
+				AB126D24182ECC7B00EBCF22 /* slot2_passme.cpp */,
 				AB06CDA5135B8AED00E977B3 /* slot2_piano.cpp */,
 				AB06CDA6135B8AED00E977B3 /* slot2_rumblepak.cpp */,
 				AB4C80F217C5DA240024D479 /* slot1comp_mc.h */,
@@ -2963,6 +2972,7 @@
 				ABB9215C17CEB53A0049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB4352B717D66187007417C8 /* fsnitro.cpp in Sources */,
 				AB4352C317D661AC007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB126D2A182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3112,6 +3122,7 @@
 				ABB9215F17CEB53A0049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB4352BA17D66187007417C8 /* fsnitro.cpp in Sources */,
 				AB4352C617D661AC007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB126D28182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3261,6 +3272,7 @@
 				ABB9215A17CEB53A0049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB4352B517D66187007417C8 /* fsnitro.cpp in Sources */,
 				AB4352C117D661AC007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB126D26182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3380,6 +3392,7 @@
 				ABB9215E17CEB53A0049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB4352B917D66187007417C8 /* fsnitro.cpp in Sources */,
 				AB4352C517D661AC007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB126D25182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3499,6 +3512,7 @@
 				ABB9216017CEB53A0049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB4352BB17D66187007417C8 /* fsnitro.cpp in Sources */,
 				AB4352C717D661AC007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB126D27182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3628,6 +3642,7 @@
 				ABB9215B17CEB53A0049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB4352B617D66187007417C8 /* fsnitro.cpp in Sources */,
 				AB4352C217D661AC007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB126D2B182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3777,6 +3792,7 @@
 				ABB9215D17CEB53A0049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB4352B817D66187007417C8 /* fsnitro.cpp in Sources */,
 				AB4352C417D661AC007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB126D29182ECC7B00EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

desmume/src/cocoa/DeSmuME (XCode 3).xcodeproj/project.pbxproj
@@ -52,6 +52,11 @@
 		AB0F29C714BE7213009ABC6F /* Icon_RotateCW_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A314BE7213009ABC6F /* Icon_RotateCW_420x420.png */; };
 		AB0F29C814BE7213009ABC6F /* Icon_ShowHUD_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A414BE7213009ABC6F /* Icon_ShowHUD_420x420.png */; };
 		AB0F29C914BE7213009ABC6F /* Icon_Speaker_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A514BE7213009ABC6F /* Icon_Speaker_420x420.png */; };
+		AB126D07182ECB9500EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D06182ECB9500EBCF22 /* slot2_passme.cpp */; };
+		AB126D08182ECB9500EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D06182ECB9500EBCF22 /* slot2_passme.cpp */; };
+		AB126D09182ECB9500EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D06182ECB9500EBCF22 /* slot2_passme.cpp */; };
+		AB126D0A182ECB9500EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D06182ECB9500EBCF22 /* slot2_passme.cpp */; };
+		AB126D0B182ECB9500EBCF22 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB126D06182ECB9500EBCF22 /* slot2_passme.cpp */; };
 		AB213D45170CB141006DDB0F /* InputProfileController.mm in Sources */ = {isa = PBXBuildFile; fileRef = AB213D44170CB141006DDB0F /* InputProfileController.mm */; };
 		AB213D46170CB141006DDB0F /* InputProfileController.mm in Sources */ = {isa = PBXBuildFile; fileRef = AB213D44170CB141006DDB0F /* InputProfileController.mm */; };
 		AB213D47170CB141006DDB0F /* InputProfileController.mm in Sources */ = {isa = PBXBuildFile; fileRef = AB213D44170CB141006DDB0F /* InputProfileController.mm */; };
@@ -1190,6 +1195,7 @@
 		AB0F29A314BE7213009ABC6F /* Icon_RotateCW_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_RotateCW_420x420.png; path = images/Icon_RotateCW_420x420.png; sourceTree = "<group>"; };
 		AB0F29A414BE7213009ABC6F /* Icon_ShowHUD_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_ShowHUD_420x420.png; path = images/Icon_ShowHUD_420x420.png; sourceTree = "<group>"; };
 		AB0F29A514BE7213009ABC6F /* Icon_Speaker_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_Speaker_420x420.png; path = images/Icon_Speaker_420x420.png; sourceTree = "<group>"; };
+		AB126D06182ECB9500EBCF22 /* slot2_passme.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = slot2_passme.cpp; sourceTree = "<group>"; };
 		AB213D43170CB141006DDB0F /* InputProfileController.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = InputProfileController.h; sourceTree = "<group>"; };
 		AB213D44170CB141006DDB0F /* InputProfileController.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; path = InputProfileController.mm; sourceTree = "<group>"; };
 		AB213E981710D074006DDB0F /* AudioToolbox.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = AudioToolbox.framework; path = System/Library/Frameworks/AudioToolbox.framework; sourceTree = SDKROOT; };
@@ -2049,6 +2055,7 @@
 				ABD1FF061345AC9B00AF11D1 /* slot2_mpcf.cpp */,
 				ABD1FF071345AC9C00AF11D1 /* slot2_none.cpp */,
 				ABD1FF081345AC9C00AF11D1 /* slot2_paddle.cpp */,
+				AB126D06182ECB9500EBCF22 /* slot2_passme.cpp */,
 				ABD1FF091345AC9C00AF11D1 /* slot2_piano.cpp */,
 				ABD1FF0A1345AC9C00AF11D1 /* slot2_rumblepak.cpp */,
 				AB4C808A17C5D7780024D479 /* slot1comp_mc.h */,
@@ -3126,6 +3133,7 @@
 				ABB9212517CEB4110049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB43527617D5BA5E007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
 				AB43528B17D5BA95007417C8 /* fsnitro.cpp in Sources */,
+				AB126D0B182ECB9500EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3264,6 +3272,7 @@
 				ABB9212217CEB4110049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB43527317D5BA5E007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
 				AB43528817D5BA95007417C8 /* fsnitro.cpp in Sources */,
+				AB126D08182ECB9500EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3432,6 +3441,7 @@
 				ABB9212117CEB4110049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB43527217D5BA5E007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
 				AB43528717D5BA95007417C8 /* fsnitro.cpp in Sources */,
+				AB126D07182ECB9500EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3600,6 +3610,7 @@
 				ABB9212417CEB4110049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB43527517D5BA5E007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
 				AB43528A17D5BA95007417C8 /* fsnitro.cpp in Sources */,
+				AB126D0A182ECB9500EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -3738,6 +3749,7 @@
 				ABB9212317CEB4110049D4C5 /* slot1comp_protocol.cpp in Sources */,
 				AB43527417D5BA5E007417C8 /* slot1_retail_mcrom_debug.cpp in Sources */,
 				AB43528917D5BA95007417C8 /* fsnitro.cpp in Sources */,
+				AB126D09182ECB9500EBCF22 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

desmume/src/cocoa/DeSmuME (XCode 4).xcodeproj/project.pbxproj
@@ -60,6 +60,9 @@
 		AB0F29B514BE7213009ABC6F /* Icon_RotateCW_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A314BE7213009ABC6F /* Icon_RotateCW_420x420.png */; };
 		AB0F29B614BE7213009ABC6F /* Icon_ShowHUD_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A414BE7213009ABC6F /* Icon_ShowHUD_420x420.png */; };
 		AB0F29B714BE7213009ABC6F /* Icon_Speaker_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A514BE7213009ABC6F /* Icon_Speaker_420x420.png */; };
+		AB107685182ECB29000D6F67 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB107684182ECB29000D6F67 /* slot2_passme.cpp */; };
+		AB107686182ECB29000D6F67 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB107684182ECB29000D6F67 /* slot2_passme.cpp */; };
+		AB107687182ECB29000D6F67 /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB107684182ECB29000D6F67 /* slot2_passme.cpp */; };
 		AB15CA7214A29EE500B8A6A4 /* cpu_detect_x86_gcc.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD3E6613AF1D6D00502E1E /* cpu_detect_x86_gcc.cpp */; };
 		AB181D0013B66889006CA82D /* datetime.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABD1FF1F1345ACBF00AF11D1 /* datetime.cpp */; };
 		AB1949DB15034F900098793E /* OESoundInterface.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABB3C6401501BB8300E0C22E /* OESoundInterface.mm */; };
@@ -719,6 +722,7 @@
 		AB0F29A314BE7213009ABC6F /* Icon_RotateCW_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_RotateCW_420x420.png; path = images/Icon_RotateCW_420x420.png; sourceTree = "<group>"; };
 		AB0F29A414BE7213009ABC6F /* Icon_ShowHUD_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_ShowHUD_420x420.png; path = images/Icon_ShowHUD_420x420.png; sourceTree = "<group>"; };
 		AB0F29A514BE7213009ABC6F /* Icon_Speaker_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_Speaker_420x420.png; path = images/Icon_Speaker_420x420.png; sourceTree = "<group>"; };
+		AB107684182ECB29000D6F67 /* slot2_passme.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = slot2_passme.cpp; sourceTree = "<group>"; };
 		AB1B9E5F1501A78000464647 /* coreaudiosound.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = coreaudiosound.cpp; sourceTree = "<group>"; };
 		AB1B9E601501A78000464647 /* ringbuffer.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = ringbuffer.cpp; sourceTree = "<group>"; };
 		AB1B9E611501A78000464647 /* coreaudiosound.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = coreaudiosound.h; sourceTree = "<group>"; };
@@ -1438,11 +1442,11 @@
 		ABB3C63A1501BB8300E0C22E /* openemu */ = {
 			isa = PBXGroup;
 			children = (
-				ABB3C6401501BB8300E0C22E /* OESoundInterface.mm */,
 				ABB3C63D1501BB8300E0C22E /* NDSGameCore.h */,
 				ABB3C63F1501BB8300E0C22E /* OENDSSystemResponderClient.h */,
 				ABB3C6411501BB8300E0C22E /* OESoundInterface.h */,
 				ABB3C63E1501BB8300E0C22E /* NDSGameCore.mm */,
+				ABB3C6401501BB8300E0C22E /* OESoundInterface.mm */,
 				ABB3C63B1501BB8300E0C22E /* DeSmuME_Prefix_OpenEmu.pch */,
 				ABB3C63C1501BB8300E0C22E /* Info (OpenEmu Plug-in).plist */,
 			);
@@ -1644,6 +1648,7 @@
 				ABD1FF061345AC9B00AF11D1 /* slot2_mpcf.cpp */,
 				ABD1FF071345AC9C00AF11D1 /* slot2_none.cpp */,
 				ABD1FF081345AC9C00AF11D1 /* slot2_paddle.cpp */,
+				AB107684182ECB29000D6F67 /* slot2_passme.cpp */,
 				ABD1FF091345AC9C00AF11D1 /* slot2_piano.cpp */,
 				ABD1FF0A1345AC9C00AF11D1 /* slot2_rumblepak.cpp */,
 				ABFE7C8517C5D03C0028DC56 /* slot1comp_mc.h */,
@@ -2346,6 +2351,7 @@
 				ABF55C2917CEB38F00A193FD /* slot1comp_protocol.cpp in Sources */,
 				AB497A9C17D583EE0045B888 /* fsnitro.cpp in Sources */,
 				AB497AA017D584570045B888 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB107686182ECB29000D6F67 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -2515,6 +2521,7 @@
 				ABF55C2817CEB38F00A193FD /* slot1comp_protocol.cpp in Sources */,
 				AB497A9B17D583EE0045B888 /* fsnitro.cpp in Sources */,
 				AB497A9F17D584570045B888 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB107685182ECB29000D6F67 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -2673,6 +2680,7 @@
 				ABF55C2A17CEB38F00A193FD /* slot1comp_protocol.cpp in Sources */,
 				AB497A9D17D583EE0045B888 /* fsnitro.cpp in Sources */,
 				AB497AA117D584570045B888 /* slot1_retail_mcrom_debug.cpp in Sources */,
+				AB107687182ECB29000D6F67 /* slot2_passme.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

desmume/src/cocoa/DeSmuME (Xcode 5).xcodeproj/project.pbxproj
@@ -61,6 +61,9 @@
 		AB0F29B614BE7213009ABC6F /* Icon_ShowHUD_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A414BE7213009ABC6F /* Icon_ShowHUD_420x420.png */; };
 		AB0F29B714BE7213009ABC6F /* Icon_Speaker_420x420.png in Resources */ = {isa = PBXBuildFile; fileRef = AB0F29A514BE7213009ABC6F /* Icon_Speaker_420x420.png */; };
 		AB15CA7214A29EE500B8A6A4 /* cpu_detect_x86_gcc.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD3E6613AF1D6D00502E1E /* cpu_detect_x86_gcc.cpp */; };
+		AB1773FD182ECA8A009F29DD /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB1773FC182ECA8A009F29DD /* slot2_passme.cpp */; };
+		AB1773FE182ECA8A009F29DD /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB1773FC182ECA8A009F29DD /* slot2_passme.cpp */; };
+		AB1773FF182ECA8A009F29DD /* slot2_passme.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB1773FC182ECA8A009F29DD /* slot2_passme.cpp */; };
 		AB181D0013B66889006CA82D /* datetime.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABD1FF1F1345ACBF00AF11D1 /* datetime.cpp */; };
 		AB1949DB15034F900098793E /* OESoundInterface.mm in Sources */ = {isa = PBXBuildFile; fileRef = ABB3C6401501BB8300E0C22E /* OESoundInterface.mm */; };
 		AB1B9E631501A78000464647 /* coreaudiosound.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB1B9E5F1501A78000464647 /* coreaudiosound.cpp */; };
@@ -719,6 +722,7 @@
 		AB0F29A314BE7213009ABC6F /* Icon_RotateCW_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_RotateCW_420x420.png; path = images/Icon_RotateCW_420x420.png; sourceTree = "<group>"; };
 		AB0F29A414BE7213009ABC6F /* Icon_ShowHUD_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_ShowHUD_420x420.png; path = images/Icon_ShowHUD_420x420.png; sourceTree = "<group>"; };
 		AB0F29A514BE7213009ABC6F /* Icon_Speaker_420x420.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_Speaker_420x420.png; path = images/Icon_Speaker_420x420.png; sourceTree = "<group>"; };
+		AB1773FC182ECA8A009F29DD /* slot2_passme.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = slot2_passme.cpp; sourceTree = "<group>"; };
 		AB1B9E5F1501A78000464647 /* coreaudiosound.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = coreaudiosound.cpp; sourceTree = "<group>"; };
 		AB1B9E601501A78000464647 /* ringbuffer.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = ringbuffer.cpp; sourceTree = "<group>"; };
 		AB1B9E611501A78000464647 /* coreaudiosound.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = coreaudiosound.h; sourceTree = "<group>"; };
@@ -1438,11 +1442,11 @@
 		ABB3C63A1501BB8300E0C22E /* openemu */ = {
 			isa = PBXGroup;
 			children = (
-				ABB3C6401501BB8300E0C22E /* OESoundInterface.mm */,
 				ABB3C63D1501BB8300E0C22E /* NDSGameCore.h */,
 				ABB3C63F1501BB8300E0C22E /* OENDSSystemResponderClient.h */,
 				ABB3C6411501BB8300E0C22E /* OESoundInterface.h */,
 				ABB3C63E1501BB8300E0C22E /* NDSGameCore.mm */,
+				ABB3C6401501BB8300E0C22E /* OESoundInterface.mm */,
 				ABB3C63B1501BB8300E0C22E /* DeSmuME_Prefix_OpenEmu.pch */,
 				ABB3C63C1501BB8300E0C22E /* Info (OpenEmu Plug-in).plist */,
 			);
@@ -1644,6 +1648,7 @@
 				ABD1FF061345AC9B00AF11D1 /* slot2_mpcf.cpp */,
 				ABD1FF071345AC9C00AF11D1 /* slot2_none.cpp */,
 				ABD1FF081345AC9C00AF11D1 /* slot2_paddle.cpp */,
+				AB1773FC182ECA8A009F29DD /* slot2_passme.cpp */,
 				ABD1FF091345AC9C00AF11D1 /* slot2_piano.cpp */,
 				ABD1FF0A1345AC9C00AF11D1 /* slot2_rumblepak.cpp */,
 				AB9038AC17C5ED2200F410BD /* slot1comp_mc.h */,
@@ -2272,6 +2277,7 @@
 				ABD1FEFD1345AC8400AF11D1 /* version.cpp in Sources */,
 				ABD1FF681345ACBF00AF11D1 /* vfat.cpp in Sources */,
 				ABE5DE95143F781900835AD8 /* videofilter.cpp in Sources */,
+				AB1773FE182ECA8A009F29DD /* slot2_passme.cpp in Sources */,
 				ABAD3E7B13AF1D6D00502E1E /* WavFile.cpp in Sources */,
 				ABD1FEFE1345AC8400AF11D1 /* wifi.cpp in Sources */,
 				AB9038B317C5ED2200F410BD /* slot1_retail_mcrom.cpp in Sources */,
@@ -2499,6 +2505,7 @@
 				AB40567E169F5DCC0016AC3E /* x86compilercontext.cpp in Sources */,
 				AB405681169F5DCC0016AC3E /* x86compilerfunc.cpp in Sources */,
 				AB405684169F5DCC0016AC3E /* x86compileritem.cpp in Sources */,
+				AB1773FD182ECA8A009F29DD /* slot2_passme.cpp in Sources */,
 				AB405687169F5DCC0016AC3E /* x86cpuinfo.cpp in Sources */,
 				AB40568A169F5DCC0016AC3E /* x86defs.cpp in Sources */,
 				AB40568D169F5DCC0016AC3E /* x86func.cpp in Sources */,
@@ -2567,6 +2574,7 @@
 				ABB3C6981501C04F00E0C22E /* crc.cpp in Sources */,
 				ABB3C6991501C04F00E0C22E /* decrypt.cpp in Sources */,
 				ABB3C69A1501C04F00E0C22E /* header.cpp in Sources */,
+				AB1773FF182ECA8A009F29DD /* slot2_passme.cpp in Sources */,
 				ABB3C69B1501C04F00E0C22E /* cache.cpp in Sources */,
 				ABB3C69C1501C04F00E0C22E /* directory.cpp in Sources */,
 				ABB3C69D1501C04F00E0C22E /* disc.cpp in Sources */,

desmume/src/cocoa/cocoa_file.mm
@@ -195,7 +195,7 @@ static NSMutableDictionary *_gURLDictionary = nil;
 	BOOL result = NO;
 	const char *romSavePath = [[romSaveURL path] cStringUsingEncoding:NSUTF8StringEncoding];
 	
-	NSInteger resultCode = MMU_new.backupDevice.importData(romSavePath);
+	NSInteger resultCode = MMU_new.backupDevice.importData(romSavePath, 0);
 	if (resultCode == 0)
 	{
 		return result;