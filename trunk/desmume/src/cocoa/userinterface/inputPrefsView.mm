0e1587d58645bb1301faa537d54b2fd210a18c46
rogerman <rogerman@5577840e-afb3-4e59-9ef3-2b7f898b1952>
2015-02-17 19:52:44 +0000

    Cocoa Port: - Fix font rendering weight of the Input Preferences view when running on OS X Yosemite.

--- trunk/desmume/src/cocoa/userinterface/inputPrefsView.mm
+++ trunk/desmume/src/cocoa/userinterface/inputPrefsView.mm
@@ -538,7 +538,7 @@
 	{
 		NSFont *newFont = [[NSFontManager sharedFontManager] fontWithFamily:[[outCell font] familyName]
 																	 traits:([item isKindOfClass:[NSArray class]]) ? NSBoldFontMask : 0
-																	 weight:0
+																	 weight:5
 																	   size:[[outCell font] pointSize]];
 		[outCell setFont:newFont];
 	}
@@ -546,7 +546,7 @@
 	{
 		NSFont *newFont = [[NSFontManager sharedFontManager] fontWithFamily:[[outCell font] familyName]
 																	 traits:([item isKindOfClass:[NSArray class]]) ? NSBoldFontMask : 0
-																	 weight:0
+																	 weight:5
 																	   size:[[outCell font] pointSize]];
 		[outCell setFont:newFont];
 	}
