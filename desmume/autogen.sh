23ae2f8a600909808431ac15236e95aabdc33122
zeromus <zeromus@users.noreply.github.com>
2016-08-17 01:23:14 -0500

    move gtk,cli ports to frontend/posix directory

--- desmume/autogen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# intltoolize is optionnal as it's only required for the gtk-glade UI.
-
-autoreconf --install --force --verbose
--- desmume/src/desmume.mk
+++
@@ -1,3 +0,0 @@
-AM_CFLAGS = 
-AM_CPPFLAGS = -I$(top_srcdir)/src -I$(top_srcdir)/src/libretro-common/include
-AM_LDFLAGS =
