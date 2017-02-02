TASEmulators/desmume

commit aff2d07146f6fe3f2d8799289efd2733c29a0289

Cocoa Port: Huge refactor to ClientDisplayView, now supporting the use of shared fetch objects and shared client objects in order to lower memory usage and CPU/GPU usage when many display views are running at the same time. - OpenGL display views now use a shared fetch object to fetch the emulated GPU framebuffers and store them in shared textures within a shared context. In conjunction with the new double-buffering support from the last commit, this eliminates the copying between the framebuffers and each display view. - OpenGL display views now use shared HQnx LUT textures, rather than having to initialize and maintain a copy of the LUT textures for each display view. - OpenGL display views no longer perform any rendering while their associated NSView is hidden, improving the performance of creating new display views. - OpenGL display views can now DMA directly from pinned-memory both custom-sized framebuffers and CPU-pixel-scaled native-sized framebuffers at the same time.

desmume/src/frontend/cocoa/cocoa_GPU.mm
@@ -21,7 +21,6 @@
 #include "utilities.h"
 
 #include "../../NDSSystem.h"
-#include "../../GPU.h"
 #include "../../rasterize.h"
 
 #ifdef MAC_OS_X_VERSION_10_7
@@ -31,8 +30,12 @@
 #endif
 
 #include <OpenGL/OpenGL.h>
+#include "userinterface/MacOGLDisplayView.h"
+#include "userinterface/MacMetalDisplayView.h"
 
+#ifdef BOOL
 #undef BOOL
+#endif
 
 GPU3DInterface *core3DList[] = {
 	&gpu3DNull,
@@ -44,15 +47,18 @@ GPU3DInterface *core3DList[] = {
 class GPUEventHandlerOSX : public GPUEventHandlerDefault
 {
 private:
+	GPUClientFetchObject *_fetchObject;
+	
 	pthread_rwlock_t _rwlockFrame;
 	pthread_mutex_t _mutex3DRender;
-	pthread_mutex_t *_mutexOutputList;
-	NSMutableArray *_cdsOutputList;
 	bool _render3DNeedsFinish;
 	
 public:
 	GPUEventHandlerOSX();
 	~GPUEventHandlerOSX();
+	
+	GPUClientFetchObject* GetFetchObject() const;
+	void SetFetchObject(GPUClientFetchObject *fetchObject);
 	
 	void FramebufferLockWrite();
 	void FramebufferLockRead();
@@ -60,12 +66,7 @@ public:
 	void Render3DLock();
 	void Render3DUnlock();
 	
-	void FrameFinish();
-	void SetVideoBuffers();
-	
 	pthread_rwlock_t* GetFrameRWLock();
-	NSMutableArray* GetOutputList();
-	void SetOutputList(NSMutableArray *outputList, pthread_mutex_t *theMutex);
 	bool GetRender3DNeedsFinish();
 	
 	virtual void DidFrameBegin(bool isFrameSkipRequested, const u8 targetBufferIndex, const size_t line);
@@ -81,6 +82,8 @@ public:
 @dynamic gpuScale;
 @dynamic gpuColorFormat;
 @dynamic gpuFrameRWLock;
+@synthesize fetchObject;
+@dynamic sharedData;
 
 @dynamic layerMainGPU;
 @dynamic layerMainBG0;
@@ -147,6 +150,20 @@ public:
 	GPU->SetEventHandler(gpuEvent);
 	GPU->SetWillAutoResolveToCustomBuffer(false);
 	
+#ifdef ENABLE_APPLE_METAL
+	if (IsOSXVersionSupported(10, 11, 0))
+	{
+		fetchObject = new MacMetalFetchObject;
+	}
+	else
+#endif
+	{
+		fetchObject = new MacOGLClientFetchObject;
+	}
+	
+	fetchObject->Init();
+	gpuEvent->SetFetchObject(fetchObject);
+	
 	return self;
 }
 
@@ -154,9 +171,20 @@ public:
 {
 	DestroyOpenGLRenderer();
 	
+	delete fetchObject;
 	delete gpuEvent;
 	
 	[super dealloc];
+}
+
+- (GPUClientFetchObject *) fetchObject
+{
+	return fetchObject;
+}
+
+- (MacClientSharedObject *) sharedData
+{
+	return (MacClientSharedObject *)fetchObject->GetClientData();
 }
 
 - (void) setGpuStateFlags:(UInt32)flags
@@ -191,13 +219,15 @@ public:
 
 - (void) setGpuDimensions:(NSSize)theDimensions
 {
-	gpuEvent->FrameFinish();
+	[[self sharedData] finishAllDisplayViewsAtIndex:0];
+	[[self sharedData] finishAllDisplayViewsAtIndex:1];
+	
 	gpuEvent->Render3DLock();
 	gpuEvent->FramebufferLockWrite();
 	
 	GPU->SetCustomFramebufferSize(theDimensions.width, theDimensions.height);
+	fetchObject->SetFetchBuffers(GPU->GetDisplayInfo());
 	
-	gpuEvent->SetVideoBuffers();
 	gpuEvent->FramebufferUnlock();
 	gpuEvent->Render3DUnlock();
 }
@@ -226,13 +256,15 @@ public:
 
 - (void) setGpuColorFormat:(NSUInteger)colorFormat
 {
-	gpuEvent->FrameFinish();
+	[[self sharedData] finishAllDisplayViewsAtIndex:0];
+	[[self sharedData] finishAllDisplayViewsAtIndex:1];
+	
 	gpuEvent->Render3DLock();
 	gpuEvent->FramebufferLockWrite();
 	
 	GPU->SetColorFormat((NDSColorFormat)colorFormat);
+	fetchObject->SetFetchBuffers(GPU->GetDisplayInfo());
 	
-	gpuEvent->SetVideoBuffers();
 	gpuEvent->FramebufferUnlock();
 	gpuEvent->Render3DUnlock();
 }
@@ -255,7 +287,7 @@ public:
 
 - (void) setOutputList:(NSMutableArray *)theOutputList mutexPtr:(pthread_mutex_t *)theMutex
 {
-	gpuEvent->SetOutputList(theOutputList, theMutex);
+	[(MacClientSharedObject *)fetchObject->GetClientData() setOutputList:theOutputList mutex:theMutex];
 }
 
 - (void) setRender3DRenderingEngine:(NSInteger)methodID
@@ -814,10 +846,196 @@ public:
 
 @end
 
+@implementation MacClientSharedObject
+
+@synthesize GPUFetchObject;
+
+- (id)init
+{
+	self = [super init];
+	if (self == nil)
+	{
+		return self;
+	}
+	
+	_rwlockFramebuffer[0] = (pthread_rwlock_t *)malloc(sizeof(pthread_rwlock_t));
+	_rwlockFramebuffer[1] = (pthread_rwlock_t *)malloc(sizeof(pthread_rwlock_t));
+	
+	pthread_rwlock_init(_rwlockFramebuffer[0], NULL);
+	pthread_rwlock_init(_rwlockFramebuffer[1], NULL);
+	
+	GPUFetchObject = nil;
+	_mutexOutputList = NULL;
+	_cdsOutputList = nil;
+	
+	return self;
+}
+
+- (void)dealloc
+{
+	pthread_mutex_t *currentMutex = _mutexOutputList;
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_lock(currentMutex);
+	}
+	
+	[_cdsOutputList release];
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_unlock(currentMutex);
+	}
+	
+	pthread_rwlock_destroy(_rwlockFramebuffer[0]);
+	pthread_rwlock_destroy(_rwlockFramebuffer[1]);
+	
+	[super dealloc];
+}
+
+- (void)handlePortMessage:(NSPortMessage *)portMessage
+{
+	NSInteger message = (NSInteger)[portMessage msgid];
+	NSArray *messageComponents = [portMessage components];
+	
+	switch (message)
+	{
+		case MESSAGE_FETCH_AND_PUSH_VIDEO:
+			[self handleFetchFromBufferIndexAndPushVideo:[messageComponents objectAtIndex:0]];
+			break;
+			
+		default:
+			[super handlePortMessage:portMessage];
+			break;
+	}
+}
+
+- (void) handleFetchFromBufferIndexAndPushVideo:(NSData *)indexData
+{
+	const NSInteger index = *(NSInteger *)[indexData bytes];
+	
+	GPUFetchObject->FetchFromBufferIndex(index);
+	[self pushVideoDataToAllDisplayViews];
+}
+
+- (const NDSDisplayInfo &) fetchDisplayInfoForIndex:(const u8)bufferIndex
+{
+	return GPUFetchObject->GetFetchDisplayInfoForBufferIndex(bufferIndex);
+}
+
+- (pthread_rwlock_t *) rwlockFramebufferAtIndex:(const u8)bufferIndex
+{
+	return _rwlockFramebuffer[bufferIndex];
+}
+
+- (void) setOutputList:(NSMutableArray *)theOutputList mutex:(pthread_mutex_t *)theMutex
+{
+	pthread_mutex_t *currentMutex = _mutexOutputList;
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_lock(currentMutex);
+	}
+	
+	[_cdsOutputList release];
+	_cdsOutputList = theOutputList;
+	[_cdsOutputList retain];
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_unlock(currentMutex);
+	}
+	
+	_mutexOutputList = theMutex;
+}
+
+- (BOOL) isCPUFilteringNeeded
+{
+	bool useCPUFilterPipeline = NO;
+	pthread_mutex_t *currentMutex = _mutexOutputList;
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_lock(currentMutex);
+	}
+	
+	for (CocoaDSOutput *cdsOutput in _cdsOutputList)
+	{
+		if ([cdsOutput isKindOfClass:[CocoaDSDisplay class]])
+		{
+			ClientDisplay3DView *cdv = [(CocoaDSDisplay *)cdsOutput clientDisplayView];
+			
+			if (!cdv->WillFilterOnGPU() && (cdv->GetPixelScaler() != VideoFilterTypeID_None))
+			{
+				useCPUFilterPipeline = YES;
+				break;
+			}
+		}
+	}
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_unlock(currentMutex);
+	}
+	
+	return useCPUFilterPipeline;
+}
+
+- (void) pushVideoDataToAllDisplayViews
+{
+	pthread_mutex_t *currentMutex = _mutexOutputList;
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_lock(currentMutex);
+	}
+	
+	for (CocoaDSOutput *cdsOutput in _cdsOutputList)
+	{
+		if ([cdsOutput isKindOfClass:[CocoaDSDisplay class]])
+		{
+			[CocoaDSUtil messageSendOneWay:[cdsOutput receivePort] msgID:MESSAGE_RECEIVE_GPU_FRAME];
+		}
+	}
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_unlock(currentMutex);
+	}
+}
+
+- (void) finishAllDisplayViewsAtIndex:(const u8)bufferIndex
+{
+	pthread_mutex_t *currentMutex = _mutexOutputList;
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_lock(currentMutex);
+	}
+	
+	for (CocoaDSOutput *cdsOutput in _cdsOutputList)
+	{
+		if ([cdsOutput isKindOfClass:[CocoaDSDisplay class]])
+		{
+			ClientDisplay3DView *cdv = [(CocoaDSDisplay *)cdsOutput clientDisplayView];
+			cdv->FinishFrameAtIndex(bufferIndex);
+		}
+	}
+	
+	if (currentMutex != NULL)
+	{
+		pthread_mutex_unlock(currentMutex);
+	}
+}
+
+@end
+
+#pragma mark -
+
 GPUEventHandlerOSX::GPUEventHandlerOSX()
 {
+	_fetchObject = nil;
 	_render3DNeedsFinish = false;
-	_mutexOutputList = NULL;
 	pthread_rwlock_init(&_rwlockFrame, NULL);
 	pthread_mutex_init(&_mutex3DRender, NULL);
 }
@@ -833,37 +1051,46 @@ GPUEventHandlerOSX::~GPUEventHandlerOSX()
 	pthread_mutex_destroy(&this->_mutex3DRender);
 }
 
+GPUClientFetchObject* GPUEventHandlerOSX::GetFetchObject() const
+{
+	return this->_fetchObject;
+}
+
+void GPUEventHandlerOSX::SetFetchObject(GPUClientFetchObject *fetchObject)
+{
+	this->_fetchObject = fetchObject;
+}
+
 void GPUEventHandlerOSX::DidFrameBegin(bool isFrameSkipRequested, const u8 targetBufferIndex, const size_t line)
 {
 	this->FramebufferLockWrite();
+	
+#if !defined(PORT_VERSION_OPENEMU)
+	if (!isFrameSkipRequested)
+	{
+		MacClientSharedObject *sharedViewObject = (MacClientSharedObject *)this->_fetchObject->GetClientData();
+		pthread_rwlock_wrlock([sharedViewObject rwlockFramebufferAtIndex:targetBufferIndex]);
+	}
+#endif
 }
 
 void GPUEventHandlerOSX::DidFrameEnd(bool isFrameSkipped, const NDSDisplayInfo &latestDisplayInfo)
 {
+#if !defined(PORT_VERSION_OPENEMU)
+	MacClientSharedObject *sharedViewObject = (MacClientSharedObject *)this->_fetchObject->GetClientData();
+	if (!isFrameSkipped)
+	{
+		this->_fetchObject->SetFetchDisplayInfo(latestDisplayInfo);
+		pthread_rwlock_unlock([sharedViewObject rwlockFramebufferAtIndex:latestDisplayInfo.bufferIndex]);
+	}
+#endif
+	
 	this->FramebufferUnlock();
 	
 #if !defined(PORT_VERSION_OPENEMU)
 	if (!isFrameSkipped)
 	{
-		if (this->_mutexOutputList != NULL)
-		{
-			pthread_mutex_lock(this->_mutexOutputList);
-		}
-		
-		NSMutableArray *outputList = this->_cdsOutputList;
-		
-		for (CocoaDSOutput *cdsOutput in outputList)
-		{
-			if ([cdsOutput isKindOfClass:[CocoaDSDisplay class]])
-			{
-				[(CocoaDSDisplay *)cdsOutput doReceiveGPUFrame];
-			}
-		}
-		
-		if (this->_mutexOutputList != NULL)
-		{
-			pthread_mutex_unlock(this->_mutexOutputList);
-		}
+		[CocoaDSUtil messageSendOneWayWithInteger:[sharedViewObject receivePort] msgID:MESSAGE_FETCH_AND_PUSH_VIDEO integerValue:latestDisplayInfo.bufferIndex];
 	}
 #endif
 }
@@ -905,56 +1132,6 @@ void GPUEventHandlerOSX::Render3DUnlock()
 	pthread_mutex_unlock(&this->_mutex3DRender);
 }
 
-void GPUEventHandlerOSX::FrameFinish()
-{
-#if !defined(PORT_VERSION_OPENEMU)
-	if (this->_mutexOutputList != NULL)
-	{
-		pthread_mutex_lock(this->_mutexOutputList);
-	}
-	
-	NSMutableArray *outputList = this->_cdsOutputList;
-	
-	for (CocoaDSOutput *cdsOutput in outputList)
-	{
-		if ([cdsOutput isKindOfClass:[CocoaDSDisplay class]])
-		{
-			[(CocoaDSDisplay *)cdsOutput finishFrame];
-		}
-	}
-	
-	if (this->_mutexOutputList != NULL)
-	{
-		pthread_mutex_unlock(this->_mutexOutputList);
-	}
-#endif
-}
-
-void GPUEventHandlerOSX::SetVideoBuffers()
-{
-#if !defined(PORT_VERSION_OPENEMU)
-	if (this->_mutexOutputList != NULL)
-	{
-		pthread_mutex_lock(this->_mutexOutputList);
-	}
-	
-	NSMutableArray *outputList = this->_cdsOutputList;
-	
-	for (CocoaDSOutput *cdsOutput in outputList)
-	{
-		if ([cdsOutput isKindOfClass:[CocoaDSDisplayVideo class]])
-		{
-			[(CocoaDSDisplayVideo *)cdsOutput resetVideoBuffers];
-		}
-	}
-	
-	if (this->_mutexOutputList != NULL)
-	{
-		pthread_mutex_unlock(this->_mutexOutputList);
-	}
-#endif
-}
-
 bool GPUEventHandlerOSX::GetRender3DNeedsFinish()
 {
 	return this->_render3DNeedsFinish;
@@ -965,16 +1142,7 @@ pthread_rwlock_t* GPUEventHandlerOSX::GetFrameRWLock()
 	return &this->_rwlockFrame;
 }
 
-NSMutableArray* GPUEventHandlerOSX::GetOutputList()
-{
-	return this->_cdsOutputList;
-}
-
-void GPUEventHandlerOSX::SetOutputList(NSMutableArray *outputList, pthread_mutex_t *theMutex)
-{
-	this->_cdsOutputList = outputList;
-	this->_mutexOutputList = theMutex;
-}
+#pragma mark -
 
 CGLContextObj OSXOpenGLRendererContext = NULL;
 

desmume/src/frontend/cocoa/ClientDisplayView.cpp
@@ -21,6 +21,8 @@
 
 ClientDisplayView::ClientDisplayView()
 {
+	_fetchObject = NULL;
+	
 	ClientDisplayViewProperties defaultProperty;
 	defaultProperty.normalWidth		= GPU_FRAMEBUFFER_NATIVE_WIDTH;
 	defaultProperty.normalHeight	= GPU_FRAMEBUFFER_NATIVE_HEIGHT * 2.0;
@@ -80,6 +82,7 @@ void ClientDisplayView::__InstanceInit(const ClientDisplayViewProperties &props)
 	memset(&_emuFrameInfo, 0, sizeof(_emuFrameInfo));
 	_hudString = "\x01"; // Char value 0x01 will represent the "text box" character, which will always be first in the string.
 	_hudNeedsUpdate = true;
+	_allowViewUpdates = true;
 	
 	FT_Error error = FT_Init_FreeType(&_ftLibrary);
 	if (error)
@@ -481,14 +484,24 @@ void ClientDisplayView::ClearHUDNeedsUpdate()
 }
 
 // NDS GPU Interface
-void ClientDisplayView::_FetchNativeDisplayByID(const NDSDisplayID displayID)
+const GPUClientFetchObject& ClientDisplayView::GetFetchObject() const
 {
-	// Do nothing. This is implementation dependent.
+	return *this->_fetchObject;
 }
 
-void ClientDisplayView::_FetchCustomDisplayByID(const NDSDisplayID displayID)
+void ClientDisplayView::SetFetchObject(GPUClientFetchObject *fetchObject)
 {
-	// Do nothing. This is implementation dependent.
+	this->_fetchObject = fetchObject;
+}
+
+bool ClientDisplayView::GetAllowViewUpdates() const
+{
+	return this->_allowViewUpdates;
+}
+
+void ClientDisplayView::SetAllowViewUpdates(const bool allowUpdates)
+{
+	this->_allowViewUpdates = allowUpdates;
 }
 
 void ClientDisplayView::_LoadNativeDisplayByID(const NDSDisplayID displayID)
@@ -501,38 +514,10 @@ void ClientDisplayView::_LoadCustomDisplayByID(const NDSDisplayID displayID)
 	// Do nothing. This is implementation dependent.
 }
 
-void ClientDisplayView::FetchDisplays()
-{
-	const bool loadMainScreen = this->_emuDisplayInfo.isDisplayEnabled[NDSDisplayID_Main] && ((this->_renderProperty.mode == ClientDisplayMode_Main) || (this->_renderProperty.mode == ClientDisplayMode_Dual));
-	const bool loadTouchScreen = this->_emuDisplayInfo.isDisplayEnabled[NDSDisplayID_Touch] && ((this->_renderProperty.mode == ClientDisplayMode_Touch) || (this->_renderProperty.mode == ClientDisplayMode_Dual));
-	
-	if (loadMainScreen)
-	{
-		if (!this->_emuDisplayInfo.didPerformCustomRender[NDSDisplayID_Main])
-		{
-			this->_FetchNativeDisplayByID(NDSDisplayID_Main);
-		}
-		else
-		{
-			this->_FetchCustomDisplayByID(NDSDisplayID_Main);
-		}
-	}
-	
-	if (loadTouchScreen)
-	{
-		if (!this->_emuDisplayInfo.didPerformCustomRender[NDSDisplayID_Touch])
-		{
-			this->_FetchNativeDisplayByID(NDSDisplayID_Touch);
-		}
-		else
-		{
-			this->_FetchCustomDisplayByID(NDSDisplayID_Touch);
-		}
-	}
-}
-
 void ClientDisplayView::LoadDisplays()
 {
+	this->_emuDisplayInfo = this->_fetchObject->GetFetchDisplayInfoForBufferIndex(this->_fetchObject->GetLastFetchIndex());
+	
 	const bool loadMainScreen = this->_emuDisplayInfo.isDisplayEnabled[NDSDisplayID_Main] && ((this->_renderProperty.mode == ClientDisplayMode_Main) || (this->_renderProperty.mode == ClientDisplayMode_Dual));
 	const bool loadTouchScreen = this->_emuDisplayInfo.isDisplayEnabled[NDSDisplayID_Touch] && ((this->_renderProperty.mode == ClientDisplayMode_Touch) || (this->_renderProperty.mode == ClientDisplayMode_Dual));
 	
@@ -985,16 +970,6 @@ void ClientDisplay3DView::SetFiltersPreferGPU(const bool preferGPU)
 void ClientDisplay3DView::SetSourceDeposterize(bool useDeposterize)
 {
 	this->_useDeposterize = (this->_canFilterOnGPU) ? useDeposterize : false;
-}
-
-void ClientDisplay3DView::SetVideoBuffers(const uint32_t colorFormat,
-										  const void *videoBufferHead,
-										  const void *nativeBuffer0,
-										  const void *nativeBuffer1,
-										  const void *customBuffer0, const size_t customWidth0, const size_t customHeight0,
-										  const void *customBuffer1, const size_t customWidth1, const size_t customHeight1)
-{
-	// Do nothing. This is implementation dependent.
 }
 
 void ClientDisplay3DView::SetHUDVertices(float viewportWidth, float viewportHeight, float *vtxBufferPtr)

desmume/src/frontend/cocoa/ClientDisplayView.h
@@ -114,6 +114,7 @@ protected:
 	ClientDisplayViewProperties _renderProperty;
 	ClientDisplayViewProperties _stagedProperty;
 	InitialTouchPressMap *_initialTouchInMajorDisplay;
+	GPUClientFetchObject *_fetchObject;
 	
 	bool _useDeposterize;
 	VideoFilterTypeID _pixelScaler;
@@ -135,6 +136,7 @@ protected:
 	NDSFrameInfo _emuFrameInfo;
 	std::string _hudString;
 	bool _hudNeedsUpdate;
+	bool _allowViewUpdates;
 	
 	FT_Library _ftLibrary;
 	const char *_lastFontFilePath;
@@ -151,8 +153,6 @@ protected:
 	virtual void _UpdateClientSize();
 	virtual void _UpdateViewScale();
 	
-	virtual void _FetchNativeDisplayByID(const NDSDisplayID displayID);
-	virtual void _FetchCustomDisplayByID(const NDSDisplayID displayID);
 	virtual void _LoadNativeDisplayByID(const NDSDisplayID displayID);
 	virtual void _LoadCustomDisplayByID(const NDSDisplayID displayID);
 	
@@ -218,11 +218,16 @@ public:
 	void ClearHUDNeedsUpdate();
 	
 	// Client view interface
-	virtual void FetchDisplays();
+	const GPUClientFetchObject& GetFetchObject() const;
+	void SetFetchObject(GPUClientFetchObject *fetchObject);
+	
+	bool GetAllowViewUpdates() const;
+	void SetAllowViewUpdates(const bool allowUpdates);
+	
 	virtual void LoadDisplays();
 	virtual void ProcessDisplays();
 	virtual void UpdateView();
-	virtual void FrameFinish() = 0;
+	virtual void FinishFrameAtIndex(const u8 bufferIndex) = 0;
 	
 	// Emulator interface
 	const NDSDisplayInfo& GetEmuDisplayInfo() const;
@@ -268,13 +273,6 @@ public:
 	bool WillFilterOnGPU() const;
 	
 	virtual void SetSourceDeposterize(const bool useDeposterize);
-	
-	virtual void SetVideoBuffers(const uint32_t colorFormat,
-								 const void *videoBufferHead,
-								 const void *nativeBuffer0,
-								 const void *nativeBuffer1,
-								 const void *customBuffer0, const size_t customWidth0, const size_t customHeight0,
-								 const void *customBuffer1, const size_t customWidth1, const size_t customHeight1);
 	
 	void SetHUDVertices(float viewportWidth, float viewportHeight, float *vtxBufferPtr);
 	void SetHUDTextureCoordinates(float *texCoordBufferPtr);

desmume/src/frontend/cocoa/OGLDisplayOutput.h
@@ -30,6 +30,7 @@
 
 #include <set>
 #include <string>
+#include <pthread.h>
 #include "../../filter/videofilter.h"
 
 #include "ClientDisplayView.h"
@@ -217,8 +218,6 @@ protected:
 	GLint _uniformFinalOutputScalar;
 	GLint _uniformFinalOutputViewSize;
 	
-	void UploadHQnxLUTs();
-	
 	void UploadVerticesOGL();
 	void UploadTexCoordsOGL();
 	void UploadTransformationOGL();
@@ -270,7 +269,7 @@ public:
 	virtual void SetVisibility(const bool visibleState);
 	
 	virtual void RenderOGL() = 0;
-	virtual void FinishOGL() {};
+	virtual void FinishOGL(const u8 bufferIndex) {};
 };
 
 class OGLHUDLayer : public OGLVideoLayer
@@ -301,32 +300,18 @@ class OGLDisplayLayer : public OGLVideoLayer
 protected:
 	bool _useShader150;
 	ShaderSupportTier _shaderSupport;
-	GLboolean _useClientStorage;
 	
 	OGLShaderProgram *_finalOutputProgram;
 	OGLFilter *_filterDeposterize[2];
 	OGLFilter *_shaderFilter[2];
 	GLint _displayTexFilter[2];
 	
-	GLuint _texVideoInputDataNativeID[2];
-	GLuint _texVideoInputDataCustomID[2];
 	GLuint _texVideoOutputID[2];
-	
-	GLenum _videoColorFormat;
-	const void *_videoSrcBufferHead;
-	void *_videoSrcNativeBuffer[2];
-	void *_videoSrcCustomBuffer[2];
-	size_t _videoSrcBufferSize;
 	
 	uint32_t *_vfMasterDstBuffer;
 	size_t _vfMasterDstBufferSize;
 	VideoFilter *_vf[2];
 	GLuint _texCPUFilterDstID[2];
-	
-	GLuint _texLQ2xLUT;
-	GLuint _texHQ2xLUT;
-	GLuint _texHQ3xLUT;
-	GLuint _texHQ4xLUT;
 	
 	GLuint _vaoMainStatesID;
 	GLuint _vboVertexID;
@@ -335,9 +320,6 @@ protected:
 	GLint _uniformFinalOutputAngleDegrees;
 	GLint _uniformFinalOutputScalar;
 	GLint _uniformFinalOutputViewSize;
-	
-	void _UploadHQnxLUTs();
-	void _DetermineTextureStorageHints(GLint &videoSrcTexStorageHint, GLint &cpuFilterTexStorageHint);
 	
 	void _ResizeCPUPixelScalerOGL(const size_t srcWidthMain, const size_t srcHeightMain, const size_t srcWidthTouch, const size_t srcHeightTouch, const size_t scaleMultiply, const size_t scaleDivide);
 	
@@ -351,30 +333,60 @@ public:
 	OGLDisplayLayer(OGLVideoOutput *oglVO);
 	virtual ~OGLDisplayLayer();
 	
-	void SetVideoBuffers(const uint32_t colorFormat,
-						 const void *videoBufferHead,
-						 const void *nativeBuffer0,
-						 const void *nativeBuffer1,
-						 const void *customBuffer0, const size_t customWidth0, const size_t customHeight0,
-						 const void *customBuffer1, const size_t customWidth1, const size_t customHeight1);
-	
 	void SetNeedsUpdateRotationScale();
-	void SetFiltersPreferGPUOGL();
-	
-	bool CanUseShaderBasedFilters();
 	
 	OutputFilterTypeID SetOutputFilterOGL(const OutputFilterTypeID filterID);
 	bool SetGPUPixelScalerOGL(const VideoFilterTypeID filterID);
 	void SetCPUPixelScalerOGL(const VideoFilterTypeID filterID);
 	
-	void CopyNativeDisplayByID_OGL(const NDSDisplayID displayID);
-	void CopyCustomDisplayByID_OGL(const NDSDisplayID displayID);
 	void LoadNativeDisplayByID_OGL(const NDSDisplayID displayID);
-	void LoadCustomDisplayByID_OGL(const NDSDisplayID displayID);
 	
 	void ProcessOGL();
 	virtual void RenderOGL();
-	virtual void FinishOGL();
+	virtual void FinishOGL(const u8 bufferIndex);
+};
+
+class OGLClientFetchObject : public GPUClientFetchObject
+{
+protected:
+	OGLContextInfo *_contextInfo;
+	GLenum _fetchColorFormatOGL;
+	GLuint _texDisplayFetchNative[2][2];
+	GLuint _texDisplayFetchCustom[2][2];
+	
+	GLuint _texLQ2xLUT;
+	GLuint _texHQ2xLUT;
+	GLuint _texHQ3xLUT;
+	GLuint _texHQ4xLUT;
+	
+	bool _useCPUFilterPipeline;
+	uint32_t *_srcNativeCloneMaster;
+	uint32_t *_srcNativeClone[2][2];
+	pthread_rwlock_t _srcCloneRWLock[2][2];
+	
+	virtual void _FetchNativeDisplayByID(const NDSDisplayID displayID, const u8 bufferIndex);
+	virtual void _FetchCustomDisplayByID(const NDSDisplayID displayID, const u8 bufferIndex);
+	
+public:
+	OGLClientFetchObject();
+	virtual ~OGLClientFetchObject();
+	
+	OGLContextInfo* GetContextInfo() const;
+	uint32_t* GetSrcClone(const NDSDisplayID displayID, const u8 bufferIndex) const;
+	GLuint GetTexNative(const NDSDisplayID displayID, const u8 bufferIndex) const;
+	GLuint GetTexCustom(const NDSDisplayID displayID, const u8 bufferIndex) const;
+	
+	// For lack of a better place, we're putting the HQnx LUTs in the fetch object because
+	// we know that it will be shared for all display views.
+	GLuint GetTexLQ2xLUT() const;
+	GLuint GetTexHQ2xLUT() const;
+	GLuint GetTexHQ3xLUT() const;
+	GLuint GetTexHQ4xLUT() const;
+	
+	void CopyFromSrcClone(uint32_t *dstBufferPtr, const NDSDisplayID displayID, const u8 bufferIndex);
+	
+	virtual void Init();
+	virtual void SetFetchBuffers(const NDSDisplayInfo &currentDisplayInfo);
 };
 
 class OGLVideoOutput : public ClientDisplay3DView
@@ -394,10 +406,7 @@ protected:
 	virtual void _UpdateClientSize();
 	virtual void _UpdateViewScale();
 	
-	virtual void _FetchNativeDisplayByID(const NDSDisplayID displayID);
-	virtual void _FetchCustomDisplayByID(const NDSDisplayID displayID);
 	virtual void _LoadNativeDisplayByID(const NDSDisplayID displayID);
-	virtual void _LoadCustomDisplayByID(const NDSDisplayID displayID);
 	
 public:
 	OGLVideoOutput();
@@ -421,17 +430,12 @@ public:
 	
 	virtual void SetFiltersPreferGPU(const bool preferGPU);
 	
-	virtual void SetVideoBuffers(const uint32_t colorFormat,
-								 const void *videoBufferHead,
-								 const void *nativeBuffer0,
-								 const void *nativeBuffer1,
-								 const void *customBuffer0, const size_t customWidth0, const size_t customHeight0,
-								 const void *customBuffer1, const size_t customWidth1, const size_t customHeight1);
-	
 	// Client view interface
 	virtual void ProcessDisplays();
-	virtual void FrameFinish();
+	virtual void FinishFrameAtIndex(const u8 bufferIndex);
 	virtual void RenderViewOGL();
+	virtual void LockDisplayTextures();
+	virtual void UnlockDisplayTextures();
 };
 
 extern void (*glBindVertexArrayDESMUME)(GLuint id);

desmume/src/frontend/cocoa/cocoa_GPU.h
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2013-2015 DeSmuME team
+	Copyright (C) 2013-2017 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -19,8 +19,38 @@
 #include <pthread.h>
 #include <libkern/OSAtomic.h>
 
+#import "cocoa_util.h"
+#include "../../GPU.h"
+
+#ifdef BOOL
+#undef BOOL
+#endif
+
+#if defined(MAC_OS_X_VERSION_10_11) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_11)
+//#define ENABLE_APPLE_METAL
+#endif
 
 class GPUEventHandlerOSX;
+
+@interface MacClientSharedObject : CocoaDSThread
+{
+	GPUClientFetchObject *GPUFetchObject;
+	pthread_rwlock_t *_rwlockFramebuffer[2];
+	pthread_mutex_t *_mutexOutputList;
+	NSMutableArray *_cdsOutputList;
+}
+
+@property (assign, nonatomic) GPUClientFetchObject *GPUFetchObject;
+
+- (const NDSDisplayInfo &) fetchDisplayInfoForIndex:(const u8)bufferIndex;
+- (pthread_rwlock_t *) rwlockFramebufferAtIndex:(const u8)bufferIndex;
+- (void) setOutputList:(NSMutableArray *)theOutputList mutex:(pthread_mutex_t *)theMutex;
+- (BOOL) isCPUFilteringNeeded;
+- (void) handleFetchFromBufferIndexAndPushVideo:(NSData *)indexData;
+- (void) pushVideoDataToAllDisplayViews;
+- (void) finishAllDisplayViewsAtIndex:(const u8)bufferIndex;
+
+@end
 
 @interface CocoaDSGPU : NSObject
 {
@@ -32,6 +62,7 @@ class GPUEventHandlerOSX;
 	
 	OSSpinLock spinlockGpuState;
 	GPUEventHandlerOSX *gpuEvent;
+	GPUClientFetchObject *fetchObject;
 }
 
 @property (assign) UInt32 gpuStateFlags;
@@ -39,6 +70,8 @@ class GPUEventHandlerOSX;
 @property (assign) NSUInteger gpuScale;
 @property (assign) NSUInteger gpuColorFormat;
 @property (readonly) pthread_rwlock_t *gpuFrameRWLock;
+@property (readonly, nonatomic) GPUClientFetchObject *fetchObject;
+@property (readonly, nonatomic) MacClientSharedObject *sharedData;
 
 @property (assign) BOOL layerMainGPU;
 @property (assign) BOOL layerMainBG0;

desmume/src/frontend/cocoa/cocoa_globals.h
@@ -404,6 +404,7 @@ enum
 	MESSAGE_SET_EMULATION_FLAGS,
 	
 	// Video Messages
+	MESSAGE_FETCH_AND_PUSH_VIDEO,
 	MESSAGE_RECEIVE_GPU_FRAME,
 	MESSAGE_CHANGE_VIEW_PROPERTIES,
 	MESSAGE_REDRAW_VIEW,

desmume/src/frontend/cocoa/cocoa_output.h
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2011 Roger Manuel
-	Copyright (C) 2011-2016 DeSmuME team
+	Copyright (C) 2011-2017 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -118,13 +118,11 @@ struct NDSFrameInfo;
 
 - (void) commitViewProperties:(const ClientDisplayViewProperties &)viewProps;
 
-- (void) doReceiveGPUFrame;
 - (void) handleReceiveGPUFrame;
 - (void) handleChangeViewProperties;
 - (void) handleRequestScreenshot:(NSData *)fileURLStringData fileTypeData:(NSData *)fileTypeData;
 - (void) handleCopyToPasteboard;
 
-- (void) finishFrame;
 - (void) takeFrameCount;
 - (void) setCPULoadAvgARM9:(uint32_t)loadAvgARM9 ARM7:(uint32_t)loadAvgARM7;
 - (NSImage *) image;
@@ -133,11 +131,7 @@ struct NDSFrameInfo;
 @end
 
 @interface CocoaDSDisplayVideo : CocoaDSDisplay
-{
-	void *_videoBuffer;
-	void *_nativeBuffer[2];
-	void *_customBuffer[2];
-	
+{	
 	OSSpinLock spinlockIsHUDVisible;
 	OSSpinLock spinlockUseVerticalSync;
 	OSSpinLock spinlockVideoFiltersPreferGPU;
@@ -165,7 +159,6 @@ struct NDSFrameInfo;
 - (void) handleReprocessRedraw;
 - (void) handleRedraw;
 
-- (void) resetVideoBuffers;
 - (void) setScaleFactor:(float)theScaleFactor;
 
 @end

commit 64d58b9b098d71b561d2be0d5511f2ae1395c106

Cocoa Port: Oops! Forgot to take out a reference to the not-yet-released Apple Metal renderer.
- Fixes building of the Xcode project. (Regression from commit aff2d07.)

desmume/src/frontend/cocoa/cocoa_GPU.mm
@@ -31,7 +31,10 @@
 
 #include <OpenGL/OpenGL.h>
 #include "userinterface/MacOGLDisplayView.h"
+
+#ifdef ENABLE_APPLE_METAL
 #include "userinterface/MacMetalDisplayView.h"
+#endif
 
 #ifdef BOOL
 #undef BOOL