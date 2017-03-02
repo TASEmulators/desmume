TASEmulators/desmume

commit ce494a2eaa9265ae4be9c99d9967d03a24f1d23f

Cocoa Port: Begin refactoring display view code to reduce the dependence on port-specific code.

desmume/src/frontend/cocoa/ClientDisplayView.cpp
@@ -1,0 +1,508 @@
+/*
+	Copyright (C) 2017 DeSmuME team
+ 
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+ 
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+ 
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ClientDisplayView.h"
+
+ClientDisplayView::ClientDisplayView()
+{
+	_property.normalWidth	= GPU_FRAMEBUFFER_NATIVE_WIDTH;
+	_property.normalHeight	= GPU_FRAMEBUFFER_NATIVE_HEIGHT * 2.0;
+	_property.clientWidth	= _property.normalWidth;
+	_property.clientHeight	= _property.normalHeight;
+	_property.rotation		= 0.0;
+	_property.viewScale		= 1.0;
+	_property.gapScale		= 0.0;
+	_property.gapDistance	= DS_DISPLAY_UNSCALED_GAP * _property.gapScale;
+	_property.mode			= ClientDisplayMode_Dual;
+	_property.layout		= ClientDisplayLayout_Vertical;
+	_property.order			= ClientDisplayOrder_MainFirst;
+	
+	_initialTouchInMajorDisplay = new InitialTouchPressMap;
+}
+
+ClientDisplayView::ClientDisplayView(const ClientDisplayViewProperties props)
+{
+	_property = props;
+	_initialTouchInMajorDisplay = new InitialTouchPressMap;
+}
+
+ClientDisplayView::~ClientDisplayView()
+{
+	delete _initialTouchInMajorDisplay;
+}
+
+ClientDisplayViewProperties ClientDisplayView::GetProperties() const
+{
+	return this->_property;
+}
+
+void ClientDisplayView::SetProperties(const ClientDisplayViewProperties props)
+{
+	this->_property = props;
+	
+	ClientDisplayView::CalculateNormalSize(props.mode, props.layout, props.gapScale, this->_property.normalWidth, this->_property.normalHeight);
+}
+
+void ClientDisplayView::SetClientSize(const double w, const double h)
+{
+	this->_property.clientWidth = w;
+	this->_property.clientHeight = h;
+	this->_property.viewScale = ClientDisplayView::GetMaxScalarWithinBounds(this->_property.normalWidth, this->_property.normalHeight, w, h);
+}
+
+double ClientDisplayView::GetRotation() const
+{
+	return this->_property.rotation;
+}
+
+void ClientDisplayView::SetRotation(const double r)
+{
+	this->_property.rotation = r;
+	
+	double checkWidth = this->_property.normalWidth;
+	double checkHeight = this->_property.normalHeight;
+	ClientDisplayView::ConvertNormalToTransformedBounds(1.0, r, checkWidth, checkHeight);
+	this->_property.viewScale = ClientDisplayView::GetMaxScalarWithinBounds(checkWidth, checkHeight, this->_property.clientWidth, this->_property.clientHeight);
+}
+
+double ClientDisplayView::GetViewScale() const
+{
+	return this->_property.viewScale;
+}
+
+ClientDisplayMode ClientDisplayView::GetMode() const
+{
+	return this->_property.mode;
+}
+
+void ClientDisplayView::SetMode(const ClientDisplayMode mode)
+{
+	this->_property.mode = mode;
+	ClientDisplayView::CalculateNormalSize(this->_property.mode, this->_property.layout, this->_property.gapScale, this->_property.normalWidth, this->_property.normalHeight);
+	this->_property.viewScale = ClientDisplayView::GetMaxScalarWithinBounds(this->_property.normalWidth, this->_property.normalHeight, this->_property.clientWidth, this->_property.clientHeight);
+}
+
+ClientDisplayLayout ClientDisplayView::GetLayout() const
+{
+	return this->_property.layout;
+}
+
+void ClientDisplayView::SetLayout(const ClientDisplayLayout layout)
+{
+	this->_property.layout = layout;
+	ClientDisplayView::CalculateNormalSize(this->_property.mode, this->_property.layout, this->_property.gapScale, this->_property.normalWidth, this->_property.normalHeight);
+	this->_property.viewScale = ClientDisplayView::GetMaxScalarWithinBounds(this->_property.normalWidth, this->_property.normalHeight, this->_property.clientWidth, this->_property.clientHeight);
+}
+
+ClientDisplayOrder ClientDisplayView::GetOrder() const
+{
+	return this->_property.order;
+}
+
+void ClientDisplayView::SetOrder(const ClientDisplayOrder order)
+{
+	this->_property.order = order;
+}
+
+double ClientDisplayView::GetGapScale() const
+{
+	return this->_property.gapScale;
+}
+
+void ClientDisplayView::SetGapWithScalar(const double gapScale)
+{
+	this->_property.gapScale = gapScale;
+	this->_property.gapDistance = (double)DS_DISPLAY_UNSCALED_GAP * gapScale;
+}
+
+double ClientDisplayView::GetGapDistance() const
+{
+	return this->_property.gapDistance;
+}
+
+void ClientDisplayView::SetGapWithDistance(const double gapDistance)
+{
+	this->_property.gapScale = gapDistance / (double)DS_DISPLAY_UNSCALED_GAP;
+	this->_property.gapDistance = gapDistance;
+}
+
+void ClientDisplayView::GetNDSPoint(const int inputID, const bool isInitialTouchPress,
+									const double clientX, const double clientY,
+									u8 &outX, u8 &outY) const
+{
+	double x = clientX;
+	double y = clientY;
+	double w = this->_property.normalWidth;
+	double h = this->_property.normalHeight;
+	
+	ClientDisplayView::ConvertNormalToTransformedBounds(1.0, this->_property.rotation, w, h);
+	const double s = ClientDisplayView::GetMaxScalarWithinBounds(w, h, this->_property.clientWidth, this->_property.clientHeight);
+	
+	ClientDisplayView::ConvertClientToNormalPoint(this->_property.normalWidth, this->_property.normalHeight,
+												  this->_property.clientWidth, this->_property.clientHeight,
+												  s,
+												  360.0 - this->_property.rotation,
+												  x, y);
+	
+	// Normalize the touch location to the DS.
+	if (this->_property.mode == ClientDisplayMode_Dual)
+	{
+		switch (this->_property.layout)
+		{
+			case ClientDisplayLayout_Horizontal:
+			{
+				if (this->_property.order == ClientDisplayOrder_MainFirst)
+				{
+					x -= GPU_FRAMEBUFFER_NATIVE_WIDTH;
+				}
+				break;
+			}
+				
+			case ClientDisplayLayout_Hybrid_3_2:
+			case ClientDisplayLayout_Hybrid_16_9:
+			case ClientDisplayLayout_Hybrid_16_10:
+			{
+				if (isInitialTouchPress)
+				{
+					const bool isClickWithinMajorDisplay = (this->_property.order == ClientDisplayOrder_TouchFirst) && (x >= 0.0) && (x < 256.0);
+					(*_initialTouchInMajorDisplay)[inputID] = isClickWithinMajorDisplay;
+				}
+				
+				const bool handleClickInMajorDisplay = (*_initialTouchInMajorDisplay)[inputID];
+				if (!handleClickInMajorDisplay)
+				{
+					const double minorDisplayScale = (this->_property.normalWidth - (double)GPU_FRAMEBUFFER_NATIVE_WIDTH) / (double)GPU_FRAMEBUFFER_NATIVE_WIDTH;
+					x = (x - GPU_FRAMEBUFFER_NATIVE_WIDTH) / minorDisplayScale;
+					y = y / minorDisplayScale;
+				}
+				break;
+			}
+				
+			default: // Default to vertical orientation.
+			{
+				if (this->_property.order == ClientDisplayOrder_TouchFirst)
+				{
+					y -= ((double)GPU_FRAMEBUFFER_NATIVE_HEIGHT + this->_property.gapDistance);
+				}
+				break;
+			}
+		}
+	}
+	
+	y = GPU_FRAMEBUFFER_NATIVE_HEIGHT - y;
+	
+	// Constrain the touch point to the DS dimensions.
+	if (x < 0.0)
+	{
+		x = 0.0;
+	}
+	else if (x > (GPU_FRAMEBUFFER_NATIVE_WIDTH - 1.0))
+	{
+		x = (GPU_FRAMEBUFFER_NATIVE_WIDTH - 1.0);
+	}
+	
+	if (y < 0.0)
+	{
+		y = 0.0;
+	}
+	else if (y > (GPU_FRAMEBUFFER_NATIVE_HEIGHT - 1.0))
+	{
+		y = (GPU_FRAMEBUFFER_NATIVE_HEIGHT - 1.0);
+	}
+	
+	outX = (u8)(x + 0.001);
+	outY = (u8)(y + 0.001);
+}
+
+/********************************************************************************************
+	ConvertNormalToTransformedBounds()
+
+	Returns the bounds of a normalized 2D surface using affine transformations.
+
+	Takes:
+		scalar - The scalar used to transform the 2D surface.
+		angleDegrees - The rotation angle, in degrees, to transform the 2D surface.
+ 		inoutWidth - The width of the normal 2D surface.
+ 		inoutHeight - The height of the normal 2D surface.
+
+	Returns:
+		The bounds of a normalized 2D surface using affine transformations.
+
+	Details:
+		The returned bounds is always a normal rectangle. Ignoring the scaling, the
+		returned bounds will always be at its smallest when the angle is at 0, 90, 180,
+		or 270 degrees, and largest when the angle is at 45, 135, 225, or 315 degrees.
+ ********************************************************************************************/
+void ClientDisplayView::ConvertNormalToTransformedBounds(const double scalar,
+														 const double angleDegrees,
+														 double &inoutWidth, double &inoutHeight)
+{
+	const double angleRadians = angleDegrees * (M_PI/180.0);
+	
+	// The points are as follows:
+	//
+	// (x[3], y[3])    (x[2], y[2])
+	//
+	//
+	//
+	// (x[0], y[0])    (x[1], y[1])
+	
+	// Do our scale and rotate transformations.
+#ifdef __ACCELERATE__
+	
+	// Note that although we only need to calculate 3 points, we include 4 points
+	// here because Accelerate prefers 16-byte alignment.
+	double x[] = {0.0, inoutWidth, inoutWidth, 0.0};
+	double y[] = {0.0, 0.0, inoutHeight, inoutHeight};
+	
+	cblas_drot(4, x, 1, y, 1, scalar * cos(angleRadians), scalar * sin(angleRadians));
+	
+#else // Keep a C-version of this transformation for reference purposes.
+	
+	const double w = scalar * inoutWidth;
+	const double h = scalar * inoutHeight;
+	const double d = hypot(w, h);
+	const double dAngle = atan2(h, w);
+	
+	const double px = w * cos(angleRadians);
+	const double py = w * sin(angleRadians);
+	const double qx = d * cos(dAngle + angleRadians);
+	const double qy = d * sin(dAngle + angleRadians);
+	const double rx = h * cos((M_PI/2.0) + angleRadians);
+	const double ry = h * sin((M_PI/2.0) + angleRadians);
+	
+	const double x[] = {0.0, px, qx, rx};
+	const double y[] = {0.0, py, qy, ry};
+	
+#endif
+	
+	// Determine the transformed width, which is dependent on the location of
+	// the x-coordinate of point (x[2], y[2]).
+	if (x[2] > 0.0)
+	{
+		if (x[2] < x[3])
+		{
+			inoutWidth = x[3] - x[1];
+		}
+		else if (x[2] < x[1])
+		{
+			inoutWidth = x[1] - x[3];
+		}
+		else
+		{
+			inoutWidth = x[2];
+		}
+	}
+	else if (x[2] < 0.0)
+	{
+		if (x[2] > x[3])
+		{
+			inoutWidth = -(x[3] - x[1]);
+		}
+		else if (x[2] > x[1])
+		{
+			inoutWidth = -(x[1] - x[3]);
+		}
+		else
+		{
+			inoutWidth = -x[2];
+		}
+	}
+	else
+	{
+		inoutWidth = fabs(x[1] - x[3]);
+	}
+	
+	// Determine the transformed height, which is dependent on the location of
+	// the y-coordinate of point (x[2], y[2]).
+	if (y[2] > 0.0)
+	{
+		if (y[2] < y[3])
+		{
+			inoutHeight = y[3] - y[1];
+		}
+		else if (y[2] < y[1])
+		{
+			inoutHeight = y[1] - y[3];
+		}
+		else
+		{
+			inoutHeight = y[2];
+		}
+	}
+	else if (y[2] < 0.0)
+	{
+		if (y[2] > y[3])
+		{
+			inoutHeight = -(y[3] - y[1]);
+		}
+		else if (y[2] > y[1])
+		{
+			inoutHeight = -(y[1] - y[3]);
+		}
+		else
+		{
+			inoutHeight = -y[2];
+		}
+	}
+	else
+	{
+		inoutHeight = fabs(y[3] - y[1]);
+	}
+}
+
+/********************************************************************************************
+	GetMaxScalarWithinBounds()
+
+	Returns the maximum scalar that a rectangle can grow, while maintaining its aspect
+	ratio, within a boundary.
+
+	Takes:
+		normalBoundsWidth - The width of the normal 2D surface.
+		normalBoundsHeight - The height of the normal 2D surface.
+		keepInBoundsWidth - The width of the keep-in 2D surface.
+		keepInBoundsHeight - The height of the keep-in 2D surface.
+
+	Returns:
+		The maximum scalar that a rectangle can grow, while maintaining its aspect ratio,
+		within a boundary.
+
+	Details:
+		If keepInBoundsWidth or keepInBoundsHeight are less than or equal to zero, the
+		returned scalar will be zero.
+ ********************************************************************************************/
+double ClientDisplayView::GetMaxScalarWithinBounds(const double normalBoundsWidth, const double normalBoundsHeight,
+												   const double keepInBoundsWidth, const double keepInBoundsHeight)
+{
+	const double maxX = (normalBoundsWidth <= 0.0) ? 0.0 : keepInBoundsWidth / normalBoundsWidth;
+	const double maxY = (normalBoundsHeight <= 0.0) ? 0.0 : keepInBoundsHeight / normalBoundsHeight;
+	
+	return (maxX <= maxY) ? maxX : maxY;
+}
+
+/********************************************************************************************
+	ConvertClientToNormalPoint()
+
+	Returns a normalized point from a point from a 2D transformed surface.
+
+	Takes:
+		normalWidth - The width of the normal 2D surface.
+		normalHeight - The height of the normal 2D surface.
+		clientWidth - The width of the client 2D surface.
+		clientHeight - The height of the client 2D surface.
+		scalar - The scalar used on the transformed 2D surface.
+		angleDegrees - The rotation angle, in degrees, of the transformed 2D surface.
+ 		inoutX - The X coordinate of a 2D point as it exists on a 2D transformed surface.
+ 		inoutY - The Y coordinate of a 2D point as it exists on a 2D transformed surface.
+	
+	Returns:
+		A normalized point from a point from a 2D transformed surface.
+
+	Details:
+		It may help to call GetMaxScalarWithinBounds() for the scalar parameter.
+ ********************************************************************************************/
+void ClientDisplayView::ConvertClientToNormalPoint(const double normalWidth, const double normalHeight,
+												   const double clientWidth, const double clientHeight,
+												   const double scalar,
+												   const double angleDegrees,
+												   double &inoutX, double &inoutY)
+{
+	// Get the coordinates of the client point and translate the coordinate
+	// system so that the origin becomes the center.
+	const double clientX = inoutX - (clientWidth / 2.0);
+	const double clientY = inoutY - (clientHeight / 2.0);
+	
+	// Perform rect-polar conversion.
+	
+	// Get the radius r with respect to the origin.
+	const double r = hypot(clientX, clientY) / scalar;
+	
+	// Get the angle theta with respect to the origin.
+	double theta = 0.0;
+	
+	if (clientX == 0.0)
+	{
+		if (clientY > 0.0)
+		{
+			theta = M_PI / 2.0;
+		}
+		else if (clientY < 0.0)
+		{
+			theta = M_PI * 1.5;
+		}
+	}
+	else if (clientX < 0.0)
+	{
+		theta = M_PI - atan2(clientY, -clientX);
+	}
+	else if (clientY < 0.0)
+	{
+		theta = atan2(clientY, clientX) + (M_PI * 2.0);
+	}
+	else
+	{
+		theta = atan2(clientY, clientX);
+	}
+	
+	// Get the normalized angle and use it to rotate about the origin.
+	// Then do polar-rect conversion and translate back to normal coordinates
+	// with a 0 degree rotation.
+	const double angleRadians = angleDegrees * (M_PI/180.0);
+	const double normalizedAngle = theta - angleRadians;
+	inoutX = (r * cos(normalizedAngle)) + (normalWidth / 2.0);
+	inoutY = (r * sin(normalizedAngle)) + (normalHeight / 2.0);
+}
+
+void ClientDisplayView::CalculateNormalSize(const ClientDisplayMode mode, const ClientDisplayLayout layout, const double gapScale,
+											double &outWidth, double &outHeight)
+{
+	if (mode == ClientDisplayMode_Dual)
+	{
+		switch (layout)
+		{
+			case ClientDisplayLayout_Horizontal:
+				outWidth  = (double)GPU_FRAMEBUFFER_NATIVE_WIDTH*2.0;
+				outHeight = (double)GPU_FRAMEBUFFER_NATIVE_HEIGHT;
+				break;
+				
+			case ClientDisplayLayout_Hybrid_3_2:
+				outWidth  = (double)GPU_FRAMEBUFFER_NATIVE_WIDTH + (128.0);
+				outHeight = (double)GPU_FRAMEBUFFER_NATIVE_HEIGHT;
+				break;
+				
+			case ClientDisplayLayout_Hybrid_16_9:
+				outWidth  = (double)GPU_FRAMEBUFFER_NATIVE_WIDTH + (64.0 * 4.0 / 3.0);
+				outHeight = (double)GPU_FRAMEBUFFER_NATIVE_HEIGHT;
+				break;
+				
+			case ClientDisplayLayout_Hybrid_16_10:
+				outWidth  = (double)GPU_FRAMEBUFFER_NATIVE_WIDTH + (51.2);
+				outHeight = (double)GPU_FRAMEBUFFER_NATIVE_HEIGHT;
+				break;
+				
+			default: // Default to vertical orientation.
+				outWidth  = (double)GPU_FRAMEBUFFER_NATIVE_WIDTH;
+				outHeight = (double)GPU_FRAMEBUFFER_NATIVE_HEIGHT*2.0 + (DS_DISPLAY_UNSCALED_GAP*gapScale);
+				break;
+		}
+	}
+	else
+	{
+		outWidth  = (double)GPU_FRAMEBUFFER_NATIVE_WIDTH;
+		outHeight = (double)GPU_FRAMEBUFFER_NATIVE_HEIGHT;
+	}
+}

desmume/src/frontend/cocoa/ClientDisplayView.h
@@ -1,0 +1,118 @@
+/*
+	Copyright (C) 2017 DeSmuME team
+ 
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+ 
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+ 
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CLIENT_DISPLAY_VIEW_H_
+#define _CLIENT_DISPLAY_VIEW_H_
+
+#include <map>
+#include "../../GPU.h"
+
+#define DS_DISPLAY_VERTICAL_GAP_TO_HEIGHT_RATIO		(21.0/46.0) // Based on the official DS specification: 21mm/46mm
+#define DS_DISPLAY_UNSCALED_GAP						(GPU_FRAMEBUFFER_NATIVE_HEIGHT * DS_DISPLAY_VERTICAL_GAP_TO_HEIGHT_RATIO)
+
+enum ClientDisplayMode
+{
+	ClientDisplayMode_Main = 0,
+	ClientDisplayMode_Touch,
+	ClientDisplayMode_Dual
+};
+
+enum ClientDisplayLayout
+{
+	ClientDisplayLayout_Vertical		= 0,
+	ClientDisplayLayout_Horizontal		= 1,
+	ClientDisplayLayout_Hybrid_3_2		= 1000,
+	ClientDisplayLayout_Hybrid_16_9		= 1001,
+	ClientDisplayLayout_Hybrid_16_10	= 1002
+};
+
+enum ClientDisplayOrder
+{
+	ClientDisplayOrder_MainFirst = 0,
+	ClientDisplayOrder_TouchFirst
+};
+
+typedef std::map<int, bool> InitialTouchPressMap;  // Key = An ID number of the host input, Value = Flag that indicates if the initial touch press was in the major display
+
+typedef struct
+{
+	double normalWidth;
+	double normalHeight;
+	double clientWidth;
+	double clientHeight;
+	double rotation;
+	double viewScale;
+	double gapScale;
+	double gapDistance;
+	ClientDisplayMode mode;
+	ClientDisplayLayout layout;
+	ClientDisplayOrder order;
+} ClientDisplayViewProperties;
+
+class ClientDisplayView
+{
+protected:
+	ClientDisplayViewProperties _property;
+	InitialTouchPressMap *_initialTouchInMajorDisplay;
+	
+public:
+	ClientDisplayView();
+	ClientDisplayView(const ClientDisplayViewProperties props);
+	~ClientDisplayView();
+	
+	ClientDisplayViewProperties GetProperties() const;
+	void SetProperties(const ClientDisplayViewProperties properties);
+	
+	void SetClientSize(const double w, const double h);
+	
+	double GetRotation() const;
+	void SetRotation(const double r);
+	double GetViewScale() const;
+	ClientDisplayMode GetMode() const;
+	void SetMode(const ClientDisplayMode mode);
+	ClientDisplayLayout GetLayout() const;
+	void SetLayout(const ClientDisplayLayout layout);
+	ClientDisplayOrder GetOrder() const;
+	void SetOrder(const ClientDisplayOrder order);
+	
+	double GetGapScale() const;
+	void SetGapWithScalar(const double gapScale);
+	double GetGapDistance() const;
+	void SetGapWithDistance(const double gapDistance);
+	
+	void GetNDSPoint(const int inputID, const bool isInitialTouchPress,
+					 const double clientX, const double clientY,
+					 u8 &outX, u8 &outY) const;
+	
+	static void ConvertNormalToTransformedBounds(const double scalar,
+												 const double angleDegrees,
+												 double &inoutWidth, double &inoutHeight);
+	
+	static double GetMaxScalarWithinBounds(const double normalBoundsWidth, const double normalBoundsHeight,
+										   const double keepInBoundsWidth, const double keepInBoundsHeight);
+	
+	static void ConvertClientToNormalPoint(const double normalBoundsWidth, const double normalBoundsHeight,
+										   const double transformBoundsWidth, const double transformBoundsHeight,
+										   const double scalar,
+										   const double angleDegrees,
+										   double &inoutX, double &inoutY);
+	
+	static void CalculateNormalSize(const ClientDisplayMode mode, const ClientDisplayLayout layout, const double gapScale,
+									double &outWidth, double &outHeight);
+};
+
+#endif // _CLIENT_DISPLAY_VIEW_H_

desmume/src/frontend/cocoa/DeSmuME (Latest).xcodeproj/project.pbxproj
@@ -835,6 +835,9 @@
 		ABACB8DC1710B621003B845D /* AudioToolbox.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = ABACB8DB1710B621003B845D /* AudioToolbox.framework */; };
 		ABACB8DD1710B656003B845D /* AudioToolbox.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = ABACB8DB1710B621003B845D /* AudioToolbox.framework */; };
 		ABACB8DE1710B65F003B845D /* AudioToolbox.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = ABACB8DB1710B621003B845D /* AudioToolbox.framework */; };
+		ABAD07DC1E19CAA6007867CA /* ClientDisplayView.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD07DA1E19CAA6007867CA /* ClientDisplayView.cpp */; };
+		ABAD07DD1E19CAA6007867CA /* ClientDisplayView.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD07DA1E19CAA6007867CA /* ClientDisplayView.cpp */; };
+		ABAD07DE1E19CAA6007867CA /* ClientDisplayView.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD07DA1E19CAA6007867CA /* ClientDisplayView.cpp */; };
 		ABAD3E7113AF1D6D00502E1E /* AAFilter.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD3E6513AF1D6D00502E1E /* AAFilter.cpp */; };
 		ABAD3E7413AF1D6D00502E1E /* FIFOSampleBuffer.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD3E6813AF1D6D00502E1E /* FIFOSampleBuffer.cpp */; };
 		ABAD3E7513AF1D6D00502E1E /* FIRFilter.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABAD3E6913AF1D6D00502E1E /* FIRFilter.cpp */; };
@@ -1499,6 +1502,8 @@
 		ABA731671BB51FDC00B26147 /* type1.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; name = type1.c; path = type1/type1.c; sourceTree = "<group>"; };
 		ABAB454E187CDB70007BE20C /* Image_GuitarGrip.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Image_GuitarGrip.png; path = images/Image_GuitarGrip.png; sourceTree = "<group>"; };
 		ABACB8DB1710B621003B845D /* AudioToolbox.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = AudioToolbox.framework; path = System/Library/Frameworks/AudioToolbox.framework; sourceTree = SDKROOT; };
+		ABAD07DA1E19CAA6007867CA /* ClientDisplayView.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = ClientDisplayView.cpp; sourceTree = "<group>"; };
+		ABAD07DB1E19CAA6007867CA /* ClientDisplayView.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ClientDisplayView.h; sourceTree = "<group>"; };
 		ABAD3E5913AF1D6D00502E1E /* AAFilter.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = AAFilter.h; sourceTree = "<group>"; };
 		ABAD3E5A13AF1D6D00502E1E /* BPMDetect.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = BPMDetect.h; sourceTree = "<group>"; };
 		ABAD3E5B13AF1D6D00502E1E /* cpu_detect.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = cpu_detect.h; sourceTree = "<group>"; };
@@ -1953,6 +1958,7 @@
 				AB3ACB6514C2361100D7D192 /* userinterface */,
 				AB82445A1704AE9A00B8EE20 /* utilities.c */,
 				ABD10AE51715FCDD00B5729D /* audiosamplegenerator.cpp */,
+				ABAD07DA1E19CAA6007867CA /* ClientDisplayView.cpp */,
 				AB1B9E5F1501A78000464647 /* coreaudiosound.cpp */,
 				AB23567216C2F6F400DA782E /* macosx_10_5_compat.cpp */,
 				ABD10AE61715FCDD00B5729D /* mic_ext.cpp */,
@@ -1961,6 +1967,7 @@
 				AB1B9E601501A78000464647 /* ringbuffer.cpp */,
 				ABD104141346652500AF11D1 /* sndOSX.cpp */,
 				ABD10AE31715FCDD00B5729D /* audiosamplegenerator.h */,
+				ABAD07DB1E19CAA6007867CA /* ClientDisplayView.h */,
 				ABA6574914511EC90077E5E9 /* cocoa_cheat.h */,
 				ABD103FE1346652500AF11D1 /* cocoa_core.h */,
 				AB58F32B1364F44B0074C376 /* cocoa_file.h */,
@@ -3781,6 +3788,7 @@
 				AB6FBEF6139B6258007BB045 /* slot1_retail_nand.cpp in Sources */,
 				ABCFA9F5178BDE920030C8BA /* encrypt.cpp in Sources */,
 				ABD1FF0E1345AC9C00AF11D1 /* slot2_expMemory.cpp in Sources */,
+				ABAD07DE1E19CAA6007867CA /* ClientDisplayView.cpp in Sources */,
 				ABD1FF0F1345AC9C00AF11D1 /* slot2_gbagame.cpp in Sources */,
 				ABD1FF101345AC9C00AF11D1 /* slot2_guitarGrip.cpp in Sources */,
 				AB000DD21CCC6B3A00413F02 /* file_path.c in Sources */,
@@ -3938,6 +3946,7 @@
 				ABFEA8341BB4EC1100B08C25 /* ftmm.c in Sources */,
 				AB4C81E41B21676C00ACECD5 /* hq3x.cpp in Sources */,
 				AB796CF815CDCBA200C59155 /* cp15.cpp in Sources */,
+				ABAD07DC1E19CAA6007867CA /* ClientDisplayView.cpp in Sources */,
 				AB796CF915CDCBA200C59155 /* cpu_detect_x86_gcc.cpp in Sources */,
 				AB796CFA15CDCBA200C59155 /* crc.cpp in Sources */,
 				AB301BDF1D9C8BAC00246A93 /* deposterize.cpp in Sources */,
@@ -4266,6 +4275,7 @@
 				AB8F3CD11A53AC2600A80BF6 /* slot2_passme.cpp in Sources */,
 				AB8F3CD21A53AC2600A80BF6 /* WavFile.cpp in Sources */,
 				AB8F3CD31A53AC2600A80BF6 /* wifi.cpp in Sources */,
+				ABAD07DD1E19CAA6007867CA /* ClientDisplayView.cpp in Sources */,
 				AB8F3CD41A53AC2600A80BF6 /* slot1_retail_mcrom.cpp in Sources */,
 				AB8F3CD51A53AC2600A80BF6 /* xstring.cpp in Sources */,
 				AB8F3CD61A53AC2600A80BF6 /* main.m in Sources */,

commit 1e96e738d216f40ed16e7f9997a4352213473306

Cocoa Port: Correct the touch display location transformation for Retina displays. (Regression from commit ce494a2. Fixes #42.)

desmume/src/frontend/cocoa/ClientDisplayView.cpp
@@ -649,12 +649,14 @@ void ClientDisplayView::GetNDSPoint(const int inputID, const bool isInitialTouch
 	double y = clientY;
 	double w = this->_renderProperty.normalWidth;
 	double h = this->_renderProperty.normalHeight;
+	const double logicalClientWidth  = this->_renderProperty.clientWidth  / this->_scaleFactor;
+	const double logicalClientHeight = this->_renderProperty.clientHeight / this->_scaleFactor;
 	
 	ClientDisplayView::ConvertNormalToTransformedBounds(1.0, this->_renderProperty.rotation, w, h);
-	const double s = ClientDisplayView::GetMaxScalarWithinBounds(w, h, this->_renderProperty.clientWidth, this->_renderProperty.clientHeight);
+	const double s = ClientDisplayView::GetMaxScalarWithinBounds(w, h, logicalClientWidth, logicalClientHeight);
 	
 	ClientDisplayView::ConvertClientToNormalPoint(this->_renderProperty.normalWidth, this->_renderProperty.normalHeight,
-												  this->_renderProperty.clientWidth, this->_renderProperty.clientHeight,
+												  logicalClientWidth, logicalClientHeight,
 												  s,
 												  this->_renderProperty.rotation,
 												  x, y);