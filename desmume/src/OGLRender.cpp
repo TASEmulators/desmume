TASEmulators/desmume

commit 8c37d4acd62f8f7180f18ab7937da91c89fbcf64

OpenGL Renderer: Remove extraneous OpenGL v1.x classes, and do more aggressive capabilities checks on init.
- Keep OpenGLRenderer_1_2 as the sole OpenGL v1.x class, and then
remove the following classes: OpenGLRenderer_1_3, OpenGLRenderer_1_4,
OpenGLRenderer_1_5.

desmume/src/OGLRender.h
@@ -311,6 +311,8 @@ enum OGLErrorCode
 {
 	OGLERROR_NOERR = RENDER3DERROR_NOERR,
 	
+	OGLERROR_DRIVER_VERSION_TOO_OLD,
+	
 	OGLERROR_BEGINGL_FAILED,
 	
 	OGLERROR_FEATURE_UNSUPPORTED,
@@ -768,43 +770,9 @@ public:
 	virtual Render3DError SetFramebufferSize(size_t w, size_t h);
 };
 
-class OpenGLRenderer_1_3 : public OpenGLRenderer_1_2
+class OpenGLRenderer_2_0 : public OpenGLRenderer_1_2
 {
 protected:
-	virtual Render3DError CreateToonTable();
-	virtual Render3DError UploadClearImage(const u16 *__restrict colorBuffer, const u32 *__restrict depthBuffer, const u8 *__restrict fogBuffer, const u8 *__restrict polyIDBuffer);
-	
-public:
-	virtual Render3DError UpdateToonTable(const u16 *toonTableBuffer);
-	virtual Render3DError SetFramebufferSize(size_t w, size_t h);
-};
-
-class OpenGLRenderer_1_4 : public OpenGLRenderer_1_3
-{
-protected:
-	virtual Render3DError InitFinalRenderStates(const std::set<std::string> *oglExtensionSet);
-};
-
-class OpenGLRenderer_1_5 : public OpenGLRenderer_1_4
-{
-protected:
-	virtual Render3DError CreateVBOs();
-	virtual void DestroyVBOs();
-	virtual Render3DError CreateVAOs();
-	
-	virtual Render3DError EnableVertexAttributes();
-	virtual Render3DError DisableVertexAttributes();
-	virtual Render3DError BeginRender(const GFX3D &engine);
-	
-public:
-	~OpenGLRenderer_1_5();
-};
-
-class OpenGLRenderer_2_0 : public OpenGLRenderer_1_5
-{
-protected:
-	virtual Render3DError InitFinalRenderStates(const std::set<std::string> *oglExtensionSet);
-	
 	virtual Render3DError EnableVertexAttributes();
 	virtual Render3DError DisableVertexAttributes();
 	

commit c0a219329045002187e2b904c8c67b5b97778143

OpenGL Renderer: Bring back OpenGLRenderer_2_0::InitFinalRenderStates(), fixing blending for the v3.2 renderer. (Regression from commit 8c37d4a.)

desmume/src/OGLRender.cpp
@@ -4428,6 +4428,29 @@ Render3DError OpenGLRenderer_1_2::SetFramebufferSize(size_t w, size_t h)
 	return OGLERROR_NOERR;
 }
 
+Render3DError OpenGLRenderer_2_0::InitFinalRenderStates(const std::set<std::string> *oglExtensionSet)
+{
+	OGLRenderRef &OGLRef = *this->ref;
+	
+	// we want to use alpha destination blending so we can track the last-rendered alpha value
+	// test: new super mario brothers renders the stormclouds at the beginning
+	
+	// Blending Support
+	glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA, GL_DST_ALPHA);
+	glBlendEquationSeparate(GL_FUNC_ADD, GL_MAX);
+	
+	// Mirrored Repeat Mode Support
+	OGLRef.stateTexMirroredRepeat = GL_MIRRORED_REPEAT;
+	
+	// Ignore our color buffer since we'll transfer the polygon alpha through a uniform.
+	OGLRef.color4fBuffer = NULL;
+	
+	// VBOs are supported here, so just use the index buffer on the GPU.
+	OGLRef.vertIndexBuffer = NULL;
+	
+	return OGLERROR_NOERR;
+}
+
 Render3DError OpenGLRenderer_2_0::EnableVertexAttributes()
 {
 	OGLRenderRef &OGLRef = *this->ref;

desmume/src/OGLRender.h
@@ -778,6 +778,8 @@ public:
 class OpenGLRenderer_2_0 : public OpenGLRenderer_1_2
 {
 protected:
+	virtual Render3DError InitFinalRenderStates(const std::set<std::string> *oglExtensionSet);
+	
 	virtual Render3DError EnableVertexAttributes();
 	virtual Render3DError DisableVertexAttributes();
 	