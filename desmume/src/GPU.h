TASEmulators/desmume

commit f8d129b164a63c02ade48561c321d3f296e08356

Use only MSB_FIRST for determining endianness

desmume/src/texcache.cpp
@@ -296,7 +296,7 @@ public:
 		//dump the palette to a temp buffer, so that we don't have to worry about memory mapping.
 		//this isnt such a problem with texture memory, because we read sequentially from it.
 		//however, we read randomly from palette memory, so the mapping is more costly.
-		#ifdef WORDS_BIGENDIAN
+		#ifdef MSB_FIRST
 			mspal.dump16(pal);
 		#else
 			mspal.dump(pal);
@@ -764,7 +764,7 @@ public:
 								break;
 								
 							case 1:
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 								tmp_col[2]	= ( (((tmp_col[0] & 0xFF000000) >> 1)+((tmp_col[1] & 0xFF000000)  >> 1)) & 0xFF000000 ) |
 											  ( (((tmp_col[0] & 0x00FF0000)      + (tmp_col[1] & 0x00FF0000)) >> 1)  & 0x00FF0000 ) |
 											  ( (((tmp_col[0] & 0x0000FF00)      + (tmp_col[1] & 0x0000FF00)) >> 1)  & 0x0000FF00 ) |
@@ -785,7 +785,7 @@ public:
 								
 							case 3:
 							{
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 								const u32 r0	= (tmp_col[0]>>24) & 0x000000FF;
 								const u32 r1	= (tmp_col[1]>>24) & 0x000000FF;
 								const u32 g0	= (tmp_col[0]>>16) & 0x000000FF;
@@ -818,7 +818,7 @@ public:
 						{
 							for (size_t i = 0; i < 4; i++)
 							{
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 								const u32 a = (tmp_col[i] >> 3) & 0x0000001F;
 								tmp_col[i] >>= 2;
 								tmp_col[i] &= 0x3F3F3F00;

desmume/src/GPU.cpp
@@ -1314,10 +1314,10 @@ void GPUEngineBase::ParseReg_BGnHOFS()
 	const IOREG_BGnHOFS &BGnHOFS = this->_IORegisterMap->BGnOFS[LAYERID].BGnHOFS;
 	this->_BGLayer[LAYERID].BGnHOFS = BGnHOFS;
 	
-#ifdef LOCAL_LE
-	this->_BGLayer[LAYERID].xOffset = BGnHOFS.Offset;
-#else
+#ifdef MSB_FIRST
 	this->_BGLayer[LAYERID].xOffset = LOCAL_TO_LE_16(BGnHOFS.value) & 0x01FF;
+#else
+	this->_BGLayer[LAYERID].xOffset = BGnHOFS.Offset;
 #endif
 }
 
@@ -1327,10 +1327,10 @@ void GPUEngineBase::ParseReg_BGnVOFS()
 	const IOREG_BGnVOFS &BGnVOFS = this->_IORegisterMap->BGnOFS[LAYERID].BGnVOFS;
 	this->_BGLayer[LAYERID].BGnVOFS = BGnVOFS;
 	
-#ifdef LOCAL_LE
-	this->_BGLayer[LAYERID].yOffset = BGnVOFS.Offset;
-#else
+#ifdef MSB_FIRST
 	this->_BGLayer[LAYERID].yOffset = LOCAL_TO_LE_16(BGnVOFS.value) & 0x01FF;
+#else
+	this->_BGLayer[LAYERID].yOffset = BGnVOFS.Offset;
 #endif
 }
 
@@ -1483,9 +1483,7 @@ void GPUEngineBase::_LineColorCopy(void *__restrict dstBuffer, const void *__res
 		
 #if defined(ENABLE_SSE2)
 		MACRODO_N( GPU_FRAMEBUFFER_NATIVE_WIDTH / (sizeof(__m128i) / PIXELBYTES), _mm_stream_si128((__m128i *)dst + (X), _mm_load_si128((__m128i *)src + (X))) );
-#elif LOCAL_LE
-		memcpy(dst, src, GPU_FRAMEBUFFER_NATIVE_WIDTH * PIXELBYTES);
-#else
+#elif MSB_FIRST
 		if (NEEDENDIANSWAP)
 		{
 			for (size_t i = 0; i < GPU_FRAMEBUFFER_NATIVE_WIDTH; i++)
@@ -1504,6 +1502,8 @@ void GPUEngineBase::_LineColorCopy(void *__restrict dstBuffer, const void *__res
 		{
 			memcpy(dst, src, GPU_FRAMEBUFFER_NATIVE_WIDTH * PIXELBYTES);
 		}
+#else
+		memcpy(dst, src, GPU_FRAMEBUFFER_NATIVE_WIDTH * PIXELBYTES);
 #endif
 	}
 	else if (!NATIVEDST && !NATIVESRC)
@@ -1515,9 +1515,7 @@ void GPUEngineBase::_LineColorCopy(void *__restrict dstBuffer, const void *__res
 		void *__restrict dst = (USELINEINDEX) ? (u8 *)dstBuffer + (lineIndex * lineWidth * PIXELBYTES) : (u8 *)dstBuffer;
 		const void *__restrict src = (USELINEINDEX) ? (u8 *)srcBuffer + (lineIndex * lineWidth * PIXELBYTES) : (u8 *)srcBuffer;
 		
-#if defined(LOCAL_LE)
-		memcpy(dst, src, lineWidth * lineCount * PIXELBYTES);
-#else
+#if defined(MSB_FIRST)
 		if (NEEDENDIANSWAP)
 		{
 			for (size_t i = 0; i < lineWidth * lineCount; i++)
@@ -1536,6 +1534,8 @@ void GPUEngineBase::_LineColorCopy(void *__restrict dstBuffer, const void *__res
 		{
 			memcpy(dst, src, lineWidth * lineCount * PIXELBYTES);
 		}
+#else
+		memcpy(dst, src, lineWidth * lineCount * PIXELBYTES);
 #endif
 	}
 	else if (NATIVEDST && !NATIVESRC)
@@ -2422,7 +2422,7 @@ void GPUEngineBase::_RenderPixelIterate_Final(GPUEngineCompositorInfo &compInfo,
 	IOREG_BGnX x = param.BGnX;
 	IOREG_BGnY y = param.BGnY;
 	
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 	// This only seems to work in the unrotated/unscaled case. I'm not too sure
 	// about how these bits should really be arranged on big-endian, but at
 	// least this arrangement fixes a bunch of games that use affine or extended

desmume/src/MMU.h
@@ -316,39 +316,39 @@ typedef union
 {
 	u8 value;
 	
-#ifdef LOCAL_LE
+#ifdef MSB_FIRST
 	struct
 	{
-		unsigned MST:3;
-		unsigned OFS:2;
-		unsigned :2;
 		unsigned Enable:1;
+		unsigned :2;
+		unsigned OFS:2;
+		unsigned MST:3;
 	};
 	
 	struct
 	{
-		unsigned MST_ABHI:2;
-		unsigned :1;
-		unsigned OFS_ABHI:2;
-		unsigned :2;
 		unsigned Enable_ABHI:1;
+		unsigned :2;
+		unsigned OFS_ABHI:2;
+		unsigned :1;
+		unsigned MST_ABHI:2;
 	};
 #else
 	struct
 	{
-		unsigned Enable:1;
-		unsigned :2;
-		unsigned OFS:2;
 		unsigned MST:3;
+		unsigned OFS:2;
+		unsigned :2;
+		unsigned Enable:1;
 	};
 	
 	struct
 	{
-		unsigned Enable_ABHI:1;
-		unsigned :2;
-		unsigned OFS_ABHI:2;
-		unsigned :1;
 		unsigned MST_ABHI:2;
+		unsigned :1;
+		unsigned OFS_ABHI:2;
+		unsigned :2;
+		unsigned Enable_ABHI:1;
 	};
 #endif
 } VRAMCNT;

desmume/src/NDSSystem.cpp
@@ -436,7 +436,7 @@ bool GameInfo::loadROM(std::string fname, u32 type)
 
 	if (res)
 	{
-#ifndef LOCAL_LE
+#ifdef MSB_FIRST
 		//endian swap necessary fields. It would be better if we made accessors for these. I wonder if you could make a macro for a field accessor that would take the bitsize and do the swap on the fly
 		struct FieldSwap {
 			const size_t offset;
@@ -2760,7 +2760,7 @@ void NDS_setTouchPos(u16 x, u16 y)
 	if(movieMode != MOVIEMODE_INACTIVE && movieMode != MOVIEMODE_FINISHED)
 	{
 		// just in case, since the movie only stores 8 bits per touch coord
-#ifdef WORDS_BIGENDIAN
+#ifdef MSB_FIRST
 		rawUserInput.touch.touchX &= 0xF00F;
 		rawUserInput.touch.touchY &= 0xF00F;
 #else

desmume/src/addons/slot1comp_protocol.cpp
@@ -99,10 +99,10 @@ void Slot1Comp_Protocol::write_command_KEY1(GC_Command command)
 			
 			//TODO - more endian-safe way of doing this (theres examples in R4)
 			{
-#ifdef LOCAL_LE
-				u64 cmd64 = bswap64(*(u64*)command.bytes);
-#else
+#ifdef MSB_FIRST
 				u64 cmd64 = *(u64*)command.bytes;
+#else
+				u64 cmd64 = bswap64(*(u64*)command.bytes);
 #endif
 				//todo - parse into blocknumber
 				u32 blocknumber = (cmd64>>44)&0xFFFF;
@@ -142,10 +142,10 @@ void Slot1Comp_Protocol::write_command_NORMAL(GC_Command command)
 			operation = eSlot1Operation_B7_Read;
 
 			//TODO - more endian-safe way of doing this (theres examples in R4)
-#ifdef LOCAL_LE
-			u64 cmd64 = bswap64(*(u64*)command.bytes);
-#else
+#ifdef MSB_FIRST
 			u64 cmd64 = *(u64*)command.bytes;
+#else
+			u64 cmd64 = bswap64(*(u64*)command.bytes);
 #endif
 			address = (u32)((cmd64 >> 24));
 			length = 0x200;

desmume/src/armcpu.h
@@ -179,7 +179,7 @@ enum Mode
 	SYS = 0x1F
 };
 
-#ifdef WORDS_BIGENDIAN
+#ifdef MSB_FIRST
 typedef union
 {
 	struct

desmume/src/cocoa/cocoa_output.mm
@@ -763,7 +763,7 @@
 	
 	pthread_rwlock_unlock(self.rwlockProducer);
 	
-#ifdef __BIG_ENDIAN__
+#ifdef MSB_FIRST
 	uint32_t *bitmapDataEnd = bitmapData + (w * h);
 	while (bitmapData < bitmapDataEnd)
 	{

commit 4e1bdab8c714a9deeab37d399db81d2e8aba4497

Fix some graphical bugs on big-endian systems. (Regressions from commits f8d129b and f97283e.)

desmume/src/GPU.h
@@ -792,7 +792,7 @@ enum GPUEngineID
 #define ADDRESS_STEP_512KB	   0x80000
 #define ADDRESS_MASK_256KB	   (ADDRESS_STEP_256KB-1)
 
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 struct _TILEENTRY
 {
 /*14*/	unsigned Palette:4;
@@ -823,7 +823,7 @@ typedef union
 */
 
 struct _COLOR { // abgr x555
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 	unsigned alpha:1;    // sometimes it is unused (pad)
 	unsigned blue:5;
 	unsigned green:5;

desmume/src/OGLRender.cpp
@@ -36,6 +36,12 @@
 #include "./utils/colorspacehandler/colorspacehandler_SSE2.h"
 #endif
 
+#if MSB_FIRST
+	#define GL_TEXTURE_SRC_FORMAT GL_UNSIGNED_INT_8_8_8_8
+#else
+	#define GL_TEXTURE_SRC_FORMAT GL_UNSIGNED_INT_8_8_8_8_REV
+#endif
+
 typedef struct
 {
 	unsigned int major;
@@ -758,11 +764,11 @@ void OpenGLTexture::Load(bool isNewTexture)
 			
 			if (isNewTexture)
 			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->_sizeS, this->_sizeT, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, textureSrc);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->_sizeS, this->_sizeT, 0, GL_RGBA, GL_TEXTURE_SRC_FORMAT, textureSrc);
 			}
 			else
 			{
-				glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, this->_sizeS, this->_sizeT, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, textureSrc);
+				glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, this->_sizeS, this->_sizeT, GL_RGBA, GL_TEXTURE_SRC_FORMAT, textureSrc);
 			}
 			break;
 		}
@@ -776,13 +782,13 @@ void OpenGLTexture::Load(bool isNewTexture)
 			
 			if (isNewTexture)
 			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->_sizeS*2, this->_sizeT*2, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, this->_upscaleBuffer);
-				glTexImage2D(GL_TEXTURE_2D, 1, GL_RGBA, this->_sizeS*1, this->_sizeT*1, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, textureSrc);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->_sizeS*2, this->_sizeT*2, 0, GL_RGBA, GL_TEXTURE_SRC_FORMAT, this->_upscaleBuffer);
+				glTexImage2D(GL_TEXTURE_2D, 1, GL_RGBA, this->_sizeS*1, this->_sizeT*1, 0, GL_RGBA, GL_TEXTURE_SRC_FORMAT, textureSrc);
 			}
 			else
 			{
-				glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, this->_sizeS*2, this->_sizeT*2, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, this->_upscaleBuffer);
-				glTexSubImage2D(GL_TEXTURE_2D, 1, 0, 0, this->_sizeS*1, this->_sizeT*1, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, textureSrc);
+				glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, this->_sizeS*2, this->_sizeT*2, GL_RGBA, GL_TEXTURE_SRC_FORMAT, this->_upscaleBuffer);
+				glTexSubImage2D(GL_TEXTURE_2D, 1, 0, 0, this->_sizeS*1, this->_sizeT*1, GL_RGBA, GL_TEXTURE_SRC_FORMAT, textureSrc);
 			}
 			break;
 		}
@@ -796,21 +802,21 @@ void OpenGLTexture::Load(bool isNewTexture)
 			
 			if (isNewTexture)
 			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->_sizeS*4, this->_sizeT*4, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, this->_upscaleBuffer);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->_sizeS*4, this->_sizeT*4, 0, GL_RGBA, GL_TEXTURE_SRC_FORMAT, this->_upscaleBuffer);
 				
 				this->_Upscale<2>(textureSrc, this->_upscaleBuffer);
-				glTexImage2D(GL_TEXTURE_2D, 1, GL_RGBA, this->_sizeS*2, this->_sizeT*2, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, this->_upscaleBuffer);
+				glTexImage2D(GL_TEXTURE_2D, 1, GL_RGBA, this->_sizeS*2, this->_sizeT*2, 0, GL_RGBA, GL_TEXTURE_SRC_FORMAT, this->_upscaleBuffer);
 				
-				glTexImage2D(GL_TEXTURE_2D, 2, GL_RGBA, this->_sizeS*1, this->_sizeT*1, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, textureSrc);
+				glTexImage2D(GL_TEXTURE_2D, 2, GL_RGBA, this->_sizeS*1, this->_sizeT*1, 0, GL_RGBA, GL_TEXTURE_SRC_FORMAT, textureSrc);
 			}
 			else
 			{
-				glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, this->_sizeS*4, this->_sizeT*4, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, this->_upscaleBuffer);
+				glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, this->_sizeS*4, this->_sizeT*4, GL_RGBA, GL_TEXTURE_SRC_FORMAT, this->_upscaleBuffer);
 				
 				this->_Upscale<2>(textureSrc, this->_upscaleBuffer);
-				glTexSubImage2D(GL_TEXTURE_2D, 1, 0, 0, this->_sizeS*2, this->_sizeT*2, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, this->_upscaleBuffer);
+				glTexSubImage2D(GL_TEXTURE_2D, 1, 0, 0, this->_sizeS*2, this->_sizeT*2, GL_RGBA, GL_TEXTURE_SRC_FORMAT, this->_upscaleBuffer);
 				
-				glTexSubImage2D(GL_TEXTURE_2D, 2, 0, 0, this->_sizeS*1, this->_sizeT*1, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, textureSrc);
+				glTexSubImage2D(GL_TEXTURE_2D, 2, 0, 0, this->_sizeS*1, this->_sizeT*1, GL_RGBA, GL_TEXTURE_SRC_FORMAT, textureSrc);
 			}
 			break;
 		}

desmume/src/frontend/cocoa/DeSmuME_Prefix.pch
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2011 Roger Manuel
-	Copyright (C) 2012 DeSmuME team
+	Copyright (C) 2012-2017 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -30,6 +30,10 @@
 
 #if defined(__i386__) || defined(__x86_64__)
 	#define HAVE_JIT
+#endif
+
+#if defined(__ppc__) || defined(__ppc64__)
+	#define MSB_FIRST 1
 #endif
 
 #define PORT_VERSION	"Cocoa"

desmume/src/texcache.cpp
@@ -472,7 +472,7 @@ TextureStore::TextureStore(const u32 texAttributes, const u32 palAttributes)
 	{
 		MemSpan currentPaletteMS = MemSpan_TexPalette(_paletteAddress, _paletteSize, false);
 		
-#ifdef WORDS_BIGENDIAN
+#ifdef MSB_FIRST
 		currentPaletteMS.dump16(_paletteColorTable);
 #else
 		currentPaletteMS.dump(_paletteColorTable);
@@ -594,7 +594,7 @@ void TextureStore::SetTexturePalette(const MemSpan &packedPalette)
 {
 	if (this->_paletteSize > 0)
 	{
-#ifdef WORDS_BIGENDIAN
+#ifdef MSB_FIRST
 		packedPalette.dump16(this->_paletteColorTable);
 #else
 		packedPalette.dump(this->_paletteColorTable);
@@ -783,7 +783,7 @@ void TextureStore::VRAMCompareAndUpdate()
 		currentPackedTexIndexMS.dump(this->_workingData + this->_packSize);
 	}
 	
-#ifdef WORDS_BIGENDIAN
+#ifdef MSB_FIRST
 	currentPaletteMS.dump16(this->_workingData + this->_packSize + this->_packIndexSize);
 #else
 	currentPaletteMS.dump(this->_workingData + this->_packSize + this->_packIndexSize);
@@ -1210,7 +1210,7 @@ void NDSTextureUnpack4x4(const size_t srcSize, const u32 *__restrict srcData, co
 					break;
 					
 				case 1:
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 					tmp_col[2]	= ( (((tmp_col[0] & 0xFF000000) >> 1)+((tmp_col[1] & 0xFF000000)  >> 1)) & 0xFF000000 ) |
 					              ( (((tmp_col[0] & 0x00FF0000)      + (tmp_col[1] & 0x00FF0000)) >> 1)  & 0x00FF0000 ) |
 					              ( (((tmp_col[0] & 0x0000FF00)      + (tmp_col[1] & 0x0000FF00)) >> 1)  & 0x0000FF00 ) |
@@ -1231,7 +1231,7 @@ void NDSTextureUnpack4x4(const size_t srcSize, const u32 *__restrict srcData, co
 					
 				case 3:
 				{
-#ifdef LOCAL_BE
+#ifdef MSB_FIRST
 					const u32 r0	= (tmp_col[0]>>24) & 0x000000FF;
 					const u32 r1	= (tmp_col[1]>>24) & 0x000000FF;
 					const u32 g0	= (tmp_col[0]>>16) & 0x000000FF;