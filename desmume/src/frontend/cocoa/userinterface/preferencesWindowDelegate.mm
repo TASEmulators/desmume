TASEmulators/desmume

commit 1e9b179bf49af529b079d474c125d8fc7267f190

Cocoa Port: Begin refactoring some of the emulation execution loop code to be less port-dependent.
- Also clean up some UI code with switching app preferences views when
running OS X Leopard.

desmume/src/frontend/cocoa/userinterface/preferencesWindowDelegate.mm
@@ -286,6 +286,12 @@
 @synthesize cheatWindowController;
 @synthesize cheatDatabaseController;
 
+@synthesize toolbarItemGeneral;
+@synthesize toolbarItemInput;
+@synthesize toolbarItemDisplay;
+@synthesize toolbarItemSound;
+@synthesize toolbarItemEmulation;
+
 @synthesize viewGeneral;
 @synthesize viewInput;
 @synthesize viewDisplay;
@@ -347,11 +353,11 @@
 	{
 		// Associates NSView objects to their respective toolbar identifiers.
 		prefViewDict = [[NSDictionary alloc] initWithObjectsAndKeys:
-						viewGeneral, @"General",
-						viewInput, @"Input",
-						viewDisplay, @"Display",
-						viewSound, @"Sound",
-						viewEmulation, @"Emulation",
+						viewGeneral,	[toolbarItemGeneral itemIdentifier],
+						viewInput,		[toolbarItemInput itemIdentifier],
+						viewDisplay,	[toolbarItemDisplay itemIdentifier],
+						viewSound,		[toolbarItemSound itemIdentifier],
+						viewEmulation,	[toolbarItemEmulation itemIdentifier],
 						nil];
 	}
 	
@@ -361,7 +367,7 @@
 	{
 		[self switchContentView:theView];
 		
-		if ([toolbarItemIdentifier isEqualToString:@"Input"])
+		if ([toolbarItemIdentifier isEqualToString:[toolbarItemDisplay itemIdentifier]])
 		{
 			[window makeFirstResponder:theView];
 		}

desmume/src/frontend/cocoa/ClientExecutionControl.cpp
@@ -1,0 +1,432 @@
+/*
+	Copyright (C) 2017 DeSmuME team
+ 
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+ 
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+ 
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "NDSSystem.h"
+
+#include "ClientExecutionControl.h"
+
+
+ClientExecutionControl::ClientExecutionControl()
+{
+	_settingsPending.cpuEngineID = CPUEmulationEngineID_Interpreter;
+	_settingsPending.JITMaxBlockSize = 12;
+	
+	_settingsPending.enableAdvancedBusLevelTiming = true;
+	_settingsPending.enableRigorous3DRenderingTiming = false;
+	_settingsPending.enableExternalBIOS = false;
+	_settingsPending.enableBIOSInterrupts = false;
+	_settingsPending.enableBIOSPatchDelayLoop = false;
+	_settingsPending.enableExternalFirmware = false;
+	_settingsPending.enableFirmwareBoot = false;
+	_settingsPending.enableDebugConsole = false;
+	_settingsPending.enableEnsataEmulation = false;
+	
+	_settingsPending.executionSpeed = 1.0f;
+	
+	_settingsPending.enableFrameSkip = true;
+	_settingsPending.framesToSkip = 0;
+	_settingsPending.frameJumpTarget = 0;
+	
+	_settingsPending.execBehavior = ExecutionBehavior_Pause;
+	_settingsPending.jumpBehavior = FrameJumpBehavior_Forward;
+	
+	pthread_mutex_init(&_mutexSettingsPendingOnReset, NULL);
+	pthread_mutex_init(&_mutexSettingsApplyOnReset, NULL);
+	pthread_mutex_init(&_mutexSettingsPendingOnExecutionLoopStart, NULL);
+	pthread_mutex_init(&_mutexSettingsApplyOnExecutionLoopStart, NULL);
+	pthread_mutex_init(&_mutexSettingsPendingOnNDSExec, NULL);
+	pthread_mutex_init(&_mutexSettingsApplyOnNDSExec, NULL);
+}
+
+ClientExecutionControl::~ClientExecutionControl()
+{
+	pthread_mutex_destroy(&this->_mutexSettingsPendingOnReset);
+	pthread_mutex_destroy(&this->_mutexSettingsApplyOnReset);
+	pthread_mutex_destroy(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	pthread_mutex_destroy(&this->_mutexSettingsApplyOnExecutionLoopStart);
+	pthread_mutex_destroy(&this->_mutexSettingsPendingOnNDSExec);
+	pthread_mutex_destroy(&this->_mutexSettingsApplyOnNDSExec);
+}
+
+CPUEmulationEngineID ClientExecutionControl::GetCPUEmulationEngineID()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnReset);
+	const CPUEmulationEngineID engineID = this->_settingsPending.cpuEngineID;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnReset);
+	
+	return engineID;
+}
+
+void ClientExecutionControl::SetCPUEmulationEngineID(CPUEmulationEngineID engineID)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnReset);
+	this->_settingsPending.cpuEngineID = engineID;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnReset);
+}
+
+uint8_t ClientExecutionControl::GetJITMaxBlockSize()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnReset);
+	const uint8_t blockSize = this->_settingsPending.JITMaxBlockSize;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnReset);
+	
+	return blockSize;
+}
+
+void ClientExecutionControl::SetJITMaxBlockSize(uint8_t blockSize)
+{
+	if (blockSize == 0)
+	{
+		blockSize = 1;
+	}
+	else if (blockSize > 100)
+	{
+		blockSize = 100;
+	}
+	
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnReset);
+	this->_settingsPending.JITMaxBlockSize = blockSize;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnReset);
+}
+
+bool ClientExecutionControl::GetEnableAdvancedBusLevelTiming()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableAdvancedBusLevelTiming;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableAdvancedBusLevelTiming(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableAdvancedBusLevelTiming = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableRigorous3DRenderingTiming()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableRigorous3DRenderingTiming;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableRigorous3DRenderingTiming(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableRigorous3DRenderingTiming = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableExternalBIOS()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableExternalBIOS;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableExternalBIOS(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableExternalBIOS = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableBIOSInterrupts()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableBIOSInterrupts;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableBIOSInterrupts(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableBIOSInterrupts = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableBIOSPatchDelayLoop()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableBIOSPatchDelayLoop;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableBIOSPatchDelayLoop(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableBIOSPatchDelayLoop = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableExternalFirmware()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableExternalFirmware;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableExternalFirmware(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableExternalFirmware = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableFirmwareBoot()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableFirmwareBoot;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableFirmwareBoot(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableFirmwareBoot = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableDebugConsole()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableDebugConsole;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableDebugConsole(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableDebugConsole = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableEnsataEmulation()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	const bool enable = this->_settingsPending.enableEnsataEmulation;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableEnsataEmulation(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	this->_settingsPending.enableEnsataEmulation = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+}
+
+bool ClientExecutionControl::GetEnableSpeedLimiter()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	const bool enable = this->_settingsPending.enableSpeedLimiter;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableSpeedLimiter(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	this->_settingsPending.enableSpeedLimiter = enable;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+}
+
+float ClientExecutionControl::GetExecutionSpeed()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	const float speedScalar = this->_settingsPending.executionSpeed;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	
+	return speedScalar;
+}
+
+void ClientExecutionControl::SetExecutionSpeed(float speedScalar)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	this->_settingsPending.executionSpeed = speedScalar;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+}
+
+bool ClientExecutionControl::GetEnableFrameSkip()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	const bool enable = this->_settingsPending.enableFrameSkip;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	
+	return enable;
+}
+
+void ClientExecutionControl::SetEnableFrameSkip(bool enable)
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	
+	if (this->_settingsPending.enableFrameSkip != enable)
+	{
+		this->_settingsPending.enableFrameSkip = enable;
+		this->_settingsPending.framesToSkip = 0;
+	}
+	
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+}
+
+uint8_t ClientExecutionControl::GetFramesToSkip()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	const uint8_t numFramesToSkip = this->_settingsPending.framesToSkip;
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	
+	return numFramesToSkip;
+}
+
+uint64_t ClientExecutionControl::GetFrameJumpTarget()
+{
+	return this->_settingsPending.frameJumpTarget;
+}
+
+void ClientExecutionControl::SetFrameJumpTarget(uint64_t newJumpTarget)
+{
+	this->_settingsPending.frameJumpTarget = newJumpTarget;
+}
+
+ExecutionBehavior ClientExecutionControl::GetExecutionBehavior()
+{
+	return this->_settingsPending.execBehavior;
+}
+
+void ClientExecutionControl::SetExecutionBehavior(ExecutionBehavior newBehavior)
+{
+	this->_settingsPending.execBehavior = newBehavior;
+}
+
+FrameJumpBehavior ClientExecutionControl::GetFrameJumpBehavior()
+{
+	return this->_settingsPending.jumpBehavior;
+}
+
+void ClientExecutionControl::SetFrameJumpBehavior(FrameJumpBehavior newBehavior)
+{
+	this->_settingsPending.jumpBehavior = newBehavior;
+}
+
+void ClientExecutionControl::FlushSettingsOnReset()
+{
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnReset);
+	
+	CommonSettings.use_jit = (this->_settingsPending.cpuEngineID == CPUEmulationEngineID_DynamicRecompiler);
+	CommonSettings.jit_max_block_size = this->_settingsPending.JITMaxBlockSize;
+	
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnReset);
+}
+
+void ClientExecutionControl::FlushSettingsOnExecutionLoopStart()
+{
+	pthread_mutex_lock(&this->_mutexSettingsApplyOnExecutionLoopStart);
+	
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	
+	const bool didFrameSkipSettingChange = (this->_settingsPending.enableFrameSkip != this->_settingsApplied.enableFrameSkip);
+	if (didFrameSkipSettingChange)
+	{
+		this->_settingsApplied.enableFrameSkip	= this->_settingsPending.enableFrameSkip;
+		this->_settingsApplied.framesToSkip		= this->_settingsPending.framesToSkip;
+	}
+	
+	const float speedScalar = (this->_settingsPending.executionSpeed > SPEED_SCALAR_MIN) ? this->_settingsPending.executionSpeed : SPEED_SCALAR_MIN;
+	const bool didExecutionSpeedChange = (speedScalar != this->_settingsApplied.executionSpeed) || (this->_settingsPending.enableSpeedLimiter != this->_settingsApplied.enableSpeedLimiter);
+	if (didExecutionSpeedChange)
+	{
+		this->_settingsApplied.enableSpeedLimiter	= this->_settingsPending.enableSpeedLimiter;
+		this->_settingsApplied.executionSpeed		= speedScalar;
+	}
+	
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnExecutionLoopStart);
+	
+	if (didFrameSkipSettingChange)
+	{
+		NDS_OmitFrameSkip(2);
+	}
+	
+	if (didExecutionSpeedChange)
+	{
+		if (this->_settingsApplied.enableSpeedLimiter)
+		{
+			this->_settingsApplied.timeBudget = this->GetFrameAbsoluteTime(1.0f/speedScalar);
+		}
+		else
+		{
+			this->_settingsApplied.timeBudget = 0;
+		}
+	}
+	
+	pthread_mutex_unlock(&this->_mutexSettingsApplyOnExecutionLoopStart);
+}
+
+void ClientExecutionControl::FlushSettingsOnNDSExec()
+{
+	pthread_mutex_lock(&this->_mutexSettingsApplyOnNDSExec);
+	
+	pthread_mutex_lock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	this->_settingsApplied.enableAdvancedBusLevelTiming		= this->_settingsPending.enableAdvancedBusLevelTiming;
+	this->_settingsApplied.enableRigorous3DRenderingTiming	= this->_settingsPending.enableRigorous3DRenderingTiming;
+	this->_settingsApplied.enableExternalBIOS				= this->_settingsPending.enableExternalBIOS;
+	this->_settingsApplied.enableBIOSInterrupts				= this->_settingsPending.enableBIOSInterrupts;
+	this->_settingsApplied.enableBIOSPatchDelayLoop			= this->_settingsPending.enableBIOSPatchDelayLoop;
+	this->_settingsApplied.enableExternalFirmware			= this->_settingsPending.enableExternalFirmware;
+	this->_settingsApplied.enableFirmwareBoot				= this->_settingsPending.enableFirmwareBoot;
+	this->_settingsApplied.enableDebugConsole				= this->_settingsPending.enableDebugConsole;
+	this->_settingsApplied.enableEnsataEmulation			= this->_settingsPending.enableEnsataEmulation;
+	
+	pthread_mutex_unlock(&this->_mutexSettingsPendingOnNDSExec);
+	
+	CommonSettings.advanced_timing			= this->_settingsApplied.enableAdvancedBusLevelTiming;
+	CommonSettings.rigorous_timing			= this->_settingsApplied.enableRigorous3DRenderingTiming;
+	CommonSettings.UseExtBIOS				= this->_settingsApplied.enableExternalBIOS;
+	CommonSettings.SWIFromBIOS				= this->_settingsApplied.enableBIOSInterrupts;
+	CommonSettings.PatchSWI3				= this->_settingsApplied.enableBIOSPatchDelayLoop;
+	CommonSettings.UseExtFirmware			= this->_settingsApplied.enableExternalFirmware;
+	CommonSettings.UseExtFirmwareSettings	= this->_settingsApplied.enableExternalFirmware;
+	CommonSettings.BootFromFirmware			= this->_settingsApplied.enableFirmwareBoot;
+	CommonSettings.DebugConsole				= this->_settingsApplied.enableDebugConsole;
+	CommonSettings.EnsataEmulation			= this->_settingsApplied.enableEnsataEmulation;
+	
+	pthread_mutex_unlock(&this->_mutexSettingsApplyOnNDSExec);
+}
+
+uint64_t ClientExecutionControl::GetFrameAbsoluteTime(double frameTimeScalar)
+{
+	// Do nothing. This is implementation dependent;
+	return 1.0;
+}

desmume/src/frontend/cocoa/ClientExecutionControl.h
@@ -1,0 +1,166 @@
+/*
+	Copyright (C) 2017 DeSmuME team
+ 
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+ 
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+ 
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CLIENT_EXECUTION_CONTROL_H_
+#define _CLIENT_EXECUTION_CONTROL_H_
+
+#include <pthread.h>
+
+#define SPEED_SCALAR_QUARTER						0.25	// Speed scalar for quarter execution speed.
+#define SPEED_SCALAR_HALF							0.5		// Speed scalar for half execution speed.
+#define SPEED_SCALAR_THREE_QUARTER					0.75	// Speed scalar for three quarters execution speed.
+#define SPEED_SCALAR_NORMAL							1.0		// Speed scalar for normal execution speed.
+#define SPEED_SCALAR_DOUBLE							2.0		// Speed scalar for double execution speed.
+#define SPEED_SCALAR_MIN							0.005	// Lower limit for the speed multiplier.
+
+#define DS_FRAMES_PER_SECOND						59.8261	// Number of DS frames per second.
+#define DS_SECONDS_PER_FRAME						(1.0 / DS_FRAMES_PER_SECOND) // The length of time in seconds that, ideally, a frame should be processed within.
+
+#define FRAME_SKIP_AGGRESSIVENESS					9.0		// Must be a value between 0.0 (inclusive) and positive infinity.
+															// This value acts as a scalar multiple of the frame skip.
+#define FRAME_SKIP_BIAS								0.1		// May be any real number. This value acts as a vector addition to the frame skip.
+#define MAX_FRAME_SKIP								(DS_FRAMES_PER_SECOND / 2.98)
+
+enum ExecutionBehavior
+{
+	ExecutionBehavior_Pause = 0,
+	ExecutionBehavior_Run,
+	ExecutionBehavior_FrameAdvance,
+	ExecutionBehavior_FrameJump
+};
+
+enum FrameJumpBehavior
+{
+	FrameJumpBehavior_Forward		= 0,
+	FrameJumpBehavior_ToFrame		= 1,
+	FrameJumpBehavior_NextMarker	= 2
+};
+
+enum CPUEmulationEngineID
+{
+	CPUEmulationEngineID_Interpreter			= 0,
+	CPUEmulationEngineID_DynamicRecompiler		= 1
+};
+
+typedef struct ClientExecutionControlSettings
+{
+	CPUEmulationEngineID cpuEngineID;
+	uint8_t JITMaxBlockSize;
+	
+	bool enableAdvancedBusLevelTiming;
+	bool enableRigorous3DRenderingTiming;
+	bool enableExternalBIOS;
+	bool enableBIOSInterrupts;
+	bool enableBIOSPatchDelayLoop;
+	bool enableExternalFirmware;
+	bool enableFirmwareBoot;
+	bool enableDebugConsole;
+	bool enableEnsataEmulation;
+	
+	bool enableSpeedLimiter;
+	float executionSpeed;
+	uint64_t timeBudget;
+	
+	bool enableFrameSkip;
+	uint8_t framesToSkip;
+	uint64_t frameJumpTarget;
+	
+	ExecutionBehavior execBehavior;
+	FrameJumpBehavior jumpBehavior;
+	
+} ClientExecutionControlSettings;
+
+class ClientExecutionControl
+{
+private:
+	ClientExecutionControlSettings _settingsPending;
+	ClientExecutionControlSettings _settingsApplied;
+	
+	pthread_mutex_t _mutexSettingsPendingOnReset;
+	pthread_mutex_t _mutexSettingsApplyOnReset;
+	
+	pthread_mutex_t _mutexSettingsPendingOnExecutionLoopStart;
+	pthread_mutex_t _mutexSettingsApplyOnExecutionLoopStart;
+	
+	pthread_mutex_t _mutexSettingsPendingOnNDSExec;
+	pthread_mutex_t _mutexSettingsApplyOnNDSExec;
+	
+public:
+	ClientExecutionControl();
+	~ClientExecutionControl();
+	
+	CPUEmulationEngineID GetCPUEmulationEngineID();
+	void SetCPUEmulationEngineID(CPUEmulationEngineID engineID);
+	
+	uint8_t GetJITMaxBlockSize();
+	void SetJITMaxBlockSize(uint8_t blockSize);
+	
+	bool GetEnableAdvancedBusLevelTiming();
+	void SetEnableAdvancedBusLevelTiming(bool enable);
+	
+	bool GetEnableRigorous3DRenderingTiming();
+	void SetEnableRigorous3DRenderingTiming(bool enable);
+	
+	bool GetEnableExternalBIOS();
+	void SetEnableExternalBIOS(bool enable);
+	
+	bool GetEnableBIOSInterrupts();
+	void SetEnableBIOSInterrupts(bool enable);
+	
+	bool GetEnableBIOSPatchDelayLoop();
+	void SetEnableBIOSPatchDelayLoop(bool enable);
+	
+	bool GetEnableExternalFirmware();
+	void SetEnableExternalFirmware(bool enable);
+	
+	bool GetEnableFirmwareBoot();
+	void SetEnableFirmwareBoot(bool enable);
+	
+	bool GetEnableDebugConsole();
+	void SetEnableDebugConsole(bool enable);
+	
+	bool GetEnableEnsataEmulation();
+	void SetEnableEnsataEmulation(bool enable);
+	
+	bool GetEnableSpeedLimiter();
+	void SetEnableSpeedLimiter(bool enable);
+	
+	float GetExecutionSpeed();
+	void SetExecutionSpeed(float speedScalar);
+	
+	bool GetEnableFrameSkip();
+	void SetEnableFrameSkip(bool enable);
+	
+	uint8_t GetFramesToSkip();
+	
+	uint64_t GetFrameJumpTarget();
+	void SetFrameJumpTarget(uint64_t newJumpTarget);
+	
+	ExecutionBehavior GetExecutionBehavior();
+	void SetExecutionBehavior(ExecutionBehavior newBehavior);
+	
+	FrameJumpBehavior GetFrameJumpBehavior();
+	void SetFrameJumpBehavior(FrameJumpBehavior newBehavior);
+	
+	void FlushSettingsOnReset();
+	void FlushSettingsOnExecutionLoopStart();
+	void FlushSettingsOnNDSExec();
+	
+	virtual uint64_t GetFrameAbsoluteTime(double frameTimeScalar);
+};
+
+#endif // _CLIENT_EXECUTION_CONTROL_H_

desmume/src/frontend/cocoa/DeSmuME (Latest).xcodeproj/project.pbxproj
@@ -895,6 +895,10 @@
 		ABB0FBDA1A9FD0260060C55A /* Icon_MicrophoneGray_256x256.png in Resources */ = {isa = PBXBuildFile; fileRef = ABB0FBD81A9FD0260060C55A /* Icon_MicrophoneGray_256x256.png */; };
 		ABB0FBDB1A9FD0260060C55A /* Icon_MicrophoneGray_256x256.png in Resources */ = {isa = PBXBuildFile; fileRef = ABB0FBD81A9FD0260060C55A /* Icon_MicrophoneGray_256x256.png */; };
 		ABB1C9451F4D6B340004844F /* macosx_10_5_compat.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AB23567216C2F6F400DA782E /* macosx_10_5_compat.cpp */; };
+		ABB1C9481F5281AE0004844F /* ClientExecutionControl.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABB1C9461F5281AE0004844F /* ClientExecutionControl.cpp */; };
+		ABB1C9491F5281AE0004844F /* ClientExecutionControl.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABB1C9461F5281AE0004844F /* ClientExecutionControl.cpp */; };
+		ABB1C94A1F5281AE0004844F /* ClientExecutionControl.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABB1C9461F5281AE0004844F /* ClientExecutionControl.cpp */; };
+		ABB1C94B1F5281AE0004844F /* ClientExecutionControl.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABB1C9461F5281AE0004844F /* ClientExecutionControl.cpp */; };
 		ABB24F6D1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABB24F6B1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp */; };
 		ABB24F6E1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABB24F6B1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp */; };
 		ABB24F6F1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp in Sources */ = {isa = PBXBuildFile; fileRef = ABB24F6B1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp */; };
@@ -1564,6 +1568,8 @@
 		ABB0FBCA1A9EED350060C55A /* Icon_MicrophoneGreen_256x256.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_MicrophoneGreen_256x256.png; path = images/Icon_MicrophoneGreen_256x256.png; sourceTree = "<group>"; };
 		ABB0FBCB1A9EED350060C55A /* Icon_MicrophoneRed_256x256.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_MicrophoneRed_256x256.png; path = images/Icon_MicrophoneRed_256x256.png; sourceTree = "<group>"; };
 		ABB0FBD81A9FD0260060C55A /* Icon_MicrophoneGray_256x256.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = Icon_MicrophoneGray_256x256.png; path = images/Icon_MicrophoneGray_256x256.png; sourceTree = "<group>"; };
+		ABB1C9461F5281AE0004844F /* ClientExecutionControl.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = ClientExecutionControl.cpp; sourceTree = "<group>"; };
+		ABB1C9471F5281AE0004844F /* ClientExecutionControl.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ClientExecutionControl.h; sourceTree = "<group>"; };
 		ABB24F6B1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = OGLDisplayOutput_3_2.cpp; sourceTree = "<group>"; };
 		ABB24F6C1A81EE92006C1108 /* OGLDisplayOutput_3_2.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = OGLDisplayOutput_3_2.h; sourceTree = "<group>"; };
 		ABB3C63B1501BB8300E0C22E /* DeSmuME_Prefix_OpenEmu.pch */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = DeSmuME_Prefix_OpenEmu.pch; sourceTree = "<group>"; };
@@ -1993,6 +1999,7 @@
 				AB82445A1704AE9A00B8EE20 /* utilities.c */,
 				ABD10AE51715FCDD00B5729D /* audiosamplegenerator.cpp */,
 				ABAD07DA1E19CAA6007867CA /* ClientDisplayView.cpp */,
+				ABB1C9461F5281AE0004844F /* ClientExecutionControl.cpp */,
 				AB1B9E5F1501A78000464647 /* coreaudiosound.cpp */,
 				AB23567216C2F6F400DA782E /* macosx_10_5_compat.cpp */,
 				ABD10AE61715FCDD00B5729D /* mic_ext.cpp */,
@@ -2002,6 +2009,7 @@
 				ABD104141346652500AF11D1 /* sndOSX.cpp */,
 				ABD10AE31715FCDD00B5729D /* audiosamplegenerator.h */,
 				ABAD07DB1E19CAA6007867CA /* ClientDisplayView.h */,
+				ABB1C9471F5281AE0004844F /* ClientExecutionControl.h */,
 				ABA6574914511EC90077E5E9 /* cocoa_cheat.h */,
 				ABD103FE1346652500AF11D1 /* cocoa_core.h */,
 				AB58F32B1364F44B0074C376 /* cocoa_file.h */,
@@ -3825,6 +3833,7 @@
 				ABD1FEF51345AC8400AF11D1 /* ROMReader.cpp in Sources */,
 				ABD1FEF61345AC8400AF11D1 /* rtc.cpp in Sources */,
 				ABD1FEF71345AC8400AF11D1 /* saves.cpp in Sources */,
+				ABB1C94A1F5281AE0004844F /* ClientExecutionControl.cpp in Sources */,
 				ABD1FEF81345AC8400AF11D1 /* slot1.cpp in Sources */,
 				ABD1FF0B1345AC9C00AF11D1 /* slot1_none.cpp in Sources */,
 				ABD1FF0C1345AC9C00AF11D1 /* slot1_r4.cpp in Sources */,
@@ -4044,6 +4053,7 @@
 				AB796D1A15CDCBA200C59155 /* MMU.cpp in Sources */,
 				ABB24F6D1A81EE92006C1108 /* OGLDisplayOutput_3_2.cpp in Sources */,
 				AB796D1B15CDCBA200C59155 /* mmx_optimized.cpp in Sources */,
+				ABB1C9481F5281AE0004844F /* ClientExecutionControl.cpp in Sources */,
 				AB2ABA3F1C9F9CFA00173B15 /* rsemaphore.c in Sources */,
 				AB796D1C15CDCBA200C59155 /* movie.cpp in Sources */,
 				AB9038B817C5ED2200F410BD /* slot1comp_rom.cpp in Sources */,
@@ -4208,6 +4218,7 @@
 				ABFEA8361BB4EC1100B08C25 /* ftmm.c in Sources */,
 				ABFEA81E1BB4EC1000B08C25 /* ftfstype.c in Sources */,
 				ABA731601BB51E7000B26147 /* pshinter.c in Sources */,
+				ABB1C9491F5281AE0004844F /* ClientExecutionControl.cpp in Sources */,
 				AB301BE01D9C8BCD00246A93 /* deposterize.cpp in Sources */,
 				ABFEA8211BB4EC1000B08C25 /* ftgasp.c in Sources */,
 				ABFEA83C1BB4EC1100B08C25 /* ftotval.c in Sources */,
@@ -4447,6 +4458,7 @@
 				ABB3C6851501C04F00E0C22E /* bilinear.cpp in Sources */,
 				ABB3C6861501C04F00E0C22E /* epx.cpp in Sources */,
 				ABB3C6871501C04F00E0C22E /* hq2x.cpp in Sources */,
+				ABB1C94B1F5281AE0004844F /* ClientExecutionControl.cpp in Sources */,
 				ABB3C6881501C04F00E0C22E /* hq4x.cpp in Sources */,
 				ABB3C6891501C04F00E0C22E /* lq2x.cpp in Sources */,
 				ABB3C68A1501C04F00E0C22E /* scanline.cpp in Sources */,

commit ccaefb18bcc5f6d58d853fd1263d12f6fd616b6d

Cocoa Port: Fix UI issue where the Input Preferences window may not be automatically assigned as the first responder. (Regression from commit 1e9b179.)

desmume/src/frontend/cocoa/userinterface/preferencesWindowDelegate.mm
@@ -367,7 +367,7 @@
 	{
 		[self switchContentView:theView];
 		
-		if ([toolbarItemIdentifier isEqualToString:[toolbarItemDisplay itemIdentifier]])
+		if ([toolbarItemIdentifier isEqualToString:[toolbarItemInput itemIdentifier]])
 		{
 			[window makeFirstResponder:theView];
 		}