TASEmulators/desmume

commit f5c9a36930f1fa0889e89080e210063bee623172

GPU: NDSDisplayInfo now reports the backlight intensity for each display. The LMNTS demo now properly fades the backlights in and out, but only on frontends that support this feature. (Fixes #59.) - Note: The backlight intensity is only emulated on frontends with 3D-based display methods, such as OpenGL and Metal. CPU-based display methods, such as DirectDraw, SDL and Cairo, are currently unsupported.

desmume/src/GPU.cpp
@@ -35,7 +35,6 @@
 #include "gfx3d.h"
 #include "debug.h"
 #include "NDSSystem.h"
-#include "readwrite.h"
 #include "matrix.h"
 #include "emufile.h"
 
@@ -7456,6 +7455,8 @@ void GPUSubsystem::Reset()
 	this->_willFrameSkip = false;
 	this->_videoFrameCount = 0;
 	this->_render3DFrameCount = 0;
+	this->_backlightIntensityTotal[NDSDisplayID_Main]  = 0.0f;
+	this->_backlightIntensityTotal[NDSDisplayID_Touch] = 0.0f;
 	
 	this->ClearWithColor(0xFFFF);
 	
@@ -7475,6 +7476,9 @@ void GPUSubsystem::Reset()
 	
 	this->_displayInfo.engineID[NDSDisplayID_Main] = GPUEngineID_Main;
 	this->_displayInfo.engineID[NDSDisplayID_Touch] = GPUEngineID_Sub;
+	
+	this->_displayInfo.backlightIntensity[NDSDisplayID_Main]  = 1.0f;
+	this->_displayInfo.backlightIntensity[NDSDisplayID_Touch] = 1.0f;
 	
 	this->_display[NDSDisplayID_Main]->SetEngineByID(GPUEngineID_Main);
 	this->_display[NDSDisplayID_Touch]->SetEngineByID(GPUEngineID_Sub);
@@ -8111,6 +8115,12 @@ void GPUSubsystem::RenderLine(const size_t l)
 			this->_displayInfo.needConvertColorFormat[NDSDisplayID_Main]  = (OUTPUTFORMAT == NDSColorFormat_BGR666_Rev);
 			this->_displayInfo.needConvertColorFormat[NDSDisplayID_Touch] = (OUTPUTFORMAT == NDSColorFormat_BGR666_Rev);
 			
+			// Set the average backlight intensity and then reset the current total.
+			this->_displayInfo.backlightIntensity[NDSDisplayID_Main]  = this->_backlightIntensityTotal[NDSDisplayID_Main]  / 263.0f;
+			this->_displayInfo.backlightIntensity[NDSDisplayID_Touch] = this->_backlightIntensityTotal[NDSDisplayID_Touch] / 263.0f;
+			this->_backlightIntensityTotal[NDSDisplayID_Main]  = 0.0f;
+			this->_backlightIntensityTotal[NDSDisplayID_Touch] = 0.0f;
+			
 			this->_engineMain->UpdateMasterBrightnessDisplayInfo(this->_displayInfo);
 			this->_engineSub->UpdateMasterBrightnessDisplayInfo(this->_displayInfo);
 			
@@ -8132,6 +8142,32 @@ void GPUSubsystem::RenderLine(const size_t l)
 			this->_frameNeedsFinish = false;
 			this->_event->DidFrameEnd(this->_willFrameSkip, this->_displayInfo);
 		}
+	}
+}
+
+void GPUSubsystem::UpdateAverageBacklightIntensityTotal()
+{
+	// The values in this table are, more or less, arbitrarily chosen.
+	//
+	// It would be nice to get some real testing done on a hardware NDS to have some more
+	// accurate intensity values for each backlight level.
+	static const float backlightLevelToIntensityTable[] = {0.100, 0.300, 0.600, 1.000};
+	
+	IOREG_POWERMANCTL POWERMANCTL;
+	IOREG_BACKLIGHTCTL BACKLIGHTCTL;
+	POWERMANCTL.value = MMU.powerMan_Reg[0];
+	BACKLIGHTCTL.value = MMU.powerMan_Reg[4];
+	
+	if (POWERMANCTL.MainBacklight_Enable != 0)
+	{
+		const BacklightLevel level = ((BACKLIGHTCTL.ExternalPowerState != 0) && (BACKLIGHTCTL.ForceMaxBrightnessWithExtPower_Enable != 0)) ? BacklightLevel_Maximum : (BacklightLevel)BACKLIGHTCTL.Level;
+		this->_backlightIntensityTotal[NDSDisplayID_Main]  += backlightLevelToIntensityTable[level];
+	}
+	
+	if (POWERMANCTL.TouchBacklight_Enable != 0)
+	{
+		const BacklightLevel level = ((BACKLIGHTCTL.ExternalPowerState != 0) && (BACKLIGHTCTL.ForceMaxBrightnessWithExtPower_Enable != 0)) ? BacklightLevel_Maximum : (BacklightLevel)BACKLIGHTCTL.Level;
+		this->_backlightIntensityTotal[NDSDisplayID_Touch] += backlightLevelToIntensityTable[level];
 	}
 }
 

desmume/src/GPU.h
@@ -764,6 +764,70 @@ typedef struct
 } GPU_IOREG;										// 0x04000000, 0x04001000: GPU registers
 #include "PACKED_END.h"
 
+typedef union
+{
+	u8 value;
+	
+	struct
+	{
+#ifndef MSB_FIRST
+		u8 SoundAmp_Enable:1;						//  0: Sound amplifier; 0=Disable, 1=Enable
+		u8 SoundAmp_Mute:1;							//  1: Sound amplifier mute control; 0=Normal, 1=Mute
+		u8 TouchBacklight_Enable:1;					//  2: Touch display backlight; 0=Disable, 1=Enable
+		u8 MainBacklight_Enable:1;					//  3: Main display backlight; 0=Disable, 1=Enable
+		u8 PowerLEDBlink_Enable:1;					//  4: Power LED blink control; 0=Disable (Power LED remains steadily ON), 1=Enable
+		u8 PowerLEDBlinkSpeed:1;					//  5: Power LED blink speed when enabled; 0=Slow, 1=Fast
+		u8 SystemPowerState:2;						//  6: NDS system power state; 0=System is powered ON, 1=System is powered OFF
+		u8 :1;										//  7: Unused bit (should always be read as 0)
+#else
+		u8 :1;										//  7: Unused bit (should always be read as 0)
+		u8 SystemPowerState:2;						//  6: NDS system power state; 0=System is powered ON, 1=System is powered OFF
+		u8 PowerLEDBlinkSpeed:1;					//  5: Power LED blink speed when enabled; 0=Slow, 1=Fast
+		u8 PowerLEDBlink_Enable:1;					//  4: Power LED blink control; 0=Disable (LED is steady ON), 1=Enable
+		u8 MainBacklight_Enable:1;					//  3: Main display backlight; 0=Disable, 1=Enable
+		u8 TouchBacklight_Enable:1;					//  2: Touch display backlight; 0=Disable, 1=Enable
+		u8 SoundAmp_Mute:1;							//  1: Sound amplifier mute control; 0=Normal, 1=Mute
+		u8 SoundAmp_Enable:1;						//  0: Sound amplifier; 0=Disable, 1=Enable
+#endif
+	};
+} IOREG_POWERMANCTL;
+
+typedef union
+{
+	u8 value;
+	
+	struct
+	{
+#ifndef MSB_FIRST
+		u8 Level:2;									// 0-1: Backlight brightness level;
+													//      0=Low
+													//      1=Medium
+													//      2=High
+													//      3=Maximum
+		u8 ForceMaxBrightnessWithExtPower_Enable:1;	//   2: Forces maximum brightness if external power is present, interacts with Bit 3; 0=Disable, 1=Enable
+		u8 ExternalPowerState:1;					//   3: External power state; 0=External power is not present, 1=External power is present
+		u8 :4;										// 4-7: Unknown bits (should always be read as 4)
+#else
+		u8 :4;										// 4-7: Unknown bits (should always be read as 4)
+		u8 ExternalPowerState:1;					//   3: External power state; 0=External power is not present, 1=External power is present
+		u8 ForceMaxBrightnessWithExtPower_Enable:1;	//   2: Forces maximum brightness if external power is present, interacts with Bit 3; 0=Disable, 1=Enable
+		u8 Level:2;									// 0-1: Backlight brightness level;
+													//      0=Low
+													//      1=Medium
+													//      2=High
+													//      3=Maximum
+#endif
+	};
+} IOREG_BACKLIGHTCTL;
+
+enum BacklightLevel
+{
+	BacklightLevel_Low					= 0,
+	BacklightLevel_Medium				= 1,
+	BacklightLevel_High					= 2,
+	BacklightLevel_Maximum				= 3
+};
+
 enum ColorEffect
 {
 	ColorEffect_Disable					= 0,
@@ -1114,6 +1178,10 @@ typedef struct
 												// framebuffer. For most NDS games, this field will be false.
 	u8 masterBrightnessMode[2][GPU_FRAMEBUFFER_NATIVE_HEIGHT]; // The master brightness mode of each display line.
 	u8 masterBrightnessIntensity[2][GPU_FRAMEBUFFER_NATIVE_HEIGHT]; // The master brightness intensity of each display line.
+	
+	float backlightIntensity[2];				// Reports the intensity of the backlight.
+												//    0.000 - The backlight is completely off.
+												//    1.000 - The backlight is at its maximum brightness.
 	
 	
 	
@@ -1671,6 +1739,7 @@ private:
 	GPUEngineA *_engineMain;
 	GPUEngineB *_engineSub;
 	NDSDisplay *_display[2];
+	float _backlightIntensityTotal[2];
 	
 	u32 _videoFrameCount;			// Internal variable that increments when a video frame is completed. Resets every 60 video frames.
 	u32 _render3DFrameCount;		// The current 3D rendering frame count, saved to this variable once every 60 video frames.
@@ -1757,6 +1826,7 @@ public:
 	void ResolveDisplayToCustomFramebuffer(const NDSDisplayID displayID, NDSDisplayInfo &mutableInfo);
 	
 	template<NDSColorFormat OUTPUTFORMAT> void RenderLine(const size_t l);
+	void UpdateAverageBacklightIntensityTotal();
 	void ClearWithColor(const u16 colorBGRA5551);
 };
 

desmume/src/NDSSystem.cpp
@@ -1533,6 +1533,8 @@ static void execHardware_hstart()
 	{
 		gfx3d_VBlankEndSignal(frameSkipper.ShouldSkip3D());
 	}
+	
+	GPU->UpdateAverageBacklightIntensityTotal();
 
 	if (nds.VCount == 263)
 	{

desmume/src/frontend/cocoa/OGLDisplayOutput.cpp
@@ -332,10 +332,11 @@ static const char *BicubicSample6x6Output_VertShader_110 = {"\
 static const char *PassthroughOutputFragShader_110 = {"\
 	VARYING vec2 texCoord[1];\n\
 	uniform sampler2DRect tex;\n\
+	uniform float backlightIntensity;\n\
 	\n\
 	void main()\n\
 	{\n\
-		OUT_FRAG_COLOR.rgb = SAMPLE3_TEX_RECT(tex, texCoord[0]);\n\
+		OUT_FRAG_COLOR.rgb = SAMPLE3_TEX_RECT(tex, texCoord[0]) * backlightIntensity;\n\
 		OUT_FRAG_COLOR.a = 1.0;\n\
 	}\n\
 "};
@@ -583,6 +584,7 @@ static const char *FilterDeposterizeFragShader_110 = {"\
 static const char *FilterBicubicBSplineFragShader_110 = {"\
 	VARYING vec2 texCoord[16];\n\
 	uniform sampler2DRect tex;\n\
+	uniform float backlightIntensity;\n\
 	\n\
 	vec4 WeightBSpline(float f)\n\
 	{\n\
@@ -624,6 +626,7 @@ static const char *FilterBicubicBSplineFragShader_110 = {"\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[14]) * wx.g\n\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[13]) * wx.b\n\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[12]) * wx.a) * wy.a;\n\
+		OUT_FRAG_COLOR.rgb *= backlightIntensity;\n\
 		OUT_FRAG_COLOR.a = 1.0;\n\
 	}\n\
 "};
@@ -631,6 +634,7 @@ static const char *FilterBicubicBSplineFragShader_110 = {"\
 static const char *FilterBicubicBSplineFastFragShader_110 = {"\
 	VARYING vec2 texCoord[1];\n\
 	uniform sampler2DRect tex;\n\
+	uniform float backlightIntensity;\n\
 	\n\
 	void main()\n\
 	{\n\
@@ -652,6 +656,7 @@ static const char *FilterBicubicBSplineFastFragShader_110 = {"\
 							   SAMPLE3_TEX_RECT(tex, vec2(t1.x, t0.y)) * s1.x) * s0.y +\n\
 							  (SAMPLE3_TEX_RECT(tex, vec2(t0.x, t1.y)) * s0.x +\n\
 							   SAMPLE3_TEX_RECT(tex, vec2(t1.x, t1.y)) * s1.x) * s1.y;\n\
+		OUT_FRAG_COLOR.rgb *= backlightIntensity;\n\
 		OUT_FRAG_COLOR.a = 1.0;\n\
 	}\n\
 "};
@@ -659,6 +664,7 @@ static const char *FilterBicubicBSplineFastFragShader_110 = {"\
 static const char *FilterBicubicMitchellNetravaliFragShader_110 = {"\
 	VARYING vec2 texCoord[16];\n\
 	uniform sampler2DRect tex;\n\
+	uniform float backlightIntensity;\n\
 	\n\
 	vec4 WeightMitchellNetravali(float f)\n\
 	{\n\
@@ -700,6 +706,7 @@ static const char *FilterBicubicMitchellNetravaliFragShader_110 = {"\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[14]) * wx.g\n\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[13]) * wx.b\n\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[12]) * wx.a) * wy.a;\n\
+		OUT_FRAG_COLOR.rgb *= backlightIntensity;\n\
 		OUT_FRAG_COLOR.a = 1.0;\n\
 	}\n\
 "};
@@ -707,6 +714,7 @@ static const char *FilterBicubicMitchellNetravaliFragShader_110 = {"\
 static const char *FilterBicubicMitchellNetravaliFastFragShader_110 = {"\
 	VARYING vec2 texCoord[1];\n\
 	uniform sampler2DRect tex;\n\
+	uniform float backlightIntensity;\n\
 	\n\
 	void main()\n\
 	{\n\
@@ -728,6 +736,7 @@ static const char *FilterBicubicMitchellNetravaliFastFragShader_110 = {"\
 							   SAMPLE3_TEX_RECT(tex, vec2(t1.x, t0.y)) * s1.x) * s0.y +\n\
 							  (SAMPLE3_TEX_RECT(tex, vec2(t0.x, t1.y)) * s0.x +\n\
 							   SAMPLE3_TEX_RECT(tex, vec2(t1.x, t1.y)) * s1.x) * s1.y;\n\
+		OUT_FRAG_COLOR.rgb *= backlightIntensity;\n\
 		OUT_FRAG_COLOR.a = 1.0;\n\
 	}\n\
 "};
@@ -739,6 +748,7 @@ static const char *FilterLanczos2FragShader_110 = {"\
 	\n\
 	VARYING vec2 texCoord[16];\n\
 	uniform sampler2DRect tex;\n\
+	uniform float backlightIntensity;\n\
 	\n\
 	vec4 WeightLanczos2(float f)\n\
 	{\n\
@@ -778,6 +788,7 @@ static const char *FilterLanczos2FragShader_110 = {"\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[14]) * wx.g\n\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[13]) * wx.b\n\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[12]) * wx.a) * wy.a;\n\
+		OUT_FRAG_COLOR.rgb *= backlightIntensity;\n\
 		OUT_FRAG_COLOR.a = 1.0;\n\
 	}\n\
 "};
@@ -795,6 +806,7 @@ static const char *FilterLanczos3FragShader_110 = {"\
 	VARYING vec2 texCoord[16];\n\
 #endif\n\
 	uniform sampler2DRect tex;\n\
+	uniform float backlightIntensity;\n\
 	\n\
 	vec3 weight3(float x)\n\
 	{\n\
@@ -938,6 +950,7 @@ static const char *FilterLanczos3FragShader_110 = {"\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[ 0] + vec2( 2.0, 3.0)) * wx1.b\n\
 							+  SAMPLE3_TEX_RECT(tex, texCoord[ 0] + vec2( 3.0, 3.0)) * wx2.b) * wy2.b;\n\
 #endif\n\
+		OUT_FRAG_COLOR.rgb *= backlightIntensity;\n\
 		OUT_FRAG_COLOR.a = 1.0;\n\
 	}\n\
 "};
@@ -5695,6 +5708,7 @@ OGLImage::OGLImage(OGLContextInfo *contextInfo, GLsizei imageWidth, GLsizei imag
 		_uniformScalar = glGetUniformLocation(finalOutputProgramID, "scalar");
 		_uniformViewSize = glGetUniformLocation(finalOutputProgramID, "viewSize");
 		_uniformRenderFlipped = glGetUniformLocation(finalOutputProgramID, "renderFlipped");
+		_uniformBacklightIntensity = glGetUniformLocation(finalOutputProgramID, "backlightIntensity");
 		glUseProgram(0);
 	}
 	else
@@ -6225,7 +6239,12 @@ void OGLImage::ProcessOGL()
 
 void OGLImage::RenderOGL()
 {
-	glUseProgram(this->_finalOutputProgram->GetProgramID());
+	if (this->_canUseShaderOutput)
+	{
+		glUseProgram(this->_finalOutputProgram->GetProgramID());
+		glUniform1f(this->_uniformBacklightIntensity, 1.0f);
+	}
+	
 	this->UploadTransformationOGL();
 	
 	if (this->_needUploadVertices)
@@ -6712,6 +6731,7 @@ OGLDisplayLayer::OGLDisplayLayer(OGLVideoOutput *oglVO)
 		_uniformScalar = glGetUniformLocation(finalOutputProgramID, "scalar");
 		_uniformViewSize = glGetUniformLocation(finalOutputProgramID, "viewSize");
 		_uniformRenderFlipped = glGetUniformLocation(finalOutputProgramID, "renderFlipped");
+		_uniformBacklightIntensity = glGetUniformLocation(finalOutputProgramID, "backlightIntensity");
 		glUseProgram(0);
 	}
 	else
@@ -7204,8 +7224,9 @@ void OGLDisplayLayer::ProcessOGL()
 void OGLDisplayLayer::RenderOGL(bool isRenderingFlipped)
 {
 	const NDSDisplayInfo &emuDisplayInfo = this->_output->GetEmuDisplayInfo();
+	const bool isShaderSupported = this->_output->GetContextInfo()->IsShaderSupported();
 	
-	if (this->_output->GetContextInfo()->IsShaderSupported())
+	if (isShaderSupported)
 	{
 		glUseProgram(this->_finalOutputProgram->GetProgramID());
 		
@@ -7258,6 +7279,11 @@ void OGLDisplayLayer::RenderOGL(bool isRenderingFlipped)
 			{
 				if (this->_output->IsSelectedDisplayEnabled(NDSDisplayID_Main))
 				{
+					if (isShaderSupported)
+					{
+						glUniform1f(this->_uniformBacklightIntensity, emuDisplayInfo.backlightIntensity[NDSDisplayID_Main]);
+					}
+					
 					glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoOutputID[NDSDisplayID_Main]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, this->_displayTexFilter[NDSDisplayID_Main]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, this->_displayTexFilter[NDSDisplayID_Main]);
@@ -7270,6 +7296,11 @@ void OGLDisplayLayer::RenderOGL(bool isRenderingFlipped)
 			{
 				if (this->_output->IsSelectedDisplayEnabled(NDSDisplayID_Touch))
 				{
+					if (isShaderSupported)
+					{
+						glUniform1f(this->_uniformBacklightIntensity, emuDisplayInfo.backlightIntensity[NDSDisplayID_Touch]);
+					}
+					
 					glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoOutputID[NDSDisplayID_Touch]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, this->_displayTexFilter[NDSDisplayID_Touch]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, this->_displayTexFilter[NDSDisplayID_Touch]);
@@ -7291,6 +7322,11 @@ void OGLDisplayLayer::RenderOGL(bool isRenderingFlipped)
 					{
 						if (this->_output->IsSelectedDisplayEnabled(majorDisplayID))
 						{
+							if (isShaderSupported)
+							{
+								glUniform1f(this->_uniformBacklightIntensity, emuDisplayInfo.backlightIntensity[majorDisplayID]);
+							}
+							
 							glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoOutputID[majorDisplayID]);
 							glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, this->_displayTexFilter[majorDisplayID]);
 							glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, this->_displayTexFilter[majorDisplayID]);
@@ -7305,6 +7341,11 @@ void OGLDisplayLayer::RenderOGL(bool isRenderingFlipped)
 				
 				if (this->_output->IsSelectedDisplayEnabled(NDSDisplayID_Main))
 				{
+					if (isShaderSupported)
+					{
+						glUniform1f(this->_uniformBacklightIntensity, emuDisplayInfo.backlightIntensity[NDSDisplayID_Main]);
+					}
+					
 					glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoOutputID[NDSDisplayID_Main]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, this->_displayTexFilter[NDSDisplayID_Main]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, this->_displayTexFilter[NDSDisplayID_Main]);
@@ -7313,6 +7354,11 @@ void OGLDisplayLayer::RenderOGL(bool isRenderingFlipped)
 				
 				if (this->_output->IsSelectedDisplayEnabled(NDSDisplayID_Touch))
 				{
+					if (isShaderSupported)
+					{
+						glUniform1f(this->_uniformBacklightIntensity, emuDisplayInfo.backlightIntensity[NDSDisplayID_Touch]);
+					}
+					
 					glBindTexture(GL_TEXTURE_RECTANGLE_ARB, this->_texVideoOutputID[NDSDisplayID_Touch]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, this->_displayTexFilter[NDSDisplayID_Touch]);
 					glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, this->_displayTexFilter[NDSDisplayID_Touch]);

desmume/src/frontend/cocoa/OGLDisplayOutput.h
@@ -213,6 +213,7 @@ protected:
 	GLint _uniformScalar;
 	GLint _uniformViewSize;
 	GLint _uniformRenderFlipped;
+	GLint _uniformBacklightIntensity;
 	
 	void UploadVerticesOGL();
 	void UploadTexCoordsOGL();
@@ -259,6 +260,7 @@ protected:
 	GLint _uniformScalar;
 	GLint _uniformViewSize;
 	GLint _uniformRenderFlipped;
+	GLint _uniformBacklightIntensity;
 	
 public:
 	virtual ~OGLVideoLayer() {};

desmume/src/frontend/cocoa/userinterface/MacMetalDisplayView.mm
@@ -1629,6 +1629,8 @@
 	// Draw the NDS displays.
 	if (_willDrawDisplays)
 	{
+		const NDSDisplayInfo &displayInfo = cdp->GetEmuDisplayInfo();
+		
 		[rce setRenderPipelineState:outputPipelineState];
 		[rce setVertexBuffer:_displayVtxPositionBuffer offset:0 atIndex:0];
 		[rce setVertexBuffer:_displayTexCoordBuffer offset:0 atIndex:1];
@@ -1640,6 +1642,7 @@
 			{
 				if (cdp->IsSelectedDisplayEnabled(NDSDisplayID_Main))
 				{
+					[rce setFragmentBytes:&displayInfo.backlightIntensity[NDSDisplayID_Main] length:sizeof(displayInfo.backlightIntensity[NDSDisplayID_Main]) atIndex:0];
 					[rce setFragmentTexture:_texDisplayOutput[NDSDisplayID_Main] atIndex:0];
 					[rce drawPrimitives:MTLPrimitiveTypeTriangleStrip vertexStart:0 vertexCount:4];
 				}
@@ -1650,6 +1653,7 @@
 			{
 				if (cdp->IsSelectedDisplayEnabled(NDSDisplayID_Touch))
 				{
+					[rce setFragmentBytes:&displayInfo.backlightIntensity[NDSDisplayID_Touch] length:sizeof(displayInfo.backlightIntensity[NDSDisplayID_Touch]) atIndex:0];
 					[rce setFragmentTexture:_texDisplayOutput[NDSDisplayID_Touch] atIndex:0];
 					[rce drawPrimitives:MTLPrimitiveTypeTriangleStrip vertexStart:4 vertexCount:4];
 				}
@@ -1669,6 +1673,7 @@
 					{
 						if (cdp->IsSelectedDisplayEnabled(majorDisplayID))
 						{
+							[rce setFragmentBytes:&displayInfo.backlightIntensity[majorDisplayID] length:sizeof(displayInfo.backlightIntensity[majorDisplayID]) atIndex:0];
 							[rce setFragmentTexture:_texDisplayOutput[majorDisplayID] atIndex:0];
 							[rce drawPrimitives:MTLPrimitiveTypeTriangleStrip vertexStart:majorDisplayVtx vertexCount:4];
 						}
@@ -1681,12 +1686,14 @@
 				
 				if (cdp->IsSelectedDisplayEnabled(NDSDisplayID_Main))
 				{
+					[rce setFragmentBytes:&displayInfo.backlightIntensity[NDSDisplayID_Main] length:sizeof(displayInfo.backlightIntensity[NDSDisplayID_Main]) atIndex:0];
 					[rce setFragmentTexture:_texDisplayOutput[NDSDisplayID_Main] atIndex:0];
 					[rce drawPrimitives:MTLPrimitiveTypeTriangleStrip vertexStart:0 vertexCount:4];
 				}
 				
 				if (cdp->IsSelectedDisplayEnabled(NDSDisplayID_Touch))
 				{
+					[rce setFragmentBytes:&displayInfo.backlightIntensity[NDSDisplayID_Touch] length:sizeof(displayInfo.backlightIntensity[NDSDisplayID_Touch]) atIndex:0];
 					[rce setFragmentTexture:_texDisplayOutput[NDSDisplayID_Touch] atIndex:0];
 					[rce drawPrimitives:MTLPrimitiveTypeTriangleStrip vertexStart:4 vertexCount:4];
 				}

desmume/src/frontend/cocoa/userinterface/MacMetalDisplayViewShaders.metal
@@ -226,17 +226,21 @@ vertex DisplayVtx display_output_bicubic_vertex(const device float2 *inPosition 
 
 //---------------------------------------
 // Input Pixel Mapping:  00
-fragment float4 output_filter_nearest(const DisplayVtx vtx [[stage_in]], const texture2d<float> tex [[texture(0)]])
+fragment float4 output_filter_nearest(const DisplayVtx vtx [[stage_in]],
+									  const texture2d<float> tex [[texture(0)]],
+									  const device float *inBacklightIntensity [[buffer(0)]])
 {
-	return tex.sample(genSampler, vtx.texCoord);
+	return float4(tex.sample(genSampler, vtx.texCoord).rgb * *inBacklightIntensity, 1.0f);
 }
 
 //---------------------------------------
 // Input Pixel Mapping:  00|01
 //                       02|03
-fragment float4 output_filter_bilinear(const DisplayVtx vtx [[stage_in]], const texture2d<float> tex [[texture(0)]])
+fragment float4 output_filter_bilinear(const DisplayVtx vtx [[stage_in]],
+									   const texture2d<float> tex [[texture(0)]],
+									   const device float *inBacklightIntensity [[buffer(0)]])
 {
-	return tex.sample(outputSamplerBilinear, vtx.texCoord);
+	return float4(tex.sample(outputSamplerBilinear, vtx.texCoord).rgb * *inBacklightIntensity, 1.0f);
 }
 
 //---------------------------------------
@@ -244,7 +248,9 @@ fragment float4 output_filter_bilinear(const DisplayVtx vtx [[stage_in]], const 
 //                       04|05|06|07
 //                       08|09|10|11
 //                       12|13|14|15
-fragment float4 output_filter_bicubic_bspline(const DisplayVtx vtx [[stage_in]], const texture2d<float> tex [[texture(0)]])
+fragment float4 output_filter_bicubic_bspline(const DisplayVtx vtx [[stage_in]],
+											  const texture2d<float> tex [[texture(0)]],
+											  const device float *inBacklightIntensity [[buffer(0)]])
 {
 	float2 f = fract(vtx.texCoord);
 	float4 wx = bicubic_weight_bspline(f.x);
@@ -271,7 +277,7 @@ fragment float4 output_filter_bicubic_bspline(const DisplayVtx vtx [[stage_in]],
 						  +  tex.sample(genSampler, vtx.texCoord, int2( 1, 2)) * wx.b
 						  +  tex.sample(genSampler, vtx.texCoord, int2( 2, 2)) * wx.a) * wy.a;
 	
-	return float4(outFragment.rgb, 1.0f);
+	return float4(outFragment.rgb * *inBacklightIntensity, 1.0f);
 }
 
 //---------------------------------------
@@ -279,7 +285,9 @@ fragment float4 output_filter_bicubic_bspline(const DisplayVtx vtx [[stage_in]],
 //                       04|05|06|07
 //                       08|09|10|11
 //                       12|13|14|15
-fragment float4 output_filter_bicubic_mitchell_netravali(const DisplayVtx vtx [[stage_in]], const texture2d<float> tex [[texture(0)]])
+fragment float4 output_filter_bicubic_mitchell_netravali(const DisplayVtx vtx [[stage_in]],
+														 const texture2d<float> tex [[texture(0)]],
+														 const device float *inBacklightIntensity [[buffer(0)]])
 {
 	float2 f = fract(vtx.texCoord);
 	float4 wx = bicubic_weight_mitchell_netravali(f.x);
@@ -306,7 +314,7 @@ fragment float4 output_filter_bicubic_mitchell_netravali(const DisplayVtx vtx [[
 						  +  tex.sample(genSampler, vtx.texCoord, int2( 1, 2)) * wx.b
 						  +  tex.sample(genSampler, vtx.texCoord, int2( 2, 2)) * wx.a) * wy.a;
 	
-	return float4(outFragment.rgb, 1.0f);
+	return float4(outFragment.rgb * *inBacklightIntensity, 1.0f);
 }
 
 //---------------------------------------
@@ -314,7 +322,9 @@ fragment float4 output_filter_bicubic_mitchell_netravali(const DisplayVtx vtx [[
 //                       04|05|06|07
 //                       08|09|10|11
 //                       12|13|14|15
-fragment float4 output_filter_lanczos2(const DisplayVtx vtx [[stage_in]], const texture2d<float> tex [[texture(0)]])
+fragment float4 output_filter_lanczos2(const DisplayVtx vtx [[stage_in]],
+									   const texture2d<float> tex [[texture(0)]],
+									   const device float *inBacklightIntensity [[buffer(0)]])
 {
 	const float2 f = fract(vtx.texCoord);
 	float4 wx = bicubic_weight_lanczos2(f.x);
@@ -341,7 +351,7 @@ fragment float4 output_filter_lanczos2(const DisplayVtx vtx [[stage_in]], const 
 								+  tex.sample(genSampler, vtx.texCoord, int2( 1, 2)) * wx.b
 								+  tex.sample(genSampler, vtx.texCoord, int2( 2, 2)) * wx.a) * wy.a;
 	
-	return float4(outFragment.rgb, 1.0f);
+	return float4(outFragment.rgb * *inBacklightIntensity, 1.0f);
 }
 
 //---------------------------------------
@@ -351,7 +361,9 @@ fragment float4 output_filter_lanczos2(const DisplayVtx vtx [[stage_in]], const 
 //                      18|19|20|21|22|23
 //                      24|25|26|27|28|29
 //                      30|31|32|33|34|35
-fragment float4 output_filter_lanczos3(const DisplayVtx vtx [[stage_in]], const texture2d<float> tex [[texture(0)]])
+fragment float4 output_filter_lanczos3(const DisplayVtx vtx [[stage_in]],
+									   const texture2d<float> tex [[texture(0)]],
+									   const device float *inBacklightIntensity [[buffer(0)]])
 {
 	const float2 f = fract(vtx.texCoord);
 	float3 wx1 = bicubic_weight_lanczos3(0.5f - f.x * 0.5f);
@@ -404,7 +416,7 @@ fragment float4 output_filter_lanczos3(const DisplayVtx vtx [[stage_in]], const 
 								+  tex.sample(genSampler, vtx.texCoord, int2( 2, 3)) * wx1.b
 								+  tex.sample(genSampler, vtx.texCoord, int2( 3, 3)) * wx2.b) * wy2.b;
 	
-	return float4(outFragment.rgb, 1.0f);
+	return float4(outFragment.rgb * *inBacklightIntensity, 1.0f);
 }
 
 #pragma mark NDS Emulation Functions

commit f4c01c4b577ba51d52fc41054f57feb28ea773fc

GPU: Fix bug where the backlight intensity would be incorrectly averaged if frameskipping is enabled. (Regression from commit f5c9a36.)

desmume/src/GPU.cpp
@@ -8115,11 +8115,9 @@ void GPUSubsystem::RenderLine(const size_t l)
 			this->_displayInfo.needConvertColorFormat[NDSDisplayID_Main]  = (OUTPUTFORMAT == NDSColorFormat_BGR666_Rev);
 			this->_displayInfo.needConvertColorFormat[NDSDisplayID_Touch] = (OUTPUTFORMAT == NDSColorFormat_BGR666_Rev);
 			
-			// Set the average backlight intensity and then reset the current total.
+			// Set the average backlight intensity over 263 H-blanks.
 			this->_displayInfo.backlightIntensity[NDSDisplayID_Main]  = this->_backlightIntensityTotal[NDSDisplayID_Main]  / 263.0f;
 			this->_displayInfo.backlightIntensity[NDSDisplayID_Touch] = this->_backlightIntensityTotal[NDSDisplayID_Touch] / 263.0f;
-			this->_backlightIntensityTotal[NDSDisplayID_Main]  = 0.0f;
-			this->_backlightIntensityTotal[NDSDisplayID_Touch] = 0.0f;
 			
 			this->_engineMain->UpdateMasterBrightnessDisplayInfo(this->_displayInfo);
 			this->_engineSub->UpdateMasterBrightnessDisplayInfo(this->_displayInfo);
@@ -8136,6 +8134,10 @@ void GPUSubsystem::RenderLine(const size_t l)
 				this->ResolveDisplayToCustomFramebuffer(NDSDisplayID_Touch, this->_displayInfo);
 			}
 		}
+		
+		// Reset the current backlight intensity total.
+		this->_backlightIntensityTotal[NDSDisplayID_Main]  = 0.0f;
+		this->_backlightIntensityTotal[NDSDisplayID_Touch] = 0.0f;
 		
 		if (this->_frameNeedsFinish)
 		{