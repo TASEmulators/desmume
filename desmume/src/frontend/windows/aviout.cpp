TASEmulators/desmume

commit db1a19ad86b97e4dfe3e9ffbb75502ab3f6182b1

Windows Port: Do a complete code cleanup and refactor of aviout.cpp to use proper C++ objects. - aviout.cpp now uses Windows-style line-endings instead of Unix-style line-endings. - AVI segments now fill up much closer to the 2 GB file limit than before. - Error handling in the file writing thread is much more robust.

desmume/src/frontend/windows/aviout.h
@@ -18,16 +18,134 @@
 #ifndef _AVIOUT_H_
 #define _AVIOUT_H_
 
-#include "types.h"
+#include <windows.h>
+#include <vfw.h>
 
-bool DRV_AviBegin(const char* fname, bool newsegment = false);
-void DRV_AviEnd(bool newsegment = false);
+#include "GPU.h"
+#include "SPU.h"
+#include "utils/task.h"
+
+#define VIDEO_STREAM				0
+#define AUDIO_STREAM				1
+#define MAX_CONVERT_THREADS			32
+
+#define AUDIO_STREAM_BUFFER_SIZE	((DESMUME_SAMPLE_RATE * sizeof(u16) * 2) / 30)		// 16-bit samples, 2 channels, 2 frames (need only 1 frame's worth, but have 2 frame's worth for safety)
+#define MAX_AVI_FILE_SIZE			(2ULL * 1024ULL * 1024ULL * 1024ULL)				// Max file size should be 2 GB due to the Video for Windows AVI limit.
+#define PENDING_BUFFER_COUNT		2													// Number of pending buffers to maintain for file writes. Each pending buffer will store 1 frame's worth of the audio/video streams.
+
+class NDSCaptureObject;
+class AVIFileStream;
+
+struct VideoConvertParam
+{
+	NDSCaptureObject *captureObj;
+
+	const void *src;
+	u8 *dst;
+
+	size_t srcOffset;
+	size_t dstOffset;
+
+	size_t firstLineIndex;
+	size_t lastLineIndex;
+	size_t frameWidth;
+};
+typedef struct VideoConvertParam VideoConvertParam;
+
+struct AVIFileWriteParam
+{
+	AVIFileStream *fs;
+	u8 *srcVideo;
+	u8 *srcAudio;
+	size_t videoBufferSize;
+	size_t audioBufferSize;
+};
+typedef struct AVIFileWriteParam AVIFileWriteParam;
+
+class AVIFileStream
+{
+private:
+	static bool __needAviLibraryInit;
+
+protected:
+	PAVIFILE _file;
+	PAVISTREAM _stream[2];
+	PAVISTREAM _compressedStream[2];
+	AVISTREAMINFO _streamInfo[2];
+
+	AVICOMPRESSOPTIONS _compressionOptions[2];
+	BITMAPINFOHEADER _bmpFormat;
+	WAVEFORMATEX _wavFormat;
+
+	char _baseFileName[MAX_PATH];
+	char _baseFileNameExt[MAX_PATH];
+	size_t _segmentNumber;
+
+	size_t _expectedFrameSize;
+	size_t _writtenBytes;
+	LONG _writtenVideoFrameCount;
+	LONG _writtenAudioSampleCount;
+
+public:
+	AVIFileStream();
+	~AVIFileStream();
+
+	HRESULT Open(const char *fileName, BITMAPINFOHEADER *bmpFormat, WAVEFORMATEX *wavFormat);
+	void Close();
+	bool IsValid();
+
+	HRESULT FlushVideo(u8 *srcBuffer, const LONG bufferSize);
+	HRESULT FlushAudio(u8 *srcBuffer, const LONG bufferSize);
+	HRESULT WriteOneFrame(u8 *srcVideo, const LONG videoBufferSize, u8 *srcAudio, const LONG audioBufferSize);
+};
+
+class NDSCaptureObject
+{
+protected:
+	AVIFileStream *_fs;
+
+	BITMAPINFOHEADER _bmpFormat;
+	WAVEFORMATEX _wavFormat;
+
+	u8 *_pendingVideoBuffer;
+	u8 _pendingAudioBuffer[AUDIO_STREAM_BUFFER_SIZE * PENDING_BUFFER_COUNT];
+	size_t _pendingAudioWriteSize[2];
+	size_t _currentBufferIndex;
+
+	size_t _numThreads;
+	Task *_fileWriteThread;
+	Task *_convertThread[MAX_CONVERT_THREADS];
+	VideoConvertParam _convertParam[MAX_CONVERT_THREADS];
+	AVIFileWriteParam _fileWriteParam;
+
+public:
+	NDSCaptureObject();
+	NDSCaptureObject(size_t videoWidth, size_t videoHeight, const WAVEFORMATEX *wfex);
+	~NDSCaptureObject();
+
+	HRESULT OpenFileStream(const char *fileName);
+	void CloseFileStream();
+	bool IsFileStreamValid();
+
+	void StartFrame();
+	void StreamWriteStart();
+	void StreamWriteFinish();
+
+	void ConvertVideoSlice555Xto888(const VideoConvertParam &param);
+	void ConvertVideoSlice888Xto888(const VideoConvertParam &param);
+
+	void ReadVideoFrame(const void *srcVideoFrame, const size_t inFrameWidth, const size_t inFrameHeight, const NDSColorFormat colorFormat);
+	void ReadAudioFrames(const void *srcAudioBuffer, const size_t inSampleCount);
+};
+
+bool DRV_AviBegin(const char *fileName);
+void DRV_AviEnd();
 bool AVI_IsRecording();
 
 void DRV_AviFrameStart();
-void DRV_AviVideoUpdate();
-void DRV_AviSoundUpdate(void* soundData, int soundLen);
-void DRV_AviFileWrite();
+void DRV_AviVideoUpdate(const NDSDisplayInfo &displayInfo);
+void DRV_AviSoundUpdate(void *soundData, const int soundLen);
+void DRV_AviFileWriteStart();
 void DRV_AviFileWriteFinish();
 
 #endif

desmume/src/frontend/windows/main.cpp
@@ -548,7 +548,7 @@ public:
 			return;
 		}
 
-		DRV_AviVideoUpdate();
+		DRV_AviVideoUpdate(latestDisplayInfo);
 	}
 };
 
@@ -2235,7 +2235,7 @@ static void StepRunLoop_Core()
 	}
 	inFrameBoundary = true;
 
-	DRV_AviFileWrite();
+	DRV_AviFileWriteStart();
 
 	CallRegisteredLuaFunctions(LUACALL_AFTEREMULATION);
 	ServiceDisplayThreadInvocations();
@@ -3515,7 +3515,6 @@ int _main()
 
 	KillDisplay();
 
-	DRV_AviFileWriteFinish();
 	DRV_AviEnd();
 	WAV_End();
 
@@ -3807,8 +3806,11 @@ void SetRotate(HWND hwnd, int rot, bool user)
 void AviEnd()
 {
 	NDS_Pause();
-	DRV_AviFileWriteFinish();
+
 	DRV_AviEnd();
+	LOG("AVI recording ended.");
+	driver->AddLine("AVI recording ended.");
+
 	NDS_UnPause();
 }
 
@@ -3868,9 +3870,21 @@ void AviRecordTo()
 	ofn.nMaxFile = MAX_PATH;
 	ofn.Flags = OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT | OFN_NOREADONLYRETURN | OFN_PATHMUSTEXIST;
 
-	if(GetSaveFileName(&ofn))
+	if (GetSaveFileName(&ofn))
 	{
-		DRV_AviBegin(folder);
+		if (AVI_IsRecording())
+		{
+			DRV_AviEnd();
+			LOG("AVI recording ended.");
+			driver->AddLine("AVI recording ended.");
+		}
+
+		bool result = DRV_AviBegin(folder);
+		if (result)
+		{
+			LOG("AVI recording started.");
+			driver->AddLine("AVI recording started.");
+		}
 	}
 
 	NDS_UnPause();

commit e825cddbe5da874f4c136a56bdea790f0f78084d

Windows Port: Fix a rare and intermittent bug that could cause the AVI recording to accidentally stop in between AVI segments. (Regression from commit db1a19a.)

desmume/src/frontend/windows/aviout.cpp
@@ -193,8 +193,13 @@ HRESULT AVIFileStream::Open()
 	error = AVIFileOpen(&this->_file, workingFileName, OF_CREATE | OF_WRITE, NULL);
 	if (FAILED(error))
 	{
+		this->_file = NULL;
 		return error;
 	}
+
+	this->_writtenVideoFrameCount = 0;
+	this->_writtenAudioSampleCount = 0;
+	this->_writtenBytes = 0;
 
 	if (this->_streamInfo[VIDEO_STREAM].dwSuggestedBufferSize > 0)
 	{
@@ -256,13 +261,31 @@ HRESULT AVIFileStream::Open()
 	return error;
 }
 
-void AVIFileStream::Close(FileStreamCloseAction theAction)
+void AVIFileStream::_CloseStreams()
 {
-	if (this->_file == NULL)
+	// _compressedStream[AUDIO_STREAM] is just a copy of _stream[AUDIO_STREAM]
+	if (this->_compressedStream[AUDIO_STREAM])
 	{
-		return;
+		AVIStreamClose(this->_compressedStream[AUDIO_STREAM]);
+		this->_compressedStream[AUDIO_STREAM] = NULL;
+		this->_stream[AUDIO_STREAM] = NULL;
 	}
 
+	if (this->_compressedStream[VIDEO_STREAM])
+	{
+		AVIStreamClose(this->_compressedStream[VIDEO_STREAM]);
+		this->_compressedStream[VIDEO_STREAM] = NULL;
+	}
+
+	if (this->_stream[VIDEO_STREAM])
+	{
+		AVIStreamClose(this->_stream[VIDEO_STREAM]);
+		this->_stream[VIDEO_STREAM] = NULL;
+	}
+}
+
+void AVIFileStream::Close(FileStreamCloseAction theAction)
+{
 	switch (theAction)
 	{
 		case FSCA_PurgeQueue:
@@ -283,57 +306,45 @@ void AVIFileStream::Close(FileStreamCloseAction theAction)
 
 		case FSCA_WriteRemainingInQueue:
 		{
-			do
+			if (this->_file != NULL)
 			{
-				slock_lock(this->_mutexQueue);
-				if (this->_writeQueue.empty())
+				do
 				{
+					slock_lock(this->_mutexQueue);
+					if (this->_writeQueue.empty())
+					{
+						slock_unlock(this->_mutexQueue);
+						break;
+					}
+
+					const AVIFileWriteParam param = this->_writeQueue.front();
 					slock_unlock(this->_mutexQueue);
-					break;
-				}
 
-				const AVIFileWriteParam param = this->_writeQueue.front();
-				slock_unlock(this->_mutexQueue);
+					this->WriteOneFrame(param);
 
-				this->WriteOneFrame(param);
+					slock_lock(this->_mutexQueue);
+					this->_writeQueue.pop();
+					slock_unlock(this->_mutexQueue);
 
-				slock_lock(this->_mutexQueue);
-				this->_writeQueue.pop();
-				slock_unlock(this->_mutexQueue);
+					ssem_signal(this->_semQueue);
 
-				ssem_signal(this->_semQueue);
-
-			} while (true);
+				} while (true);
+			}
 			break;
 		}
+
+		case FSCA_DoNothing:
+		default:
+			break;
 	}
 
-	// _compressedStream[AUDIO_STREAM] is just a copy of _stream[AUDIO_STREAM]
-	if (this->_compressedStream[AUDIO_STREAM])
+	this->_CloseStreams();
+
+	if (this->_file != NULL)
 	{
-		AVIStreamClose(this->_compressedStream[AUDIO_STREAM]);
-		this->_compressedStream[AUDIO_STREAM] = NULL;
-		this->_stream[AUDIO_STREAM] = NULL;
+		AVIFileClose(this->_file);
+		this->_file = NULL;
 	}
-
-	if (this->_compressedStream[VIDEO_STREAM])
-	{
-		AVIStreamClose(this->_compressedStream[VIDEO_STREAM]);
-		this->_compressedStream[VIDEO_STREAM] = NULL;
-	}
-
-	if (this->_stream[VIDEO_STREAM])
-	{
-		AVIStreamClose(this->_stream[VIDEO_STREAM]);
-		this->_stream[VIDEO_STREAM] = NULL;
-	}
-
-	AVIFileClose(this->_file);
-	this->_file = NULL;
-
-	this->_writtenVideoFrameCount = 0;
-	this->_writtenAudioSampleCount = 0;
-	this->_writtenBytes = 0;
 }
 
 bool AVIFileStream::IsValid()
@@ -451,10 +462,14 @@ HRESULT AVIFileStream::WriteOneFrame(const AVIFileWriteParam &param)
 	const size_t futureFrameSize = this->_writtenBytes + this->_expectedFrameSize;
 	if (futureFrameSize >= MAX_AVI_FILE_SIZE)
 	{
-		this->Close(FSCA_DoNothing);
+		PAVIFILE oldFile = this->_file;
+
+		this->_CloseStreams();
 		this->_segmentNumber++;
 
 		error = this->Open();
+		AVIFileClose(oldFile);
+
 		if (FAILED(error))
 		{
 			EMU_PrintError("Error creating new AVI segment.");

desmume/src/frontend/windows/aviout.h
@@ -104,6 +104,8 @@ protected:
 	slock_t *_mutexQueue;
 	AVIWriteQueue _writeQueue;
 
+	void _CloseStreams();
+
 public:
 	AVIFileStream();
 	~AVIFileStream();