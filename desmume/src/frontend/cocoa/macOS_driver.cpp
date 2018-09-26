d9aa1ac31e59bb2de84dc245d449045fb58f3f70
rogerman <rogerman@users.noreply.github.com>
2018-09-26 02:26:19 -0700

    Cocoa Port: Update all build schemes in "DeSmuME (Latest).xcodeproj" for compatibility with Xcode 10.

--- desmume/src/frontend/cocoa/macOS_driver.cpp
+++ desmume/src/frontend/cocoa/macOS_driver.cpp
@@ -15,6 +15,8 @@
 	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <unistd.h>
+
 #include "macOS_driver.h"
 #include "ClientAVCaptureObject.h"
 #include "ClientExecutionControl.h"
