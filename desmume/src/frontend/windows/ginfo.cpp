TASEmulators/desmume

commit bb3802287308a2348477134921b79531a6b61005

Firmware: Replace the old NDS_fw_config_data struct with the new FirmwareConfig struct, which allows for easy modification of all the user-related settings, including all the WiFi settings. Also do a massive refactor and cleanup of the firmware-related code. - Also exposes all of the WiFi-related firmware settings in the Cocoa port UI.

desmume/src/frontend/windows/main.cpp
@@ -2247,20 +2247,36 @@ int _main()
 	wifiHandler->SetPCapInterface(winpcapInterface);
 	wifiHandler->SetSocketsSupported(isSocketsSupported);
 
-	// Get the host's IP4 address.
-	char hostname[256];
-	if (gethostname(hostname, 256) != 0)
-		strncpy(hostname, "127.0.0.1", 256);
-
-	hostent *he = gethostbyname(hostname);
-	unsigned long ipaddr;
-	if (he == NULL || he->h_addr_list[0] == NULL)
-		ipaddr = 0x0100007F; // 127.0.0.1
-	else
-		ipaddr = *(unsigned long*)he->h_addr_list[0];
-
-	wifiHandler->SetIP4Address(ipaddr);
-	wifiHandler->SetUniqueMACValue((u32)GetCurrentProcessId());
+	// Generate the unique MAC address for Ad-hoc mode.
+	{
+		// Get the host's IP4 address.
+		char hostname[256];
+		if (gethostname(hostname, 256) != 0)
+			strncpy(hostname, "127.0.0.1", 256);
+		
+		hostent *he = gethostbyname(hostname);
+		u32 ipaddr;
+		if (he == NULL || he->h_addr_list[0] == NULL)
+			ipaddr = 0x0100007F; // 127.0.0.1
+		else
+			ipaddr = *(u32 *)he->h_addr_list[0];
+		
+		u32 hash = (u32)GetCurrentProcessId();
+		
+		while ((hash & 0xFF000000) == 0)
+		{
+			hash <<= 1;
+		}
+		
+		hash >>= 1;
+		hash += ipAddr >> 8;
+		hash &= 0x00FFFFFF;
+		
+		wifiHandler->SetUserMACValues(hash >> 16, (hash >> 8) & 0xFF, hash & 0xFF);
+		wifiHandler->SetMACModeForComm(WifiCommInterfaceID_AdHoc, WifiMACMode_Manual);
+		//wifiHandler->SetFirmwareMACMode(FirmwareMACMode_Manual);
+	}
+	
 	wifiHandler->SetCommInterfaceID(CommonSettings.wifi.mode);
 	wifiHandler->SetBridgeDeviceIndex(CommonSettings.wifi.infraBridgeAdapter);
 
@@ -2425,12 +2441,12 @@ int _main()
 	FilterUpdate(MainWindow->getHWnd(),false);
 
 	//default the firmware settings, they may get changed later
-	NDS_FillDefaultFirmwareConfigData(&CommonSettings.fw_config);
+	NDS_GetDefaultFirmwareConfig(CommonSettings.fwConfig);
 	// Read the firmware settings from the init file
-	CommonSettings.fw_config.fav_colour = GetPrivateProfileInt("Firmware","favColor", 10, IniName);
-	CommonSettings.fw_config.birth_month = GetPrivateProfileInt("Firmware","bMonth", 7, IniName);
-	CommonSettings.fw_config.birth_day = GetPrivateProfileInt("Firmware","bDay", 15, IniName);
-	CommonSettings.fw_config.language = GetPrivateProfileInt("Firmware","Language", 1, IniName);
+	CommonSettings.fwConfig.favoriteColor = GetPrivateProfileInt("Firmware","favColor", 10, IniName);
+	CommonSettings.fwConfig.birthdayMonth = GetPrivateProfileInt("Firmware","bMonth", 7, IniName);
+	CommonSettings.fwConfig.birthdayDay = GetPrivateProfileInt("Firmware","bDay", 15, IniName);
+	CommonSettings.fwConfig.language = GetPrivateProfileInt("Firmware","Language", 1, IniName);
 
 	{
 		/*
@@ -2440,21 +2456,21 @@ int _main()
 		char temp_str[27];
 		int char_index;
 		GetPrivateProfileString("Firmware","nickName", "yopyop", temp_str, 11, IniName);
-		CommonSettings.fw_config.nickname_len = strlen( temp_str);
+		CommonSettings.fwConfig.nicknameLength = strlen( temp_str);
 
-		if (CommonSettings.fw_config.nickname_len == 0) {
+		if (CommonSettings.fwConfig.nicknameLength == 0) {
 			strcpy( temp_str, "yopyop");
-			CommonSettings.fw_config.nickname_len = strlen( temp_str);
+			CommonSettings.fwConfig.nicknameLength = strlen( temp_str);
 		}
 
-		for ( char_index = 0; char_index < CommonSettings.fw_config.nickname_len; char_index++) {
-			CommonSettings.fw_config.nickname[char_index] = temp_str[char_index];
+		for ( char_index = 0; char_index < CommonSettings.fwConfig.nicknameLength; char_index++) {
+			CommonSettings.fwConfig.nickname[char_index] = temp_str[char_index];
 		}
 
 		GetPrivateProfileString("Firmware","Message", "DeSmuME makes you happy!", temp_str, 27, IniName);
-		CommonSettings.fw_config.message_len = strlen( temp_str);
-		for ( char_index = 0; char_index < CommonSettings.fw_config.message_len; char_index++) {
-			CommonSettings.fw_config.message[char_index] = temp_str[char_index];
+		CommonSettings.fwConfig.messageLength = strlen( temp_str);
+		for ( char_index = 0; char_index < CommonSettings.fwConfig.messageLength; char_index++) {
+			CommonSettings.fwConfig.message[char_index] = temp_str[char_index];
 		}
 	}
 
@@ -2467,7 +2483,7 @@ int _main()
 	}
 
 	//not supported; use the GUI
-	//if(cmdline.language != -1) CommonSettings.fw_config.language = cmdline.language;
+	//if(cmdline.language != -1) CommonSettings.fwConfig.language = cmdline.language;
 
 	cmdline.process_movieCommands();
 	

desmume/src/frontend/posix/cli/main.cpp
@@ -501,12 +501,12 @@ int main(int argc, char ** argv) {
   const SDL_VideoInfo *videoInfo;
 
   /* the firmware settings */
-  struct NDS_fw_config_data fw_config;
+  FirmwareConfig fw_config;
 
   NDS_Init();
 
   /* default the firmware settings, they may get changed later */
-  NDS_FillDefaultFirmwareConfigData( &fw_config);
+  NDS_GetDefaultFirmwareConfig(fw_config);
 
   init_config( &my_config);
 
@@ -621,7 +621,7 @@ int main(int argc, char ** argv) {
 #endif
 
   /* Create the dummy firmware */
-  NDS_CreateDummyFirmware( &fw_config);
+  NDS_InitFirmwareWithConfig(fw_config);
 
   if ( !my_config.disable_sound) {
     SPU_ChangeSoundCore(SNDCORE_SDL, 735 * 4);

desmume/src/frontend/posix/gtk-glade/main.cpp
@@ -354,12 +354,12 @@ joinThread_gdb( void *thread_handle) {
 static int
 common_gtk_glade_main( struct configured_features *my_config) {
         /* the firmware settings */
-        struct NDS_fw_config_data fw_config;
+        FirmwareConfig fw_config;
 	gchar *uifile;
 	GKeyFile *keyfile;
 
         /* default the firmware settings, they may get changed later */
-        NDS_FillDefaultFirmwareConfigData( &fw_config);
+        NDS_GetDefaultFirmwareConfig(fw_config);
 
         /* use any language set on the command line */
         if ( my_config->firmware_language != -1) {
@@ -384,7 +384,7 @@ common_gtk_glade_main( struct configured_features *my_config) {
 	desmume_init();
 
         /* Create the dummy firmware */
-        NDS_CreateDummyFirmware( &fw_config);
+        NDS_InitFirmwareWithConfig(fw_config);
 
         /*
          * Activate the GDB stubs

desmume/src/frontend/posix/gtk/main.cpp
@@ -3005,10 +3005,10 @@ common_gtk_main( class configured_features *my_config)
     GtkWidget *pToolBar;
 
     /* the firmware settings */
-    struct NDS_fw_config_data fw_config;
+    FirmwareConfig fw_config;
 
     /* default the firmware settings, they may get changed later */
-    NDS_FillDefaultFirmwareConfigData( &fw_config);
+    NDS_GetDefaultFirmwareConfig(fw_config);
 
     /* use any language set on the command line */
     if ( my_config->firmware_language != -1) {
@@ -3130,7 +3130,7 @@ common_gtk_main( class configured_features *my_config)
 #endif
 
     /* Create the dummy firmware */
-    NDS_CreateDummyFirmware( &fw_config);
+    NDS_InitFirmwareWithConfig(fw_config);
 
     /* Initialize joysticks */
     if(!init_joy()) return 1;

desmume/src/NDSSystem.cpp
@@ -127,7 +127,7 @@ int NDS_GetCPUCoreCount()
 
 void NDS_SetupDefaultFirmware()
 {
-	NDS_FillDefaultFirmwareConfigData(&CommonSettings.fw_config);
+	NDS_GetDefaultFirmwareConfig(CommonSettings.fwConfig);
 }
 
 void NDS_RunAdvansceneAutoImport()
@@ -2161,7 +2161,7 @@ static void PrepareBiosARM7()
 	NDS_ARM7.BIOS_loaded = false;
 	memset(MMU.ARM7_BIOS, 0, sizeof(MMU.ARM7_BIOS));
 
-	if(CommonSettings.UseExtBIOS == true)
+	if(CommonSettings.UseExtBIOS)
 	{
 		//read arm7 bios from inputfile and flag it if it succeeds
 		FILE *arm7inf = fopen(CommonSettings.ARM7BIOS,"rb");
@@ -2219,7 +2219,7 @@ static void PrepareBiosARM9()
 	memset(MMU.ARM9_BIOS, 0, sizeof(MMU.ARM9_BIOS));
 	NDS_ARM9.BIOS_loaded = false;
 
-	if(CommonSettings.UseExtBIOS == true)
+	if(CommonSettings.UseExtBIOS)
 	{
 		//read arm9 bios from inputfile and flag it if it succeeds
 		FILE* arm9inf = fopen(CommonSettings.ARM9BIOS,"rb");
@@ -2434,7 +2434,7 @@ bool NDS_FakeBoot()
 	// Create the dummy firmware
 	//EDIT - whats dummy firmware and how is relating to the above?
 	//it seems to be emplacing basic firmware data into MMU.fw.data
-	NDS_CreateDummyFirmware(&CommonSettings.fw_config);
+	NDS_InitFirmwareWithConfig(CommonSettings.fwConfig);
 	
 	//firmware loads the game card arm9 and arm7 programs as specified in rom header
 	{
@@ -2473,7 +2473,7 @@ bool NDS_FakeBoot()
 	//TBD - this code is really clunky
 	//it seems to be copying the MMU.fw.data data into RAM in the user memory stash locations
 	u8 temp_buffer[NDS_FW_USER_SETTINGS_MEM_BYTE_COUNT];
-	if ( copy_firmware_user_data( temp_buffer, MMU.fw.data)) {
+	if ( copy_firmware_user_data( temp_buffer, MMU.fw.data._raw)) {
 		for ( int fw_index = 0; fw_index < NDS_FW_USER_SETTINGS_MEM_BYTE_COUNT; fw_index++)
 			_MMU_write08<ARMCPU_ARM9>(0x027FFC80 + fw_index, temp_buffer[fw_index]);
 	}

desmume/src/NDSSystem.h
@@ -24,6 +24,7 @@
 
 #include "types.h"
 #include "ROMReader.h"
+#include "firmware.h"
 #include "wifi.h"
 
 class CFIRMWARE;
@@ -213,7 +214,7 @@ enum NDS_CONSOLE_TYPE
 	NDS_CONSOLE_TYPE_FAT = 0xFF,
 	NDS_CONSOLE_TYPE_LITE = 0x20,
 	NDS_CONSOLE_TYPE_IQUE = 0x43,
-	NDS_CONSOLE_TYPE_IQUE_LIE = 0x63,
+	NDS_CONSOLE_TYPE_IQUE_LITE = 0x63,
 	NDS_CONSOLE_TYPE_DSI = 0xFE
 };
 
@@ -289,39 +290,6 @@ struct NDSSystem
 
 	bool isInVblank() const { return VCount >= 192; } 
 	bool isIn3dVblank() const { return VCount >= 192 && VCount<215; } 
-};
-
-/** /brief A touchscreen calibration point.
- */
-struct NDS_fw_touchscreen_cal {
-  u16 adc_x;
-  u16 adc_y;
-
-  u8 screen_x;
-  u8 screen_y;
-};
-
-#define MAX_FW_NICKNAME_LENGTH 10
-#define MAX_FW_MESSAGE_LENGTH 26
-
-struct NDS_fw_config_data
-{
-  NDS_CONSOLE_TYPE ds_type;
-
-  u8 fav_colour;
-  u8 birth_month;
-  u8 birth_day;
-
-  u16 nickname[MAX_FW_NICKNAME_LENGTH];
-  u8 nickname_len;
-
-  u16 message[MAX_FW_MESSAGE_LENGTH];
-  u8 message_len;
-
-  u8 language;
-
-  //touchscreen calibration
-  NDS_fw_touchscreen_cal touch_cal[2];
 };
 
 extern NDSSystem nds;
@@ -608,7 +576,7 @@ extern struct TCommonSettings
 	bool UseExtFirmwareSettings;
 	char Firmware[256];
 	bool BootFromFirmware;
-	NDS_fw_config_data fw_config;
+	FirmwareConfig fwConfig;
 
 	NDS_CONSOLE_TYPE ConsoleType;
 	bool DebugConsole;

desmume/src/firmware.h
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2009-2015 DeSmuME Team
+	Copyright (C) 2009-2018 DeSmuME Team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -30,24 +30,387 @@
 //extension of the firmware user settings file
 #define FW_CONFIG_FILE_EXT "dfc"
 
-struct NDS_fw_config_data;
+#define MAX_FW_NICKNAME_LENGTH 10
+#define MAX_FW_MESSAGE_LENGTH 26
+
+struct FirmwareConfig
+{
+	u8 consoleType;
+	
+	u8 favoriteColor;
+	u8 birthdayMonth;
+	u8 birthdayDay;
+	
+	// Add one character to these string buffers just in case clients
+	// try to terminate a max length string with a null character.
+	u16 nickname[MAX_FW_NICKNAME_LENGTH+1];
+	u8 nicknameLength;
+	u16 message[MAX_FW_MESSAGE_LENGTH+1];
+	u8 messageLength;
+	
+	u8 language;
+	u8 backlightLevel;
+	
+	u16 tscADC_x1;
+	u16 tscADC_y1;
+	u8 tscPixel_x1;
+	u8 tscPixel_y1;
+	u16 tscADC_x2;
+	u16 tscADC_y2;
+	u8 tscPixel_x2;
+	u8 tscPixel_y2;
+	
+	u8 MACAddress[6];
+	
+	u8 ipv4Address_AP1[4];
+	u8 ipv4Gateway_AP1[4];
+	u8 ipv4PrimaryDNS_AP1[4];
+	u8 ipv4SecondaryDNS_AP1[4];
+	u8 subnetMask_AP1;
+	
+	u8 ipv4Address_AP2[4];
+	u8 ipv4Gateway_AP2[4];
+	u8 ipv4PrimaryDNS_AP2[4];
+	u8 ipv4SecondaryDNS_AP2[4];
+	u8 subnetMask_AP2;
+	
+	u8 ipv4Address_AP3[4];
+	u8 ipv4Gateway_AP3[4];
+	u8 ipv4PrimaryDNS_AP3[4];
+	u8 ipv4SecondaryDNS_AP3[4];
+	u8 subnetMask_AP3;
+};
+typedef FirmwareConfig FirmwareConfig;
+
+#include "PACKED.h"
+typedef union __PACKED
+{
+	u64 encryption;
+	
+	struct
+	{
+		u64 timestamp:40;
+		u64 consoleType:8;
+		u64 unused:16;
+	};
+} FW_HEADER_KEY;
+
+typedef union __PACKED
+{
+	u16 value;
+	
+	struct
+	{
+		u16 language:3;
+		u16 gbaModeScreenSelection:1;
+		u16 backlightLevel:2;
+		u16 bootmenuDisable:1;
+		u16 :1;
+		
+		u16 :1;
+		u16 settingsLost:1;
+		u16 settingsOkay1:1;
+		u16 :1;
+		u16 settingsOkay2:1;
+		u16 settingsOkay3:1;
+		u16 settingsOkay4:1;
+		u16 settingsOkay5:1;
+	};
+} FW_USERSETTINGS_LANGUAGE_FLAGS;
+
+typedef struct __PACKED
+{
+	u16 part3_rom_gui9_addr;			// 0x0000
+	u16 part4_rom_wifi7_addr;			// 0x0002
+	u16 part34_gui_wifi_crc16;			// 0x0004
+	u16 part12_boot_crc16;				// 0x0006
+	
+	union
+	{
+		u32 identifierValue;			// 0x0008
+		u8 identifier[4];				// 0x0008
+	};
+	
+	u16 part1_rom_boot9_addr;			// 0x000C
+	u16 part1_ram_boot9_addr;			// 0x000E
+	u16 part2_rom_boot7_addr;			// 0x0010
+	u16 part2_ram_boot7_addr;			// 0x0012
+	u16 shift;							// 0x0014
+	u16 part5_data_gfx_addr;			// 0x0016
+	
+	FW_HEADER_KEY key;					// 0x0018
+	
+	u16 userSettingsOffset;				// 0x0020
+	u16 UNKNOWN1;						// 0x0022
+	u16 UNKNOWN2;						// 0x0024
+	u16 part5_crc16;					// 0x0026
+	u16 unused2;						// 0x0028
+} FWHeader;
+
+typedef struct __PACKED
+{
+	u16 crc16;							// 0x002A
+	u16 length;							// 0x002C
+	u8 unused1;							// 0x002E
+	u8 version;							// 0x002F
+	u8 unused2[6];						// 0x0030
+	u8 MACAddr[6];						// 0x0036
+	u16 channels;						// 0x003C
+	u16 flags;							// 0x003E
+	u8 rfType;							// 0x0040
+	u8 rfBits;							// 0x0041
+	u8 rfEntries;						// 0x0042
+	u8 UNKNOWN1;						// 0x0043
+	
+	u16 wifiConfig146;					// 0x0044
+	u16 wifiConfig148;					// 0x0046
+	u16 wifiConfig14A;					// 0x0048
+	u16 wifiConfig14C;					// 0x004A
+	u16 wifiConfig120;					// 0x004C
+	u16 wifiConfig122;					// 0x004E
+	u16 wifiConfig154;					// 0x0050
+	u16 wifiConfig144;					// 0x0052
+	u16 wifiConfig130;					// 0x0054
+	u16 wifiConfig132;					// 0x0056
+	u16 wifiConfig140;					// 0x0058
+	u16 wifiConfig142;					// 0x005A
+	u16 wifiConfigPowerTX;				// 0x005C
+	u16 wifiConfig124;					// 0x005E
+	u16 wifiConfig128;					// 0x0060
+	u16 wifiConfig150;					// 0x0062
+	
+	u8 bbData[105];						// 0x0064
+	u8 unused3;							// 0x00CD
+	
+	union
+	{
+		struct
+		{
+			u8 rfValue[36];				// 0x00CE
+			u8 rfChannelValue24[84];	// 0x00F2
+			u8 bbChannelValue8[14];		// 0x0146
+			u8 rfChannelValue8[14];		// 0x0154
+		} Type2;
+		
+		struct
+		{
+			u8 type3Data[148];			// 0x00CE
+		} Type3;
+	};
+	
+	u8 UNKNOWN2;						// 0x0162
+	u8 unused4;							// 0x0163
+	u8 unused5[156];					// 0x0164
+} FWWifiInfo;
+
+typedef struct
+{
+	u8 UNKNOWN1[64];
+	u8 SSID[32];
+	u8 SSID_WEP[32];
+	u8 wepKey1[16];
+	u8 wepKey2[16];
+	u8 wepKey3[16];
+	u8 wepKey4[16];
+	u8 ipv4Address[4];
+	u8 ipv4Gateway[4];
+	u8 ipv4PrimaryDNS[4];
+	u8 ipv4SecondaryDNS[4];
+	u8 subnetMask;
+	u8 wepKeyAOSS[20];
+	u8 UNKNOWN2;
+	u8 wepMode;
+	u8 configureMode;
+	u8 zero;
+	u8 UNKNOWN3;
+	u16 mtuSize;
+	u8 UNKNOWN4[3];
+	u8 connectStatus;
+	u8 wfcUserID[6];
+	u8 UNKNOWN5[8];
+	u16 crc16;
+} FWAccessPointSettings;
+
+typedef struct
+{
+	u8 proxyAuthenticationUsername[32];
+	u8 proxyAuthenticationPassword[32];
+	u8 SSID[32];
+	u8 SSID_WEP[32];
+	u8 wepKey1[16];
+	u8 wepKey2[16];
+	u8 wepKey3[16];
+	u8 wepKey4[16];
+	u8 ipv4Address[4];
+	u8 ipv4Gateway[4];
+	u8 ipv4PrimaryDNS[4];
+	u8 ipv4SecondaryDNS[4];
+	u8 subnetMask;
+	u8 wepKeyAOSS[20];
+	u8 UNKNOWN2;
+	u8 wepMode;
+	u8 configureMode;
+	u8 ssidLength;
+	u8 UNKNOWN3;
+	u16 mtuSize;
+	u8 UNKNOWN4[3];
+	u8 connectStatus;
+	u8 unused1[14];
+	u16 crc16_1;
+	u8 UNKNOWN5[32];
+	u8 wpaKey[16];
+	u8 unused2[82];
+	u8 securityMode;
+	u8 proxyEnable;
+	u8 proxyAuthentication;
+	u8 proxyName[32];
+	u8 unused3[65];
+	u16 proxyPort;
+	u8 unused4[21];
+	u16 crc16_2;
+} FWExtAccessPointSettings;
+
+typedef struct
+{
+	u16 version;
+	u8 favoriteColor;
+	u8 birthdayMonth;
+	u8 birthdayDay;
+	u8 unused1;
+	u16 nickname[10];
+	u16 nicknameLength;
+	u16 message[26];
+	u16 messageLength;
+	u8 alarmHour;
+	u8 alarmMinute;
+	u16 unused2;
+	u8 alarmEnable;
+	u8 unused3;
+	u16 tscADC_x1;
+	u16 tscADC_y1;
+	u8 tscPixel_x1;
+	u8 tscPixel_y1;
+	u16 tscADC_x2;
+	u16 tscADC_y2;
+	u8 tscPixel_x2;
+	u8 tscPixel_y2;
+	FW_USERSETTINGS_LANGUAGE_FLAGS languageFlags;
+	u8 year;
+	u8 UNKNOWN1;
+	u32 rtcOffset;
+	u32 unused4;
+	
+	u16 updateCounter;
+	u16 crc16;
+	
+	union
+	{
+		struct
+		{
+			u8 unused5[140];
+		} nds;
+		
+		struct
+		{
+			u8 UNKNOWN2;
+			u8 extendedLanguage;
+			u16 supportedLanguage;
+			u8 unused6[134];
+			u16 crc16Extended;
+		} dsi;
+	};
+	
+} FWUserSettings;
+
+typedef struct
+{
+	union
+	{
+		u64 identifierValue;
+		u8 identifier[8];
+	};
+	
+	u16 crc16Value;
+	u16 crc16Length;
+	u8 version;
+	u8 updateCount;
+	u8 bootMenuFlags;
+	u8 gbaBorder;
+	u16 tempCalibrationTP0;
+	u16 tempCalibrationTP1;
+	u16 tempCalibrationDegrees;
+	u8 tempFlags;
+	u8 backlightIntensity;
+	u32 dateCenturyOffset;
+	u8 dateMonthRecoveryValue;
+	u8 dateDayRecoveryValue;
+	u8 dateYearRecoveryValue;
+	u8 dateTimeFlags;
+	u8 dateSeparator;
+	u8 timeSeparator;
+	u8 decimalSeparator;
+	u8 thousandsSeparator;
+	u8 daylightSavingsTimeNth;
+	u8 daylightSavingsTimeDay;
+	u8 daylightSavingsTimeMonth;
+	u8 daylightSavingsTimeFlags;
+} FWExtUserSettings;
+
+typedef union
+{
+	u8 _raw[262144];
+	
+	struct
+	{
+		FWHeader header;
+		FWWifiInfo wifiInfo;
+		
+		union
+		{
+			struct
+			{
+				u8 codeAndData[260096];
+			} nds;
+			
+			struct
+			{
+				u8 zeroBuffer[255];
+				u8 value0x80;
+				u8 ffBuffer[127231];
+				u8 bootFlags;
+				FWExtAccessPointSettings wifiAP4;
+				FWExtAccessPointSettings wifiAP5;
+				FWExtAccessPointSettings wifiAP6;
+				u8 UNKNOWN1[131072];
+			} dsi;
+		};
+		
+		FWAccessPointSettings wifiAP1;
+		FWAccessPointSettings wifiAP2;
+		FWAccessPointSettings wifiAP3;
+		
+		u8 unused[256];
+		
+		FWUserSettings userSettings0;
+		FWUserSettings userSettings1;
+	};
+	
+} NDSFirmwareData;
+#include "PACKED_END.h"
 
 class CFIRMWARE
 {
 private:
-	u8		*tmp_data9;
-	u8		*tmp_data7;
-	u32		size9, size7;
-	u32		userDataAddr;
-
-	u16		getBootCodeCRC16();
-	u32		decrypt(const u8 *in, u8* &out);
-	u32		decompress(const u8 *in, u8* &out);
-
-	bool	successLoad;
+	FWHeader _header;
+	bool	_isLoaded;
+	u32		_userDataAddr;
+	
+	u16		_getBootCodeCRC16(const u8 *arm9Data, const u32 arm9Size, const u8 *arm7Data, const u32 arm7Size);
+	u32		_decrypt(const u8 *in, u8* &out);
+	u32		_decompress(const u8 *in, u8* &out);
 
 public:
-	CFIRMWARE(): size9(0), size7(0), ARM9bootAddr(0), ARM7bootAddr(0), patched(0), userDataAddr(0x3FE00), successLoad(false) {};
+	CFIRMWARE(): _userDataAddr(0x3FE00), _isLoaded(false) {};
 	
 	bool load();
 	bool unpack();
@@ -56,43 +419,15 @@ public:
 
 	static std::string GetExternalFilePath();
 
-	u32 getID() { return header.fw_identifier; }
-	bool loaded() { return successLoad; }
-	void *getTouchCalibrate();
-
-	struct HEADER
-	{
-		u16	part3_rom_gui9_addr;		// 000h
-		u16	part4_rom_wifi7_addr;		// 002h
-		u16	part34_gui_wifi_crc16;		// 004h
-		u16	part12_boot_crc16;			// 006h
-		u32	fw_identifier;				// 008h
-		u16	part1_rom_boot9_addr;		// 00Ch
-		u16	part1_ram_boot9_addr;		// 00Eh
-		u16	part2_rom_boot7_addr;		// 010h
-		u16	part2_ram_boot7_addr;		// 012h
-		u16	shift_amounts;				// 014h
-		u16	part5_data_gfx_addr;		// 016h
-
-		u8	fw_timestamp[5];			// 018h
-		u8	console_type;				// 01Dh
-		u16	unused1;					// 01Eh
-		u16	user_settings_offset;		// 020h
-		u16	unknown1;					// 022h
-		u16	unknown2;					// 024h
-		u16	part5_crc16;				// 026h
-		u16	unused2;					// 028h	- FFh filled 
-	} header;
-
-	u32		ARM9bootAddr;
-	u32		ARM7bootAddr;
-	bool	patched;
+	bool loaded();
+	void* getTouchCalibrate();
 };
 
 int copy_firmware_user_data( u8 *dest_buffer, const u8 *fw_data);
-int NDS_CreateDummyFirmware(NDS_fw_config_data *user_settings);
-void NDS_FillDefaultFirmwareConfigData(NDS_fw_config_data *fw_config);
-void NDS_PatchFirmwareMAC();
+
+void NDS_GetDefaultFirmwareConfig(FirmwareConfig &outConfig);
+void NDS_InitFirmwareWithConfig(const FirmwareConfig &inConfig);
+void NDS_OverrideFirmwareMAC(const u8 inMAC[6]);
 
 struct fw_memory_chip
 {
@@ -102,8 +437,9 @@ struct fw_memory_chip
 	u8 addr_size;    //size of addr when writing/reading
 
 	BOOL write_enable;	//is write enabled ?
-
-	u8 *data;       //memory data
+	
+	NDSFirmwareData data;
+	
 	u32 size;       //memory size
 	BOOL writeable_buffer;	//is "data" writeable ?
 	int type; //type of Memory
@@ -125,4 +461,3 @@ void mc_load_file(fw_memory_chip *mc, const char* filename); /* load save file a
 void mc_free(fw_memory_chip *mc);    /* delete mc memory */
 
 #endif
-

commit e2d60f62fd0cd12ea0e9ad29a579074135f430c3

Windows Port: Fix compiling for Windows port. (Regression from commit bb38022.)

desmume/src/frontend/windows/ginfo.cpp
@@ -90,7 +90,7 @@ LRESULT GInfo_Paint(HWND hDlg, WPARAM wParam, LPARAM lParam)
 
 	const RomBanner& banner = gameInfo.getRomBanner();
 
-	sprintf(text, "%ws", banner.titles[CommonSettings.fw_config.language]);	// TODO: langID from real/fake fw
+	sprintf(text, "%ws", banner.titles[CommonSettings.fwConfig.language]);	// TODO: langID from real/fake fw
 	SetWindowText(GetDlgItem(hDlg, IDC_GI_TITLE), text);
 
 	sprintf(text, "%ws", banner.titles[0]);

desmume/src/frontend/windows/main.cpp
@@ -2269,7 +2269,7 @@ int _main()
 		}
 		
 		hash >>= 1;
-		hash += ipAddr >> 8;
+		hash += ipaddr >> 8;
 		hash &= 0x00FFFFFF;
 		
 		wifiHandler->SetUserMACValues(hash >> 16, (hash >> 8) & 0xFF, hash & 0xFF);

desmume/src/frontend/windows/memView.cpp
@@ -79,7 +79,7 @@ u8 memRead8 (MemRegionType regionType, HWAddressType address)
 		MMU_DumpMemBlock(ARMCPU_ARM7, address, 1, &value);
 		return value;
 	case MEMVIEW_FIRMWARE:
-		value = MMU.fw.data[address];
+		value = MMU.fw.data._raw[address];
 		return value;
 	case MEMVIEW_FULL:
 		MMU_DumpMemBlock(0, address, 1, &value);
@@ -133,7 +133,7 @@ void memWrite8(MemRegionType regionType, HWAddressType address, u8 value)
 		MMU_write8(ARMCPU_ARM7, address, value);
 		break;
 	case MEMVIEW_FIRMWARE:
-		MMU.fw.data[address] = value;
+		MMU.fw.data._raw[address] = value;
 		break;
 	case MEMVIEW_ROM:
 		gameInfo.reader->Seek(gameInfo.fROM, address, SEEK_SET);
@@ -157,7 +157,7 @@ void memWrite16(MemRegionType regionType, HWAddressType address, u16 value)
 		MMU_write16(ARMCPU_ARM7, address, value);
 		break;
 	case MEMVIEW_FIRMWARE:
-		*((u16*)&MMU.fw.data[address]) = value;
+		*((u16*)&MMU.fw.data._raw[address]) = value;
 		break;
 	case MEMVIEW_ROM:
 		gameInfo.reader->Seek(gameInfo.fROM, address, SEEK_SET);
@@ -181,7 +181,7 @@ void memWrite32(MemRegionType regionType, HWAddressType address, u32 value)
 		MMU_write32(ARMCPU_ARM7, address, value);
 		break;
 	case MEMVIEW_FIRMWARE:
-		*((u32*)&MMU.fw.data[address]) = value;
+		*((u32*)&MMU.fw.data._raw[address]) = value;
 		break;
 	case MEMVIEW_ROM:
 		gameInfo.reader->Seek(gameInfo.fROM, address, SEEK_SET);