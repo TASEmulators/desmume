TASEmulators/desmume

commit 7080e2156bbba2cec9f42ec1ca4790bdd32b68c6

OpenGL Renderer: Rework the rendering shaders so that the shader program code is more dynamically generated. This may yield some performance improvement for certain 3D rendering cases, especially when running on lesser GPUs with fewer and/or slower shader execution units.

desmume/src/OGLRender.h
@@ -365,19 +365,14 @@ struct OGLVertex
 
 struct OGLRenderStates
 {
-	GLvec2 framebufferSize;
-	GLint toonShadingMode;
-	GLuint enableAlphaTest;
 	GLuint enableAntialiasing;
-	GLuint enableEdgeMarking;
 	GLuint enableFogAlphaOnly;
-	GLuint useWDepth;
 	GLuint clearPolyID;
 	GLfloat clearDepth;
 	GLfloat alphaTestRef;
 	GLfloat fogOffset;
 	GLfloat fogStep;
-	GLfloat pad[3]; // This needs to be here to preserve alignment
+	GLfloat pad_0; // This needs to be here to preserve alignment
 	GLvec4 fogColor;
 	GLvec4 fogDensity[32]; // Array of floats need to be padded as vec4
 	GLvec4 edgeColor[8];
@@ -404,6 +399,23 @@ struct OGLPolyStates
 		GLubyte texParam[4];
 	};
 };
+
+union OGLGeometryFlags
+{
+	u8 value;
+	
+	struct
+	{
+		u8 EnableWDepth:1;
+		u8 EnableAlphaTest:1;
+		u8 EnableTextureSampling:1;
+		u8 EnableFog:1;
+		u8 EnableEdgeMark:1;
+		u8 ToonShadingMode:1;
+		u8 :3;
+	};
+};
+typedef OGLGeometryFlags OGLGeometryFlags;
 
 struct OGLRenderRef
 {	
@@ -450,8 +462,8 @@ struct OGLRenderRef
 	
 	// Shader states
 	GLuint vertexGeometryShaderID;
-	GLuint fragmentGeometryShaderID;
-	GLuint programGeometryID;
+	GLuint fragmentGeometryShaderID[256];
+	GLuint programGeometryID[256];
 	
 	GLuint vtxShaderGeometryZeroDstAlphaID;
 	GLuint fragShaderGeometryZeroDstAlphaID;
@@ -463,7 +475,8 @@ struct OGLRenderRef
 	
 	GLuint vertexEdgeMarkShaderID;
 	GLuint vertexFogShaderID;
-	GLuint vertexFramebufferOutputShaderID;
+	GLuint vertexFramebufferOutput6665ShaderID;
+	GLuint vertexFramebufferOutput8888ShaderID;
 	GLuint fragmentEdgeMarkShaderID;
 	GLuint fragmentFogShaderID;
 	GLuint fragmentFramebufferRGBA6665OutputShaderID;
@@ -473,18 +486,10 @@ struct OGLRenderRef
 	GLuint programFramebufferRGBA6665OutputID;
 	GLuint programFramebufferRGBA8888OutputID;
 	
-	GLint uniformFramebufferSize_ConvertRGBA6665;
-	GLint uniformFramebufferSize_ConvertRGBA8888;
 	GLint uniformTexInFragColor_ConvertRGBA6665;
 	GLint uniformTexInFragColor_ConvertRGBA8888;
-	GLint uniformStateToonShadingMode;
-	GLint uniformStateEnableAlphaTest;
-	GLint uniformStateEnableAntialiasing;
-	GLint uniformStateEnableEdgeMarking;
 	GLint uniformStateEnableFogAlphaOnly;
-	GLint uniformStateUseWDepth;
 	GLint uniformStateAlphaTestRef;
-	GLint uniformFramebufferSize_EdgeMark;
 	GLint uniformStateClearPolyID;
 	GLint uniformStateClearDepth;
 	GLint uniformStateEdgeColor;
@@ -503,11 +508,11 @@ struct OGLRenderRef
 	GLint uniformPolyEnableTexture;
 	GLint uniformPolyEnableFog;
 	GLint uniformTexSingleBitAlpha;
-	GLint uniformTexDrawOpaque;
+	GLint uniformTexDrawOpaque[256];
 	
-	GLint uniformPolyStateIndex;
-	GLint uniformPolyDepthOffsetMode;
-	GLint uniformPolyDrawShadow;
+	GLint uniformPolyStateIndex[256];
+	GLint uniformPolyDepthOffsetMode[256];
+	GLint uniformPolyDrawShadow[256];
 	
 	GLuint texToonTableID;
 	
@@ -640,11 +645,9 @@ protected:
 	
 	bool _emulateShadowPolygon;
 	bool _emulateSpecialZeroAlphaBlending;
+	bool _emulateNDSDepthCalculation;
 	bool _emulateDepthEqualsTestTolerance;
 	bool _emulateDepthLEqualPolygonFacing;
-	
-	float _clearDepth;
-	
 	
 	FragmentColor *_mappedFramebuffer;
 	FragmentColor *_workingTextureUnpackBuffer;
@@ -652,6 +655,7 @@ protected:
 	bool _needsZeroDstAlphaPass;
 	size_t _currentPolyIndex;
 	OGLTextureUnitID _lastTextureDrawTarget;
+	OGLGeometryFlags _geometryProgramFlags;
 	
 	bool _enableMultisampledRendering;
 	int _selectedMultisampleSize;
@@ -687,12 +691,22 @@ protected:
 	virtual Render3DError CreateMultisampledFBO(GLsizei numSamples) = 0;
 	virtual void DestroyMultisampledFBO() = 0;
 	virtual void ResizeMultisampledFBOs(GLsizei numSamples) = 0;
-	virtual Render3DError InitGeometryProgram(const char *geometryVtxShaderCString, const char *geometryFragShaderCString,
-											  const char *geometryAlphaVtxShaderCString, const char *geometryAlphaFragShaderCString,
-											  const char *geometryMSAlphaVtxShaderCString, const char *geometryMSAlphaFragShaderCString) = 0;
-	virtual void DestroyGeometryProgram() = 0;
 	virtual Render3DError CreateVAOs() = 0;
 	virtual void DestroyVAOs() = 0;
+	
+	virtual Render3DError CreateGeometryPrograms() = 0;
+	virtual void DestroyGeometryPrograms() = 0;
+	virtual Render3DError CreateGeometryZeroDstAlphaProgram(const char *vtxShaderCString, const char *fragShaderCString) = 0;
+	virtual void DestroyGeometryZeroDstAlphaProgram() = 0;
+	virtual Render3DError CreateEdgeMarkProgram(const char *vtxShaderCString, const char *fragShaderCString) = 0;
+	virtual void DestroyEdgeMarkProgram() = 0;
+	virtual Render3DError CreateFogProgram(const char *vtxShaderCString, const char *fragShaderCString) = 0;
+	virtual void DestroyFogProgram() = 0;
+	virtual Render3DError CreateFramebufferOutput6665Program(const char *vtxShaderCString, const char *fragShaderCString) = 0;
+	virtual void DestroyFramebufferOutput6665Program() = 0;
+	virtual Render3DError CreateFramebufferOutput8888Program(const char *vtxShaderCString, const char *fragShaderCString) = 0;
+	virtual void DestroyFramebufferOutput8888Program() = 0;
+	
 	virtual Render3DError InitFinalRenderStates(const std::set<std::string> *oglExtensionSet) = 0;
 	virtual Render3DError InitTables() = 0;
 	virtual Render3DError InitPostprocessingPrograms(const char *edgeMarkVtxShader,
@@ -702,18 +716,7 @@ protected:
 													 const char *framebufferOutputVtxShader,
 													 const char *framebufferOutputRGBA6665FragShader,
 													 const char *framebufferOutputRGBA8888FragShader) = 0;
-	virtual Render3DError DestroyPostprocessingPrograms() = 0;
-	virtual Render3DError InitEdgeMarkProgramBindings() = 0;
-	virtual Render3DError InitEdgeMarkProgramShaderLocations() = 0;
-	virtual Render3DError InitFogProgramBindings() = 0;
-	virtual Render3DError InitFogProgramShaderLocations() = 0;
-	virtual Render3DError InitFramebufferOutputProgramBindings() = 0;
-	virtual Render3DError InitFramebufferOutputShaderLocations() = 0;
 	
-	virtual Render3DError InitGeometryProgramBindings() = 0;
-	virtual Render3DError InitGeometryProgramShaderLocations() = 0;
-	virtual Render3DError InitGeometryZeroDstAlphaProgramBindings() = 0;
-	virtual Render3DError InitGeometryZeroDstAlphaProgramShaderLocations() = 0;
 	virtual Render3DError CreateToonTable() = 0;
 	virtual Render3DError DestroyToonTable() = 0;
 	virtual Render3DError UploadClearImage(const u16 *__restrict colorBuffer, const u32 *__restrict depthBuffer, const u8 *__restrict fogBuffer, const u8 *__restrict polyIDBuffer) = 0;
@@ -735,7 +738,12 @@ public:
 	virtual Render3DError InitExtensions() = 0;
 	
 	bool IsExtensionPresent(const std::set<std::string> *oglExtensionSet, const std::string extensionName) const;
-	bool ValidateShaderCompile(GLuint theShader) const;
+	Render3DError ShaderProgramCreate(GLuint &vtxShaderID,
+									  GLuint &fragShaderID,
+									  GLuint &programID,
+									  const char *vtxShaderCString,
+									  const char *fragShaderCString);
+	bool ValidateShaderCompile(GLenum shaderType, GLuint theShader) const;
 	bool ValidateShaderProgramLink(GLuint theProgram) const;
 	void GetVersion(unsigned int *major, unsigned int *minor, unsigned int *revision) const;
 	void SetVersion(unsigned int major, unsigned int minor, unsigned int revision);
@@ -761,17 +769,22 @@ protected:
 	virtual void ResizeMultisampledFBOs(GLsizei numSamples);
 	virtual Render3DError CreateVAOs();
 	virtual void DestroyVAOs();
+	
+	virtual Render3DError CreateGeometryPrograms();
+	virtual void DestroyGeometryPrograms();
+	virtual Render3DError CreateGeometryZeroDstAlphaProgram(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual void DestroyGeometryZeroDstAlphaProgram();
+	virtual Render3DError CreateEdgeMarkProgram(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual void DestroyEdgeMarkProgram();
+	virtual Render3DError CreateFogProgram(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual void DestroyFogProgram();
+	virtual Render3DError CreateFramebufferOutput6665Program(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual void DestroyFramebufferOutput6665Program();
+	virtual Render3DError CreateFramebufferOutput8888Program(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual void DestroyFramebufferOutput8888Program();
+	
 	virtual Render3DError InitFinalRenderStates(const std::set<std::string> *oglExtensionSet);
 	virtual Render3DError InitTables();
-	
-	virtual Render3DError InitGeometryProgram(const char *geometryVtxShaderCString, const char *geometryFragShaderCString,
-											  const char *geometryAlphaVtxShaderCString, const char *geometryAlphaFragShaderCString,
-											  const char *geometryMSAlphaVtxShaderCString, const char *geometryMSAlphaFragShaderCString);
-	virtual Render3DError InitGeometryProgramBindings();
-	virtual Render3DError InitGeometryProgramShaderLocations();
-	virtual Render3DError InitGeometryZeroDstAlphaProgramBindings();
-	virtual Render3DError InitGeometryZeroDstAlphaProgramShaderLocations();
-	virtual void DestroyGeometryProgram();
 	virtual Render3DError InitPostprocessingPrograms(const char *edgeMarkVtxShader,
 													 const char *edgeMarkFragShader,
 													 const char *fogVtxShader,
@@ -779,13 +792,6 @@ protected:
 													 const char *framebufferOutputVtxShader,
 													 const char *framebufferOutputRGBA6665FragShader,
 													 const char *framebufferOutputRGBA8888FragShader);
-	virtual Render3DError DestroyPostprocessingPrograms();
-	virtual Render3DError InitEdgeMarkProgramBindings();
-	virtual Render3DError InitEdgeMarkProgramShaderLocations();
-	virtual Render3DError InitFogProgramBindings();
-	virtual Render3DError InitFogProgramShaderLocations();
-	virtual Render3DError InitFramebufferOutputProgramBindings();
-	virtual Render3DError InitFramebufferOutputShaderLocations();
 	
 	virtual Render3DError CreateToonTable();
 	virtual Render3DError DestroyToonTable();

desmume/src/OGLRender_3_2.h
@@ -63,12 +63,7 @@ class OpenGLRenderer_3_2 : public OpenGLRenderer_2_1
 {
 protected:
 	virtual Render3DError InitExtensions();
-	virtual Render3DError InitEdgeMarkProgramBindings();
-	virtual Render3DError InitEdgeMarkProgramShaderLocations();
-	virtual Render3DError InitFogProgramBindings();
-	virtual Render3DError InitFogProgramShaderLocations();
-	virtual Render3DError InitFramebufferOutputProgramBindings();
-	virtual Render3DError InitFramebufferOutputShaderLocations();
+	
 	virtual Render3DError CreateFBOs();
 	virtual void DestroyFBOs();
 	virtual Render3DError CreateMultisampledFBO(GLsizei numSamples);
@@ -77,11 +72,15 @@ protected:
 	virtual Render3DError CreateVAOs();
 	virtual void DestroyVAOs();
 	
-	virtual Render3DError InitGeometryProgramBindings();
-	virtual Render3DError InitGeometryProgramShaderLocations();
-	virtual Render3DError InitGeometryZeroDstAlphaProgramBindings();
-	virtual Render3DError InitGeometryZeroDstAlphaProgramShaderLocations();
-	virtual void DestroyGeometryProgram();
+	virtual Render3DError CreateGeometryPrograms();
+	virtual void DestroyGeometryPrograms();
+	virtual Render3DError CreateGeometryZeroDstAlphaProgram(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual Render3DError CreateMSGeometryZeroDstAlphaProgram(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual void DestroyMSGeometryZeroDstAlphaProgram();
+	virtual Render3DError CreateEdgeMarkProgram(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual Render3DError CreateFogProgram(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual Render3DError CreateFramebufferOutput6665Program(const char *vtxShaderCString, const char *fragShaderCString);
+	virtual Render3DError CreateFramebufferOutput8888Program(const char *vtxShaderCString, const char *fragShaderCString);
 	
 	virtual void GetExtensionSet(std::set<std::string> *oglExtensionSet);
 	virtual Render3DError EnableVertexAttributes();

commit 7bb438020b42fb7efc1a89fb809feec2cec2f822

OpenGL Renderer: Fix bug where the OpenGL renderer would completely fail to run if the user's ancient GPU doesn't support shaders. (Regression from commit 7080e21.) - Also do some minor improvements to the code robustness when creating an OpenGLRenderer object.

desmume/src/OGLRender.cpp
@@ -794,7 +794,7 @@ void main()\n\
 }\n\
 "};
 
-bool IsVersionSupported(unsigned int checkVersionMajor, unsigned int checkVersionMinor, unsigned int checkVersionRevision)
+bool IsOpenGLDriverVersionSupported(unsigned int checkVersionMajor, unsigned int checkVersionMinor, unsigned int checkVersionRevision)
 {
 	bool result = false;
 	
@@ -1027,7 +1027,7 @@ static Render3D* OpenGLRendererCreate()
 	
 	if (!BEGINGL())
 	{
-		INFO("OpenGL<%s,%s>: Could not initialize -- BEGINGL() failed.\n",require_profile?"force":"auto",enable_3_2?"3_2":"old");
+		INFO("OpenGL<%s,%s>: Could not initialize -- BEGINGL() failed.\n", require_profile?"force":"auto", enable_3_2?"3_2":"old");
 		return NULL;
 	}
 	
@@ -1050,7 +1050,7 @@ static Render3D* OpenGLRendererCreate()
 	// Check the driver's OpenGL version
 	OGLGetDriverVersion(oglVersionString, &_OGLDriverVersion.major, &_OGLDriverVersion.minor, &_OGLDriverVersion.revision);
 	
-	if (!IsVersionSupported(OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_MAJOR, OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_MINOR, OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_REVISION))
+	if (!IsOpenGLDriverVersionSupported(OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_MAJOR, OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_MINOR, OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_REVISION))
 	{
 		INFO("OpenGL: Driver does not support OpenGL v%u.%u.%u or later. Disabling 3D renderer.\n[ Driver Info -\n    Version: %s\n    Vendor: %s\n    Renderer: %s ]\n",
 			 OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_MAJOR, OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_MINOR, OGLRENDER_MINIMUM_DRIVER_VERSION_REQUIRED_REVISION,
@@ -1085,17 +1085,17 @@ static Render3D* OpenGLRendererCreate()
 	{
 		OGLLoadEntryPoints_Legacy();
 		
-		if (IsVersionSupported(2, 1, 0))
+		if (IsOpenGLDriverVersionSupported(2, 1, 0))
 		{
 			newRenderer = new OpenGLRenderer_2_1;
 			newRenderer->SetVersion(2, 1, 0);
 		}
-		else if (IsVersionSupported(2, 0, 0))
+		else if (IsOpenGLDriverVersionSupported(2, 0, 0))
 		{
 			newRenderer = new OpenGLRenderer_2_0;
 			newRenderer->SetVersion(2, 0, 0);
 		}
-		else if (IsVersionSupported(1, 2, 0))
+		else if (IsOpenGLDriverVersionSupported(1, 2, 0))
 		{
 			newRenderer = new OpenGLRenderer_1_2;
 			newRenderer->SetVersion(1, 2, 0);
@@ -1120,12 +1120,12 @@ static Render3D* OpenGLRendererCreate()
 			INFO("OpenGL: This driver does not support the minimum feature set required to run this renderer. Disabling 3D renderer.\n[ Driver Info -\n    Version: %s\n    Vendor: %s\n    Renderer: %s ]\n",
 				 oglVersionString, oglVendorString, oglRendererString);
 		}
-		else if (IsVersionSupported(1, 5, 0) && (error == OGLERROR_VBO_UNSUPPORTED))
+		else if (newRenderer->IsVersionSupported(1, 5, 0) && (error == OGLERROR_VBO_UNSUPPORTED))
 		{
 			INFO("OpenGL: VBOs are not available, even though this version of OpenGL requires them. Disabling 3D renderer.\n[ Driver Info -\n    Version: %s\n    Vendor: %s\n    Renderer: %s ]\n",
 				 oglVersionString, oglVendorString, oglRendererString);
 		}
-		else if ( IsVersionSupported(2, 0, 0) &&
+		else if ( newRenderer->IsVersionSupported(2, 0, 0) &&
 			(error == OGLERROR_SHADER_CREATE_ERROR ||
 			 error == OGLERROR_VERTEX_SHADER_PROGRAM_LOAD_ERROR ||
 			 error == OGLERROR_FRAGMENT_SHADER_PROGRAM_LOAD_ERROR) )
@@ -1133,12 +1133,12 @@ static Render3D* OpenGLRendererCreate()
 			INFO("OpenGL: Shaders are not working, even though they should be on this version of OpenGL. Disabling 3D renderer.\n[ Driver Info -\n    Version: %s\n    Vendor: %s\n    Renderer: %s ]\n",
 				 oglVersionString, oglVendorString, oglRendererString);
 		}
-		else if (IsVersionSupported(2, 1, 0) && (error == OGLERROR_PBO_UNSUPPORTED))
+		else if (newRenderer->IsVersionSupported(2, 1, 0) && (error == OGLERROR_PBO_UNSUPPORTED))
 		{
 			INFO("OpenGL: PBOs are not available, even though this version of OpenGL requires them. Disabling 3D renderer.\n[ Driver Info -\n    Version: %s\n    Vendor: %s\n    Renderer: %s ]\n",
 				 oglVersionString, oglVendorString, oglRendererString);
 		}
-		else if (IsVersionSupported(3, 0, 0) && (error == OGLERROR_FBO_CREATE_ERROR) && (OGLLoadEntryPoints_3_2_Func != NULL))
+		else if (newRenderer->IsVersionSupported(3, 0, 0) && (error == OGLERROR_FBO_CREATE_ERROR) && (OGLLoadEntryPoints_3_2_Func != NULL))
 		{
 			INFO("OpenGL: FBOs are not available, even though this version of OpenGL requires them. Disabling 3D renderer.\n[ Driver Info -\n    Version: %s\n    Vendor: %s\n    Renderer: %s ]\n",
 				 oglVersionString, oglVendorString, oglRendererString);
@@ -1410,6 +1410,20 @@ void OpenGLRenderer::SetVersion(unsigned int major, unsigned int minor, unsigned
 	this->versionMajor = major;
 	this->versionMinor = minor;
 	this->versionRevision = revision;
+}
+
+bool OpenGLRenderer::IsVersionSupported(unsigned int checkVersionMajor, unsigned int checkVersionMinor, unsigned int checkVersionRevision) const
+{
+	bool result = false;
+	
+	if ( (this->versionMajor > checkVersionMajor) ||
+		 (this->versionMajor >= checkVersionMajor && this->versionMinor > checkVersionMinor) ||
+		 (this->versionMajor >= checkVersionMajor && this->versionMinor >= checkVersionMinor && this->versionRevision >= checkVersionRevision) )
+	{
+		result = true;
+	}
+	
+	return result;
 }
 
 Render3DError OpenGLRenderer::_FlushFramebufferFlipAndConvertOnCPU(const FragmentColor *__restrict srcFramebuffer,
@@ -2497,7 +2511,7 @@ Render3DError OpenGLRenderer_1_2::InitExtensions()
 	{
 		INFO("OpenGL: Shaders are unsupported. Disabling shaders and using fixed-function pipeline. Some emulation features will be disabled.\n");
 		
-		if (IsVersionSupported(2, 0, 0))
+		if (this->IsVersionSupported(2, 0, 0))
 		{
 			return error;
 		}
@@ -2511,7 +2525,7 @@ Render3DError OpenGLRenderer_1_2::InitExtensions()
 	else
 	{
 		error = OGLERROR_VBO_UNSUPPORTED;
-		if (IsVersionSupported(1, 5, 0))
+		if (this->IsVersionSupported(1, 5, 0))
 		{
 			return error;
 		}
@@ -2527,7 +2541,7 @@ Render3DError OpenGLRenderer_1_2::InitExtensions()
 	else
 	{
 		error = OGLERROR_PBO_UNSUPPORTED;
-		if (IsVersionSupported(2, 1, 0))
+		if (this->IsVersionSupported(2, 1, 0))
 		{
 			return error;
 		}
@@ -5433,15 +5447,18 @@ Render3DError OpenGLRenderer_1_2::SetFramebufferSize(size_t w, size_t h)
 		free_aligned(oldFramebufferColor);
 	}
 	
-	// Recreate shaders that use the framebuffer size.
-	glUseProgram(0);
-	this->DestroyEdgeMarkProgram();
-	this->DestroyFramebufferOutput6665Program();
-	this->DestroyFramebufferOutput8888Program();
-	
-	this->CreateEdgeMarkProgram(EdgeMarkVtxShader_100, EdgeMarkFragShader_100);
-	this->CreateFramebufferOutput6665Program(FramebufferOutputVtxShader_100, FramebufferOutputRGBA6665FragShader_100);
-	this->CreateFramebufferOutput8888Program(FramebufferOutputVtxShader_100, FramebufferOutputRGBA8888FragShader_100);
+	if (this->isShaderSupported)
+	{
+		// Recreate shaders that use the framebuffer size.
+		glUseProgram(0);
+		this->DestroyEdgeMarkProgram();
+		this->DestroyFramebufferOutput6665Program();
+		this->DestroyFramebufferOutput8888Program();
+		
+		this->CreateEdgeMarkProgram(EdgeMarkVtxShader_100, EdgeMarkFragShader_100);
+		this->CreateFramebufferOutput6665Program(FramebufferOutputVtxShader_100, FramebufferOutputRGBA6665FragShader_100);
+		this->CreateFramebufferOutput8888Program(FramebufferOutputVtxShader_100, FramebufferOutputRGBA8888FragShader_100);
+	}
 	
 	if (oglrender_framebufferDidResizeCallback != NULL)
 	{

desmume/src/OGLRender.h
@@ -596,7 +596,7 @@ void ENDGL();
 extern void (*OGLLoadEntryPoints_3_2_Func)();
 extern void (*OGLCreateRenderer_3_2_Func)(OpenGLRenderer **rendererPtr);
 
-bool IsVersionSupported(unsigned int checkVersionMajor, unsigned int checkVersionMinor, unsigned int checkVersionRevision);
+bool IsOpenGLDriverVersionSupported(unsigned int checkVersionMajor, unsigned int checkVersionMinor, unsigned int checkVersionRevision);
 
 class OpenGLTexture : public Render3DTexture
 {
@@ -763,6 +763,7 @@ public:
 	bool ValidateShaderProgramLink(GLuint theProgram) const;
 	void GetVersion(unsigned int *major, unsigned int *minor, unsigned int *revision) const;
 	void SetVersion(unsigned int major, unsigned int minor, unsigned int revision);
+	bool IsVersionSupported(unsigned int checkVersionMajor, unsigned int checkVersionMinor, unsigned int checkVersionRevision) const;
 	
 	virtual FragmentColor* GetFramebuffer();
 	virtual GLsizei GetLimitedMultisampleSize() const;

desmume/src/OGLRender_3_2.cpp
@@ -761,7 +761,7 @@ void main()\n\
 
 void OGLCreateRenderer_3_2(OpenGLRenderer **rendererPtr)
 {
-	if (IsVersionSupported(3, 2, 0))
+	if (IsOpenGLDriverVersionSupported(3, 2, 0))
 	{
 		*rendererPtr = new OpenGLRenderer_3_2;
 		(*rendererPtr)->SetVersion(3, 2, 0);