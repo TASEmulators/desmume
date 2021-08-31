TASEmulators/desmume

commit 03b948109596f66f4d0afd30ed517c37c1672e65

Cocoa Port: Add thread names for this port's own threads too.

desmume/src/frontend/cocoa/cocoa_GPU.mm
@@ -1769,6 +1769,11 @@ CGLPBufferObj OSXOpenGLRendererPBuffer = NULL;
 
 static void* RunFetchThread(void *arg)
 {
+	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
+	{
+		pthread_setname_np("Video Fetch");
+	}
+	
 	MacClientSharedObject *sharedData = (MacClientSharedObject *)arg;
 	[sharedData runFetchLoop];
 	

desmume/src/frontend/cocoa/cocoa_core.mm
@@ -1142,6 +1142,11 @@ volatile bool execute = true;
 
 static void* RunCoreThread(void *arg)
 {
+	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
+	{
+		pthread_setname_np("Emulation Core");
+	}
+	
 	CoreThreadParam *param = (CoreThreadParam *)arg;
 	CocoaDSCore *cdsCore = (CocoaDSCore *)param->cdsCore;
 	ClientExecutionControl *execControl = [cdsCore execControl];

desmume/src/frontend/cocoa/cocoa_output.mm
@@ -1,6 +1,6 @@
 /*
 	Copyright (C) 2011 Roger Manuel
-	Copyright (C) 2011-2018 DeSmuME team
+	Copyright (C) 2011-2021 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -1314,6 +1314,11 @@
 
 static void* RunOutputThread(void *arg)
 {
+	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
+	{
+		pthread_setname_np("EmulationOutput");
+	}
+	
 	CocoaDSOutput *cdsDisplayOutput = (CocoaDSOutput *)arg;
 	[cdsDisplayOutput runMessageLoop];
 	

desmume/src/frontend/cocoa/userinterface/MacAVCaptureTool.mm
@@ -1463,6 +1463,11 @@ ClientAVCaptureError FFmpegFileStream::WriteOneFrame(const AVStreamWriteParam &p
 
 static void* RunAVCaptureCloseThread(void *arg)
 {
+	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
+	{
+		pthread_setname_np("AVCapture Close");
+	}
+	
 	/*
 	ClientAVCaptureObject *captureObject = (ClientAVCaptureObject *)arg;
 	FFmpegFileStream *ffmpegFS = (FFmpegFileStream *)captureObject->GetOutputFileStream();

desmume/src/frontend/cocoa/userinterface/MacScreenshotCaptureTool.mm
@@ -131,6 +131,11 @@
 
 static void* RunFileWriteThread(void *arg)
 {
+	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
+	{
+		pthread_setname_np("Mac File Write");
+	}
+	
 	// Copy the rendering properties from the calling thread.
 	MacCaptureToolParams *inParams = (MacCaptureToolParams *)arg;
 	MacCaptureToolParams param;

desmume/src/frontend/cocoa/userinterface/appDelegate.mm
@@ -241,6 +241,11 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
+	{
+		pthread_setname_np("App Main Thread");
+	}
+	
 	EmuControllerDelegate *emuControl = (EmuControllerDelegate *)[emuControlController content];
 	
 	// Determine if the app was run for the first time.

commit 3f47c4ad7d1bad13f794213fe7cc568eb207c8d5

Cocoa Port: Fix building for Xcode 3. (Regression from commit 03b9481.)

desmume/src/frontend/cocoa/cocoa_GPU.mm
@@ -1769,10 +1769,12 @@ CGLPBufferObj OSXOpenGLRendererPBuffer = NULL;
 
 static void* RunFetchThread(void *arg)
 {
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
 	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
 	{
 		pthread_setname_np("Video Fetch");
 	}
+#endif
 	
 	MacClientSharedObject *sharedData = (MacClientSharedObject *)arg;
 	[sharedData runFetchLoop];

desmume/src/frontend/cocoa/cocoa_core.mm
@@ -1142,10 +1142,12 @@ volatile bool execute = true;
 
 static void* RunCoreThread(void *arg)
 {
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
 	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
 	{
 		pthread_setname_np("Emulation Core");
 	}
+#endif
 	
 	CoreThreadParam *param = (CoreThreadParam *)arg;
 	CocoaDSCore *cdsCore = (CocoaDSCore *)param->cdsCore;

desmume/src/frontend/cocoa/cocoa_output.mm
@@ -1314,10 +1314,12 @@
 
 static void* RunOutputThread(void *arg)
 {
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
 	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
 	{
 		pthread_setname_np("EmulationOutput");
 	}
+#endif
 	
 	CocoaDSOutput *cdsDisplayOutput = (CocoaDSOutput *)arg;
 	[cdsDisplayOutput runMessageLoop];

desmume/src/frontend/cocoa/userinterface/MacAVCaptureTool.mm
@@ -1463,10 +1463,12 @@ ClientAVCaptureError FFmpegFileStream::WriteOneFrame(const AVStreamWriteParam &p
 
 static void* RunAVCaptureCloseThread(void *arg)
 {
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
 	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
 	{
 		pthread_setname_np("AVCapture Close");
 	}
+#endif
 	
 	/*
 	ClientAVCaptureObject *captureObject = (ClientAVCaptureObject *)arg;

desmume/src/frontend/cocoa/userinterface/MacScreenshotCaptureTool.mm
@@ -131,10 +131,12 @@
 
 static void* RunFileWriteThread(void *arg)
 {
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
 	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
 	{
 		pthread_setname_np("Mac File Write");
 	}
+#endif
 	
 	// Copy the rendering properties from the calling thread.
 	MacCaptureToolParams *inParams = (MacCaptureToolParams *)arg;

desmume/src/frontend/cocoa/userinterface/appDelegate.mm
@@ -241,10 +241,12 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
 	if (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6)
 	{
 		pthread_setname_np("App Main Thread");
 	}
+#endif
 	
 	EmuControllerDelegate *emuControl = (EmuControllerDelegate *)[emuControlController content];
 	

desmume/src/utils/task.cpp
@@ -133,7 +133,7 @@ void Task::Impl::start(bool spinlock, int threadPriority, const char *name)
 	sthread_setname(this->_thread, name);
 #else
 	
-#if defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
+#if defined(DESMUME_COCOA) && defined(MAC_OS_X_VERSION_10_6) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
 	// Setting the thread name on macOS uses pthread_setname_np(), but this requires macOS v10.6 or later.
 	this->needSetThreadName = (kCFCoreFoundationVersionNumber >= kCFCoreFoundationVersionNumber10_6) && (name != NULL);
 #else