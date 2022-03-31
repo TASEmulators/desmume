1cec82613bc3c0c89bafad21c7764d0d89b6a6c1
tech-ticks <65084428+tech-ticks@users.noreply.github.com>
2022-03-31 02:21:00 +0200

    interface: Fix macOS build (#485)
    * Compile macOS interface with clang
    
    * Fix OpenGL dependency on macOS

--- desmume/src/frontend/posix/shared/mic_openal.cpp
+++ desmume/src/frontend/posix/shared/mic_openal.cpp
@@ -18,8 +18,13 @@
 
 #ifndef WIN32
 
-#include <AL/al.h>
-#include <AL/alc.h>
+#ifdef __APPLE__
+  #include <OpenAL/al.h>
+  #include <OpenAL/alc.h>
+#else
+  #include <AL/al.h>
+  #include <AL/alc.h>
+#endif
 
 #include "types.h"
 #include "mic.h"
