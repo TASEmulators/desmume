TASEmulators/desmume

commit 8a9fec431a0e9b17d535e258ae7b135b19bc3001

matrix.h: stream_copy_fast() for AltiVec and NEON now mirror their associated buffer_copy_fast() functions, replacing memcpy(). - Since stream_copy_fast() and buffer_copy_fast() are intended for small finite-sized buffers only, we shouldn't need a full memcpy() call with these.

desmume/src/matrix.h
@@ -617,15 +617,17 @@ static void memset_u32_fast(void *dst, const u32 val)
 }
 
 template <size_t VECLENGTH>
-static void stream_copy_fast(void *__restrict dst, void *__restrict src)
-{
-	memcpy(dst, src, VECLENGTH);
-}
-
-template <size_t VECLENGTH>
 static void buffer_copy_fast(void *__restrict dst, void *__restrict src)
 {
 	MACRODO_N( VECLENGTH / sizeof(uint8x16x4_t), vst1q_u8_x4((u8 *)dst + ((X) * sizeof(uint8x16x4_t)), vld1q_u8_x4((u8 *)src + ((X) * sizeof(uint8x16x4_t)))) );
+}
+
+template <size_t VECLENGTH>
+static void stream_copy_fast(void *__restrict dst, void *__restrict src)
+{
+	// NEON doesn't have the same temporal/caching distinctions that SSE and AVX do,
+	// so just use buffer_copy_fast() for this function too.
+	buffer_copy_fast<VECLENGTH>(dst, src);
 }
 
 template <size_t VECLENGTH>
@@ -740,15 +742,17 @@ static void memset_u32_fast(void *dst, const u32 val)
 }
 
 template <size_t VECLENGTH>
-static void stream_copy_fast(void *__restrict dst, void *__restrict src)
-{
-	memcpy(dst, src, VECLENGTH);
-}
-
-template <size_t VECLENGTH>
 static void buffer_copy_fast(void *__restrict dst, void *__restrict src)
 {
 	MACRODO_N( VECLENGTH / sizeof(v128s8), vec_st(vec_ld((X)*sizeof(v128s8),src), (X)*sizeof(v128s8), dst) );
+}
+
+template <size_t VECLENGTH>
+static void stream_copy_fast(void *__restrict dst, void *__restrict src)
+{
+	// AltiVec doesn't have the same temporal/caching distinctions that SSE and AVX do,
+	// so just use buffer_copy_fast() for this function too.
+	buffer_copy_fast<VECLENGTH>(dst, src, VECLENGTH);
 }
 
 template <class T, size_t VECLENGTH, bool NEEDENDIANSWAP>

commit b1d49d14ecf4f1e30803ca24f91c6064fdc8dea3

matrix.h: Fix compiling on AltiVec-enabled systems. (Regression from commit 8a9fec43.)

desmume/src/matrix.h
@@ -744,7 +744,7 @@ static void memset_u32_fast(void *dst, const u32 val)
 template <size_t VECLENGTH>
 static void buffer_copy_fast(void *__restrict dst, void *__restrict src)
 {
-	MACRODO_N( VECLENGTH / sizeof(v128s8), vec_st(vec_ld((X)*sizeof(v128s8),src), (X)*sizeof(v128s8), dst) );
+	MACRODO_N( VECLENGTH / sizeof(v128s8), vec_st(vec_ld((X)*sizeof(v128s8),(u8 *__restrict)src), (X)*sizeof(v128s8), (u8 *__restrict)dst) );
 }
 
 template <size_t VECLENGTH>
@@ -752,7 +752,7 @@ static void stream_copy_fast(void *__restrict dst, void *__restrict src)
 {
 	// AltiVec doesn't have the same temporal/caching distinctions that SSE and AVX do,
 	// so just use buffer_copy_fast() for this function too.
-	buffer_copy_fast<VECLENGTH>(dst, src, VECLENGTH);
+	buffer_copy_fast<VECLENGTH>(dst, src);
 }
 
 template <class T, size_t VECLENGTH, bool NEEDENDIANSWAP>