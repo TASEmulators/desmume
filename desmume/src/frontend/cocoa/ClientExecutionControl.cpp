0b0bee78cca94ec9e840011cc70c960acdca475e
rogerman <rogerman@users.noreply.github.com>
2022-04-24 14:56:07 -0700

    Cocoa Port: Remove the dev+ requirement for Dynamic Recompiler on ARM CPUs. The Dynamic Recompiler option is now available for users running Apple Silicon Macs!

--- desmume/src/frontend/cocoa/ClientExecutionControl.cpp
+++ desmume/src/frontend/cocoa/ClientExecutionControl.cpp
@@ -197,7 +197,7 @@ const char* ClientExecutionControl::GetCPUEmulationEngineName()
 
 void ClientExecutionControl::SetCPUEmulationEngineByID(CPUEmulationEngineID engineID)
 {
-#if !defined(__i386__) && !defined(__x86_64__) && !defined(__aarch64__)
+#ifndef HAVE_JIT
 	engineID = CPUEmulationEngineID_Interpreter;
 #endif
 	
