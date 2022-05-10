TASEmulators/desmume

commit 3c04e8f4c7a26f698a72303952b9a364f7f34758

Cocoa Port (OpenEmu Plug-in): Add OpenGL as an option for the emulated GPU rendering engine. - Now that we can use OpenGL, we can increase the 3D render scaling to up to 8x for machines that can handle it. - Also add the Fragment Sampling Hack option for SoftRasterizer for certain games that need it to 'fix' texture rendering. - Also add the Smooth Textures option for OpenGL for games that can benefit from it.

desmume/src/frontend/cocoa/cocoa_GPU.mm
@@ -172,7 +172,6 @@ GPU3DInterface *core3DList[GPU_3D_RENDERER_COUNT+1] = {
 	bool isTempContextCreated = OSXOpenGLRendererInit();
 	if (isTempContextCreated)
 	{
-		CGLContextObj prevContext = CGLGetCurrentContext();
 		OSXOpenGLRendererBegin();
 		
 		GLint maxSamplesOGL = 0;
@@ -187,7 +186,6 @@ GPU3DInterface *core3DList[GPU_3D_RENDERER_COUNT+1] = {
 		
 		OSXOpenGLRendererEnd();
 		DestroyOpenGLRenderer();
-		CGLSetCurrentContext(prevContext);
 	}
 	
 	return self;
@@ -1817,6 +1815,7 @@ bool GPUEventHandlerAsync::GetRender3DNeedsFinish()
 #pragma mark -
 
 CGLContextObj OSXOpenGLRendererContext = NULL;
+CGLContextObj OSXOpenGLRendererContextPrev = NULL;
 SILENCE_DEPRECATION_MACOS_10_7( CGLPBufferObj OSXOpenGLRendererPBuffer = NULL );
 
 bool OSXOpenGLRendererInit()
@@ -1832,6 +1831,7 @@ bool OSXOpenGLRendererInit()
 
 bool OSXOpenGLRendererBegin()
 {
+	OSXOpenGLRendererContextPrev = CGLGetCurrentContext();
 	CGLSetCurrentContext(OSXOpenGLRendererContext);
 	
 	return true;
@@ -1839,7 +1839,7 @@ bool OSXOpenGLRendererBegin()
 
 void OSXOpenGLRendererEnd()
 {
-	
+	CGLSetCurrentContext(OSXOpenGLRendererContextPrev);
 }
 
 bool OSXOpenGLRendererFramebufferDidResize(const bool isFBOSupported, size_t w, size_t h)
@@ -1944,11 +1944,14 @@ void DestroyOpenGLRenderer()
 		return;
 	}
 	
+	OSXOpenGLRendererEnd();
+	
 	SILENCE_DEPRECATION_MACOS_10_7( CGLReleasePBuffer(OSXOpenGLRendererPBuffer) );
 	OSXOpenGLRendererPBuffer = NULL;
 	
 	CGLReleaseContext(OSXOpenGLRendererContext);
 	OSXOpenGLRendererContext = NULL;
+	OSXOpenGLRendererContextPrev = NULL;
 }
 
 void RequestOpenGLRenderer_3_2(bool request_3_2)

desmume/src/frontend/cocoa/openemu/NDSGameCore.h
@@ -88,14 +88,24 @@ class OE_OGLDisplayPresenter;
 #define NDSDISPLAYMODE_NAMEKEY_HUD_REALTIMECLOCK           "Real-Time Clock"
 #define NDSDISPLAYMODE_NAMEKEY_HUD_INPUT                   "Input"
 
+#define NDSDISPLAYMODE_NAMEKEY_GPU_ENGINE_SOFTWARE         "Use SoftRasterizer Engine"
+#define NDSDISPLAYMODE_NAMEKEY_GPU_ENGINE_OPENGL           "Use OpenGL Engine"
+
 #define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_1X         "1x Native Resolution"
 #define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_2X         "2x Resolution"
 #define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_3X         "3x Resolution"
 #define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_4X         "4x Resolution"
+#define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_5X         "5x Resolution"
+#define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_6X         "6x Resolution"
+#define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_7X         "7x Resolution"
+#define NDSDISPLAYMODE_NAMEKEY_3D_RENDERSCALING_8X         "8x Resolution"
 
 #define NDSDISPLAYMODE_NAMEKEY_3D_TEXTURESCALING_1X        "1x Native Texture"
 #define NDSDISPLAYMODE_NAMEKEY_3D_TEXTURESCALING_2X        "2x Texture Upscaling"
 #define NDSDISPLAYMODE_NAMEKEY_3D_TEXTURESCALING_4X        "4x Texture Upscaling"
+
+#define NDSDISPLAYMODE_NAMEKEY_GPU_SOFTWARE_FRAGMENTSAMPLINGHACK  "Fragment Sampling Hack"
+#define NDSDISPLAYMODE_NAMEKEY_GPU_OPENGL_SMOOTHTEXTURES   "Smooth Textures"
 	
 #define NDSDISPLAYMODE_PREFKEY_DISPLAYMODE                 "displayMode"
 #define NDSDISPLAYMODE_PREFKEY_LAYOUT                      "layout"
@@ -113,6 +123,9 @@ class OE_OGLDisplayPresenter;
 #define NDSDISPLAYMODE_PREFKEY_HUD_CPULOADAVERAGE          "hud_cpuloadaverage"
 #define NDSDISPLAYMODE_PREFKEY_HUD_REALTIMECLOCK           "hud_realtimeclock"
 #define NDSDISPLAYMODE_PREFKEY_HUD_INPUT                   "hud_input"
+#define NDSDISPLAYMODE_PREFKEY_GPU_ENGINE                  "gpu_engine"
+#define NDSDISPLAYMODE_PREFKEY_GPU_SOFTWARE_FRAGMENTSAMPLINGHACK  "gpu_software_fragmentsamplinghack"
+#define NDSDISPLAYMODE_PREFKEY_GPU_OPENGL_SMOOTHTEXTURES   "gpu_opengl_smoothtextures"
 #define NDSDISPLAYMODE_PREFKEY_3D_RENDERSCALING            "3D_renderscaling"
 #define NDSDISPLAYMODE_PREFKEY_3D_TEXTURESCALING           "3D_texturescaling"
 
@@ -164,10 +177,20 @@ enum NDSDisplayOptionID
 	NDSDisplayOptionID_HUD_RealTimeClock,
 	NDSDisplayOptionID_HUD_Input,
 	
+	NDSDisplayOptionID_GPU_Engine_Software,
+	NDSDisplayOptionID_GPU_Engine_OpenGL,
+	
+	NDSDisplayOptionID_GPU_Software_FragmentSamplingHack,
+	NDSDisplayOptionID_GPU_OpenGL_SmoothTextures,
+	
 	NDSDisplayOptionID_3D_RenderScaling_1x,
 	NDSDisplayOptionID_3D_RenderScaling_2x,
 	NDSDisplayOptionID_3D_RenderScaling_3x,
 	NDSDisplayOptionID_3D_RenderScaling_4x,
+	NDSDisplayOptionID_3D_RenderScaling_5x,
+	NDSDisplayOptionID_3D_RenderScaling_6x,
+	NDSDisplayOptionID_3D_RenderScaling_7x,
+	NDSDisplayOptionID_3D_RenderScaling_8x,
 	
 	NDSDisplayOptionID_3D_TextureScaling_1x,
 	NDSDisplayOptionID_3D_TextureScaling_2x,
@@ -211,10 +234,17 @@ enum NDSDisplayOptionID
                                                       (1ULL << NDSDisplayOptionID_VideoSourceTouch_ForceMain) | \
                                                       (1ULL << NDSDisplayOptionID_VideoSourceTouch_ForceSub))
 
+#define NDSDISPLAYMODE_GROUPBITMASK_GPUENGINE        ((1ULL << NDSDisplayOptionID_GPU_Engine_Software) | \
+													  (1ULL << NDSDisplayOptionID_GPU_Engine_OpenGL))
+
 #define NDSDISPLAYMODE_GROUPBITMASK_RENDERSCALING    ((1ULL << NDSDisplayOptionID_3D_RenderScaling_1x) | \
                                                       (1ULL << NDSDisplayOptionID_3D_RenderScaling_2x) | \
                                                       (1ULL << NDSDisplayOptionID_3D_RenderScaling_3x) | \
-                                                      (1ULL << NDSDisplayOptionID_3D_RenderScaling_4x))
+                                                      (1ULL << NDSDisplayOptionID_3D_RenderScaling_4x) | \
+                                                      (1ULL << NDSDisplayOptionID_3D_RenderScaling_5x) | \
+                                                      (1ULL << NDSDisplayOptionID_3D_RenderScaling_6x) | \
+                                                      (1ULL << NDSDisplayOptionID_3D_RenderScaling_7x) | \
+                                                      (1ULL << NDSDisplayOptionID_3D_RenderScaling_8x))
 
 #define NDSDISPLAYMODE_GROUPBITMASK_TEXTURESCALING   ((1ULL << NDSDisplayOptionID_3D_TextureScaling_1x) | \
                                                       (1ULL << NDSDisplayOptionID_3D_TextureScaling_2x) | \

commit 0f6cbffe77ee30059e01d904e3598c286d31bdfc

Cocoa Port: Fix a bug where repeatedly using save states in quick succession can cause intermittent failures with HUD rendering when using OpenGL display views. (Regression from commit 3c04e8f4.)

desmume/src/frontend/cocoa/cocoa_GPU.mm
@@ -1839,7 +1839,13 @@ bool OSXOpenGLRendererBegin()
 
 void OSXOpenGLRendererEnd()
 {
+#ifndef PORT_VERSION_OS_X_APP
+	// The OpenEmu plug-in needs the context reset after 3D rendering since OpenEmu's context
+	// is assumed to be the default context. However, resetting the context for our standalone
+	// app can cause problems since the core emulator's context is assumed to be the default
+	// context. So reset the context for OpenEmu and skip resetting for us.
 	CGLSetCurrentContext(OSXOpenGLRendererContextPrev);
+#endif
 }
 
 bool OSXOpenGLRendererFramebufferDidResize(const bool isFBOSupported, size_t w, size_t h)