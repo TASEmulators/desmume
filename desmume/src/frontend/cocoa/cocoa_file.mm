0346f7b9466d6d3998bb9d4195279f87921d575f
rogerman <rogerman@users.noreply.github.com>
2022-05-21 19:41:57 -0700

    Cocoa Port: Bump version number to 0.9.13.

--- desmume/src/frontend/cocoa/cocoa_file.mm
+++ desmume/src/frontend/cocoa/cocoa_file.mm
@@ -730,6 +730,14 @@ static NSMutableDictionary *_gURLDictionary = nil;
 		portStr = @"Cocoa";
 	}
 	
+	versionURL = [CocoaDSFile userAppSupportURL:nil version:@"0.9.13"];
+	versionPath = [versionURL path];
+	if (versionPath != nil && [[[filePath stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] isEqualToString:versionPath])
+	{
+		versionStr = @"0.9.13";
+		portStr = @"Cocoa";
+	}
+	
 	fileVersion = [[versionStr stringByAppendingString:@" "] stringByAppendingString:portStr];
 	
 	return fileVersion;
