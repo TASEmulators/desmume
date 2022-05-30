323e3389e0c849dfb7cad81eb3c252b4f437cf05
Lino5000 <bathy2oo2@gmail.com>
2022-05-30 17:45:06 +1000

    Allow Right Shift to be used as a binding for Linux CLI, since it is the default for Select

--- desmume/src/frontend/posix/shared/ctrlssdl.cpp
+++ desmume/src/frontend/posix/shared/ctrlssdl.cpp
@@ -513,7 +513,6 @@ process_ctrls_event( SDL_Event& event,
                 break;
             case SDLK_RSHIFT:
                 shift_pressed |= 2;
-                break;
             default:
                 key = lookup_key(event.key.keysym.sym);
                 ADD_KEY( cfg->keypad, key );
