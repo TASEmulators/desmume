TASEmulators/desmume

commit 5426509ede05396ebb1c8f2cff7118c64e117153

GFX3D: Render states and geometry lists are now copied/swapped more consistently on flush. (Related to commit 8438a5a6.) - Viewports are now processed on VIEWPORT register write instead of being processed at render time. - CLEAR_DEPTH, CLRIMAGE_OFFSET, EDGE_COLOR, FOG_TABLE, and TOON_TABLE register writes are now handled more consistently. - The fogDensityTable check for force-drawing clear images in gfx3d_VBlankEndSignal() has been removed. Changes done in commit 8438a5a6 will always causes this check to fail, and this commit will always cause this check to fail. Therefore, this check is now obsolete. - Change a bunch of GFX3D-related structs from C++ style constructed structs into C-style POD structs.

desmume/src/rasterize.cpp
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2009-2022 DeSmuME team
+	Copyright (C) 2009-2023 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -313,25 +313,25 @@ FORCEINLINE int edge_fx_fl::Step() {
 
 
 
-static FORCEINLINE void alphaBlend(FragmentColor &dst, const FragmentColor src)
+static FORCEINLINE void alphaBlend(const bool isAlphaBlendingEnabled, const FragmentColor inSrc, FragmentColor &outDst)
 {
-	if (src.a == 0)
+	if (inSrc.a == 0)
 	{
 		return;
 	}
 	
-	if (src.a == 31 || dst.a == 0 || !gfx3d.renderState.enableAlphaBlending)
+	if (inSrc.a == 31 || outDst.a == 0 || !isAlphaBlendingEnabled)
 	{
-		dst = src;
+		outDst = inSrc;
 	}
 	else
 	{
-		const u8 alpha = src.a + 1;
+		const u8 alpha = inSrc.a + 1;
 		const u8 invAlpha = 32 - alpha;
-		dst.r = (alpha*src.r + invAlpha*dst.r) >> 5;
-		dst.g = (alpha*src.g + invAlpha*dst.g) >> 5;
-		dst.b = (alpha*src.b + invAlpha*dst.b) >> 5;
-		dst.a = max(src.a, dst.a);
+		outDst.r = (alpha*inSrc.r + invAlpha*outDst.r) >> 5;
+		outDst.g = (alpha*inSrc.g + invAlpha*outDst.g) >> 5;
+		outDst.b = (alpha*inSrc.b + invAlpha*outDst.b) >> 5;
+		outDst.a = max(inSrc.a, outDst.a);
 	}
 }
 
@@ -424,6 +424,8 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_shade(const PolygonMode polygonMode,
 		return;
 	}
 	
+	const GFX3D_State &renderState = *this->_softRender->currentRenderState;
+	
 	static const FragmentColor colorWhite = MakeFragmentColor(0x3F, 0x3F, 0x3F, 0x1F);
 	const FragmentColor mainTexColor = (this->_currentTexture->IsSamplingEnabled()) ? this->_sample(texCoordU, texCoordV) : colorWhite;
 	
@@ -468,7 +470,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_shade(const PolygonMode polygonMode,
 		{
 			const FragmentColor toonColor = this->_softRender->toonColor32LUT[src.r >> 1];
 			
-			if (gfx3d.renderState.shading == PolygonShadingMode_Highlight)
+			if (renderState.DISP3DCNT.PolygonShading == PolygonShadingMode_Highlight)
 			{
 				// Tested in the "Shadows of Almia" logo in the Pokemon Ranger: Shadows of Almia title screen.
 				// Also tested in Advance Wars: Dual Strike and Advance Wars: Days of Ruin when tiles highlight
@@ -502,6 +504,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_shade(const PolygonMode polygonMode,
 template<bool RENDERER> template<bool ISFRONTFACING, bool ISSHADOWPOLYGON>
 FORCEINLINE void RasterizerUnit<RENDERER>::_pixel(const POLYGON_ATTR polyAttr, const bool isTranslucent, const size_t fragmentIndex, FragmentColor &dstColor, float r, float g, float b, float invu, float invv, float z, float w)
 {
+	const GFX3D_State &renderState = *this->_softRender->currentRenderState;
 	FragmentColor newDstColor32;
 	FragmentColor shaderOutput;
 	bool isOpaquePixel;
@@ -522,7 +525,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel(const POLYGON_ATTR polyAttr, c
 
 	// Note that an IEEE-754 single-precision float uses a 23-bit significand. Therefore, we will multiply the
 	// Z-depth by a 22-bit significand for safety.
-	const u32 newDepth = (gfx3d.renderState.wbuffer) ? u32floor(w * 4096.0f) : u32floor(z * 4194303.0f) << 2;
+	const u32 newDepth = (renderState.SWAP_BUFFERS.DepthMode) ? u32floor(w * 4096.0f) : u32floor(z * 4194303.0f) << 2;
 	
 	// run the depth test
 	bool depthFail = false;
@@ -642,7 +645,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel(const POLYGON_ATTR polyAttr, c
 		dstAttributeTranslucentPolyID = polyAttr.PolygonID;
 		
 		//alpha blending and write color
-		alphaBlend(dstColor, shaderOutput);
+		alphaBlend((renderState.DISP3DCNT.EnableAlphaBlending != 0), shaderOutput, dstColor);
 		
 		dstAttributeIsFogged = (dstAttributeIsFogged && polyAttr.Fog_Enable);
 	}
@@ -779,6 +782,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_drawscanline(const POLYGON_ATTR poly
 template<bool RENDERER> template<bool ISFRONTFACING, bool ISSHADOWPOLYGON>
 FORCEINLINE void RasterizerUnit<RENDERER>::_pixel_SSE2(const POLYGON_ATTR polyAttr, const bool isTranslucent, const size_t fragmentIndex, FragmentColor &dstColor, const __m128 &srcColorf, float invu, float invv, float z, float w)
 {
+	const GFX3D_State &renderState = *this->_softRender->currentRenderState;
 	FragmentColor newDstColor32;
 	FragmentColor shaderOutput;
 	bool isOpaquePixel;
@@ -799,7 +803,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel_SSE2(const POLYGON_ATTR polyAt
 	
 	// Note that an IEEE-754 single-precision float uses a 23-bit significand. Therefore, we will multiply the
 	// Z-depth by a 22-bit significand for safety.
-	const u32 newDepth = (gfx3d.renderState.wbuffer) ? u32floor(w * 4096.0f) : u32floor(z * 4194303.0f) << 2;
+	const u32 newDepth = (renderState.SWAP_BUFFERS.DepthMode) ? u32floor(w * 4096.0f) : u32floor(z * 4194303.0f) << 2;
 	
 	// run the depth test
 	bool depthFail = false;
@@ -891,7 +895,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel_SSE2(const POLYGON_ATTR polyAt
 	this->_shade<ISSHADOWPOLYGON>((PolygonMode)polyAttr.Mode, newDstColor32, shaderOutput, invu * w, invv * w);
 	
 	// handle alpha test
-	if ( shaderOutput.a == 0 || (this->_softRender->currentRenderState->enableAlphaTest && shaderOutput.a < this->_softRender->currentRenderState->alphaTestRef) )
+	if ( shaderOutput.a == 0 || (renderState.DISP3DCNT.EnableAlphaTest && (shaderOutput.a < renderState.alphaTestRef)) )
 	{
 		return;
 	}
@@ -917,7 +921,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel_SSE2(const POLYGON_ATTR polyAt
 		dstAttributeTranslucentPolyID = polyAttr.PolygonID;
 		
 		//alpha blending and write color
-		alphaBlend(dstColor, shaderOutput);
+		alphaBlend((renderState.DISP3DCNT.EnableAlphaBlending != 0), shaderOutput, dstColor);
 		
 		dstAttributeIsFogged = (dstAttributeIsFogged && polyAttr.Fog_Enable);
 	}
@@ -1910,21 +1914,13 @@ void SoftRasterizerRenderer::_TransformVertices()
 			vert.fcolor[2] /= vertw;
 			
 			//viewport transformation
-			VIEWPORT viewport;
-			viewport.decode(poly.poly->viewport);
-			vert.coord[0] *= viewport.width;
-			vert.coord[0] += viewport.x;
-			
-			// The maximum viewport y-value is 191. Values above 191 need to wrap
-			// around and go negative.
-			//
-			// Test case: The Homie Rollerz character select screen sets the y-value
-			// to 253, which then wraps around to -2.
-			vert.coord[1] *= viewport.height;
-			vert.coord[1] += (viewport.y > 191) ? (viewport.y - 0xFF) : viewport.y;
-			vert.coord[1] = 192 - vert.coord[1];
-
+			vert.coord[0] *= poly.poly->viewport.width;
+			vert.coord[0] += poly.poly->viewport.x;
 			vert.coord[0] *= wScalar;
+			
+			vert.coord[1] *= poly.poly->viewport.height;
+			vert.coord[1] += poly.poly->viewport.y;
+			vert.coord[1] = 192 - vert.coord[1];
 			vert.coord[1] *= hScalar;
 			
 			//here is a hack which needs to be removed.
@@ -2010,7 +2006,7 @@ Render3DError SoftRasterizerRenderer::ApplyRenderingSettings(const GFX3D_State &
 	return Render3D::ApplyRenderingSettings(renderState);
 }
 
-Render3DError SoftRasterizerRenderer::BeginRender(const GFX3D &engine)
+Render3DError SoftRasterizerRenderer::BeginRender(const GFX3D_State &renderState, const GFX3D_GeometryList &renderGList)
 {
 	// Force all threads to finish before rendering with new data
 	for (size_t i = 0; i < this->_threadCount; i++)
@@ -2019,10 +2015,10 @@ Render3DError SoftRasterizerRenderer::BeginRender(const GFX3D &engine)
 	}
 	
 	// Keep the current render states for later use
-	this->currentRenderState = (GFX3D_State *)&engine.renderState;
-	this->_clippedPolyCount = engine.clippedPolyCount;
-	this->_clippedPolyOpaqueCount = engine.clippedPolyOpaqueCount;
-	memcpy(this->_clippedPolyList, engine.clippedPolyList, this->_clippedPolyCount * sizeof(CPoly));
+	this->currentRenderState = (GFX3D_State *)&renderState;
+	this->_clippedPolyCount = renderGList.clippedPolyCount;
+	this->_clippedPolyOpaqueCount = renderGList.clippedPolyOpaqueCount;
+	memcpy(this->_clippedPolyList, renderGList.clippedPolyList, this->_clippedPolyCount * sizeof(CPoly));
 	
 	const bool doMultithreadedStateSetup = (this->_threadCount >= 2);
 	
@@ -2038,16 +2034,16 @@ Render3DError SoftRasterizerRenderer::BeginRender(const GFX3D &engine)
 	}
 	
 	// Convert the toon table colors
-	ColorspaceConvertBuffer555To6665Opaque<false, false, BESwapDst>(engine.renderState.u16ToonTable, (u32 *)this->toonColor32LUT, 32);
+	ColorspaceConvertBuffer555To6665Opaque<false, false, BESwapDst>(renderState.toonTable16, (u32 *)this->toonColor32LUT, 32);
 	
 	if (this->_enableEdgeMark)
 	{
-		this->_UpdateEdgeMarkColorTable(this->currentRenderState->edgeMarkColorTable);
+		this->_UpdateEdgeMarkColorTable(renderState.edgeMarkColorTable);
 	}
 	
 	if (this->_enableFog)
 	{
-		this->_UpdateFogTable(this->currentRenderState->fogDensityTable);
+		this->_UpdateFogTable(renderState.fogDensityTable);
 	}
 	
 	if (doMultithreadedStateSetup)
@@ -2107,7 +2103,7 @@ void SoftRasterizerRenderer::_UpdateEdgeMarkColorTable(const u16 *edgeMarkColorT
 	//we can do this by rendering a 3d frame and then freezing the system, but only changing the edge mark colors
 	for (size_t i = 0; i < 8; i++)
 	{
-		this->_edgeMarkTable[i].color = LE_TO_LOCAL_32( COLOR555TO6665(edgeMarkColorTable[i] & 0x7FFF, (this->currentRenderState->enableAntialiasing) ? 0x10 : 0x1F) );
+		this->_edgeMarkTable[i].color = LE_TO_LOCAL_32( COLOR555TO6665(edgeMarkColorTable[i] & 0x7FFF, (this->currentRenderState->DISP3DCNT.EnableAntialiasing) ? 0x10 : 0x1F) );
 		
 		//zero 20-jun-2013 - this doesnt make any sense. at least, it should be related to the 0x8000 bit. if this is undocumented behaviour, lets write about which scenario proves it here, or which scenario is requiring this code.
 		//// this seems to be the only thing that selectively disables edge marking
@@ -2390,7 +2386,7 @@ Render3DError SoftRasterizerRenderer::EndRender()
 			this->_threadPostprocessParam[0].enableEdgeMarking = this->_enableEdgeMark;
 			this->_threadPostprocessParam[0].enableFog = this->_enableFog;
 			this->_threadPostprocessParam[0].fogColor = this->currentRenderState->fogColor;
-			this->_threadPostprocessParam[0].fogAlphaOnly = this->currentRenderState->enableFogAlphaOnly;
+			this->_threadPostprocessParam[0].fogAlphaOnly = (this->currentRenderState->DISP3DCNT.FogOnlyAlpha != 0);
 			
 			this->RenderEdgeMarkingAndFog(this->_threadPostprocessParam[0]);
 		}
@@ -2426,7 +2422,7 @@ Render3DError SoftRasterizerRenderer::RenderFinish()
 				this->_threadPostprocessParam[i].enableEdgeMarking = this->_enableEdgeMark;
 				this->_threadPostprocessParam[i].enableFog = this->_enableFog;
 				this->_threadPostprocessParam[i].fogColor = this->currentRenderState->fogColor;
-				this->_threadPostprocessParam[i].fogAlphaOnly = this->currentRenderState->enableFogAlphaOnly;
+				this->_threadPostprocessParam[i].fogAlphaOnly = (this->currentRenderState->DISP3DCNT.FogOnlyAlpha != 0);
 				
 				this->_task[i].execute(&SoftRasterizer_RunRenderEdgeMarkAndFog, &this->_threadPostprocessParam[i]);
 			}

desmume/src/GPU.h
@@ -2,7 +2,7 @@
 	Copyright (C) 2006 yopyop
 	Copyright (C) 2006-2007 Theo Berkau
 	Copyright (C) 2007 shash
-	Copyright (C) 2009-2022 DeSmuME team
+	Copyright (C) 2009-2023 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -581,7 +581,7 @@ typedef union
 		u8 PolygonShading:1;				//     1: Polygon shading mode, interacts with POLYGON_ATTR (0x40004A4); 0=Toon Shading, 1=Highlight Shading
 		u8 EnableAlphaTest:1;				//     2: Perform alpha test, interacts with ALPHA_TEST_REF (0x4000340); 0=Disable, 1=Enable
 		u8 EnableAlphaBlending:1;			//     3: Perform alpha blending, interacts with POLYGON_ATTR (0x40004A4); 0=Disable, 1=Enable
-		u8 EnableAntiAliasing:1;			//     4: Render polygon edges with antialiasing; 0=Disable, 1=Enable
+		u8 EnableAntialiasing:1;			//     4: Render polygon edges with antialiasing; 0=Disable, 1=Enable
 		u8 EnableEdgeMarking:1;				//     5: Perform polygon edge marking, interacts with EDGE_COLOR (0x4000330); 0=Disable, 1=Enable
 		u8 FogOnlyAlpha:1;					//     6: Apply fog to the alpha channel only, interacts with FOG_COLOR (0x4000358) / FOG_TABLE (0x4000360); 0=Color+Alpha, 1=Alpha
 		u8 EnableFog:1;						//     7: Perform fog rendering, interacts with FOG_COLOR (0x4000358) / FOG_OFFSET (0x400035C) / FOG_TABLE (0x4000360);
@@ -600,7 +600,7 @@ typedef union
 											//        0=Disable, 1=Enable
 		u8 FogOnlyAlpha:1;					//     6: Apply fog to the alpha channel only, interacts with FOG_COLOR (0x4000358) / FOG_TABLE (0x4000360); 0=Color+Alpha, 1=Alpha
 		u8 EnableEdgeMarking:1;				//     5: Perform polygon edge marking, interacts with EDGE_COLOR (0x4000330); 0=Disable, 1=Enable
-		u8 EnableAntiAliasing:1;			//     4: Render polygon edges with antialiasing; 0=Disable, 1=Enable
+		u8 EnableAntialiasing:1;			//     4: Render polygon edges with antialiasing; 0=Disable, 1=Enable
 		u8 EnableAlphaBlending:1;			//     3: Perform alpha blending, interacts with POLYGON_ATTR (0x40004A4); 0=Disable, 1=Enable
 		u8 EnableAlphaTest:1;				//     2: Perform alpha test, interacts with ALPHA_TEST_REF (0x4000340); 0=Disable, 1=Enable
 		u8 PolygonShading:1;				//     1: Polygon shading mode, interacts with POLYGON_ATTR (0x40004A4); 0=Toon Shading, 1=Highlight Shading

desmume/src/MMU.cpp
@@ -1,7 +1,7 @@
 /*
 	Copyright (C) 2006 yopyop
 	Copyright (C) 2007 shash
-	Copyright (C) 2007-2022 DeSmuME team
+	Copyright (C) 2007-2023 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -1830,13 +1830,28 @@ static void writereg_POWCNT1(const int size, const u32 adr, const u32 val)
 	if(wasGeomEnabled && !isGeomEnabled)
 	{
 		//kill the geometry data when the power goes off
-		//but save these tables, first. they shouldnt be cleared.
 		//so, so bad. we need to model this with hardware-like operations instead of c++ code
-		GFX3D_State prior = gfx3d.state;
-		reconstruct(&gfx3d.state);
-		memcpy(gfx3d.state.u16ToonTable, prior.u16ToonTable, sizeof(prior.u16ToonTable));
-		//dont think we should save this one: it's sent with 3d commands, not random bonus immediate register writes like the toon table
-		//memcpy(gfx3d.state.shininessTable, prior.shininessTable, sizeof(prior.shininessTable)); 
+		
+		// TODO: Test which geometry data should be cleared on power off.
+		// The code below doesn't make any sense. You would think that only the data that
+		// is derived from geometry commands (either via GXFIFO or if writing to registers
+		// 0x04000440 - 0x040006A4) is what should be cleared. And that outside of geometry
+		// command data, other data (even if related to the 3D engine) shouldn't be touched.
+		// This will need further testing, but for now, we'll leave things as they are.
+		//    - 2023/01/22, rogerman
+		gfx3d.pendingState.DISP3DCNT.value = 0;
+		gfx3d.pendingState.DISP3DCNT.EnableTexMapping = 1;
+		gfx3d.pendingState.DISP3DCNT.PolygonShading = PolygonShadingMode_Toon;
+		gfx3d.pendingState.DISP3DCNT.EnableAlphaTest = 1;
+		gfx3d.pendingState.DISP3DCNT.EnableAlphaBlending = 1;
+		gfx3d.pendingState.SWAP_BUFFERS.value = 0;
+		gfx3d.pendingState.alphaTestRef = 0;
+		gfx3d.pendingState.clearDepth = DS_DEPTH15TO24(0x7FFF);
+		gfx3d.pendingState.clearColor = 0;
+		gfx3d.pendingState.fogColor = 0;
+		gfx3d.pendingState.fogOffset = 0;
+		gfx3d.pendingState.fogShift = 0;
+		memset(gfx3d.pendingState.shininessTable, 0, sizeof(gfx3d.pendingState.shininessTable));
 	}
 }
 
@@ -3271,6 +3286,28 @@ void FASTCALL _MMU_ARM9_write08(u32 adr, u8 val)
 				return;
 			}
 			
+			switch (adr >> 4)
+			{
+				case 0x400033: // Edge Mark Color Table
+					MMU.ARM9_REG[adr & 0xFFF] = val;
+					gfx3d_UpdateEdgeMarkColorTable<u8>((u8)(adr & 0x0000000F), val);
+					return;
+					
+				case 0x400036:
+				case 0x400037: // Fog Table
+					MMU.ARM9_REG[adr & 0xFFF] = val & 0x7F;
+					gfx3d_UpdateFogTable<u8>((u8)(adr & 0x0000001F), val & 0x7F); // Drop the highest bit of each 8-bit value to limit the range to [0...127]
+					return;
+					
+				case 0x400038:
+				case 0x400039:
+				case 0x40003A:
+				case 0x40003B: // Toon Table
+					MMU.ARM9_REG[adr & 0xFFF] = val;
+					gfx3d_UpdateToonTable<u8>((u8)(adr & 0x0000003F), val);
+					return;
+			}
+			
 			GPUEngineA *mainEngine = GPU->GetEngineMain();
 			GPUEngineB *subEngine = GPU->GetEngineSub();
 			
@@ -3615,18 +3652,6 @@ void FASTCALL _MMU_ARM9_write08(u32 adr, u8 val)
 				case REG_DIVCNT+3: printf("ERROR 8bit DIVCNT+3 WRITE\n"); return;
 #endif
 					
-					//fog table: only write bottom 7 bits
-				case eng_3D_FOG_TABLE+0x00: case eng_3D_FOG_TABLE+0x01: case eng_3D_FOG_TABLE+0x02: case eng_3D_FOG_TABLE+0x03: 
-				case eng_3D_FOG_TABLE+0x04: case eng_3D_FOG_TABLE+0x05: case eng_3D_FOG_TABLE+0x06: case eng_3D_FOG_TABLE+0x07: 
-				case eng_3D_FOG_TABLE+0x08: case eng_3D_FOG_TABLE+0x09: case eng_3D_FOG_TABLE+0x0A: case eng_3D_FOG_TABLE+0x0B: 
-				case eng_3D_FOG_TABLE+0x0C: case eng_3D_FOG_TABLE+0x0D: case eng_3D_FOG_TABLE+0x0E: case eng_3D_FOG_TABLE+0x0F: 
-				case eng_3D_FOG_TABLE+0x10: case eng_3D_FOG_TABLE+0x11: case eng_3D_FOG_TABLE+0x12: case eng_3D_FOG_TABLE+0x13: 
-				case eng_3D_FOG_TABLE+0x14: case eng_3D_FOG_TABLE+0x15: case eng_3D_FOG_TABLE+0x16: case eng_3D_FOG_TABLE+0x17: 
-				case eng_3D_FOG_TABLE+0x18: case eng_3D_FOG_TABLE+0x19: case eng_3D_FOG_TABLE+0x1A: case eng_3D_FOG_TABLE+0x1B: 
-				case eng_3D_FOG_TABLE+0x1C: case eng_3D_FOG_TABLE+0x1D: case eng_3D_FOG_TABLE+0x1E: case eng_3D_FOG_TABLE+0x1F: 
-					val &= 0x7F;
-					break;
-					
 					//ensata putchar port
 				case 0x04FFF000:
 					if(nds.ensataEmulation)
@@ -3663,8 +3688,28 @@ void FASTCALL _MMU_ARM9_write08(u32 adr, u8 val)
 					
 				case eng_3D_CLEAR_COLOR+0: case eng_3D_CLEAR_COLOR+1:
 				case eng_3D_CLEAR_COLOR+2: case eng_3D_CLEAR_COLOR+3:
-					T1WriteByte((u8*)&gfx3d.state.clearColor,adr-eng_3D_CLEAR_COLOR,val); 
+					T1WriteByte((u8*)&gfx3d.pendingState.clearColor, adr-eng_3D_CLEAR_COLOR, val);
 					break;
+					
+				case eng_3D_CLEAR_DEPTH:
+					HostWriteByte(MMU.ARM9_REG, 0x0354, val);
+					gfx3d_glClearDepth<u8, 0>(val);
+					return;
+					
+				case eng_3D_CLEAR_DEPTH+1:
+					HostWriteByte(MMU.ARM9_REG, 0x0355, val);
+					gfx3d_glClearDepth<u8, 1>(val);
+					return;
+					
+				case eng_3D_CLRIMAGE_OFFSET:
+					HostWriteByte(MMU.ARM9_REG, 0x0356, val);
+					gfx3d_glClearImageOffset<u8, 0>(val);
+					return;
+					
+				case eng_3D_CLRIMAGE_OFFSET+1:
+					HostWriteByte(MMU.ARM9_REG, 0x0357, val);
+					gfx3d_glClearImageOffset<u8, 1>(val);
+					return;
 					
 				case REG_VRAMCNTA:
 				case REG_VRAMCNTB:
@@ -3761,13 +3806,23 @@ void FASTCALL _MMU_ARM9_write16(u32 adr, u16 val)
 			
 			switch (adr >> 4)
 			{
-					//toon table
-				case 0x0400038:
-				case 0x0400039:
-				case 0x040003A:
-				case 0x040003B:
-					((u16 *)(MMU.ARM9_REG))[(adr & 0xFFF)>>1] = val;
-					gfx3d_UpdateToonTable((adr & 0x3F) >> 1, val);
+				case 0x400033: // Edge Mark Color Table
+					((u16 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> (sizeof(u16) >> 1)] = val;
+					gfx3d_UpdateEdgeMarkColorTable<u16>((u8)(adr & 0x0000000F), val);
+					return;
+					
+				case 0x400036:
+				case 0x400037: // Fog Table
+					((u16 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> (sizeof(u16) >> 1)] = val & 0x7F7F;
+					gfx3d_UpdateFogTable<u16>((u8)(adr & 0x0000001F), val & 0x7F7F); // Drop the highest bit of each 8-bit value to limit the range to [0...127]
+					return;
+					
+				case 0x400038:
+				case 0x400039:
+				case 0x40003A:
+				case 0x40003B: // Toon Table
+					((u16 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> (sizeof(u16) >> 1)] = val;
+					gfx3d_UpdateToonTable<u16>((u8)(adr & 0x0000003F), val);
 					return;
 			}
 			
@@ -4262,14 +4317,6 @@ void FASTCALL _MMU_ARM9_write16(u32 adr, u16 val)
 					MMU_new.gxstat.write(16,adr,val);
 					break;
 					
-					//fog table: only write bottom 7 bits
-				case eng_3D_FOG_TABLE+0x00: case eng_3D_FOG_TABLE+0x02: case eng_3D_FOG_TABLE+0x04: case eng_3D_FOG_TABLE+0x06:
-				case eng_3D_FOG_TABLE+0x08: case eng_3D_FOG_TABLE+0x0A: case eng_3D_FOG_TABLE+0x0C: case eng_3D_FOG_TABLE+0x0E:
-				case eng_3D_FOG_TABLE+0x10: case eng_3D_FOG_TABLE+0x12: case eng_3D_FOG_TABLE+0x14: case eng_3D_FOG_TABLE+0x16:
-				case eng_3D_FOG_TABLE+0x18: case eng_3D_FOG_TABLE+0x1A: case eng_3D_FOG_TABLE+0x1C: case eng_3D_FOG_TABLE+0x1E:
-					val &= 0x7F7F;
-					break;
-					
 					// Alpha test reference value - Parameters:1
 				case eng_3D_ALPHA_TEST_REF:
 					HostWriteWord(MMU.ARM9_REG, 0x0340, val);
@@ -4278,13 +4325,18 @@ void FASTCALL _MMU_ARM9_write16(u32 adr, u16 val)
 					
 				case eng_3D_CLEAR_COLOR:
 				case eng_3D_CLEAR_COLOR+2:
-					T1WriteWord((u8*)&gfx3d.state.clearColor,adr-eng_3D_CLEAR_COLOR,val);
+					T1WriteWord((u8 *)&gfx3d.pendingState.clearColor, adr-eng_3D_CLEAR_COLOR, val);
 					break;
 					
 					// Clear background depth setup - Parameters:2
 				case eng_3D_CLEAR_DEPTH:
 					HostWriteWord(MMU.ARM9_REG, 0x0354, val);
-					gfx3d_glClearDepth(val);
+					gfx3d_glClearDepth<u16, 0>(val);
+					return;
+					
+				case eng_3D_CLRIMAGE_OFFSET:
+					HostWriteWord(MMU.ARM9_REG, 0x0356, val);
+					gfx3d_glClearImageOffset<u16, 0>(val);
 					return;
 					
 					// Fog Color - Parameters:4b
@@ -4477,16 +4529,23 @@ void FASTCALL _MMU_ARM9_write32(u32 adr, u32 val)
 			// lookups by the compiler
 			switch (adr >> 4)
 			{
-				case 0x400033:		//edge color table
-					((u32 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> 2] = val;
+				case 0x400033: // Edge Mark Color Table
+					((u32 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> (sizeof(u32) >> 1)] = val;
+					gfx3d_UpdateEdgeMarkColorTable<u32>((u8)(adr & 0x0000000F), val);
+					return;
+					
+				case 0x400036:
+				case 0x400037: // Fog Table
+					((u32 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> (sizeof(u32) >> 1)] = val & 0x7F7F7F7F;
+					gfx3d_UpdateFogTable<u32>((u8)(adr & 0x0000001F), val & 0x7F7F7F7F); // Drop the highest bit of each 8-bit value to limit the range to [0...127]
 					return;
 					
 				case 0x400038:
 				case 0x400039:
 				case 0x40003A:
-				case 0x40003B:		//toon table
-					((u32 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> 2] = val;
-					gfx3d_UpdateToonTable((adr & 0x3F) >> 1, val);
+				case 0x40003B: // Toon Table
+					((u32 *)(MMU.ARM9_REG))[(adr & 0xFFF) >> (sizeof(u32) >> 1)] = val;
+					gfx3d_UpdateToonTable<u32>((u8)(adr & 0x0000003F), val);
 					return;
 					
 				case 0x400040:
@@ -4790,12 +4849,6 @@ void FASTCALL _MMU_ARM9_write32(u32 adr, u32 val)
 					
 				case REG_POWCNT1: writereg_POWCNT1(32,adr,val); break;
 					
-					//fog table: only write bottom 7 bits
-				case eng_3D_FOG_TABLE+0x00: case eng_3D_FOG_TABLE+0x04: case eng_3D_FOG_TABLE+0x08: case eng_3D_FOG_TABLE+0x0C:
-				case eng_3D_FOG_TABLE+0x10: case eng_3D_FOG_TABLE+0x14: case eng_3D_FOG_TABLE+0x18: case eng_3D_FOG_TABLE+0x1C:
-					val &= 0x7F7F7F7F;
-					break;
-					
 					//ensata handshaking port?
 				case 0x04FFF010:
 					if(nds.ensataEmulation && nds.ensataHandshake == ENSATA_HANDSHAKE_ack && val == 0x13579bdf)
@@ -4828,13 +4881,14 @@ void FASTCALL _MMU_ARM9_write32(u32 adr, u32 val)
 					return;
 					
 				case eng_3D_CLEAR_COLOR:
-					T1WriteLong((u8*)&gfx3d.state.clearColor,0,val); 
+					T1WriteLong((u8 *)&gfx3d.pendingState.clearColor, 0, val);
 					break;
 					
 					// Clear background depth setup - Parameters:2
 				case eng_3D_CLEAR_DEPTH:
 					HostWriteLong(MMU.ARM9_REG, 0x0354, val);
-					gfx3d_glClearDepth(val);
+					gfx3d_glClearDepth<u16, 0>((u16)(val & 0x0000FFFF));
+					gfx3d_glClearImageOffset<u16, 0>((u16)(val >> 16));
 					return;
 					
 					// Fog Color - Parameters:4b

desmume/src/OGLRender.h
@@ -1,7 +1,7 @@
 /*
 	Copyright (C) 2006 yopyop
 	Copyright (C) 2006-2007 shash
-	Copyright (C) 2008-2022 DeSmuME team
+	Copyright (C) 2008-2023 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -604,8 +604,6 @@ struct OGLRenderRef
 };
 
 struct GFX3D_State;
-struct POLYLIST;
-struct INDEXLIST;
 struct POLY;
 class OpenGLRenderer;
 
@@ -876,13 +874,13 @@ protected:
 	virtual void _SetupGeometryShaders(const OGLGeometryFlags flags);
 	virtual Render3DError EnableVertexAttributes();
 	virtual Render3DError DisableVertexAttributes();
-	virtual Render3DError ZeroDstAlphaPass(const CPoly *clippedPolyList, const size_t clippedPolyCount, bool enableAlphaBlending, size_t indexOffset, POLYGON_ATTR lastPolyAttr);
+	virtual Render3DError ZeroDstAlphaPass(const CPoly *clippedPolyList, const size_t clippedPolyCount, const size_t clippedPolyOpaqueCount, bool enableAlphaBlending, size_t indexOffset, POLYGON_ATTR lastPolyAttr);
 	virtual void _ResolveWorkingBackFacing();
 	virtual void _ResolveGeometry();
 	virtual Render3DError ReadBackPixels();
 	
 	// Base rendering methods
-	virtual Render3DError BeginRender(const GFX3D &engine);
+	virtual Render3DError BeginRender(const GFX3D_State &renderState, const GFX3D_GeometryList &renderGList);
 	virtual Render3DError RenderGeometry();
 	virtual Render3DError PostprocessFramebuffer();
 	virtual Render3DError EndRender();
@@ -893,7 +891,7 @@ protected:
 	virtual void SetPolygonIndex(const size_t index);
 	virtual Render3DError SetupPolygon(const POLY &thePoly, bool treatAsTranslucent, bool willChangeStencilBuffer);
 	virtual Render3DError SetupTexture(const POLY &thePoly, size_t polyRenderIndex);
-	virtual Render3DError SetupViewport(const u32 viewportValue);
+	virtual Render3DError SetupViewport(const GFX3D_Viewport viewport);
 	
 	virtual Render3DError DrawShadowPolygon(const GLenum polyPrimitive, const GLsizei vertIndexCount, const GLushort *indexBufferPtr, const bool performDepthEqualTest, const bool enableAlphaDepthWrite, const bool isTranslucent, const u8 opaquePolyID);
 	
@@ -916,7 +914,7 @@ protected:
 	virtual Render3DError EnableVertexAttributes();
 	virtual Render3DError DisableVertexAttributes();
 	
-	virtual Render3DError BeginRender(const GFX3D &engine);
+	virtual Render3DError BeginRender(const GFX3D_State &renderState, const GFX3D_GeometryList &renderGList);
 	
 	virtual Render3DError SetupTexture(const POLY &thePoly, size_t polyRenderIndex);
 };

commit c9a4d9c4b8e754cf215ddc2b8e095db35941e755

SoftRasterizer: Fix compiling issue for non-SSE2 systems. (Regression from commit 5426509e.)

desmume/src/rasterize.cpp
@@ -619,7 +619,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel(const POLYGON_ATTR polyAttr, c
 	
 	// handle alpha test
 	if ( shaderOutput.a == 0 ||
-		(this->_softRender->currentRenderState->enableAlphaTest && shaderOutput.a < this->_softRender->currentRenderState->alphaTestRef) )
+	    (renderState.DISP3DCNT.EnableAlphaTest && (shaderOutput.a < renderState.alphaTestRef)) )
 	{
 		return;
 	}
@@ -895,7 +895,8 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel_SSE2(const POLYGON_ATTR polyAt
 	this->_shade<ISSHADOWPOLYGON>((PolygonMode)polyAttr.Mode, newDstColor32, shaderOutput, invu * w, invv * w);
 	
 	// handle alpha test
-	if ( shaderOutput.a == 0 || (renderState.DISP3DCNT.EnableAlphaTest && (shaderOutput.a < renderState.alphaTestRef)) )
+	if ( shaderOutput.a == 0 ||
+	    (renderState.DISP3DCNT.EnableAlphaTest && (shaderOutput.a < renderState.alphaTestRef)) )
 	{
 		return;
 	}