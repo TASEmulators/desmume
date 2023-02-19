TASEmulators/desmume

commit ab38d17feae672950fc83e08580df9ff0d579036

OpenGL Renderer: Fix rendering on very very very old GPUs, such as the ATi Mobility X1600 (circa 2006). - This change basically returns us to using 1D textures for color LUTs instead of using uniform arrays. 1D textures seem to be more compatible for most older hardware. - Unfortunately, while most older GPUs will work better with this change, this may break the OpenGL renderer on even older GPUs, such as the GeForce 7800 GT (circa 2005). - I'm estimating that more old GPUs benefit from this change than not, and so using 1D textures is what will stand. The vast majority of users will be using hardware newer than this, and so anyone who can't run OpenGL renderer in 2022 can just switch to SoftRasterizer.

desmume/src/OGLRender_3_2.cpp
@@ -1,7 +1,7 @@
 /*
 	Copyright (C) 2006 yopyop
 	Copyright (C) 2006-2007 shash
-	Copyright (C) 2008-2021 DeSmuME team
+	Copyright (C) 2008-2022 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -1187,7 +1187,7 @@ Render3DError OpenGLRenderer_3_2::CreateGeometryPrograms()
 	}
 	
 	glGenTextures(1, &OGLRef.texFogDensityTableID);
-	glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_FogDensityTable);
+	glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_LookupTable);
 	glBindTexture(GL_TEXTURE_1D, OGLRef.texFogDensityTableID);
 	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -1622,7 +1622,7 @@ Render3DError OpenGLRenderer_3_2::CreateFogProgram(const OGLFogProgramKey fogPro
 	const GLint uniformTexFogDensityTable = glGetUniformLocation(shaderID.program, "texFogDensityTable");
 	glUniform1i(uniformTexGDepth, OGLTextureUnitID_DepthStencil);
 	glUniform1i(uniformTexGFog, OGLTextureUnitID_FogAttr);
-	glUniform1i(uniformTexFogDensityTable, OGLTextureUnitID_FogDensityTable);
+	glUniform1i(uniformTexFogDensityTable, OGLTextureUnitID_LookupTable);
 	
 	if (!this->_isDualSourceBlendingSupported)
 	{
@@ -2111,7 +2111,7 @@ Render3DError OpenGLRenderer_3_2::BeginRender(const GFX3D &engine)
 			fogDensityTable[i] = (engine.renderState.fogDensityTable[i] == 127) ? 255 : engine.renderState.fogDensityTable[i] << 1;
 		}
 		
-		glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_FogDensityTable);
+		glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_LookupTable);
 		glTexSubImage1D(GL_TEXTURE_1D, 0, 0, 32, GL_RED, GL_UNSIGNED_BYTE, fogDensityTable);
 	}
 	

desmume/src/OGLRender.h
@@ -324,7 +324,7 @@ enum OGLTextureUnitID
 	OGLTextureUnitID_GPolyID,
 	OGLTextureUnitID_FogAttr,
 	OGLTextureUnitID_PolyStates,
-	OGLTextureUnitID_FogDensityTable
+	OGLTextureUnitID_LookupTable,
 };
 
 enum OGLBindingPointID
@@ -523,6 +523,8 @@ struct OGLRenderRef
 	GLuint texGDepthStencilID;
 	GLuint texFinalColorID;
 	GLuint texFogDensityTableID;
+	GLuint texToonTableID;
+	GLuint texEdgeColorTableID;
 	GLuint texMSGColorID;
 	GLuint texMSGWorkingID;
 	
@@ -564,11 +566,9 @@ struct OGLRenderRef
 	GLint uniformStateEnableFogAlphaOnly;
 	GLint uniformStateClearPolyID;
 	GLint uniformStateClearDepth;
-	GLint uniformStateEdgeColor;
 	GLint uniformStateFogColor;
 	
 	GLint uniformStateAlphaTestRef[256];
-	GLint uniformStateToonColor[256];
 	GLint uniformPolyTexScale[256];
 	GLint uniformPolyMode[256];
 	GLint uniformPolyIsWireframe[256];
@@ -721,6 +721,7 @@ protected:
 	bool _emulateSpecialZeroAlphaBlending;
 	bool _emulateNDSDepthCalculation;
 	bool _emulateDepthLEqualPolygonFacing;
+	bool _isDepthLEqualPolygonFacingSupported;
 	
 	FragmentColor *_mappedFramebuffer;
 	FragmentColor *_workingTextureUnpackBuffer;

commit 5457932d75a02b11569add9180d085fce0fc588b

OpenGL Renderer: Fix bug where the Depth L-Equal Polygon Facing option would fail to become enabled when running OpenGL 3.2 or later. (Regression from commit ab38d17. Note that this bug does affect the 0.9.13 release build.)

desmume/src/OGLRender_3_2.cpp
@@ -791,6 +791,7 @@ Render3DError OpenGLRenderer_3_2::InitExtensions()
 		INFO("OpenGL: Driver does not support at least 2x multisampled FBOs.\n");
 	}
 	
+	this->_isDepthLEqualPolygonFacingSupported = this->isShaderSupported && this->isVBOSupported && this->isFBOSupported;
 	this->_enableMultisampledRendering = ((this->_selectedMultisampleSize >= 2) && this->isMultisampledFBOSupported);
 	
 	this->InitFinalRenderStates(&oglExtensionSet); // This must be done last