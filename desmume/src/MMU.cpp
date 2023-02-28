TASEmulators/desmume

commit 97848fce8c53b1568e3c945bef0392866b273e89

GFX3D: Move some last bits of static data floating around in gfx3d.cpp into the GFX3D struct, and then make the instantiation of the struct itself static.

desmume/src/MMU.cpp
@@ -1829,28 +1829,7 @@ static void writereg_POWCNT1(const int size, const u32 adr, const u32 val)
 	bool isGeomEnabled = !!nds.power1.gfx3d_geometry;
 	if(wasGeomEnabled && !isGeomEnabled)
 	{
-		//kill the geometry data when the power goes off
-		//so, so bad. we need to model this with hardware-like operations instead of c++ code
-		
-		// TODO: Test which geometry data should be cleared on power off.
-		// The code below doesn't make any sense. You would think that only the data that
-		// is derived from geometry commands (either via GXFIFO or if writing to registers
-		// 0x04000440 - 0x040006A4) is what should be cleared. And that outside of geometry
-		// command data, other data (even if related to the 3D engine) shouldn't be touched.
-		// This will need further testing, but for now, we'll leave things as they are.
-		//    - 2023/01/22, rogerman
-		gfx3d.pendingState.DISP3DCNT.value = 0;
-		gfx3d.pendingState.DISP3DCNT.EnableTexMapping = 1;
-		gfx3d.pendingState.DISP3DCNT.PolygonShading = PolygonShadingMode_Toon;
-		gfx3d.pendingState.DISP3DCNT.EnableAlphaTest = 1;
-		gfx3d.pendingState.DISP3DCNT.EnableAlphaBlending = 1;
-		gfx3d.pendingState.SWAP_BUFFERS.value = 0;
-		gfx3d.pendingState.alphaTestRef = 0;
-		gfx3d.pendingState.clearDepth = DS_DEPTH15TO24(0x7FFF);
-		gfx3d.pendingState.clearColor = 0;
-		gfx3d.pendingState.fogColor = 0;
-		gfx3d.pendingState.fogOffset = 0;
-		gfx3d.pendingState.fogShift = 0;
+		GFX3D_HandleGeometryPowerOff();
 	}
 }
 
@@ -1986,7 +1965,7 @@ u32 TGXSTAT::read32()
 	if(gxFIFO.size==0) ret |= BIT(26); //fifo empty
 	//determine busy flag.
 	//if we're waiting for a flush, we're busy
-	if(isSwapBuffers) ret |= BIT(27);
+	if(GFX3D_IsSwapBuffersPending()) ret |= BIT(27);
 	//if fifo is nonempty, we're busy
 	if(gxFIFO.size!=0) ret |= BIT(27);
 
@@ -1995,7 +1974,7 @@ u32 TGXSTAT::read32()
 	
 	ret |= ((gxfifo_irq & 0x3) << 30); //user's irq flags
 
-	//printf("vc=%03d Returning gxstat read: %08X (isSwapBuffers=%d)\n",nds.VCount,ret,isSwapBuffers);
+	//printf("vc=%03d Returning gxstat read: %08X (isSwapBuffers=%d)\n", nds.VCount, ret, (GFX3D_IsSwapBuffersPending()) ? 1 : 0);
 
 	//ret = (2 << 8);
 	//INFO("gxSTAT 0x%08X (proj %i, pos %i)\n", ret, _hack_getMatrixStackLevel(1), _hack_getMatrixStackLevel(2));
@@ -3685,10 +3664,25 @@ void FASTCALL _MMU_ARM9_write08(u32 adr, u8 val)
 				case REG_IF+2: REG_IF_WriteByte<ARMCPU_ARM9>(2,val); break;
 				case REG_IF+3: REG_IF_WriteByte<ARMCPU_ARM9>(3,val); break;
 					
-				case eng_3D_CLEAR_COLOR+0: case eng_3D_CLEAR_COLOR+1:
-				case eng_3D_CLEAR_COLOR+2: case eng_3D_CLEAR_COLOR+3:
-					T1WriteByte((u8*)&gfx3d.pendingState.clearColor, adr-eng_3D_CLEAR_COLOR, val);
-					break;
+				case eng_3D_CLEAR_COLOR:
+					HostWriteByte(MMU.ARM9_REG, 0x0350, val);
+					gfx3d_glClearColor<u8>(0, val);
+					return;
+					
+				case eng_3D_CLEAR_COLOR+1:
+					HostWriteByte(MMU.ARM9_REG, 0x0351, val);
+					gfx3d_glClearColor<u8>(1, val);
+					return;
+					
+				case eng_3D_CLEAR_COLOR+2:
+					HostWriteByte(MMU.ARM9_REG, 0x0352, val);
+					gfx3d_glClearColor<u8>(2, val);
+					return;
+					
+				case eng_3D_CLEAR_COLOR+3:
+					HostWriteByte(MMU.ARM9_REG, 0x0353, val);
+					gfx3d_glClearColor<u8>(3, val);
+					return;
 					
 				case eng_3D_CLEAR_DEPTH:
 					HostWriteByte(MMU.ARM9_REG, 0x0354, val);
@@ -4323,9 +4317,14 @@ void FASTCALL _MMU_ARM9_write16(u32 adr, u16 val)
 					return;
 					
 				case eng_3D_CLEAR_COLOR:
+					T1WriteWord(MMU.ARM9_REG, 0x0350, val);
+					gfx3d_glClearColor<u16>(0, val);
+					return;
+					
 				case eng_3D_CLEAR_COLOR+2:
-					T1WriteWord((u8 *)&gfx3d.pendingState.clearColor, adr-eng_3D_CLEAR_COLOR, val);
-					break;
+					T1WriteWord(MMU.ARM9_REG, 0x0352, val);
+					gfx3d_glClearColor<u16>(2, val);
+					return;
 					
 					// Clear background depth setup - Parameters:2
 				case eng_3D_CLEAR_DEPTH:
@@ -4880,8 +4879,9 @@ void FASTCALL _MMU_ARM9_write32(u32 adr, u32 val)
 					return;
 					
 				case eng_3D_CLEAR_COLOR:
-					T1WriteLong((u8 *)&gfx3d.pendingState.clearColor, 0, val);
-					break;
+					T1WriteLong(MMU.ARM9_REG, 0x0350, val);
+					gfx3d_glClearColor<u32>(0, val);
+					return;
 					
 					// Clear background depth setup - Parameters:2
 				case eng_3D_CLEAR_DEPTH:

desmume/src/GPU.cpp
@@ -2,7 +2,7 @@
 	Copyright (C) 2006 yopyop
 	Copyright (C) 2006-2007 Theo Berkau
 	Copyright (C) 2007 shash
-	Copyright (C) 2008-2021 DeSmuME team
+	Copyright (C) 2008-2023 DeSmuME team
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -4728,8 +4728,8 @@ void GPUSubsystem::_UpdateFPSRender3D()
 	this->_videoFrameIndex++;
 	if (this->_videoFrameIndex == 60)
 	{
-		this->_render3DFrameCount = gfx3d.render3DFrameCount;
-		gfx3d.render3DFrameCount = 0;
+		this->_render3DFrameCount = GFX3D_GetRender3DFrameCount();
+		GFX3D_ResetRender3DFrameCount();
 		this->_videoFrameIndex = 0;
 	}
 }

desmume/src/gfx3d.h
@@ -787,7 +787,7 @@ struct GFX3D_GeometryList
 };
 typedef struct GFX3D_GeometryList GFX3D_GeometryList;
 
-struct LegacyGFX3DStateSFormat
+struct GFX3D_State_LegacySave
 {
 	u32 enableTexturing;
 	u32 enableAlphaTest;
@@ -816,7 +816,7 @@ struct LegacyGFX3DStateSFormat
 	u32 activeFlushCommand;
 	u32 pendingFlushCommand;
 };
-typedef struct LegacyGFX3D_StateSFormat LegacyGFX3D_StateSFormat;
+typedef struct GFX3D_State_LegacySave GFX3D_State_LegacySave;
 
 struct GeometryEngineLegacySave
 {
@@ -877,9 +877,13 @@ typedef struct GeometryEngineLegacySave GeometryEngineLegacySave;
 
 struct GFX3D_LegacySave
 {
-	u32 clCommand; // Exists purely for save state compatibility, historically went unused.
-	u32 clIndex; // Exists purely for save state compatibility, historically went unused.
-	u32 clIndex2; // Exists purely for save state compatibility, historically went unused.
+	GFX3D_State_LegacySave statePending;
+	GFX3D_State_LegacySave stateApplied;
+	
+	u32 clCommand; // Exists purely for save state compatibility, historically went unused since 09/20/2009.
+	u32 clIndex; // Exists purely for save state compatibility, historically went unused since 09/20/2009.
+	u32 clIndex2; // Exists purely for save state compatibility, historically went unused since 09/20/2009.
+	u32 isSwapBuffersPending;
 	u32 isDrawPending;
 	
 	IOREG_VIEWPORT rawPolyViewport[POLYLIST_SIZE]; // Historically, pending polygons kept a copy of the current viewport as a raw register value.
@@ -904,6 +908,11 @@ struct GFX3D
 	
 	u8 pendingListIndex;
 	u8 appliedListIndex;
+	bool isSwapBuffersPending;
+	bool isDrawPending;
+
+	POLYGON_ATTR regPolyAttrPending;
+	POLYGON_ATTR regPolyAttrApplied;
 	u32 render3DFrameCount; // Increments when gfx3d_doFlush() is called. Resets every 60 video frames.
 	
 	// Working lists for rendering.
@@ -913,13 +922,11 @@ struct GFX3D
 	CACHE_ALIGN float rawPolySortYMax[POLYLIST_SIZE]; // Temp buffer used for processing polygon Y-sorting
 	
 	// Everything below is for save state compatibility.
-	GeometryEngineLegacySave gEngineLegacySave;
 	GFX3D_LegacySave legacySave;
+	GeometryEngineLegacySave gEngineLegacySave;
 	PAGE_ALIGN FragmentColor framebufferNativeSave[GPU_FRAMEBUFFER_NATIVE_WIDTH * GPU_FRAMEBUFFER_NATIVE_HEIGHT]; // Rendered 3D framebuffer that is saved in RGBA8888 color format at the native size.
 };
 typedef struct GFX3D GFX3D;
-
-extern GFX3D gfx3d;
 
 class NDSGeometryEngine
 {
@@ -1110,13 +1117,12 @@ public:
 
 extern CACHE_ALIGN u32 dsDepthExtend_15bit_to_24bit[32768];
 
-extern u32 isSwapBuffers;
-
 void gfx3d_glFlush(const u32 v);
 // end GE commands
 
 void gfx3d_glFogColor(const u32 v);
 void gfx3d_glFogOffset(const u32 v);
+template<typename T> void gfx3d_glClearColor(const u8 offset, const T v);
 template<typename T, size_t ADDROFFSET> void gfx3d_glClearDepth(const T v);
 template<typename T, size_t ADDROFFSET> void gfx3d_glClearImageOffset(const T v);
 void gfx3d_glSwapScreen(u32 screen);
@@ -1156,6 +1162,13 @@ void ParseReg_DISP3DCNT();
 
 u8 GFX3D_GetMatrixStackIndex(const MatrixMode whichMatrix);
 void GFX3D_ResetMatrixStackPointer();
+
+bool GFX3D_IsSwapBuffersPending();
+
+void GFX3D_HandleGeometryPowerOff();
+
+u32 GFX3D_GetRender3DFrameCount();
+void GFX3D_ResetRender3DFrameCount();
 
 template<ClipperMode CLIPPERMODE> PolygonType GFX3D_GenerateClippedPoly(const u16 rawPolyIndex, const PolygonType rawPolyType, const VERT *(&rawVtx)[4], CPoly &outCPoly);
 template<ClipperMode CLIPPERMODE> PolygonType GFX3D_GenerateClippedPoly(const u16 rawPolyIndex, const PolygonType rawPolyType, const NDSVertex *(&rawVtx)[4], CPoly &outCPoly);

commit 90c8411cbfb8f6cae50e181e8c6b52f6b147c443

GFX3D: Fix clear color on big-endian systems. (Regression from commit 97848fc.)

desmume/src/MMU.cpp
@@ -3665,22 +3665,22 @@ void FASTCALL _MMU_ARM9_write08(u32 adr, u8 val)
 				case REG_IF+3: REG_IF_WriteByte<ARMCPU_ARM9>(3,val); break;
 					
 				case eng_3D_CLEAR_COLOR:
-					HostWriteByte(MMU.ARM9_REG, 0x0350, val);
+					T1WriteByte(MMU.ARM9_REG, 0x0350, val);
 					gfx3d_glClearColor<u8>(0, val);
 					return;
 					
 				case eng_3D_CLEAR_COLOR+1:
-					HostWriteByte(MMU.ARM9_REG, 0x0351, val);
+					T1WriteByte(MMU.ARM9_REG, 0x0351, val);
 					gfx3d_glClearColor<u8>(1, val);
 					return;
 					
 				case eng_3D_CLEAR_COLOR+2:
-					HostWriteByte(MMU.ARM9_REG, 0x0352, val);
+					T1WriteByte(MMU.ARM9_REG, 0x0352, val);
 					gfx3d_glClearColor<u8>(2, val);
 					return;
 					
 				case eng_3D_CLEAR_COLOR+3:
-					HostWriteByte(MMU.ARM9_REG, 0x0353, val);
+					T1WriteByte(MMU.ARM9_REG, 0x0353, val);
 					gfx3d_glClearColor<u8>(3, val);
 					return;
 					

desmume/src/gfx3d.cpp
@@ -2809,7 +2809,27 @@ void gfx3d_glFogOffset(const u32 v)
 template <typename T>
 void gfx3d_glClearColor(const u8 offset, const T v)
 {
+#ifndef MSB_FIRST
 	((T *)&gfx3d.pendingState.clearColor)[offset >> (sizeof(T) >> 1)] = v;
+#else
+	switch (sizeof(T))
+	{
+		case 1:
+			((T *)&gfx3d.pendingState.clearColor)[offset >> (sizeof(T) >> 1)] = v;
+			break;
+			
+		case 2:
+			((T *)&gfx3d.pendingState.clearColor)[offset >> (sizeof(T) >> 1)] = LE_TO_LOCAL_16(v);
+			break;
+			
+		case 4:
+			((T *)&gfx3d.pendingState.clearColor)[offset >> (sizeof(T) >> 1)] = LE_TO_LOCAL_32(v);
+			break;
+			
+		default:
+			break;
+	}
+#endif
 }
 
 template void gfx3d_glClearColor< u8>(const u8 offset, const  u8 v);