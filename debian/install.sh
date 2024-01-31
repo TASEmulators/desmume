3ab7355ddb03e8b595e8511091abe3cfc9897801
Kimpe Andy <andykimpe@gmail.com>
2024-01-31 13:14:12 +0100

    Update install.sh

--- debian/install.sh
+++ debian/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 source=$1
-pushd desmume/src/frontend/posix
-DESTDIR=$source/debian/desmume make install
+prefix=$2
+destdir=$3
+pushd $source/desmume/src/frontend/posix
+DESTDIR=$destdir make install
 popd
-mv $source/debian/desmume/usr/games/share/pixmaps/DeSmuME.xpm \
-$source/debian/desmume/usr/share/pixmaps/
-rm -rf $source/debian/desmume/usr/share/games/pixmaps
-mv $source/debian/desmume/usr/games/share/applications/desmume*.desktop \
-$source/debian/desmume/usr/share/applications/
-rm -rf $source/debian/desmume/usr/games/share/applications
+mv $destdir/$prefix/games/share/pixmaps/DeSmuME.xpm \
+$destdir/$prefix/share/pixmaps/
+rm -rf $destdir/$prefix/share/games/pixmaps
+mv $destdir/$prefix/games/share/applications/desmume*.desktop \
+$destdir/$prefix/share/applications/
+rm -rf $destdir/$prefix/games/share/applications
