bd6a421c0cd8c9194218227922377f0f64251b4f
rofl0r <rofl0r@users.noreply.github.com>
2025-06-24 12:16:52 +0000

    agg: don't leak non-const reference to const private member.
    patch taken from NetBSD.

--- desmume/src/frontend/windows/agg/include/agg_renderer_outline_aa.h
+++ desmume/src/frontend/windows/agg/include/agg_renderer_outline_aa.h
@@ -1375,7 +1375,6 @@ namespace agg
         //---------------------------------------------------------------------
         void profile(const line_profile_aa& prof) { m_profile = &prof; }
         const line_profile_aa& profile() const { return *m_profile; }
-        line_profile_aa& profile() { return *m_profile; }
 
         //---------------------------------------------------------------------
         int subpixel_width() const { return m_profile->subpixel_width(); }
