TASEmulators/desmume

commit 38f63130e203e6b278ff649191efe436ad676204

Use explicit std:: prefix
Using `using std::*` made me wonder whether these types and functions
were from std, libc symbols, or custom ones.

desmume/src/rasterize.cpp
@@ -68,16 +68,6 @@
 #include "filter/filter.h"
 #include "filter/xbrz.h"
 
-
-using std::min;
-using std::max;
-using std::swap;
-
-template<typename T> T _min(T a, T b, T c) { return min(min(a,b),c); }
-template<typename T> T _max(T a, T b, T c) { return max(max(a,b),c); }
-template<typename T> T _min(T a, T b, T c, T d) { return min(_min(a,b,d),c); }
-template<typename T> T _max(T a, T b, T c, T d) { return max(_max(a,b,d),c); }
-
 static u8 modulate_table[64][64];
 static u8 decal_table[32][64][64];
 
@@ -501,7 +491,7 @@ static FORCEINLINE void alphaBlend(const bool isAlphaBlendingEnabled, const Colo
 		outDst.r = (alpha*inSrc.r + invAlpha*outDst.r) >> 5;
 		outDst.g = (alpha*inSrc.g + invAlpha*outDst.g) >> 5;
 		outDst.b = (alpha*inSrc.b + invAlpha*outDst.b) >> 5;
-		outDst.a = max(inSrc.a, outDst.a);
+		outDst.a = std::max(inSrc.a, outDst.a);
 	}
 }
 
@@ -518,7 +508,7 @@ static FORCEINLINE void EdgeBlend(Color4u8 &dst, const Color4u8 src)
 		dst.r = (alpha*src.r + invAlpha*dst.r) >> 5;
 		dst.g = (alpha*src.g + invAlpha*dst.g) >> 5;
 		dst.b = (alpha*src.b + invAlpha*dst.b) >> 5;
-		dst.a = max(src.a, dst.a);
+		dst.a = std::max(src.a, dst.a);
 	}
 }
 
@@ -635,9 +625,9 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_shade(const PolygonMode polygonMode,
 				outColor.b = modulate_table[mainTexColor.b][vtxColor.r];
 				outColor.a = modulate_table[GFX3D_5TO6_LOOKUP(mainTexColor.a)][GFX3D_5TO6_LOOKUP(vtxColor.a)] >> 1;
 				
-				outColor.r = min<u8>(0x3F, (outColor.r + toonColor.r));
-				outColor.g = min<u8>(0x3F, (outColor.g + toonColor.g));
-				outColor.b = min<u8>(0x3F, (outColor.b + toonColor.b));
+				outColor.r = std::min<u8>(0x3F, (outColor.r + toonColor.r));
+				outColor.g = std::min<u8>(0x3F, (outColor.g + toonColor.g));
+				outColor.b = std::min<u8>(0x3F, (outColor.b + toonColor.b));
 			}
 			else
 			{
@@ -677,8 +667,8 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_pixel(const POLYGON_ATTR polyAttr, c
 		// The EQUAL depth test is used if the polygon requests it. Note that the NDS doesn't perform
 		// a true EQUAL test -- there is a set tolerance to it that makes it easier for pixels to
 		// pass the depth test.
-		const u32 minDepth = (u32)max<s32>(0x00000000, (s32)dstAttributeDepth - DEPTH_EQUALS_TEST_TOLERANCE);
-		const u32 maxDepth = min<u32>(0x00FFFFFF, dstAttributeDepth + DEPTH_EQUALS_TEST_TOLERANCE);
+		const u32 minDepth = (u32)std::max<s32>(0x00000000, (s32)dstAttributeDepth - DEPTH_EQUALS_TEST_TOLERANCE);
+		const u32 maxDepth = std::min<u32>(0x00FFFFFF, dstAttributeDepth + DEPTH_EQUALS_TEST_TOLERANCE);
 		
 		if (depth < minDepth || depth > maxDepth)
 		{
@@ -816,7 +806,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_drawscanline(const POLYGON_ATTR poly
 				rightWidth++;
 			}
 			
-			rasterWidth = max(1, max(abs(leftWidth), abs(rightWidth)));
+			rasterWidth = std::max({1, abs(leftWidth), abs(rightWidth)});
 		}
 		else
 		{
@@ -983,9 +973,9 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_drawscanline(const POLYGON_ATTR poly
 		
 		if (this->_softRender->_enableFragmentSamplingHack)
 		{
-			vtxColor.r = max<u8>( 0x00, (u8)min<float>(63.0f, vtxColorFloatInterpolated.r / invWFloatInterpolated) );
-			vtxColor.g = max<u8>( 0x00, (u8)min<float>(63.0f, vtxColorFloatInterpolated.g / invWFloatInterpolated) );
-			vtxColor.b = max<u8>( 0x00, (u8)min<float>(63.0f, vtxColorFloatInterpolated.b / invWFloatInterpolated) );
+			vtxColor.r = std::max<u8>( 0x00, (u8)std::min<float>(63.0f, vtxColorFloatInterpolated.r / invWFloatInterpolated) );
+			vtxColor.g = std::max<u8>( 0x00, (u8)std::min<float>(63.0f, vtxColorFloatInterpolated.g / invWFloatInterpolated) );
+			vtxColor.b = std::max<u8>( 0x00, (u8)std::min<float>(63.0f, vtxColorFloatInterpolated.b / invWFloatInterpolated) );
 			vtxColor.a = vtxColorInterpolated.a;
 			
 			texCoord.s = (s32)(texCoordFloatInterpolated.s * (float)texScalingFactor / invWFloatInterpolated);
@@ -993,9 +983,9 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_drawscanline(const POLYGON_ATTR poly
 		}
 		else
 		{
-			vtxColor.r = max<u8>( 0x00, (u8)min<s64>(0x3F, vtxColorInterpolated.r / invWInterpolated) );
-			vtxColor.g = max<u8>( 0x00, (u8)min<s64>(0x3F, vtxColorInterpolated.g / invWInterpolated) );
-			vtxColor.b = max<u8>( 0x00, (u8)min<s64>(0x3F, vtxColorInterpolated.b / invWInterpolated) );
+			vtxColor.r = std::max<u8>( 0x00, (u8)std::min<s64>(0x3F, vtxColorInterpolated.r / invWInterpolated) );
+			vtxColor.g = std::max<u8>( 0x00, (u8)std::min<s64>(0x3F, vtxColorInterpolated.g / invWInterpolated) );
+			vtxColor.b = std::max<u8>( 0x00, (u8)std::min<s64>(0x3F, vtxColorInterpolated.b / invWInterpolated) );
 			vtxColor.a = vtxColorInterpolated.a;
 			
 			texCoord.s = (s32)(texCoordInterpolated.s * texScalingFactor / invWInterpolated);
@@ -1042,7 +1032,7 @@ void RasterizerUnit<RENDERER>::_runscanlines(const POLYGON_ATTR polyAttr, const 
 	//oh lord, hack city for edge drawing
 
 	//do not overstep either of the edges
-	s32 rasterHeight = min(left.height, right.height);
+	s32 rasterHeight = std::min(left.height, right.height);
 	bool first = true;
 
 	//HACK: special handling for horizontal line poly
@@ -1082,8 +1072,8 @@ void RasterizerUnit<RENDERER>::_runscanlines(const POLYGON_ATTR polyAttr, const 
 					const size_t nxr = right.x;
 					if (top)
 					{
-						const size_t xs = min(xl, xr);
-						const size_t xe = max(xl, xr);
+						const size_t xs = std::min(xl, xr);
+						const size_t xe = std::max(xl, xr);
 						for (size_t x = xs; x <= xe; x++)
 						{
 							const size_t adr = (y * framebufferWidth) + x;
@@ -1094,8 +1084,8 @@ void RasterizerUnit<RENDERER>::_runscanlines(const POLYGON_ATTR polyAttr, const 
 					}
 					else if (bottom)
 					{
-						const size_t xs = min(xl, xr);
-						const size_t xe = max(xl, xr);
+						const size_t xs = std::min(xl, xr);
+						const size_t xe = std::max(xl, xr);
 						for (size_t x = xs; x <= xe; x++)
 						{
 							const size_t adr = (y * framebufferWidth) + x;
@@ -1106,8 +1096,8 @@ void RasterizerUnit<RENDERER>::_runscanlines(const POLYGON_ATTR polyAttr, const 
 					}
 					else
 					{
-						size_t xs = min(xl, nxl);
-						size_t xe = max(xl, nxl);
+						size_t xs = std::min(xl, nxl);
+						size_t xe = std::max(xl, nxl);
 						for (size_t x = xs; x <= xe; x++)
 						{
 							const size_t adr = (y * framebufferWidth) + x;
@@ -1116,8 +1106,8 @@ void RasterizerUnit<RENDERER>::_runscanlines(const POLYGON_ATTR polyAttr, const 
 							dstColor[adr].b = 0;
 						}
 						
-						xs = min(xr, nxr);
-						xe = max(xr, nxr);
+						xs = std::min(xr, nxr);
+						xe = std::max(xr, nxr);
 						for (size_t x = xs; x <= xe; x++)
 						{
 							const size_t adr = (y * framebufferWidth) + x;
@@ -1139,12 +1129,12 @@ void RasterizerUnit<RENDERER>::_runscanlines(const POLYGON_ATTR polyAttr, const 
 template<bool RENDERER> template<int TYPE>
 FORCEINLINE void RasterizerUnit<RENDERER>::_rot_verts()
 {
-#define ROTSWAP(X) if(TYPE>X) swap(this->_currentVtx[X-1],this->_currentVtx[X]);
+#define ROTSWAP(X) if(TYPE>X) std::swap(this->_currentVtx[X-1],this->_currentVtx[X]);
 	ROTSWAP(1); ROTSWAP(2); ROTSWAP(3); ROTSWAP(4);
 	ROTSWAP(5); ROTSWAP(6); ROTSWAP(7); ROTSWAP(8); ROTSWAP(9);
 #undef ROTSWAP
 	
-#define ROTSWAP(X) if(TYPE>X) swap(this->_currentPrecalc[X-1],this->_currentPrecalc[X]);
+#define ROTSWAP(X) if(TYPE>X) std::swap(this->_currentPrecalc[X-1],this->_currentPrecalc[X]);
 	ROTSWAP(1); ROTSWAP(2); ROTSWAP(3); ROTSWAP(4);
 	ROTSWAP(5); ROTSWAP(6); ROTSWAP(7); ROTSWAP(8); ROTSWAP(9);
 #undef ROTSWAP
@@ -1165,8 +1155,8 @@ void RasterizerUnit<RENDERER>::_sort_verts()
 	{
 		for (size_t i = 0; i < TYPE/2; i++)
 		{
-			swap(this->_currentVtx[i], this->_currentVtx[TYPE-i-1]);
-			swap(this->_currentPrecalc[i], this->_currentPrecalc[TYPE-i-1]);
+			std::swap(this->_currentVtx[i], this->_currentVtx[TYPE-i-1]);
+			std::swap(this->_currentPrecalc[i], this->_currentPrecalc[TYPE-i-1]);
 		}
 	}
 
@@ -2112,13 +2102,13 @@ void SoftRasterizerRenderer::_UpdateEdgeMarkColorTable(const u16 *edgeMarkColorT
 
 void SoftRasterizerRenderer::_UpdateFogTable(const u8 *fogDensityTable)
 {
-	const s32 fogOffset = min<s32>( max<s32>((s32)this->currentRenderState->fogOffset, 0), 32768 );
+	const s32 fogOffset = std::min<s32>( std::max<s32>((s32)this->currentRenderState->fogOffset, 0), 32768 );
 	const s32 fogStep = 0x400 >> this->currentRenderState->fogShift;
 	
 	if (fogStep <= 0)
 	{
-		const s32 iMin = min<s32>( max<s32>(fogOffset, 0), 32768);
-		const s32 iMax = min<s32>( max<s32>(fogOffset + 1, 0), 32768);
+		const s32 iMin = std::min<s32>( std::max<s32>(fogOffset, 0), 32768);
+		const s32 iMax = std::min<s32>( std::max<s32>(fogOffset + 1, 0), 32768);
 		
 		// If the fog factor is 127, then treat it as 128.
 		u8 fogWeight = (fogDensityTable[0] >= 127) ? 128 : fogDensityTable[0];
@@ -2131,8 +2121,8 @@ void SoftRasterizerRenderer::_UpdateFogTable(const u8 *fogDensityTable)
 	}
 	
 	const s32 fogShiftInv = 10 - this->currentRenderState->fogShift;
-	const s32 iMin = min<s32>( max<s32>((( 1 + 1) << fogShiftInv) + fogOffset + 1 - fogStep, 0), 32768);
-	const s32 iMax = min<s32>( max<s32>(((32 + 1) << fogShiftInv) + fogOffset + 1 - fogStep, 0), 32768);
+	const s32 iMin = std::min<s32>( std::max<s32>((( 1 + 1) << fogShiftInv) + fogOffset + 1 - fogStep, 0), 32768);
+	const s32 iMax = std::min<s32>( std::max<s32>(((32 + 1) << fogShiftInv) + fogOffset + 1 - fogStep, 0), 32768);
 	assert(iMin <= iMax);
 	
 	// If the fog factor is 127, then treat it as 128.

desmume/src/NDSSystem.cpp
@@ -104,9 +104,6 @@ CFIRMWARE *extFirmwareObj = NULL;
 std::vector<u32> memReadBreakPoints;
 std::vector<u32> memWriteBreakPoints;
 
-using std::min;
-using std::max;
-
 bool singleStep;
 bool nds_debug_continuing[2];
 int lagframecounter;
@@ -1657,7 +1654,7 @@ FORCEINLINE u64 _fast_min(u64 a, u64 b)
 	//you might find that this is faster on a 64bit system; someone should try it
 	//http://aggregate.org/MAGIC/#Integer%20Selection
 	//u64 ret = (((((s64)(a-b)) >> (64-1)) & (a^b)) ^ b);
-	//assert(ret==min(a,b));
+	//assert(ret==std::min(a,b));
 	//return ret;	
 	
 	//but this ends up being the fastest on 32bits
@@ -1899,7 +1896,7 @@ static FORCEINLINE s32 minarmtime(s32 arm9, s32 arm7)
 {
 	if(doarm9)
 		if(doarm7)
-			return min(arm9,arm7);
+			return std::min(arm9,arm7);
 		else
 			return arm9;
 	else
@@ -1963,7 +1960,7 @@ static /*donotinline*/ std::pair<s32,s32> armInnerLoop(
 			else
 			{
 				s32 temp = arm9;
-				arm9 = min(s32next, arm9 + kIrqWait);
+				arm9 = std::min(s32next, arm9 + kIrqWait);
 				nds.idleCycles[0] += arm9-temp;
 				if (gxFIFO.size < 255) nds.freezeBus &= ~1;
 			}
@@ -2004,7 +2001,7 @@ static /*donotinline*/ std::pair<s32,s32> armInnerLoop(
 			else
 			{
 				s32 temp = arm7;
-				arm7 = min(s32next, arm7 + kIrqWait);
+				arm7 = std::min(s32next, arm7 + kIrqWait);
 				nds.idleCycles[1] += arm7-temp;
 				if(arm7 == s32next)
 				{
@@ -2124,7 +2121,7 @@ void NDS_exec(s32 nb)
 
 			//find next work unit:
 			u64 next = sequencer.findNext();
-			next = min(next,nds_timer+kMaxWork); //lets set an upper limit for now
+			next = std::min(next,nds_timer+kMaxWork); //lets set an upper limit for now
 
 			//printf("%d\n",(next-nds_timer));
 
@@ -2853,14 +2850,14 @@ u16 NDS_getADCTouchPosX(int scrX_lsl4)
 {
 	scrX_lsl4 >>= 4;
 	int rv = ((scrX_lsl4 - TSCal.scr.x1 + 1) * TSCal.adc.width) / TSCal.scr.width + TSCal.adc.x1;
-	rv = min(0xFFF, max(0, rv));
+	rv = std::min(0xFFF, std::max(0, rv));
 	return (u16)(rv);
 }
 u16 NDS_getADCTouchPosY(int scrY_lsl4)
 {
 	scrY_lsl4 >>= 4;
 	int rv = ((scrY_lsl4 - TSCal.scr.y1 + 1) * TSCal.adc.height) / TSCal.scr.height + TSCal.adc.y1;
-	rv = min(0xFFF, max(0, rv));
+	rv = std::min(0xFFF, std::max(0, rv));
 	return (u16)(rv);
 }
 

desmume/src/frontend/posix/gtk/config.cpp
@@ -21,9 +21,6 @@
 
 #include "config.h"
 
-using std::string;
-using std::vector;
-
 namespace desmume {
 namespace config {
 
@@ -81,10 +78,10 @@ void value<float>::save() {
 	g_key_file_set_double(this->mKeyFile, this->mSection.c_str(), this->mKey.c_str(), this->mData);
 }
 
-/* class value<string> */
+/* class value<std::string> */
 
 template<>
-void value<string>::load() {
+void value<std::string>::load() {
 	char* val = g_key_file_get_string(this->mKeyFile, this->mSection.c_str(), this->mKey.c_str(), NULL);
 	if (val != NULL) {
 		this->mData = val;
@@ -93,14 +90,14 @@ void value<string>::load() {
 }
 
 template<>
-void value<string>::save() {
+void value<std::string>::save() {
 	g_key_file_set_string(this->mKeyFile, this->mSection.c_str(), this->mKey.c_str(), this->mData.c_str());
 }
 
-/* class value<vector<int> > */
+/* class value<std::vector<int>> */
 
 template<>
-void value<vector<int> >::load() {
+void value<std::vector<int>>::load() {
 	gsize l;
 	int* val = g_key_file_get_integer_list(this->mKeyFile, this->mSection.c_str(), this->mKey.c_str(), &l, NULL);
 	if(val)
@@ -112,7 +109,7 @@ void value<vector<int> >::load() {
 }
 
 template<>
-void value<vector<int> >::save() {
+void value<std::vector<int>>::save() {
 	g_key_file_set_integer_list(this->mKeyFile, this->mSection.c_str(), this->mKey.c_str(), this->mData.data(), this->mData.size());
 }
 

desmume/src/gfx3d.cpp
@@ -256,9 +256,6 @@ public:
 #define GFX_DELAY(x) NDS_RescheduleGXFIFO(1);
 #define GFX_DELAY_M2(x) NDS_RescheduleGXFIFO(1);
 
-using std::max;
-using std::min;
-
 Viewer3D_State viewer3D;
 
 static GFX3D_IOREG *_GFX3D_IORegisterMap = NULL;
@@ -3314,7 +3311,7 @@ size_t gfx3d_PerformClipping(const GFX3D_GeometryList &gList, CPoly *outCPolyUns
 			// - Divide by w = 20.43 --> 20.31
 			// - Divide by 2 = 20.31 --> 20.30
 			// - Keep the sign bit = 20.30 --> 20.31
-			vtx64.z = max<s64>(0x0000000000000000LL, min<s64>(0x000000007FFFFFFFLL, vtx64.z));
+			vtx64.z = std::max<s64>(0x0000000000000000LL, std::min<s64>(0x000000007FFFFFFFLL, vtx64.z));
 			
 			// At the very least, we need to save the transformed position so that
 			// we can use it to calculate the polygon facing later.
@@ -3388,8 +3385,8 @@ void GFX3D_GenerateRenderLists(const ClipperMode clippingMode, const GFX3D_State
 	}
 
 #ifdef _SHOW_VTX_COUNTERS
-	max_polys = max((u32)outGList.rawPolyCount, max_polys);
-	max_verts = max((u32)outGList.rawVertCount, max_verts);
+	max_polys = std::max((u32)outGList.rawPolyCount, max_polys);
+	max_verts = std::max((u32)outGList.rawVertCount, max_verts);
 	osd->addFixed(180, 20, "%i/%i", outGList.rawPolyCount, outGList.rawVertCount);		// current
 	osd->addFixed(180, 35, "%i/%i", max_polys, max_verts);		// max
 #endif
@@ -3455,8 +3452,8 @@ void GFX3D_GenerateRenderLists(const ClipperMode clippingMode, const GFX3D_State
 			
 			y = 4096LL - y;
 			
-			gfx3d.rawPolySortYMin[rawPolyIndex] = min<s64>(gfx3d.rawPolySortYMin[rawPolyIndex], y);
-			gfx3d.rawPolySortYMax[rawPolyIndex] = max<s64>(gfx3d.rawPolySortYMax[rawPolyIndex], y);
+			gfx3d.rawPolySortYMin[rawPolyIndex] = std::min<s64>(gfx3d.rawPolySortYMin[rawPolyIndex], y);
+			gfx3d.rawPolySortYMax[rawPolyIndex] = std::max<s64>(gfx3d.rawPolySortYMax[rawPolyIndex], y);
 		}
 	}
 

desmume/src/texcache.cpp
@@ -40,9 +40,6 @@
 #include "./utils/colorspacehandler/colorspacehandler_AltiVec.h"
 #endif
 
-using std::min;
-using std::max;
-
 //only dump this from ogl renderer. for now, softrasterizer creates things in an incompatible pixel format
 //#define DEBUG_DUMP_TEXTURE
 
@@ -78,11 +75,11 @@ public:
 	int memcmp(void* buf2, int cmpSize=-1)
 	{
 		if(cmpSize==-1) cmpSize = this->size;
-		cmpSize = min(this->size,cmpSize);
+		cmpSize = std::min(this->size,cmpSize);
 		for(int i=0;i<numItems;i++)
 		{
 			Item &item = items[i];
-			int todo = min((int)item.len,cmpSize);
+			int todo = std::min((int)item.len,cmpSize);
 			cmpSize -= todo;
 			int temp = ::memcmp(item.ptr,((u8*)buf2)+item.ofs,todo);
 			if(temp) return temp;
@@ -98,13 +95,13 @@ public:
 	int dump(void* buf, int dumpSize=-1) const
 	{
 		if(dumpSize==-1) dumpSize = this->size;
-		dumpSize = min(this->size,dumpSize);
+		dumpSize = std::min(this->size,dumpSize);
 		u8* bufptr = (u8*)buf;
 		int done = 0;
 		for(int i=0;i<numItems;i++)
 		{
 			Item item = items[i];
-			int todo = min((int)item.len,dumpSize);
+			int todo = std::min((int)item.len,dumpSize);
 			dumpSize -= todo;
 			done += todo;
 			memcpy(bufptr,item.ptr,todo);
@@ -120,14 +117,14 @@ public:
 	int dump16(void* buf, int dumpSize=-1) const
 	{
 		if(dumpSize==-1) dumpSize = this->size;
-		dumpSize = min(this->size,dumpSize);
+		dumpSize = std::min(this->size,dumpSize);
 		u16* bufptr = (u16*)buf;
 		int done = 0;
 		for(int i=0;i<numItems;i++)
 		{
 			Item item = items[i];
 			u8 * src = (u8 *) item.ptr;
-			int todo = min((int)item.len,dumpSize);
+			int todo = std::min((int)item.len,dumpSize);
 			dumpSize -= todo;
 			done += todo;
 			for(int j = 0;j < todo / 2;j++)
@@ -153,7 +150,7 @@ static MemSpan MemSpan_TexMem(u32 ofs, u32 len)
 		MemSpan::Item &curr = ret.items[ret.numItems++];
 		curr.start = ofs&0x1FFFF;
 		u32 slot = (ofs>>17)&3; //slots will wrap around
-		curr.len = min(len,0x20000-curr.start);
+		curr.len = std::min(len,0x20000-curr.start);
 		curr.ofs = currofs;
 		len -= curr.len;
 		ofs += curr.len;
@@ -182,7 +179,7 @@ static MemSpan MemSpan_TexPalette(u32 ofs, u32 len, bool silent)
 			PROGINFO("Texture palette overruns texture memory. Wrapping at palette slot 0.\n");
 			slot -= 5;
 		}
-		curr.len = min(len,0x4000-curr.start);
+		curr.len = std::min(len,0x4000-curr.start);
 		curr.ofs = currofs;
 		len -= curr.len;
 		ofs += curr.len;

commit 472fe89165ace1a951b5fd328060fba5ded5fa58

Fix building for the Cocoa port. (Regression from commit 38f6313.)

desmume/src/rasterize.cpp
@@ -806,7 +806,7 @@ FORCEINLINE void RasterizerUnit<RENDERER>::_drawscanline(const POLYGON_ATTR poly
 				rightWidth++;
 			}
 			
-			rasterWidth = std::max({1, abs(leftWidth), abs(rightWidth)});
+			rasterWidth = std::max( 1, std::max(std::abs(leftWidth), std::abs(rightWidth)) );
 		}
 		else
 		{