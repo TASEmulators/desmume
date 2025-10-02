2ab82a6c0646ff5509ff0b2f5ac7ddace458b9da
rogerman <rogerman@users.noreply.github.com>
2025-10-02 16:06:59 -0700

    Windows Port: Fix bug where using a fixed window size for the display window would cause the display window to get shrunk by the GPU Scaling Factor. (Regression from commit d5e4ed5.)

--- desmume/src/frontend/windows/main.cpp
+++ desmume/src/frontend/windows/main.cpp
@@ -653,9 +653,6 @@ void ScaleScreen(float factor, bool user)
 			factor = 1.5f;
 		else if(factor==kScale2point5x)
 			factor = 2.5f;
-		
-		//don't incorporate prescale into these calculations
-		factor /= (float)WinGPUEvent->GetFramebufferDimensionsByScaleFactorInteger();
 
 		if (video.layout == 0)
 			MainWindow->setClientSize((int)(video.rotatedwidthgap() * factor), (int)(video.rotatedheightgap() * factor));
