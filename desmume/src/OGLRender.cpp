TASEmulators/desmume

commit 8238c353fc274a1d205c79d6bdfe14610c577092

Render3D: Do some last minor refactoring tweaks. (Related to commit 2a52196.)

desmume/src/OGLRender.cpp
@@ -670,9 +670,9 @@ bool IsOpenGLDriverVersionSupported(unsigned int checkVersionMajor, unsigned int
 }
 
 static void OGLGetDriverVersion(const char *oglVersionString,
-								unsigned int *versionMajor,
-								unsigned int *versionMinor,
-								unsigned int *versionRevision)
+                                unsigned int *versionMajor,
+                                unsigned int *versionMinor,
+                                unsigned int *versionRevision)
 {
 	size_t versionStringLength = 0;
 	
@@ -791,10 +791,10 @@ static bool ValidateShaderCompileOGL(GLenum shaderType, GLuint theShader)
 }
 
 Render3DError ShaderProgramCreateOGL(GLuint &vtxShaderID,
-									 GLuint &fragShaderID,
-									 GLuint &programID,
-									 const char *vtxShaderCString,
-									 const char *fragShaderCString)
+                                     GLuint &fragShaderID,
+                                     GLuint &programID,
+                                     const char *vtxShaderCString,
+                                     const char *fragShaderCString)
 {
 	Render3DError error = OGLERROR_NOERR;
 	
@@ -2222,6 +2222,13 @@ Render3DError OpenGLRenderer::ApplyRenderingSettings(const GFX3D_State &renderSt
 {
 	Render3DError error = RENDER3DERROR_NOERR;
 	
+	if (!BEGINGL())
+	{
+		return OGLERROR_BEGINGL_FAILED;
+	}
+	
+	glFinish();
+	
 	const bool didSelectedMultisampleSizeChange = (this->_selectedMultisampleSize != CommonSettings.GFX3D_Renderer_MultisampleSize);
 	const bool didEmulateNDSDepthCalculationChange = (this->_emulateNDSDepthCalculation != CommonSettings.OpenGL_Emulation_NDSDepthCalculation);
 	const bool didEnableTextureSmoothingChange = (this->_enableTextureSmoothing != CommonSettings.GFX3D_Renderer_TextureSmoothing);
@@ -2247,6 +2254,8 @@ Render3DError OpenGLRenderer::ApplyRenderingSettings(const GFX3D_State &renderSt
 	error = Render3D::ApplyRenderingSettings(renderState);
 	if (error != RENDER3DERROR_NOERR)
 	{
+		glFinish();
+		ENDGL();
 		return error;
 	}
 	
@@ -2255,11 +2264,6 @@ Render3DError OpenGLRenderer::ApplyRenderingSettings(const GFX3D_State &renderSt
 		didEnableTextureSmoothingChange ||
 		didEmulateDepthLEqualPolygonFacingChange)
 	{
-		if (!BEGINGL())
-		{
-			return OGLERROR_BEGINGL_FAILED;
-		}
-		
 		if (didSelectedMultisampleSizeChange)
 		{
 			GLsizei sampleSize = this->GetLimitedMultisampleSize();
@@ -2281,14 +2285,15 @@ Render3DError OpenGLRenderer::ApplyRenderingSettings(const GFX3D_State &renderSt
 				this->DestroyGeometryPrograms();
 				this->_feature.supportShaders = false;
 				
+				glFinish();
 				ENDGL();
 				return error;
 			}
 		}
-		
-		ENDGL();
 	}
 	
+	glFinish();
+	ENDGL();
 	return error;
 }
 
@@ -2342,9 +2347,9 @@ OpenGLRenderer_1_2::~OpenGLRenderer_1_2()
 		this->DestroyFogPrograms();
 		this->DestroyFramebufferOutput6665Programs();
 		this->DestroyFramebufferOutput8888Programs();
+		
+		this->_feature.supportShaders = false;
 	}
-	
-	this->_feature.supportShaders = false;
 	
 	DestroyVAOs();
 	DestroyVBOs();
@@ -2797,7 +2802,7 @@ Render3DError OpenGLRenderer_1_2::CreateFBOs()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)this->_framebufferWidth, (GLsizei)this->_framebufferHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)this->_framebufferWidth, (GLsizei)this->_framebufferHeight, 0, this->_feature.readPixelsBestFormat, this->_feature.readPixelsBestDataType, NULL);
 	
 	glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_GPolyID);
 	glBindTexture(GL_TEXTURE_2D, OGLRef.texGPolyID);
@@ -2805,7 +2810,7 @@ Render3DError OpenGLRenderer_1_2::CreateFBOs()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)this->_framebufferWidth, (GLsizei)this->_framebufferHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)this->_framebufferWidth, (GLsizei)this->_framebufferHeight, 0, this->_feature.readPixelsBestFormat, this->_feature.readPixelsBestDataType, NULL);
 	
 	glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_FogAttr);
 	glBindTexture(GL_TEXTURE_2D, OGLRef.texGFogAttrID);
@@ -2813,7 +2818,7 @@ Render3DError OpenGLRenderer_1_2::CreateFBOs()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)this->_framebufferWidth, (GLsizei)this->_framebufferHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)this->_framebufferWidth, (GLsizei)this->_framebufferHeight, 0, this->_feature.readPixelsBestFormat, this->_feature.readPixelsBestDataType, NULL);
 	
 	GLint *tempClearImageBuffer = (GLint *)calloc(GPU_FRAMEBUFFER_NATIVE_WIDTH * GPU_FRAMEBUFFER_NATIVE_HEIGHT, sizeof(GLint));
 	
@@ -5473,13 +5478,13 @@ Render3DError OpenGLRenderer_1_2::SetFramebufferSize(size_t w, size_t h)
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8_EXT, (GLsizei)w, (GLsizei)h, 0, GL_DEPTH_STENCIL_EXT, GL_UNSIGNED_INT_24_8_EXT, NULL);
 		
 		glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_GColor);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)w, (GLsizei)h, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)w, (GLsizei)h, 0, this->_feature.readPixelsBestFormat, this->_feature.readPixelsBestDataType, NULL);
 		
 		glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_GPolyID);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)w, (GLsizei)h, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)w, (GLsizei)h, 0, this->_feature.readPixelsBestFormat, this->_feature.readPixelsBestDataType, NULL);
 		
 		glActiveTexture(GL_TEXTURE0 + OGLTextureUnitID_FogAttr);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)w, (GLsizei)h, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, (GLsizei)w, (GLsizei)h, 0, this->_feature.readPixelsBestFormat, this->_feature.readPixelsBestDataType, NULL);
 	}
 	
 	glActiveTexture(GL_TEXTURE0);

desmume/src/OGLRender.h
@@ -1006,15 +1006,6 @@ public:
 	virtual Render3DError InitExtensions() = 0;
 	
 	bool IsExtensionPresent(const std::set<std::string> *oglExtensionSet, const std::string extensionName) const;
-	Render3DError ShaderProgramCreate(GLuint &vtxShaderID,
-									  GLuint &fragShaderID,
-									  GLuint &programID,
-									  const char *vtxShaderCString,
-									  const char *fragShaderCString);
-	bool ValidateShaderCompile(GLenum shaderType, GLuint theShader) const;
-	bool ValidateShaderProgramLink(GLuint theProgram) const;
-	void GetVersion(unsigned int *major, unsigned int *minor, unsigned int *revision) const;
-	void SetVersion(unsigned int major, unsigned int minor, unsigned int revision);
 	bool IsVersionSupported(unsigned int checkVersionMajor, unsigned int checkVersionMinor, unsigned int checkVersionRevision) const;
 	
 	virtual const Color4u8* GetFramebuffer32() const;

desmume/src/OGLRender_3_2.cpp
@@ -1451,7 +1451,7 @@ Render3DError OpenGLRenderer_3_2::InitExtensions()
 	
 	// Texture Buffer Object support. OpenGL v3.2 Core Profile natively supports this, but OpenGL ES requires v3.2.
 	this->_feature.supportTBO = ( (this->_feature.variantID & OpenGLVariantFamily_CoreProfile) != 0) ||
-								(((this->_feature.variantID & OpenGLVariantFamily_ES3) != 0) && ((this->_feature.variantID & 0x000F) >= 0x0002) );
+	                            (((this->_feature.variantID & OpenGLVariantFamily_ES3) != 0) && ((this->_feature.variantID & 0x000F) >= 0x0002) );
 	
 	this->_feature.supportSampleShading         = this->IsExtensionPresent(&oglExtensionSet, "GL_ARB_sample_shading"); // Supported in OpenGL v3.2 Core Profile. Unsupported in OpenGL ES.
 	this->_feature.supportShaderFixedLocation   = IsOpenGLDriverVersionSupported(3, 3, 0); // Requires OpenGL v3.3 or later. Supported in OpenGL ES v3.0.
@@ -1498,6 +1498,7 @@ Render3DError OpenGLRenderer_3_2::InitExtensions()
 	this->_rsResource = new OpenGLRenderStatesResource();
 	this->_gResource = new OpenGLGeometryResource(OpenGLVariantID_CoreProfile_3_2);
 	
+	// Load and create shaders. Return on any error, since v3.2 Core Profile makes shaders mandatory.
 	error = this->CreateGeometryPrograms();
 	if (error != OGLERROR_NOERR)
 	{
@@ -2809,7 +2810,6 @@ Render3DError OpenGLRenderer_3_2::ZeroDstAlphaPass(const POLY *rawPolyList, cons
 	glDepthMask(GL_FALSE);
 	glDrawBuffer(GL_NONE);
 	
-	glBindBuffer(GL_ARRAY_BUFFER, OGLRef.vboPostprocessVtxID);
 	glBindVertexArray(OGLRef.vaoPostprocessStatesID);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 	
@@ -2939,7 +2939,6 @@ Render3DError OpenGLRenderer_3_2::_FramebufferConvertColorFormat()
 		glDisable(GL_STENCIL_TEST);
 		glDisable(GL_BLEND);
 		
-		glBindBuffer(GL_ARRAY_BUFFER, OGLRef.vboPostprocessVtxID);
 		glBindVertexArray(OGLRef.vaoPostprocessStatesID);
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 		glBindVertexArray(0);
@@ -3231,7 +3230,6 @@ Render3DError OpenGLRenderer_3_2::PostprocessFramebuffer()
 	glViewport(0, 0, (GLsizei)this->_framebufferWidth, (GLsizei)this->_framebufferHeight);
 	glDisable(GL_DEPTH_TEST);
 	
-	glBindBuffer(GL_ARRAY_BUFFER, OGLRef.vboPostprocessVtxID);
 	glBindVertexArray(OGLRef.vaoPostprocessStatesID);
 	
 	if (this->_enableEdgeMark)
@@ -3418,7 +3416,6 @@ Render3DError OpenGLRenderer_3_2::ClearUsingImage(const u16 *__restrict colorBuf
 			glDisable(GL_STENCIL_TEST);
 			glEnable(GL_DEPTH_TEST);
 			glDepthFunc(GL_ALWAYS);
-			glBindBuffer(GL_ARRAY_BUFFER, OGLRef.vboPostprocessVtxID);
 			glBindVertexArray(OGLRef.vaoPostprocessStatesID);
 			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 			glBindVertexArray(0);

desmume/src/OGLRender_ES3.cpp
@@ -354,7 +354,7 @@ Render3DError OpenGLESRenderer_3_0::InitExtensions()
 	
 	// Texture Buffer Object support. OpenGL v3.2 Core Profile natively supports this, but OpenGL ES requires v3.2.
 	this->_feature.supportTBO = ( (this->_feature.variantID & OpenGLVariantFamily_CoreProfile) != 0) ||
-								(((this->_feature.variantID & OpenGLVariantFamily_ES3) != 0) && ((this->_feature.variantID & 0x000F) >= 0x0002) );
+	                            (((this->_feature.variantID & OpenGLVariantFamily_ES3) != 0) && ((this->_feature.variantID & 0x000F) >= 0x0002) );
 
 	// Apple-specific extensions
 	this->_feature.supportTextureRange_APPLE    = this->IsExtensionPresent(&oglExtensionSet, "GL_APPLE_texture_range");
@@ -408,6 +408,7 @@ Render3DError OpenGLESRenderer_3_0::InitExtensions()
 		this->_gResource = new OpenGLGeometryResource(OpenGLVariantID_ES3_3_0);
 	}
 	
+	// Load and create shaders. Return on any error, since ES 3.0 makes shaders mandatory.
 	error = this->CreateGeometryPrograms();
 	if (error != OGLERROR_NOERR)
 	{

desmume/src/rasterize.cpp
@@ -1764,7 +1764,7 @@ SoftRasterizerRenderer::SoftRasterizerRenderer()
 	_debug_drawClippedUserPoly = 0;
 	
 	_renderGeometryNeedsFinish = false;
-	_framebufferAttributes = NULL;
+	_framebufferAttributes = new FragmentAttributesBuffer(_framebufferWidth * _framebufferHeight);
 	
 	_enableHighPrecisionColorInterpolation = CommonSettings.GFX3D_HighResolutionInterpolateColor;
 	_enableLineHack = CommonSettings.GFX3D_LineHack;

desmume/src/render3D.cpp
@@ -1185,8 +1185,8 @@ void Render3D_AltiVec::_ClearImageBaseLoop(const u16 *__restrict inColor16, cons
 		vec_st( vec_msum(calcDepth3, calcDepthMul, calcDepthAdd), 48, outDepth24 + i);
 		
 		// Write the fog flags to the fog flag buffer.
-		const v128u16 clearFogLo = vec_sr(clearDepthLo, ((v128u16){15,15,15,15,15,15,15,15}));
-		const v128u16 clearFogHi = vec_sr(clearDepthHi, ((v128u16){15,15,15,15,15,15,15,15}));
+		const v128u16 clearFogLo = vec_sr(clearDepthLo, vec_splat_u16(15));
+		const v128u16 clearFogHi = vec_sr(clearDepthHi, vec_splat_u16(15));
 		vec_st( vec_pack(clearFogLo, clearFogHi), 0, outFog + i );
 	}
 }

commit 588b1ad15fad6e854a4957d55b0a03f1804ce46c

OpenGL Renderer: Remove extraneous glFinish() calls that cause a minor performance loss on older GPUs. (Regression from commit 8238c35.)

desmume/src/OGLRender.cpp
@@ -3135,8 +3135,6 @@ Render3DError OpenGLRenderer::ApplyRenderingSettings(const GFX3D_State &renderSt
 		return OGLERROR_BEGINGL_FAILED;
 	}
 	
-	glFinish();
-	
 	const bool didSelectedMultisampleSizeChange = (this->_selectedMultisampleSize != CommonSettings.GFX3D_Renderer_MultisampleSize);
 	const bool didEmulateNDSDepthCalculationChange = (this->_emulateNDSDepthCalculation != CommonSettings.OpenGL_Emulation_NDSDepthCalculation);
 	const bool didEnableTextureSmoothingChange = (this->_enableTextureSmoothing != CommonSettings.GFX3D_Renderer_TextureSmoothing);
@@ -3200,7 +3198,6 @@ Render3DError OpenGLRenderer::ApplyRenderingSettings(const GFX3D_State &renderSt
 		}
 	}
 	
-	glFinish();
 	ENDGL();
 	return error;
 }