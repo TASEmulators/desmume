TASEmulators/desmume

commit df8816f44ff23d3e0aca7deb288c638bfe2c1d10

add system for per-game AR-style cheats (re #483)

desmume/src/NDSSystem.cpp
@@ -2246,6 +2246,8 @@ void NDS_exec(s32 nb)
 		cheats->process(CHEAT_TYPE_INTERNAL);
 		CHEATS::ResetJitIfNeeded();
 	}
+	for(auto& cheat : CommonSettings.gamehacks.flags.cheats)
+		cheat->Run();
 
 	GDBSTUB_MUTEX_UNLOCK();
 }
@@ -3462,6 +3464,53 @@ bool TCommonSettings::single_core()
 	return (num_cores == 1);
 }
 
+//library of individual game hacks
+namespace
+{
+	struct rabbids_us : public GameHackCheat
+	{
+		void Run() override
+		{
+			if(0xe28dd02c == read32(0x203cf18))
+				write32(0x203cf18, 0xeb0f0878);
+			write32(0x23ff100, 0xe59d1000);
+			write(0x23ff104, 48, "\x01\x00\x51\xE3\x03\x00\x00\x1A\x10\x10\x8F\xE2\x1E\x00\x91\xE8\x00\x30\x81\xE5\x00\x40\x82\xE5\x2C\xD0\x8D\xE2\x1E\xFF\x2F\xE1\xC4\xD5\x18\x02\xCC\xD5\x18\x02\x21\x59\xA0\xE3\x00\x40\xA0\xE3");
+		}
+	};
+
+	struct rabbids_eu : public GameHackCheat
+	{
+		void Run() override
+		{
+			if(0xe28dd02c == read32(0x203d108))
+				write32(0x203d108, 0xeb0efcbc);
+			write32(0x23ff100, 0xe59d1000);
+			write(0x23ff104, 48, "\x01\x00\x51\xE3\x03\x00\x00\x1A\x10\x10\x8F\xE2\x1E\x00\x91\xE8\x00\x30\x81\xE5\x00\x40\x82\xE5\x2C\xD0\x8D\xE2\x1E\xFF\x2F\xE1\x4C\xDA\x18\x02\x54\xDA\x18\x02\x21\x59\xA0\xE3\x00\x40\xA0\xE3");
+		}
+	};
+}
+
+void GameHackCheat::write08(uint32_t address, uint8_t value)
+{
+	_MMU_ARM9_write08(address, value);
+}
+
+void GameHackCheat::write(uint32_t address, size_t sz, void* value)
+{
+	for(size_t i = 0; i < sz; i++)
+		write08(address + i, ((uint8_t*)value)[i]);
+}
+
+void GameHackCheat::write32(uint32_t address, uint32_t value)
+{
+	_MMU_ARM9_write32(address, value);
+}
+
+uint32_t GameHackCheat::read32(uint32_t address)
+{
+	return _MMU_ARM9_read32(address);
+}
+
 void TCommonSettings::GameHacks::apply()
 {
 	clear();
@@ -3469,9 +3518,13 @@ void TCommonSettings::GameHacks::apply()
 
 	flags.overclock = gameInfo.IsCode("IPK") || gameInfo.IsCode("IPG"); //HG/SS
 	flags.stylusjitter = gameInfo.IsCode("YDM"); //CSI: Dark Motives
+	
+	//Rabbids Go Home
+	if(gameInfo.IsCode("VRGE")) flags.cheats.emplace_back(new rabbids_us());
+	if(gameInfo.IsCode("VRGV")) flags.cheats.emplace_back(new rabbids_eu());
 }
 
 void TCommonSettings::GameHacks::clear()
 {
-	memset(&flags,0,sizeof(flags));
+	reconstruct(&flags);
 }

desmume/src/NDSSystem.h
@@ -22,6 +22,7 @@
 #include <string.h>
 #include <string>
 #include <vector>
+#include <memory>
 
 #include "types.h"
 #include "ROMReader.h"
@@ -487,7 +488,17 @@ enum MicMode
 	MicMode_Physical      = 3
 };
 
-extern struct TCommonSettings
+struct GameHackCheat
+{
+	virtual void Run() {}
+
+	void write08(uint32_t address, uint8_t value);
+	void write(uint32_t address, size_t sz, void* value);
+	void write32(uint32_t address, uint32_t value);
+	uint32_t read32(uint32_t address);
+};
+
+struct TCommonSettings
 {
 	bool GFX3D_HighResolutionInterpolateColor;
 	bool GFX3D_EdgeMark;
@@ -536,10 +547,12 @@ extern struct TCommonSettings
 
 		struct
 		{
-			bool overclock;
-			bool stylusjitter;
+			bool overclock = false;
+			bool stylusjitter = false;
+			std::vector<std::shared_ptr<GameHackCheat>> cheats;
 		} flags;
 		
+
 		void apply();
 		void clear();
 	} gamehacks;
@@ -594,7 +607,9 @@ extern struct TCommonSettings
 	
 	TCommonSettings();
 	bool single_core();
-} CommonSettings;
+};
+
+extern TCommonSettings CommonSettings;
 
 void NDS_RunAdvansceneAutoImport();
 

commit a0b088ea76ea9227ccf5e0425a3b76fecd358bea

Fix compiling for non-Windows platforms. (Regression from commit df8816f.)

desmume/src/NDSSystem.cpp
@@ -2246,8 +2246,8 @@ void NDS_exec(s32 nb)
 		cheats->process(CHEAT_TYPE_INTERNAL);
 		CHEATS::ResetJitIfNeeded();
 	}
-	for(auto& cheat : CommonSettings.gamehacks.flags.cheats)
-		cheat->Run();
+	
+	CommonSettings.gamehacks.execute();
 
 	GDBSTUB_MUTEX_UNLOCK();
 }
@@ -3469,44 +3469,44 @@ namespace
 {
 	struct rabbids_us : public GameHackCheat
 	{
-		void Run() override
+		virtual void Run()
 		{
 			if(0xe28dd02c == read32(0x203cf18))
 				write32(0x203cf18, 0xeb0f0878);
 			write32(0x23ff100, 0xe59d1000);
-			write(0x23ff104, 48, "\x01\x00\x51\xE3\x03\x00\x00\x1A\x10\x10\x8F\xE2\x1E\x00\x91\xE8\x00\x30\x81\xE5\x00\x40\x82\xE5\x2C\xD0\x8D\xE2\x1E\xFF\x2F\xE1\xC4\xD5\x18\x02\xCC\xD5\x18\x02\x21\x59\xA0\xE3\x00\x40\xA0\xE3");
+			write(0x23ff104, 48, (char*)"\x01\x00\x51\xE3\x03\x00\x00\x1A\x10\x10\x8F\xE2\x1E\x00\x91\xE8\x00\x30\x81\xE5\x00\x40\x82\xE5\x2C\xD0\x8D\xE2\x1E\xFF\x2F\xE1\xC4\xD5\x18\x02\xCC\xD5\x18\x02\x21\x59\xA0\xE3\x00\x40\xA0\xE3");
 		}
 	};
 
 	struct rabbids_eu : public GameHackCheat
 	{
-		void Run() override
+		virtual void Run()
 		{
 			if(0xe28dd02c == read32(0x203d108))
 				write32(0x203d108, 0xeb0efcbc);
 			write32(0x23ff100, 0xe59d1000);
-			write(0x23ff104, 48, "\x01\x00\x51\xE3\x03\x00\x00\x1A\x10\x10\x8F\xE2\x1E\x00\x91\xE8\x00\x30\x81\xE5\x00\x40\x82\xE5\x2C\xD0\x8D\xE2\x1E\xFF\x2F\xE1\x4C\xDA\x18\x02\x54\xDA\x18\x02\x21\x59\xA0\xE3\x00\x40\xA0\xE3");
+			write(0x23ff104, 48, (char*)"\x01\x00\x51\xE3\x03\x00\x00\x1A\x10\x10\x8F\xE2\x1E\x00\x91\xE8\x00\x30\x81\xE5\x00\x40\x82\xE5\x2C\xD0\x8D\xE2\x1E\xFF\x2F\xE1\x4C\xDA\x18\x02\x54\xDA\x18\x02\x21\x59\xA0\xE3\x00\x40\xA0\xE3");
 		}
 	};
 }
 
-void GameHackCheat::write08(uint32_t address, uint8_t value)
+void GameHackCheat::write08(u32 address, u8 value)
 {
 	_MMU_ARM9_write08(address, value);
 }
 
-void GameHackCheat::write(uint32_t address, size_t sz, void* value)
+void GameHackCheat::write(u32 address, size_t sz, void* value)
 {
 	for(size_t i = 0; i < sz; i++)
-		write08(address + i, ((uint8_t*)value)[i]);
+		write08(address + (u32)i, ((u8*)value)[i]);
 }
 
-void GameHackCheat::write32(uint32_t address, uint32_t value)
+void GameHackCheat::write32(u32 address, u32 value)
 {
 	_MMU_ARM9_write32(address, value);
 }
 
-uint32_t GameHackCheat::read32(uint32_t address)
+u32 GameHackCheat::read32(u32 address)
 {
 	return _MMU_ARM9_read32(address);
 }
@@ -3520,11 +3520,36 @@ void TCommonSettings::GameHacks::apply()
 	flags.stylusjitter = gameInfo.IsCode("YDM"); //CSI: Dark Motives
 	
 	//Rabbids Go Home
-	if(gameInfo.IsCode("VRGE")) flags.cheats.emplace_back(new rabbids_us());
-	if(gameInfo.IsCode("VRGV")) flags.cheats.emplace_back(new rabbids_eu());
+	if(gameInfo.IsCode("VRGE")) flags.cheats.push_back(new rabbids_us());
+	if(gameInfo.IsCode("VRGV")) flags.cheats.push_back(new rabbids_eu());
 }
 
 void TCommonSettings::GameHacks::clear()
 {
-	reconstruct(&flags);
+	flags.reset();
+}
+
+void TCommonSettings::GameHacks::execute()
+{
+	const size_t cheatCount = flags.cheats.size();
+	
+	for (size_t i = 0; i < cheatCount; i++)
+	{
+		flags.cheats[i]->Run();
+	}
+}
+
+void TCommonSettings::GameHacks::GameHacksFlags::reset()
+{
+	const size_t cheatCount = cheats.size();
+	
+	overclock = false;
+	stylusjitter = false;
+	
+	for (int i = (int)cheatCount - 1; i > 0; i--)
+	{
+		delete cheats[i];
+	}
+	
+	cheats.clear();
 }

desmume/src/NDSSystem.h
@@ -22,7 +22,6 @@
 #include <string.h>
 #include <string>
 #include <vector>
-#include <memory>
 
 #include "types.h"
 #include "ROMReader.h"
@@ -492,10 +491,10 @@ struct GameHackCheat
 {
 	virtual void Run() {}
 
-	void write08(uint32_t address, uint8_t value);
-	void write(uint32_t address, size_t sz, void* value);
-	void write32(uint32_t address, uint32_t value);
-	uint32_t read32(uint32_t address);
+	void write08(u32 address, u8 value);
+	void write(u32 address, size_t sz, void* value);
+	void write32(u32 address, u32 value);
+	u32 read32(u32 address);
 };
 
 struct TCommonSettings
@@ -545,16 +544,19 @@ struct TCommonSettings
 	{
 		bool en;
 
-		struct
+		struct GameHacksFlags
 		{
-			bool overclock = false;
-			bool stylusjitter = false;
-			std::vector<std::shared_ptr<GameHackCheat>> cheats;
+			bool overclock;
+			bool stylusjitter;
+			std::vector<GameHackCheat*> cheats;
+			
+			void reset();
 		} flags;
 		
 
 		void apply();
 		void clear();
+		void execute();
 	} gamehacks;
 
 	int StylusPressure;